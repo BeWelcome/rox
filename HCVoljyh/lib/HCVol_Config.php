5076240c4b6f06fa52e21b69409cde154943f172
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2006-11-22 18:47:44 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@31 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/lib/HCVol_Config.php
+++ HCVoljyh/lib/HCVol_Config.php
@@ -7,8 +7,7 @@ $_SYSHCVOL['MessageSenderMail']='message@openhc.org' ; // This is the default ma
 $_SYSHCVOL['ferrorsSenderMail']='ferrors@openhc.org' ; // This is the mail in case of mail error
 $_SYSHCVOL['SignupSenderMail']='signup@openhc.org' ; // This is the mail use by signup page for sending access
 
-$_SYSHCVOL['LenghtComments']=array('OnlyChatMail','OnlyOnce','hewasmyguest','hehostedme','Itrusthim','MoreThanAMonth','MoreThanAYear','IIntroduceHimToHospitality','HeIntroducemeToHospitality','HeIsMyFamily','HeHisMyOldCloseFriend','HeIsMyNeigbour') ; // This is the possible qualities for Comments
-$_SYSHCVOL['QualityComments']=array('Good','Neutral','Bad') ; // This is the possible lenght for Comments
+$_SYSHCVOL['QualityComments']=array('Good','Neutral','Bad') ; // This is the possible Qualifier for the comments
 
 $_SYSHCVOL['SiteStatus']="Open" ; // This can be "Closed" or "Open", depend if the site is to be closed or open
 $_SYSHCVOL['SiteCloseMessage']="The site is temporary closed" ; // Message wich is displayed when the site is closed

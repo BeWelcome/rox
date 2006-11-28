5559be28085bf80f4ae847f2907722ad83c6cfe0
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2006-11-28 19:20:55 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@75 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- test.php
+++ test.php
@@ -1,3 +1,9 @@
 <font face=tahoma><b>Very welcome to HCVol server!</b></font>
 <br>
 This is a simple test<br>
+
+<?php
+$email="jyhegron@laposte.net";
+$res=mail($email,"subject","test body") ;
+echo "sent to $email \$res=$res<br>" ;
+?>

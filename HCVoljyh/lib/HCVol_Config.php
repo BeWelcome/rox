efac33714f61c2b75449f0ad5e3c16624f817cfa
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2006-12-16 10:56:48 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@96 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/lib/HCVol_Config.php
+++ HCVoljyh/lib/HCVol_Config.php
@@ -18,6 +18,7 @@ $_SYSHCVOL['LenghtComments']=array('OnlyChatMail', 'OnlyOnce', 'hewasmyguest', '
 $_SYSHCVOL['EvaluateEventMessageReceived']="Yes" ;// If set to "Yes" events messages received is evaludated at each page refresh
 
 $_SYSHCVOL['DomainName']='http://ns20516.ovh.net/HCVoljyh' ; // This is the name of the web site (overwrite)
+$_SYSHCVOL['UploadPictMaxSize']=100000 ; // This define the size of the max loaded pictures
 
 
 ?>

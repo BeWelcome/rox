3b4531d07d45970c679c75953f2b457de1f22df8
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2006-12-18 21:51:24 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@118 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ HCVoljyh/layout/header_micha.php
@@ -0,0 +1,25 @@
+<?php
+echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n" ;
+echo "<html xmlns=\"http://www.w3.org/1999/xhtml\">" ;
+
+echo "<head>\n" ;
+echo "  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n" ;
+if (isset($title)) {
+  echo "  <title>",$title,"</title>\n" ;
+}
+else {
+  echo "\n<title>",$_SYSHCVOL['SiteName'],"</title>\n" ;
+}
+echo "<LINK REL=\"SHORTCUT ICON\" HREF=\"favicon.ico\">\n" ;
+echo"  <link href=\"stylesheets/undohtml.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\" />" ;
+echo"  <link href=\"stylesheets/screen_micha.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\" />" ;
+echo "</head>\n" ;
+echo "<html>\n" ;
+echo "<body>\n" ;
+
+if ($_SYSHCVOL['SiteStatus']=='Closed') {
+  echo "<br><br>",$_SYSHCVOL['SiteCloseMessage'],"<br>\n"  ;
+	echo "</body>\n</html>\n" ;
+	exit(0) ;
+}
+?>

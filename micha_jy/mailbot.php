032f7c9bced5a75812651677dd4dec80b360fb41
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2006-12-18 21:58:43 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@124 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ micha_jy/mailbot.php
@@ -0,0 +1,46 @@
+<?php
+// Mail bot is a php script used to send automatically the mail
+include "lib/dbaccess.php" ;
+require_once "lib/FunctionsMessages.php" ;
+require_once "layout/Error.php" ;
+
+
+if (IsLogged()) {
+  if (HasRight("RunBot")<=0) {
+	  echo "This need right <b>RunBot</b>" ;
+		exit(0) ;
+	}
+	$IdSender=$_SESSION['IdMember'] ;
+}
+else {  // case not logged
+// todo check if not logged that this script is effectively runned by the cron
+	$IdSender=0 ; /// todo here need to set the Bot id
+} // not logged
+
+$str="select messages.*,Username from messages,members where messages.IdSender=members.id and messages.Status='ToSend'" ;
+$qry=sql_query($str) ;
+$count=0 ;
+while ($rr=mysql_fetch_object($qry)) {
+  $MemberIdLanguage=0 ; // todo fin the real default language of the receiver
+	$subj=ww("YouveGotAMail",$rr->Username) ;
+	$urltoreply=$_SYSHCVOL['SiteName']."/MyMessages.php" ;
+	$text=ww("YouveGotAMailText",$rr->Username,$rr->Message,$urltoreply) ;
+	hvol_mail($Email,$subj,$text,$hh,$_SYSHCVOL['MessageSenderMail'],$MemberIdLanguage,"","","") ;
+	$str="update messages set Status='Sent',IdSender=".$IdSender.",DateSent=now() where id=".$rr->id ;
+	sql_query($str) ;
+	
+	$count++ ;
+}
+$sResult=$count." Messages sent" ;
+
+if (IsLogged()) {
+	  LogStr("Manual mail triggering ".$sResult,"Sending Mail") ;
+    echo $sResult ;
+}
+else {
+	  LogStr("Auto mail triggering ".$sResult,"Sending Mail") ;
+}
+
+
+
+?>

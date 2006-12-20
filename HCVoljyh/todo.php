d2c44364a29a156c1e5ea90e821eacf1b27ac743
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2006-12-20 19:51:21 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@135 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ HCVoljyh/todo.php
@@ -0,0 +1,19 @@
+<?php
+include "lib/dbaccess.php" ;
+require_once "lib/FunctionsLogin.php" ;
+require_once "layout/error.php" ;
+
+  $action=GetParam("action");
+	
+  switch($action) {
+	  case "logout" :
+		  Logout("main.php") ;
+			exit(0) ;
+	}
+	
+	  $errcode="ErrorTodoPage" ;
+	  DisplayError(ww($errcode,$_SERVER["PHP_SELF"])) ;
+		exit(0) ;
+	
+
+?>

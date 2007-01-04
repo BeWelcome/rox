7d3b8dd792ff4d3da8f409c87c8c01145d78fe38
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-04 20:49:00 +0000

    Fixing and Error.php (for error.php)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@169 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/main.php
+++ HCVoljyh/main.php
@@ -1,7 +1,7 @@
 <?php
 include "lib/dbaccess.php" ;
 require_once "lib/FunctionsLogin.php" ;
-require_once "layout/Error.php" ;
+require_once "layout/error.php" ;
 
   switch(GetParam("action")) {
 	  case "login" :

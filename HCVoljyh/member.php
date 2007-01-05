1a65571806657b0aa4969dc99d2d2a29d77ac22f
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-05 11:45:14 +0000

    comments with better layout
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@186 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/member.php
+++ HCVoljyh/member.php
@@ -62,6 +62,7 @@ require_once "layout/error.php" ;
 	  $profilewarning="WARNING the status of ".$m->Username." is set to ".$m->Status ;
 	} 
 
+// Load photo data
 	$photo="" ;
 	$phototext="" ;
 	$str="select * from membersphotos where IdMember=".$IdMember." and SortOrder=".$photorank ;
@@ -69,51 +70,42 @@ require_once "layout/error.php" ;
 	if (!isset($rr->FilePath)and ($photorank>0)) {
 	  $rr=LoadRow("select * from membersphotos where IdMember=".$IdMember." and SortOrder=0") ;
 	}
-	
-	if ($m->IdCity>0) {
-	   $rWhere=LoadRow("select cities.Name as cityname,regions.Name as regionname,countries.Name as countryname from cities,countries,regions where cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=".$m->IdCity) ;
-	}
-	
-	
 	if (isset($rr->FilePath)) {
 	  $photo=$rr->FilePath ;
 	  $phototext=FindTrad($rr->Comment) ;
 		$photorank=$rr->SortOrder;
 	} 
+	$m->photo=$photo ;
+	$m->photorank=$photorank ;
+	$m->phototext=$phototext ;
 	
-  $TGroups=array() ;
-// Try to load groups and caracteristics where the member belong to
-  $str="select membersgroups.Comment as Comment,groups.Name as Name from groups,membersgroups where membersgroups.IdGroup=groups.id and membersgroups.Status='In' and membersgroups.IdMember=".$m->id ;
-	$qry=mysql_query($str) ;
-	$TGroups=array() ;
-	while ($rr=mysql_fetch_object($qry)) {
-	  array_push($TGroups,$rr) ;
+	
+// Load geography
+	if ($m->IdCity>0) {
+	   $rWhere=LoadRow("select cities.Name as cityname,regions.Name as regionname,countries.Name as countryname from cities,countries,regions where cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=".$m->IdCity) ;
+     $m->cityname=$rWhere->cityname ;
+		 $m->regionname=$rWhere->regionname;
+		 $m->countryname=$rWhere->countryname ;
 	}
 	
-	$NbTrust=0 ;
-	$NbComment=0 ;
+	
+// Load nbcomments nbtrust
+	$m->NbTrust=0 ;
+	$m->NbComment=0 ;
   $rr=LoadRow("select count(*) as cnt from comments where IdToMember=".$m->id." and Quality='Good'") ;
-	if (isset($rr->cnt)) $NbTrust=$rr->cnt ;
+	if (isset($rr->cnt)) $m->NbTrust=$rr->cnt ;
   $rr=LoadRow("select count(*) as cnt from comments where IdToMember=".$m->id) ;
-	if (isset($rr->cnt)) $NbComment=$rr->cnt ;
+	if (isset($rr->cnt)) $m->NbComment=$rr->cnt ;
 	
-	if ($m->LastLogin=="11/30/99 00:00:00") $LastLogin= "never";
-	else $LastLogin=localdate($m->LastLogin) ;
+	if ($m->LastLogin=="11/30/99 00:00:00") $m->LastLogin= ww("NeverLog");
+	else $m->LastLogin=localdate($m->LastLogin) ;
 	
-// 	$age  22 years old, // todo compute age and add a description
-  $age=fage($m->BirthDate,$m->HideBirthDate) ;
+// Load Age
+  $m->age=fage($m->BirthDate,$m->HideBirthDate) ;
 	
+// Load full name
 	$m->FullName=fFullName($m)  ;
 
-// Load the language the members nows
-  $TLanguages=array() ;
-  $str="select memberslanguageslevel.IdLanguage as IdLanguage,languages.Name as Name,memberslanguageslevel.Level from memberslanguageslevel,languages where memberslanguageslevel.IdMember=".$m->id." and memberslanguageslevel.IdLanguage=languages.id" ;
-	$qry=mysql_query($str) ;
-	while ($rr=mysql_fetch_object($qry)) {
-	  array_push($TLanguages,$rr) ;
-	}
-	$m->TLanguages=$TLanguages ;
-	
 // Load Address data
 	$rr=LoadRow("select * from addresses where IdMember=".$m->id," and Rank=0 limit 1") ;
 	if (isset($rr->id)) {
@@ -122,6 +114,17 @@ require_once "layout/error.php" ;
 		$m->IdGettingThere=$rr->IdGettingThere ;
 	}
 	
+// Try to load groups and caracteristics where the member belong to
+  $TGroups=array() ;
+  $str="select membersgroups.Comment as Comment,groups.Name as Name from groups,membersgroups where membersgroups.IdGroup=groups.id and membersgroups.Status='In' and membersgroups.IdMember=".$m->id ;
+	$qry=mysql_query($str) ;
+	$TGroups=array() ;
+	while ($rr=mysql_fetch_object($qry)) {
+	  array_push($TGroups,$rr) ;
+	}
+	
+
+// Load phone
 	if ($m->HomePhoneNumber>0) {
 	  $m->DisplayHomePhoneNumber=PublicReadCrypted($m->HomePhoneNumber,ww("Hidden")) ;
 	}
@@ -142,6 +145,17 @@ require_once "layout/error.php" ;
 	if ($m->OtherRestrictions>0) $m->OtherRestrictions=FindTrad($m->OtherRestrictions) ;
 	else $m->OtherRestrictions="" ;
 
+
+// Load the language the members nows
+  $TLanguages=array() ;
+  $str="select memberslanguageslevel.IdLanguage as IdLanguage,languages.Name as Name,memberslanguageslevel.Level from memberslanguageslevel,languages where memberslanguageslevel.IdMember=".$m->id." and memberslanguageslevel.IdLanguage=languages.id" ;
+	$qry=mysql_query($str) ;
+	while ($rr=mysql_fetch_object($qry)) {
+	  array_push($TLanguages,$rr) ;
+	}
+	$m->TLanguages=$TLanguages ;
+	
+
 // see if the visit of the profile need to be logged
   if (($IdMember!=$_SESSION["IdMember"]) and ($_SESSION["IdMember"]!=1) and (IsLogged())) { // don't log admin visits or visit on self profile
 	  $str="insert into recentvisits(IdMember,IdVisitor) values(".$m->id.",".$_SESSION["IdMember"].")" ;
@@ -150,6 +164,6 @@ require_once "layout/error.php" ;
 
 	
   include "layout/member.php" ;
-  DisplayMember($m,$photo,$phototext,$photorank,$rWhere->cityname,$rWhere->regionname,$rWhere->countryname,$profilewarning,$TGroups,$LastLogin,$NbComment,$NbTrust,$age) ;
+  DisplayMember($m,$profilewarning,$TGroups) ;
 
 ?>
--- HCVoljyh/viewcomments.php
+++ HCVoljyh/viewcomments.php
@@ -5,6 +5,7 @@ require_once "layout/error.php" ;
 require_once "layout/viewcomments.php" ;
 
   $IdMember=GetParam("cid",$_SESSION['IdMember']) ;
+  $photorank=0 ; // Alway use picture 0 of view comment 
 	
   switch(GetParam("action")) {
 	  case "logout" :
@@ -13,6 +14,76 @@ require_once "layout/viewcomments.php" ;
 	}
 	
 
+
+// Try to load the member
+	if (is_numeric($IdMember)) {
+	  $str="select * from members where id=".$IdMember.$wherestatus ;
+	}
+	else {
+		$str="select * from members where Username='".$IdMember."'".$wherestatus ;
+	}
+
+	$m=LoadRow($str) ;
+
+	if (!isset($m->id)) {
+	  $errcode="ErrorNoSuchMember" ;
+	  DisplayError(ww($errcode,$IdMember)) ;
+//		die("ErrorMessage=".$ErrorMessage) ;
+		exit(0) ;
+	}
+
+	$IdMember=$m->id ; // to be sure to have a numeric ID
+	
+	$profilewarning="" ;
+	if ($m->Status!="Active") {
+	  $profilewarning="WARNING the status of ".$m->Username." is set to ".$m->Status ;
+	} 
+
+// Load photo data
+	$photo="" ;
+	$phototext="" ;
+	$str="select * from membersphotos where IdMember=".$IdMember." and SortOrder=".$photorank ;
+	$rr=LoadRow($str) ;
+	if (!isset($rr->FilePath)and ($photorank>0)) {
+	  $rr=LoadRow("select * from membersphotos where IdMember=".$IdMember." and SortOrder=0") ;
+	}
+	if (isset($rr->FilePath)) {
+	  $photo=$rr->FilePath ;
+	  $phototext=FindTrad($rr->Comment) ;
+		$photorank=$rr->SortOrder;
+	} 
+	$m->photo=$photo ;
+	$m->photorank=$photorank ;
+	$m->phototext=$phototext ;
+	
+	
+// Load geography
+	if ($m->IdCity>0) {
+	   $rWhere=LoadRow("select cities.Name as cityname,regions.Name as regionname,countries.Name as countryname from cities,countries,regions where cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=".$m->IdCity) ;
+     $m->cityname=$rWhere->cityname ;
+		 $m->regionname=$rWhere->regionname;
+		 $m->countryname=$rWhere->countryname ;
+	}
+	
+	
+// Load nbcomments nbtrust
+	$m->NbTrust=0 ;
+	$m->NbComment=0 ;
+  $rr=LoadRow("select count(*) as cnt from comments where IdToMember=".$m->id." and Quality='Good'") ;
+	if (isset($rr->cnt)) $m->NbTrust=$rr->cnt ;
+  $rr=LoadRow("select count(*) as cnt from comments where IdToMember=".$m->id) ;
+	if (isset($rr->cnt)) $m->NbComment=$rr->cnt ;
+	
+	if ($m->LastLogin=="11/30/99 00:00:00") $m->LastLogin= ww("NeverLog");
+	else $m->LastLogin=localdate($m->LastLogin) ;
+	
+// Load Age
+  $m->age=fage($m->BirthDate,$m->HideBirthDate) ;
+	
+// Load full name
+	$m->FullName=fFullName($m)  ;
+
+	
 // Try to load the Comments, prepare the layout data
   $rWho=LoadRow("select * from members where id=".$IdMember) ;
   $str="select comments.*,members.Username as Commenter from comments,members where IdToMember=".$IdMember." and members.id=comments.IdFromMember" ;
@@ -22,6 +93,6 @@ require_once "layout/viewcomments.php" ;
 	  array_push($TCom,$rWhile) ;
 	}
 	
-  DisplayComments($TCom,$rWho->Username) ; // call the layout
+  DisplayComments($m,$TCom) ; // call the layout
 
 ?>

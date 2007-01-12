513acf00588c027b496fe3cbdfe16ebe755232ca
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-12 20:13:11 +0000

    Layout + activation of admincomment notification in case of bad layout
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@338 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/addcomments.php
+++ HCVoljyh/addcomments.php
@@ -29,25 +29,36 @@ require_once "layout/error.php" ;
 	    $qry=sql_query($str) ;
 	    $TCom=mysql_fetch_object($qry) ;
 			$newdate="<font color=gray><font size=1>comment date ".date("F j, Y, g:i a")." (UTC)</font></font><br>" ;
+			
+			$AdminAction="NothingNeeded"  ;
+			if ($Quality=="Bad") {
+			  $AdminAction="AdminCommentMustCheck'"  ;
+			}
 			if (!isset($TCom->id)) {
 			  $TextWhere=$newdate.$TextWhere ;
-			  $str="insert into comments(IdToMember,IdFromMember,Lenght,Quality,TextWhere,TextFree,created) values (".$IdMember.",".$_SESSION['IdMember'].",'".$LenghtComments."','".$Quality."','".addslashes($TextWhere)."','".addslashes($TextFree)."',now())" ;
+			  $str="insert into comments(IdToMember,IdFromMember,Lenght,Quality,TextWhere,TextFree,AdminAction,created) values (".$IdMember.",".$_SESSION['IdMember'].",'".$LenghtComments."','".$Quality."','".addslashes($TextWhere)."','".addslashes($TextFree)."','".$AdminAction."',now())" ;
 			}
 			else {
 			  $TextFree=$TCom->TextFree."<hr>".$newdate.$TextWhere."<br>".$TextFree ;
-			  $str="update comments set IdToMember=".$IdMember.",IdFromMember=".$_SESSION['IdMember'].",Lenght='".$LenghtComments."',Quality='".$Quality."',TextFree='".addslashes($TextFree)."' where id=".$TCom->id ;
+			  $str="update comments set AdminAction='".$AdminAction."',IdToMember=".$IdMember.",IdFromMember=".$_SESSION['IdMember'].",Lenght='".$LenghtComments."',Quality='".$Quality."',TextFree='".addslashes($TextFree)."' where id=".$TCom->id ;
 			}
 	    $qry=sql_query($str) or die("error<br>".$str) ;
 			
 			$m=LoadRow("select * from members where id=".$IdMember) ;
 			$mCommenter=LoadRow("select Username from members where id=".$_SESSION['IdMember']) ;
 
-			// todo notify by mail the new commented guy
 			$defLanguage=GetDefaultLanguage($IdMember) ;
 			$subj=wwinlang("NewCommentSubjFrom",$defLanguage,$mCommenter->Username) ;
 			$text=wwinlang("NewCommentTextFrom",$defLanguage,$mCommenter->Username,ww("CommentQuality_".$Quality),GetParam("TextWhere"),GetParam("TextFree")) ;
 			hvol_mail(GetEmail($IdMember),$subj,$text,"",$_SYSHCVOL['CommentNotificationSenderMail'],$defLanguage,"","","") ;
 			
+			if ($Quality=="Bad") {
+			  $subj="Bad Comment from ".GetUsername($IdMember)." to ".$mCommenter->Username ;
+			  $text=" Check the comment a bad comment has made by ".GetUsername($IdMember)."\n" ;
+				$text.=$mCommenter->Username."\n".ww("CommentQuality_".$Quality)."\n".GetParam("TextWhere")."\n".GetParam("TextFree") ;
+			  hvol_mail($_SYSHCVOL['CommentNotificationSenderMail'],$subj,$text,"",$_SYSHCVOL['CommentNotificationSenderMail'],$defLanguage,"","","") ;
+			}
+
 			LogStr("Adding a comment quality <b>".$Quality."</b> on ".$m->Username,"Comment") ;
 			
 

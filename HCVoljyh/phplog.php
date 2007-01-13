0c41d072df3111bed7a6d173cebafbb39242b8ac
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-13 17:25:06 +0000

    fixing phplogs syntax  ;-)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@359 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/phplog.php
+++ HCVoljyh/phplog.php
@@ -1,11 +1,16 @@
-<?
+<?php
 // THis file display the last errors
 
-  $filename="/etc/httpd/logs/error_log");
-  $ss="tail ".$filename ;
+  $NbLines=10 ;
+	if (isset($_GET["showerror"])) {
+	  if ($_GET["showerror"]>0) 
+		  $NbLines=$_GET["showerror"] ;
+	}
+  $filename="/etc/httpd/logs/error_log";
+  $ss="tail --lines=".$NbLines." ".$filename ;
   echo "\n<TABLE style='border:1px solid #cccccc;' cellPadding=3 cellSpacing=0 width=100% class=s>\n" ;
-  echo "<TR><TH bgColor=#99ccff class=header>$ss</TH>\n" ;
-  echo "<TR><TD bgColor=#ffffff class=s>" ;
+  echo "<TR><TH colspan=2 bgColor=#cccccc class=header>$ss</TH>\n" ;
+  echo "<TR><TD>",$NbLines--,"</TD><TD bgColor=#ffff99 class=s>" ;
   $sresult=htmlentities(system($ss)) ;
 	echo str_replace("\n","<br><br>",$sresult) ;
 	echo "</TD>\n</TABLE>\n<br>" ;

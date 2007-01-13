b5cd20a583cae0eeee32113a86baa514f187dbec
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-13 22:08:38 +0000

    Using simplificated layout
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@375 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/layout/error.php
+++ HCVoljyh/layout/error.php
@@ -7,43 +7,15 @@ function DisplayError($ErrorMessage="No Error Message") {
   include "header_micha.php" ;
 
 	Menu1("error.php",ww('MainPage')) ; // Displays the top menu
-
-	Menu2($_SERVER["PHP_SELF"]) ;
-
-echo "\n<div id=\"maincontent\">\n" ;
-echo "  <div id=\"topcontent\">" ;
-echo "					<h3>",$errcode,"</h3>\n" ;
-echo "\n  </div>\n" ;
-echo "</div>\n" ;
-
-echo "\n  <div id=\"columns\">\n" ;
-//menumember("member.php?cid=".$m->id,$m->id,$NbComment) ;
-echo "		<div id=\"columns-low\">\n" ;
-
-ShowActions() ; // Show the Actions
-ShowAds() ; // Show the Ads
-
-
-echo "		<div id=\"columns-middle\">\n" ;
-echo "			<div id=\"content\">\n" ;
-echo "				<div class=\"info\">\n" ;
+	Menu2($_SERVER["PHP_SELF"]) ; // Display the second menu
+	DisplayHeaderWithColumns($errcode) ; // Display the heade
 
   echo "<table bgcolor=#ffffcc >" ;
   echo "<TR><td>",$ErrorMessage,"</TD><br>" ;
   echo "</table>" ;
 
 
-echo "\n         </div>\n"; // Class info 
-echo "       </div>\n";  // content
-echo "     </div>\n";  // columns-midle
-	
-
-echo "   </div>\n";  // columns-low
-echo " </div>\n";  // columns
-
-
-echo "					<div class=\"user-content\">\n" ;
   include "footer.php" ;
-echo "					\n</div>\n" ;
+	exit(0) ; // To be sure that member don't go furthet after an error
 }
 ?>

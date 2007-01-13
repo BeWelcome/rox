fb34f298aaece92e9e16fb3276da60b54e4d732c
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-13 17:30:39 +0000

    with header
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@360 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/phplog.php
+++ HCVoljyh/phplog.php
@@ -1,3 +1,10 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+<head>
+<title>Last error log</title>
+</head>
+<body>
 <?php
 // THis file display the last errors
 
@@ -8,11 +15,19 @@
 	}
   $filename="/etc/httpd/logs/error_log";
   $ss="tail --lines=".$NbLines." ".$filename ;
+	$tt=explode("\n",$ss) ;
   echo "\n<TABLE style='border:1px solid #cccccc;' cellPadding=3 cellSpacing=0 width=100% class=s>\n" ;
   echo "<TR><TH colspan=2 bgColor=#cccccc class=header>$ss</TH>\n" ;
-  echo "<TR><TD>",$NbLines--,"</TD><TD bgColor=#ffff99 class=s>" ;
-  $sresult=htmlentities(system($ss)) ;
-	echo str_replace("\n","<br><br>",$sresult) ;
-	echo "</TD>\n</TABLE>\n<br>" ;
+	for ($count=0;$count<NbLines;$count++) {
+    echo "<TR><TD>",$NbLine-$count,"</TD><TD bgColor=#ffff99 class=s>" ;
+    $sresult=htmlentities(system($tt[$count])) ;
+	  echo str_replace("\n","<br><br>",$sresult) ;
+	  echo "</TD>\n" ;
+	}
+	echo "</TABLE>\n<br>" ;
+	echo "</body></html>" ;
+
+	exit(0) ;
 	
 ?>
+

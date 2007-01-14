2568dc1926e1ee64cb8953661cd84a52f1423c2f
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-14 16:13:52 +0000

    Sample programs
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@393 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ HCVoljyh/helloworld.php
@@ -0,0 +1,8 @@
+<?php
+include "lib/dbaccess.php" ;  // include the DBaccess routines, + lib files + session managment
+require_once "layout/helloworld.php" ; // Must include the proper layout file	
+
+// Call the layout
+DisplayHelloWorld() ;
+
+?>
---
+++ HCVoljyh/helloworld2.php
@@ -0,0 +1,21 @@
+<?php
+
+// This simple program will fetch the username of the current user and the 
+// will just display hello world in the environment of bewelcome providing 
+// a parameter $Data which is a structure with a $Data->Username field 
+
+include "lib/dbaccess.php" ;  // include the DBaccess routines, + lib files + session managment
+require_once "layout/helloworld2.php" ; // Must include the proper layout file	
+
+MustLog() ; // This will force the user to be log (call the login if he he is not)
+
+// When a user is logged, the session variable $_SESSION['IdMember'] always contain
+// the id of its entry in the members table 
+
+$Data->Username=fUsername($_SESSION['IdMember']) ; // This will retrieve the username of the current logged user
+																								 	 // fUsername() is one of the many tool function in FunctionsTools.php
+
+// Call the layout (with the parameter)
+DisplayHelloWorld2($Data) ;
+
+?>

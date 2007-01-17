dbabcb782719212a38e04bd06f84e85cde2204e7
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-17 15:15:28 +0000

    Good display after message has been sent
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@442 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/contactmember.php
+++ HCVoljyh/contactmember.php
@@ -5,22 +5,13 @@ require_once "lib/FunctionsMessages.php" ;
 require_once "layout/error.php" ;
 include "layout/contactmember.php" ;
 
-  $IdMember=GetParam("cid",0) ; // find the concerned member 
+  $IdMember=IdMember(GetParam("cid",0)) ; // find the concerned member 
   $Message=GetParam("Message","") ; // find the Message
   $iMes=GetParam("iMes",o) ; // find Message number 
 	$IdSender=$_SESSION["IdMember"] ;
   $photorank=0 ; // Alway use picture 0 on contact member 
 
-// Try to load the member
-	if (is_numeric($IdMember)) {
-	  $str="select * from members where id=".$IdMember." and Status='Active'" ;
-	}
-	else {
-		$str="select * from members where Username='".$IdMember."' and Status='Active'" ;
-	}
-
-	$m=LoadRow($str) ;
-
+	$m=LoadRow("select * from members where id=".$IdMember." and Status='Active'") ;
 	if (!isset($m->id)) {
 	  $errcode="ErrorNoSuchMember" ;
 	  DisplayError(ww($errcode,$IdMember)) ;
@@ -28,51 +19,6 @@ include "layout/contactmember.php" ;
 		exit(0) ;
 	}
 
-	$IdMember=$m->id ; // to be sure to have a numeric ID
-	
-  switch(GetParam("action")) {
-	
-	  case "sendmessage" :
-		  if (GetParam("IamAwareOfSpamCheckingRules")!="on") { // check if has accepted the vondition of sending
-			  $Warning=ww("MustAcceptConditionForSending") ;
-        DisplayContactMember($m,$Message,$iMes,$Warning) ;
-			}
-			$Status="ToSend" ; // todo compute a real status
-			if ($iMes!=0) {
-			  $str="update messages set Messages='".addslashes($Message)."',IdReceiver=".$IdMember.",IdSender=".$IdSender."InFolder='Normal',Status='".$Status."'" ;
-			  sql_query($str) ;
-			}
-			else {
-			  $str="insert into messages(created,Message,IdReceiver,IdSender,Status,InFolder) values(now(),'".addslashes($Message)."',".$IdMember.",".$IdSender.",'".$Status."','Normal') " ;
-			  sql_query($str) ;
-			  $iMes=mysql_insert_id() ;
-			}
-			
-      $result=ww("YourMessageWillBeProcessed",$iMes) ;		  
-      DisplayResult($m,$Message,$result) ;
-		  exit(0) ;
-	  case ww("SaveAsDraft") :
-			if ($iMes!=0) {
-			  $str="update messages set Messages='".addslashes($Message)."',IdReceiver=".$IdMember.",IdSender=".$IdSender."InFolder='Draft',Status='Draft'" ;
-			  sql_query($str) ;
-			}
-			else {
-			  $str="insert into messages(created,Message,IdReceiver,IdSender,Status,InFolder) values(now(),'".addslashes($Message)."',".$IdMember.",".$IdSender.",'Draft','Draft') " ;
-			  sql_query($str) ;
-			  $iMes=mysql_insert_id() ;
-			}
-			
-			ComputeSpamCheck($iMes) ;
-			
-      $result=ww("YourMessageIsSavedAsDraft",$iMes) ;		  
-      DisplayResult($m,$Message,$result) ;
-		  exit(0) ;
-	  case "logout" :
-		  Logout("main.php") ;
-			exit(0) ;
-	}
-	
-	
 // Load photo data
 	$photo="" ;
 	$phototext="" ;
@@ -116,6 +62,50 @@ include "layout/contactmember.php" ;
 	
 // Load full name
 	$m->FullName=fFullName($m)  ;	
+
+
+  switch(GetParam("action")) {
+	
+	  case "sendmessage" :
+		  if (GetParam("IamAwareOfSpamCheckingRules")!="on") { // check if has accepted the vondition of sending
+			  $Warning=ww("MustAcceptConditionForSending") ;
+        DisplayContactMember($m,$Message,$iMes,$Warning) ;
+			}
+			$Status="ToSend" ; // todo compute a real status
+			if ($iMes!=0) {
+			  $str="update messages set Messages='".addslashes($Message)."',IdReceiver=".$IdMember.",IdSender=".$IdSender."InFolder='Normal',Status='".$Status."'" ;
+			  sql_query($str) ;
+			}
+			else {
+			  $str="insert into messages(created,Message,IdReceiver,IdSender,Status,InFolder) values(now(),'".addslashes($Message)."',".$IdMember.",".$IdSender.",'".$Status."','Normal') " ;
+			  sql_query($str) ;
+			  $iMes=mysql_insert_id() ;
+			}
+			
+      $result=ww("YourMessageWillBeProcessed",$iMes) ;		  
+      DisplayResult($m,$Message,$result) ;
+		  exit(0) ;
+	  case ww("SaveAsDraft") :
+			if ($iMes!=0) {
+			  $str="update messages set Messages='".addslashes($Message)."',IdReceiver=".$IdMember.",IdSender=".$IdSender."InFolder='Draft',Status='Draft'" ;
+			  sql_query($str) ;
+			}
+			else {
+			  $str="insert into messages(created,Message,IdReceiver,IdSender,Status,InFolder) values(now(),'".addslashes($Message)."',".$IdMember.",".$IdSender.",'Draft','Draft') " ;
+			  sql_query($str) ;
+			  $iMes=mysql_insert_id() ;
+			}
+			
+			ComputeSpamCheck($iMes) ;
+			
+      $result=ww("YourMessageIsSavedAsDraft",$iMes) ;		  
+      DisplayResult($m,$Message,$result) ;
+		  exit(0) ;
+	  case "logout" :
+		  Logout("main.php") ;
+			exit(0) ;
+	}
+	
 
   DisplayContactMember($m,$Message,$iMes,"") ;
 

703899c0fbdcb9a3947badb3a7a0eb47d2e649e3
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-18 09:57:09 +0000

    adding different cache options
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@470 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/member.php
+++ HCVoljyh/member.php
@@ -39,7 +39,7 @@ require_once "layout/error.php" ;
 	  $wherestatus="" ;
 	}
 // Try to load the member
-  $str="select * from members where id=".$IdMember.$wherestatus ;
+  $str="select SQL_CACHE * from members where id=".$IdMember.$wherestatus ;
 
 	$m=LoadRow($str) ;
 
@@ -58,10 +58,10 @@ require_once "layout/error.php" ;
 // Load photo data
 	$photo="" ;
 	$phototext="" ;
-	$str="select * from membersphotos where IdMember=".$IdMember." and SortOrder=".$photorank ;
+	$str="select SQL_CACHE * from membersphotos where IdMember=".$IdMember." and SortOrder=".$photorank ;
 	$rr=LoadRow($str) ;
 	if (!isset($rr->FilePath)and ($photorank>0)) {
-	  $rr=LoadRow("select * from membersphotos where IdMember=".$IdMember." and SortOrder=0") ;
+	  $rr=LoadRow("select SQL_CACHE * from membersphotos where IdMember=".$IdMember." and SortOrder=0") ;
 	}
 	if (isset($rr->FilePath)) {
 	  $photo=$rr->FilePath ;
@@ -75,7 +75,7 @@ require_once "layout/error.php" ;
 	
 // Load geography
 	if ($m->IdCity>0) {
-	   $rWhere=LoadRow("select cities.Name as cityname,regions.Name as regionname,countries.Name as countryname from cities,countries,regions where cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=".$m->IdCity) ;
+	   $rWhere=LoadRow("select SQL_CACHE cities.Name as cityname,regions.Name as regionname,countries.Name as countryname from cities,countries,regions where cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=".$m->IdCity) ;
      $m->cityname=$rWhere->cityname ;
 		 $m->regionname=$rWhere->regionname;
 		 $m->countryname=$rWhere->countryname ;
@@ -85,9 +85,9 @@ require_once "layout/error.php" ;
 // Load nbcomments nbtrust
 	$m->NbTrust=0 ;
 	$m->NbComment=0 ;
-  $rr=LoadRow("select count(*) as cnt from comments where IdToMember=".$m->id." and Quality='Good'") ;
+  $rr=LoadRow("select SQL_CACHE count(*) as cnt from comments where IdToMember=".$m->id." and Quality='Good'") ;
 	if (isset($rr->cnt)) $m->NbTrust=$rr->cnt ;
-  $rr=LoadRow("select count(*) as cnt from comments where IdToMember=".$m->id) ;
+  $rr=LoadRow("select SQL_CACHE count(*) as cnt from comments where IdToMember=".$m->id) ;
 	if (isset($rr->cnt)) $m->NbComment=$rr->cnt ;
 	
 	if ($m->LastLogin=="11/30/99 00:00:00") $m->LastLogin= ww("NeverLog");
@@ -100,7 +100,7 @@ require_once "layout/error.php" ;
 	$m->FullName=fFullName($m)  ;
 
 // Load Address data
-	$rr=LoadRow("select * from addresses where IdMember=".$m->id," and Rank=0 limit 1") ;
+	$rr=LoadRow("select SQL_CACHE * from addresses where IdMember=".$m->id," and Rank=0 limit 1") ;
 	if (isset($rr->id)) {
 	  $m->Address=PublicReadCrypted($rr->HouseNumber,"*")." ".PublicReadCrypted($rr->StreetName,ww("MemberDontShowStreetName")) ;
 	  $m->Zip=PublicReadCrypted($rr->Zip,ww("ZipIsCrypted")) ;
@@ -109,7 +109,7 @@ require_once "layout/error.php" ;
 	
 // Try to load groups and caracteristics where the member belong to
   $TGroups=array() ;
-  $str="select membersgroups.Comment as Comment,groups.Name as Name from groups,membersgroups where membersgroups.IdGroup=groups.id and membersgroups.Status='In' and membersgroups.IdMember=".$m->id ;
+  $str="select SQL_CACHE membersgroups.Comment as Comment,groups.Name as Name,groups.id as IdGroup from groups,membersgroups where membersgroups.IdGroup=groups.id and membersgroups.Status='In' and membersgroups.IdMember=".$m->id ;
 	$qry=mysql_query($str) ;
 	$TGroups=array() ;
 	while ($rr=mysql_fetch_object($qry)) {
@@ -141,7 +141,7 @@ require_once "layout/error.php" ;
 
 // Load the language the members nows
   $TLanguages=array() ;
-  $str="select memberslanguageslevel.IdLanguage as IdLanguage,languages.Name as Name,memberslanguageslevel.Level from memberslanguageslevel,languages where memberslanguageslevel.IdMember=".$m->id." and memberslanguageslevel.IdLanguage=languages.id" ;
+  $str="select SQL_CACHE memberslanguageslevel.IdLanguage as IdLanguage,languages.Name as Name,memberslanguageslevel.Level from memberslanguageslevel,languages where memberslanguageslevel.IdMember=".$m->id." and memberslanguageslevel.IdLanguage=languages.id" ;
 	$qry=mysql_query($str) ;
 	while ($rr=mysql_fetch_object($qry)) {
 	  array_push($TLanguages,$rr) ;
--- HCVoljyh/members.php
+++ HCVoljyh/members.php
@@ -12,11 +12,11 @@ require_once "layout/error.php" ;
 
 
   if (IsLogged()) {
-	  $str="select members.*,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,regions,members left join membersphotos on membersphotos.IdMember=members.id where cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=members.IdCity and status='Active' GROUP BY members.id order by members.LastLogin desc" ;
+	  $str="select SQL_CACHE members.*,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,regions,members left join membersphotos on membersphotos.IdMember=members.id where cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=members.IdCity and status='Active' GROUP BY members.id order by members.LastLogin desc" ;
   }
   else {
 	  // Todo there only select profile publics
-	  $str="select members.*,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,regions,memberspublicprofiles,members left join membersphotos on membersphotos.IdMember=members.id where cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=members.IdCity and status='Active' and memberspublicprofiles.IdMember=members.id GROUP BY members.id order by members.LastLogin desc" ;
+	  $str="select SQL_CACHE members.*,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,regions,memberspublicprofiles,members left join membersphotos on membersphotos.IdMember=members.id where cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=members.IdCity and status='Active' and memberspublicprofiles.IdMember=members.id GROUP BY members.id order by members.LastLogin desc" ;
   }
 
 	

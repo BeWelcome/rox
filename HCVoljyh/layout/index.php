c4084def23f1b3833e3f9bb26fc482f46bce237b
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-20 10:03:51 +0000

    Introducing index page
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@492 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ HCVoljyh/layout/index.php
@@ -0,0 +1,49 @@
+<?php
+require_once ("Menus.php");
+
+function DisplayIndexLogged($Username) {
+	global $title;
+	$title = ww('IndexPage') ;
+
+	include "header.php";
+
+	Menu1("", $title); // Displays the top menu
+	Menu2("", ww('MainPage')); // Displays the second menu
+
+	echo "<br><br><br><br><br><br><br><br><center><table width=\"60%\">" ;
+	echo "\n<tr><td colspan=2 align=center>" ;
+	echo "Hello <b>",$Username,"</b>\n";
+	echo "</td>" ;
+	echo "</table>\n" ;
+	echo "</center>\n";
+
+	echo "</center>\n";
+	include "footer.php";
+}
+
+function DisplayNotLogged() {
+	global $title;
+	$title = ww('IndexPage') ;
+
+	include "header.php";
+
+	Menu1("", $title); // Displays the top menu
+	Menu2("", $title); // Displays the second menu
+
+    DisplayHeaderShortUserContent($title);
+
+	echo "<center><table width=\"60%\">" ;
+	echo "\n<tr><td colspan=2>" ;
+	echo ww("Whatisthistext");
+	echo "</td>" ;
+	echo "\n<tr align=center><td>" ;
+	echo "<a href=\"login.php\">",ww("Login"),"</a>" ;
+	echo "</td>" ;
+	echo "<td>" ;
+	echo " <a href=\"signup.php\">",ww("Signup"),"</a>" ;
+	echo "</td>\n" ;
+	echo "</table>\n" ;
+	echo "</center>\n";
+	include "footer.php";
+}
+?>

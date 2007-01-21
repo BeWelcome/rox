7203f11b6cfafe23deea47112b08ae68efbde37f
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-21 13:39:10 +0000

    english flag before french flag
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@516 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/layout/footer.php
+++ HCVoljyh/layout/footer.php
@@ -27,34 +27,30 @@ if ($_SERVER['QUERY_STRING'] != "") {
 	}
 }
 
-if ($_SESSION['lang'] != 'fr')
-	echo "  <a href=\"", $langurl, "lang=fr\"><img border=0 height=10 src=\"images/fr.gif\" alt=\"Français\" width=16></a>\n";
 if ($_SESSION['lang'] != 'eng')
-	echo "  <a href=\"", $langurl, "lang=eng\"><img border=0 height=10 src=\"images/en.gif\" alt=\"English\" width=16></a>\n";
+	echo "  <a href=\"", $langurl, "lang=eng\"><img border=0 height=10 src=\"images/en.gif\" title=\"English\" width=16></a>\n";
+if ($_SESSION['lang'] != 'fr')
+	echo "  <a href=\"", $langurl, "lang=fr\"><img border=0 height=10 src=\"images/fr.gif\" title=\"Français\" width=16></a>\n";
 if ($_SESSION['lang'] != 'esp')
-	echo "  <a href=\"", $langurl, "lang=esp\"><img border=0 height=10 src=\"images/esp.gif\" alt=\"Español\" width=16></a>\n";
+	echo "  <a href=\"", $langurl, "lang=esp\"><img border=0 height=10 src=\"images/esp.gif\" title=\"Español\" width=16></a>\n";
 if ($_SESSION['lang'] != 'de')
-	echo "  <a href=\"", $langurl, "lang=de\"><img border=0 height=10 src=\"images/de.gif\" alt=\"Deutsch\" width=16></a>\n";
+	echo "  <a href=\"", $langurl, "lang=de\"><img border=0 height=10 src=\"images/de.gif\" title=\"Deutsch\" width=16></a>\n";
 if ($_SESSION['lang'] != 'it')
-	echo "  <a href=\"", $langurl, "lang=it\"><img border=0 height=10 src=\"images/it.gif\" alt=\"Italiano\" width=16></a>\n";
+	echo "  <a href=\"", $langurl, "lang=it\"><img border=0 height=10 src=\"images/it.gif\" title=\"Italiano\" width=16></a>\n";
 if ($_SESSION['lang'] != 'ru')
-	echo "  <a href=\"", $langurl, "lang=ru\"><img border=0 height=10 src=\"images/ru.gif\" alt=\"russian\" width=16></a>\n";
+	echo "  <a href=\"", $langurl, "lang=ru\"><img border=0 height=10 src=\"images/ru.gif\" title=\"russian\" width=16></a>\n";
 if ($_SESSION['lang'] != 'pl')
-	echo "  <a href=\"", $langurl, "lang=pl\"><img border=0 height=10 src=\"images/pl.gif\" alt=\"polish\" width=16></a>\n";
+	echo "  <a href=\"", $langurl, "lang=pl\"><img border=0 height=10 src=\"images/pl.gif\" title=\"polish\" width=16></a>\n";
 if ($_SESSION['lang'] != 'espe')
-	echo "  <a href=\"", $langurl, "lang=espe\"><img border=0 height=10 src=\"images/esper.gif\" alt=\"esperanto\" width=16></a>\n";
+	echo "  <a href=\"", $langurl, "lang=espe\"><img border=0 height=10 src=\"images/esper.gif\" title=\"esperanto\" width=16></a>\n";
 
 //if ($_SESSION['switchtrans']!='on') echo "<a href=\"",$langurl,"switchtrans=off\"><img border=0 height=10 src=\"images/showtransarray.gif\" alt=\"switch to translation mode\" width=16></a>&nbsp;" ;
 if ($_SESSION['switchtrans'] == 'on') {
 	//  echo "<a href=\"",$langurl,"switchtrans=off\"><img border=0 height=10 src=\"images/showtransarray.gif\" alt=\"remove translation mode\" width=16></a>&nbsp;" ;
 	$pagetotranslate = $_SERVER['PHP_SELF'];
-	if ($pagetotranslate {
-		0 }
-	== "/")
-	$pagetotranslate {
-		0 }
-	= "_";
-	echo "  <a href=\"adminwords.php?showtransarray=1&pagetotranslate=" . $pagetotranslate . "\" target=new><img border=0 height=10 src=\"images/switchtrans.gif\" alt=\"go to current translation list for " . $_SERVER['PHP_SELF'] . "\" width=16></a>&nbsp;\n";
+	if ($pagetotranslate { 0 }	== "/")
+	   $pagetotranslate { 0 }= "_";
+	echo "  <a href=\"adminwords.php?showtransarray=1&pagetotranslate=" . $pagetotranslate . "\" target=new><img border=0 height=10 src=\"images/switchtrans.gif\" title=\"go to current translation list for " . $_SERVER['PHP_SELF'] . "\" width=16></a>&nbsp;\n";
 }
 echo "\n  </td>";
 echo "\n  </table>";

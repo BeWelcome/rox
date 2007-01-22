4446c5c0508446aedc0399c1eb9da51a64f75cd4
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-22 13:08:09 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@539 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/lib/FunctionsTools.php
+++ HCVoljyh/lib/FunctionsTools.php
@@ -507,6 +507,11 @@ function hcvol_sendmail($to, $mail_subject, $text, $textinhtml = "", $hh = "", $
 		$use_html = "yes";
 	}
 
+	$headers .= "To: $to\r\n";
+	$headers .= "Subject: $mail_subject\r\n";
+	$headers .= "Return-Path: $From\r\n";
+	$headers .= "Organization: " . $_SYSHCVOL['SiteName']."\r\n";
+
 	if (!(strstr($headers, "From:")) and ($From != "")) {
 		$headers = $headers . "From:" . $From . "\r\n";
 	}
@@ -521,11 +526,7 @@ function hcvol_sendmail($to, $mail_subject, $text, $textinhtml = "", $hh = "", $
 		$headers = $headers . "Reply-To:" . $_SYSHCVOL['MessageSenderMail'] ;
 	}
 
-	$headers .= "To: $to\r\n";
-	$headers .= "Subject: $mail_subject\r\n";
-	$headers .= "Return-Path: $From\r\n";
 
-	$headers = $headers . "Organization: " . $_SYSHCVOL['SiteName'];
 
 	if ($use_html == "yes") {
 		if ($verbose)

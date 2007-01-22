3afcffa52fa61c36c572291d509564443c1d884f
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-22 13:36:06 +0000

    end of debugging ?
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@547 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/adminaccepter.php
+++ HCVoljyh/adminaccepter.php
@@ -107,7 +107,7 @@ switch (GetParam("action")) {
 		$Email = AdminReadCrypted($m->Email);
 		$subj = ww("SignupSubjRejected",$_SYSHCVOL['SiteName']);
 		$text = ww("SignupYouHaveBeenRejected", $m->Username,$_SYSHCVOL['SiteName']);
-		echo "$subj<br>$text<br> sent to $Email<br> from ".$_SYSHCVOL['AccepterSenderMail'] ;
+//		echo "$subj<br>$text<br> sent to $Email<br> from ".$_SYSHCVOL['AccepterSenderMail'] ;
 		hvol_mail($Email,$subj,"text as test   ", "", $_SYSHCVOL['TestMail'], 0, "yes", "", "");
 		hvol_mail($Email, "test"           , $text, "", $_SYSHCVOL['AccepterSenderMail'],0, "yes", "", "");
 

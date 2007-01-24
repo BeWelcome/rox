183d71ade1ef17c574ceaffe39559314092bfcee
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-24 08:39:33 +0000

    Fixing the missing members in the member list (these wiithout pictures)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@581 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/members.php
+++ HCVoljyh/members.php
@@ -10,10 +10,10 @@ switch (GetParam("action")) {
 }
 
 if (IsLogged()) {
-	$str = "select SQL_CACHE members.*,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,regions,members left join membersphotos on membersphotos.IdMember=members.id where cities.IdRegion=regions.id and countries.id=regions.IdCountry and membersphotos.SortOrder=0 and cities.id=members.IdCity and status='Active' GROUP BY members.id order by members.LastLogin desc";
+	$str = "select SQL_CACHE members.*,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,regions,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=members.IdCity and status='Active' GROUP BY members.id order by members.LastLogin desc";
 } else {
 	// Todo there only select profile publics
-	$str = "select SQL_CACHE members.*,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,regions,memberspublicprofiles,members left join membersphotos on membersphotos.IdMember=members.id where cities.IdRegion=regions.id and countries.id=regions.IdCountry and membersphotos.SortOrder=0 and cities.id=members.IdCity and status='Active' and memberspublicprofiles.IdMember=members.id GROUP BY members.id order by members.LastLogin desc";
+	$str = "select SQL_CACHE members.*,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,regions,memberspublicprofiles,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=members.IdCity and status='Active' and memberspublicprofiles.IdMember=members.id GROUP BY members.id order by members.LastLogin desc";
 }
 
 $TData = array ();

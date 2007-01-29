4c339f3a36a1f5257712dc77717987305713fb2e
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-29 22:41:01 +0000

    Adding picture revolver
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@623 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/contactmember.php
+++ HCVoljyh/contactmember.php
@@ -3,66 +3,12 @@ include "lib/dbaccess.php";
 require_once "lib/FunctionsMessages.php";
 require_once "layout/error.php";
 include "layout/contactmember.php";
+require_once "prepare_profile_header.php";
 
 $IdMember = IdMember(GetParam("cid", 0)); // find the concerned member 
 $Message = GetParam("Message", ""); // find the Message
 $iMes = GetParam("iMes", 0); // find Message number 
 $IdSender = $_SESSION["IdMember"];
-$photorank = 0; // Alway use picture 0 on contact member 
-
-$m = LoadRow("select * from members where id=" . $IdMember . " and Status='Active'");
-if (!isset ($m->id)) {
-	$errcode = "ErrorNoSuchMember";
-	DisplayError(ww($errcode, $IdMember));
-	//		die("ErrorMessage=".$ErrorMessage) ;
-	exit (0);
-}
-
-// Load photo data
-$photo = "";
-$phototext = "";
-$str = "select * from membersphotos where IdMember=" . $IdMember . " and SortOrder=" . $photorank;
-$rr = LoadRow($str);
-if (!isset ($rr->FilePath) and ($photorank > 0)) {
-	$rr = LoadRow("select * from membersphotos where IdMember=" . $IdMember . " and SortOrder=0");
-}
-if (isset ($rr->FilePath)) {
-	$photo = $rr->FilePath;
-	$phototext = FindTrad($rr->Comment);
-	$photorank = $rr->SortOrder;
-}
-$m->photo = $photo;
-$m->photorank = $photorank;
-$m->phototext = $phototext;
-
-// Load geography
-if ($m->IdCity > 0) {
-	$rWhere = LoadRow("select cities.Name as cityname,regions.Name as regionname,countries.Name as countryname from cities,countries,regions where cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=" . $m->IdCity);
-	$m->cityname = $rWhere->cityname;
-	$m->regionname = $rWhere->regionname;
-	$m->countryname = $rWhere->countryname;
-}
-
-// Load nbcomments nbtrust
-$m->NbTrust = 0;
-$m->NbComment = 0;
-$rr = LoadRow("select count(*) as cnt from comments where IdToMember=" . $m->id . " and Quality='Good'");
-if (isset ($rr->cnt))
-	$m->NbTrust = $rr->cnt;
-$rr = LoadRow("select count(*) as cnt from comments where IdToMember=" . $m->id);
-if (isset ($rr->cnt))
-	$m->NbComment = $rr->cnt;
-
-if ($m->LastLogin == "11/30/99 00:00:00")
-	$m->LastLogin = ww("NeverLog");
-else
-	$m->LastLogin = localdate($m->LastLogin);
-
-// Load Age
-$m->age = fage($m->BirthDate, $m->HideBirthDate);
-
-// Load full name
-$m->FullName = fFullName($m);
 
 $JoinMemberPictRes="no" ;
 if (GetParam("JoinMemberPict")=="on") {

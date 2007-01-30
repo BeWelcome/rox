a22609de9567c42d8bbe7d558e8cb54f274cffa0
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-30 19:28:57 +0000

    simplifications
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@644 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/members.php
+++ HCVoljyh/members.php
@@ -1,18 +1,14 @@
 <?php
 include "lib/dbaccess.php";
-require_once "lib/FunctionsLogin.php";
 require_once "layout/error.php";
 
 switch (GetParam("action")) {
-	case "logout" :
-		Logout("login.php");
-		exit (0);
+
 }
 
 if (IsLogged()) {
 	$str = "select SQL_CACHE members.*,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,regions,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=members.IdCity and status='Active' GROUP BY members.id order by members.LastLogin desc";
 } else {
-	// Todo there only select profile publics
 	$str = "select SQL_CACHE members.*,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,regions,memberspublicprofiles,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=members.IdCity and status='Active' and memberspublicprofiles.IdMember=members.id GROUP BY members.id order by members.LastLogin desc";
 }
 

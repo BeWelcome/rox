05afd49943ae2d3e48c825b155be9b1df52f586d
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-31 14:18:25 +0000

    layout improvment
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@670 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/layout/feedback.php
+++ HCVoljyh/layout/feedback.php
@@ -33,14 +33,14 @@ function DisplayFeedback($tlist,$Message="",$IdCategory=0) {
 		 echo "</select>\n</td>\n";
 		 echo "<tr><td>", ww("FeedBackEnterYourQuestion"), "</td>";
 		 echo "<td><textarea name=FeedbackQuestion cols=70 rows=9>", "</textarea></td>\n";
-		 echo "<tr><td>", ww("FeedBackUrgentQuestion"), "</td>";
-		 echo "<td><input type=checkbox name=urgent></td>";
+		 echo "<tr><td>", ww("FeedBackUrgentQuestion");
+		 echo " <input type=checkbox name=urgent></td>";
 		 if (!IsLogged()) {
-		 	 echo "<tr><td>", ww("FeedBackEmailNeeded"), "</td>";
-			 echo "<td><input type=text name=Email></td>\n";
+		 	 echo "<td>", ww("FeedBackEmailNeeded");
+			 echo " <input type=text name=Email></td>\n";
 		 } else {
-		   	 echo "<tr><td>", ww("FeedBackIWantAnAnswer"), "</td>";
-			 echo "<td><input type=checkbox name=answerneededt></td>\n";
+		   	 echo "<td align=center>", ww("FeedBackIWantAnAnswer")";
+			 echo " <input type=checkbox name=answerneededt></td>\n";
 		 }
 		 echo "<tr><td colspan=3 align=center><input type=submit name=submit value=submit></td>\n";
 		 echo "<input name=action type=hidden value=ask>\n" ;

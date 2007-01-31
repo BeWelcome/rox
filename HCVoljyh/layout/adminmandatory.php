01a2b6cdc8c6b7c7508d63360eb24c2efb9ef8d1
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-01-31 22:46:01 +0000

    progress
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@676 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- HCVoljyh/layout/adminmandatory.php
+++ HCVoljyh/layout/adminmandatory.php
@@ -9,7 +9,8 @@ function ShowList($TData,$bgcolor="white",$title="") {
 	for ($ii = 0; $ii < $max; $ii++) {
 		$m = $TData[$ii];
 		$count++;
-		echo "<tr><td colspan=2>", LinkWithUsername($m->Username), "</td><td colspan=2>", $m->ProfileSummary, "</td>\n";
+		echo "<tr><td colspan=1>", LinkWithUsername($m->Username), "</td><td colspan=3>", $m->ProfileSummary, "</td>\n";
+		echo "<tr><td colspan=4>OldName: ",$m->OldFirstName," <i>",$m->OldSecondName,"</i> ",$m->OldLastName,"</td>\n";
 		echo "<td rowspan=3>";
 		if ($m->Status != "Active")
 			echo "<a href=\"adminaccepter.php?cid=", $m->id, "&action=accept\">accept</a><br>";
@@ -19,9 +20,15 @@ function ShowList($TData,$bgcolor="white",$title="") {
 		echo "<a href=\"contactmember.php?cid=", $m->id, "\">contact</a><br>";
 		echo "<a href=\"updatemandatory.php?cid=", $m->id, "\">update mandatory</a>";
 		echo "</td>";
-		echo "<tr><td>", $m->HouseNumber, "</td><td colspan=2>", $m->StreetName, "</td><td>", $m->Zip, "</td>\n";
+		echo "<tr><td>Old Address: ", $m->OldHouseNumber, "</td><td colspan=2>", $m->OldStreetName, "</td><td>", $m->OldZip, "</td>\n";
 		echo "<tr><td colspan=4><font color=gray><b>", $m->countryname, " > ", $m->regionname, " > ", $m->cityname, "</b></font></td>\n";
-		echo "<tr><td colspan=4><font color=green><b><i>", $m->FeedBack, "</i></b></font></td><td></td>\n";
+//		echo "<tr><td colspan=4><font color=green><b><i>", $m->FeedBack, "</i></b></font></td><td></td>\n";
+// new values
+		echo "<tr><td colspan=5>New Name: " ;
+		echo $m->FirstName," <i>",$m->SecondName,"</i> ",$m->LastName;
+		echo "</td>\n";
+		echo "<tr><td>New Address: ", $m->HouseNumber, "</td><td colspan=2>", $m->StreetName, "</td><td>", $m->Zip, "</td>\n";
+		echo "<tr><td colspan=5>$m->Comment</td>\n";
 		echo "<tr><td colspan=5><hr></td>\n";
 	}
 	echo "<tr><td align=left colspan=2>Total</td><td align=left colspan=2>$count</td>";
@@ -46,7 +53,7 @@ function DisplayAdminMandatory($TData, $lastaction = "") {
 
 	echo "<center>";
 
-	ShowList($Tpending,"#ffff66"," Members to accept");
+	ShowList($Tpending,"#ffff66"," Members to update");
 
 	echo "<hr><h3> Pending Mandatory</h3>";
 	ShowList($TData);

48cfbfc984c27567694ae776220bd205f513b5fc
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-03 19:21:58 +0000

    init of session varaible after session initialization
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@750 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- lib/session.php
+++ lib/session.php
@@ -24,4 +24,37 @@ if (isset ($_SESSION['IdMember']) and ($_SESSION['IdMember'] != 0)) { // if the 
 }
 // end Previous identity cookie checking	
 
+// -----------------------------------------------------------------------------
+// Test if member as requested to change language
+$newlang = "";
+if (GetParam("lang") != "") {
+	SwitchToNewLang(GetParam("lang"));
+}
+if (!isset ($_SESSION['lang'])) {
+	SwitchToNewLang("eng");
+}
+
+// -----------------------------------------------------------------------------
+// test if member use the switchtrans switch to record use of words on its page 
+if ((isset ($_GET['switchtrans'])) and ($_GET['switchtrans'] != "")) {
+	if (!isset ($_SESSION['switchtrans'])) {
+		$_SESSION['switchtrans'] = "on";
+	} else {
+		if ($_SESSION['switchtrans'] == "on") {
+			$_SESSION['switchtrans'] = "off";
+		} else {
+			$_SESSION['switchtrans'] = "on";
+		}
+	}
+} // end of switchtrans
+
+if (isset ($_GET['forcewordcodelink'])) { // use to force a linj to each word 
+	//code on display
+	$_SESSION['forcewordcodelink'] = $_GET['forcewordcodelink'];
+}
+
+// end of Test if member as requested to change language
+// -----------------------------------------------------------------------------
+
+
 ?>

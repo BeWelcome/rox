52de24dbd2eaedf2b984b7a460f1d46f6b686360
hkroger <hkroger@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-03 14:50:25 +0000

    restructure lib
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@733 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ HCVoljyh/lib/session.php
@@ -0,0 +1,27 @@
+<?php
+session_cache_expire(30); // session will expire after 30 minutes
+session_start();
+
+if (!isset ($_GET['showtransarray'])) 
+{
+	$_SESSION['TranslationArray'] = array (); // initialize $_SESSION['TranslationArray'] if not currently switching to adminwords
+}
+
+// Previous identity cookie checking	
+if (isset ($_SESSION['IdMember']) and ($_SESSION['IdMember'] != 0)) { // if the session Id is set
+	if (isset ($_COOKIE['MyBWId']) and ($_COOKIE['MyBWId'] != 0)) { // If there is already a cookie ide set, we are going to check if it match the data of the connected member 
+		if ($_COOKIE['MyBWId'] != $_SESSION['IdMember']) { // Test if it match
+			if (!isset ($_COOKIE['MyBWusername'])) {
+				$than = "than user id=<b>" . $_COOKIE['MyBWId'] . "</b>";
+			} else {
+				$than = "than username:<b>" . $_COOKIE['MyBWusername'] . "</b>";
+			}
+			$errortolog = "Using same computer " . $than; // The error will be log by LogStr when database will be opened
+		} // end of test if it match
+	}
+	setcookie("MyBWId", $_SESSION['IdMember'], time() + 31974000, "/"); // Record the member id in the cookie
+	setcookie("MyBWusername", $_SESSION['Username'], time() + 31974000, "/"); // record the usename in the cookie
+}
+// end Previous identity cookie checking	
+
+?>

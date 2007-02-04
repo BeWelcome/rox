8424c191d01019a3ec17abefa2b7e394f475806b
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-04 09:33:15 +0000

    Adding the layout global functions
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@780 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/login.php
+++ layout/login.php
@@ -7,26 +7,10 @@ function DisplayLogin($nextlink = "") {
 
 	Menu1("login.php", ww('login')); // Displays the top menu
 
-	Menu2($_SERVER["PHP_SELF"]);
+	Menu2("");
 
-	echo "	\n<div id=\"columns\">\n";
-	echo "		\n<div id=\"columns-low\">\n";
+	DisplayHeaderWithColumns(); // Display the header
 
-	echo "<div class=\"clear\" />";
-
-	echo "\n    <!-- rightnav -->";
-	echo "     <div id=\"columns-right\">\n";
-	echo "       <ul>";
-	echo "         <li class=\"label\">", ww("Ads"), "</li>";
-	echo "         <li></li>";
-	echo "       </ul>\n";
-	echo "     </div>\n";
-
-	echo "\n    <!-- middlenav -->";
-
-	echo "     <div id=\"columns-middle\">\n";
-	echo "					<div id=\"content\">";
-	echo "						<div class=\"info\">";
 	echo "<form method=POST action=login.php>\n<table>";
 	echo "<tr><td colspan=2>", ww("thisisadraft"), "</td>\n";
 	echo "<input type=hidden name=action value=login>\n";
@@ -36,20 +20,12 @@ function DisplayLogin($nextlink = "") {
 	echo "<tr><td colspan=2 align=center><input type=submit value='submit'></td>";
 	echo "\n</form>\n</table>\n";
 
-	echo "<br>";
-	echo "<br>";
-	echo "<br>";
+
 	echo "<br>";
 
 	echo ww("NotYetMember");
 	echo "<br>";
-	echo "<br>";
 	echo ww("SignupLink");
-
-	echo "					</div>";
-	echo "				</div>";
-	echo "			</div>";
-	echo "		</div>";
 
 	include "footer.php";
 	return;

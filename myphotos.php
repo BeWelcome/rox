1eb6cd2d54735faa366b7b092908e18cd2549424
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-04 19:24:32 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@802 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- myphotos.php
+++ myphotos.php
@@ -12,7 +12,7 @@ if (GetParam("cid")!="") {
 		if (!isset($Photo->id)) {
 		   $Photo=LoadRow("select membersphotos.*,Username from membersphotos,members where members.id=".IdMember("admin")." and members.id=membersphotos.IdMember and membersphotos.SortOrder=0") ;
 		}
-		$fpath=str_replace("/membersphoto","MembersPhotos",$Photo->FilePath) ;
+		$fpath=str_replace("/memberphotos","MembersPhotos",$Photo->FilePath) ;
 		$ff=fopen($fpath, 'rb') ;
 		if (!$ff) die ("cant open file ".$fpath) ;
 		fpassthru($ff) ;

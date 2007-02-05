a80c97d5c467e93af6c4e55e7088fda23e3f0663
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-05 17:33:33 +0000

    photo in lowercase
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@835 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- membersbycities.php
+++ membersbycities.php
@@ -7,7 +7,7 @@ switch ($action) {
 }
 
 // prepare the countries list
-$str = "select members.id as IdMember,members.Username as Username,countries.id as id,countries.Name as CountryName,regions.Name as RegionName,cities.Name as CityName  from countries,members,cities,regions where members.IdCity=cities.id and members.Status='Active' and cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=".GetParam("IdCity")."  and members.Status='Active' order by countries.id,regions.id,cities.id ";
+$str = "select members.id as IdMember,members.Username as Username,countries.id as id,countries.Name as CountryName,regions.Name as RegionName,cities.Name as CityName,members.ProfileSummary  from countries,members,cities,regions where members.IdCity=cities.id and members.Status='Active' and cities.IdRegion=regions.id and countries.id=regions.IdCountry and cities.id=".GetParam("IdCity")."  and members.Status='Active' order by countries.id,regions.id,cities.id ";
 $qry = mysql_query($str);
 $TList = array ();
 while ($rWhile = mysql_fetch_object($qry)) {
@@ -16,6 +16,9 @@ while ($rWhile = mysql_fetch_object($qry)) {
 	   	  $rWhile->Username="not public profile" ;
 	   } 
 	}
+	$rWhile->ProfileSummary=FindTrad($rWhile->ProfileSummary) ;
+   $photo=LoadRow("select SQL_CACHE * from membersphotos where IdMember=" . $rWhile->IdMember . " and SortOrder=0");
+	if (isset($photo->FilePath)) $rWhile->photo=$photo->FilePath ; 
 	array_push($TList, $rWhile);
 }
 

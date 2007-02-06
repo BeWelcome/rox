dee8bea1b2e883084fcecb77228b57414cf477b6
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-06 10:38:33 +0000

    IndexMicha=1
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@847 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 require_once "lib/init.php";
 include "layout/index.php";
 
+if (GetParam("IndexMicha","no")=="") {
+  DisplayIndex();
+  exit(0) ;
+} 
+
 if (IsLoggedIn()) {
   DisplayIndexLogged($_SESSION["Username"]);
 }

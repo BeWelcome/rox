14dd6c04e4999b38b6b811d2c991b9df2e5721f5
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-09 22:05:43 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@874 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/Menus_micha.php
+++ layout/Menus_micha.php
@@ -262,15 +262,12 @@ function ShowAds() {
 	?>
 <script type="text/javascript"><!--
 google_ad_client = "pub-2715182874315259";
-google_ad_width = 234;
-google_ad_height = 60;
-google_ad_format = "234x60_as";
-google_ad_type = "text";
+google_ad_width = 120;
+google_ad_height = 90;
+google_ad_format = "120x90_0ads_al";
 google_ad_channel = "";
 //--></script>
-<script type="text/javascript"
-  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
+
 	<?php
 	echo "       </ul>\n";
 	echo "     </div>\n";

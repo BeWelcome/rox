5d1f7fa43a618f4010921165029f3037e6cc3392
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-09 22:13:49 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@877 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/Menus_micha.php
+++ layout/Menus_micha.php
@@ -260,16 +260,6 @@ function ShowAds() {
 	echo "       <ul>\n";
 	echo "         <li class=\"label\">", ww("Ads"), "</li>\n";
 	?>
-<script type="text/javascript"><!--
-google_ad_client = "pub-2715182874315259";
-google_ad_width = 120;
-google_ad_height = 90;
-google_ad_format = "120x90_0ads_al";
-google_ad_channel = "";
-//--></script>
-<script type="text/javascript"
-  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
 
 <script type="text/javascript"><!--
 google_ad_client = "pub-2715182874315259";

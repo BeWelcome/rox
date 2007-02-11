f9b8efc49241ea6ae17ef4f163ed214cf7a5a240
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-11 16:58:26 +0000

    better google ads
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@899 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/Menus_micha.php
+++ layout/Menus_micha.php
@@ -259,18 +259,33 @@ function ShowAds() {
 	echo "     <div id=\"col2_content\" class=\"clearfix\">\n";
 	echo "       <ul>\n";
 	echo "         <li class=\"label\">", ww("Ads"), "</li>\n";
+	echo wwinlang("googleads1","prog") ;
+	echo wwinlang("googleads2","prog") ;
 	?>
 <script type="text/javascript"><!--
 google_ad_client = "pub-2715182874315259";
 google_ad_width = 120;
-google_ad_height = 90;
-google_ad_format = "120x90_0ads_al";
+google_ad_height = 600;
+google_ad_format = "120x600_as";
+google_ad_type = "text_image"; 
 google_ad_channel = "";
 //--></script>
 <script type="text/javascript"
-  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+  src="http://pagead2.googlesyndication.com/pagead/show_ads.js ">
 </script>
-
+ 
+ 
+<script type="text/javascript"><!--
+google_ad_client = "pub-2715182874315259";
+google_ad_width = 180;
+google_ad_height = 150;
+google_ad_format = "180x150_as";
+google_ad_type = "text_image"; 
+google_ad_channel = "";
+//--></script>
+<script type="text/javascript"
+  src="http://pagead2.googlesyndication.com/pagead/show_ads.js ">
+</script>
 
 	<?php
 	echo "       </ul>\n";

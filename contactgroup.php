3495f9f705c11604d2a173e01c8d04d96ce2058a
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-16 09:04:10 +0000

    storing sent messages in Groups.Messages
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@971 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- contactgroup.php
+++ contactgroup.php
@@ -22,14 +22,12 @@ if (GetParam("JoinMemberPict")=="on") {
   $JoinMemberPictRes="yes" ;
 }
 
-$rr=LoadRow("select id from membersgroups where IdGroup=".$IdGroup." and CanSendGroupMessage='no' and IacceptMassMailFromThisGroup='yes'") ;
+$rr=LoadRow("select id from membersgroups where IdGroup=".$IdGroup." and IdMember=".$IdSender." and CanSendGroupMessage='no' and IacceptMassMailFromThisGroup='yes'") ;
 if (isset($rr->id)) {
 	$errcode = "ErrorYouCantPostToThisGroup";
 	DisplayError(ww($errcode));
 	exit (0);
 }
-
-
 
 switch (GetParam("action")) {
 
@@ -46,7 +44,7 @@ switch (GetParam("action")) {
 		$Status = "ToSend"; // todo compute a real status
 		
 		$str="select membersgroups.*,groups.Name from membersgroups,groups where groups.id=membersgroups.IdGroup and IdGroup=".$IdGroup." and IacceptMassMailFromThisGroup='yes'" ;
-	    $SenderMail=GetEmail($_SESSION['IdMember']) ; 
+	    $SenderMail=GetEmail($IdSender) ; 
 		$qry=sql_query($str) ;
 		while ($rr=mysql_fetch_object($qry)) {
 		   $defLanguage=GetDefaultLanguage($rr->IdMember) ;
@@ -54,10 +52,13 @@ switch (GetParam("action")) {
 		   $count++ ;
 		   $subj = "BW group ".$groupname." : ".stripslashes($Title);
 		   $text = stripslashes($Message);
-	       $Email=GetEmail($rr->IdMember) ; 
+	       $Email=GetEmail($rr->IdMember) ;
 		   bw_mail($Email, $subj, $text, "", $SenderMail, $defLanguage, "html", "", "");
 //echo "send to ".$Email." <br>".$subj."<br>".$text."<br>from ".$SenderMail."<br>\n" ;
 		}
+		$str="INSERT INTO groupsmessages ( id , created , Title ,Message , IdSender , IdGroup ) VALUES ( NULL , NOW( ) , '".$Title."', '".$Message."', ".$IdSender.",".$IdGroup.")" ;
+		sql_query($str) ; // store sent message 
+
 		if ($count>0) LogStr("sending ".stripslashes($Title)."<br>".stripslashes($Message)."<br> to ".$count." People","GroupMessage") ; 
 		
 		

2b24b78d53b1a98e33f9b60e445cb4a3be641718
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-19 18:08:39 +0000

    test
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1020 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- updatestats.php
+++ updatestats.php
@@ -22,7 +22,8 @@ $rr=LoadRow("select count(*) as cnt from members where Status='Active'") ;
 $NbActiveMembers=$rr->cnt ;
 
 // Number of member with at least one positive comment
-$rr=LoadRow("select count(*) as cnt from members,comments where Status='Active' and members.id=comments.IdToMember and FIND_IN_SET('ITrusthim',Lenght)") ;
+//$rr=LoadRow("select count(*) as cnt from members,comments where Status='Active' and members.id=comments.IdToMember and FIND_IN_SET('ITrusthim',Lenght)") ;
+$rr=LoadRow("select count(distinct(members.id)) as cnt from members,comments where Status='Active' and members.id=comments.IdToMember and comments.Quality='Good'") ;
 $NbMemberWithOneTrust=$rr->cnt ;
 
 $d1=GetParam("d1",strftime("%Y-%m-%d 00:00:00",mktime(0, 0, 0, date("m")  , date("d")-1, date("Y")))) ;

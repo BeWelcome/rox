9f4e2ff34c75c140dafcd35e0c38ed3fc35f2197
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-19 18:02:45 +0000

    test
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1019 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- updatestats.php
+++ updatestats.php
@@ -17,16 +17,19 @@ if (IsLoggedIn()) {
 	$_SESSION['IdMember'] = 0;
 } // not logged
 
+// Number of member
 $rr=LoadRow("select count(*) as cnt from members where Status='Active'") ;
 $NbActiveMembers=$rr->cnt ;
 
+// Number of member with at least one positive comment
 $rr=LoadRow("select count(*) as cnt from members,comments where Status='Active' and members.id=comments.IdToMember and FIND_IN_SET('ITrusthim',Lenght)") ;
 $NbMemberWithOneTrust=$rr->cnt ;
 
 $d1=GetParam("d1",strftime("%Y-%m-%d 00:00:00",mktime(0, 0, 0, date("m")  , date("d")-1, date("Y")))) ;
-$d2==GetParam("d2",strftime("%Y-%m-%d 00:00:00",mktime(0, 0, 0, date("m")  , date("d"), date("Y")))); 
+$d2=GetParam("d2",strftime("%Y-%m-%d 00:00:00",mktime(0, 0, 0, date("m")  , date("d"), date("Y")))); 
 
-$str="select count(*) as cnt from ".$_SYSHCVOL['ARCH_DB'].".logs,members where type='Login' and ".$_SYSHCVOL['ARCH_DB'].".logs.created between '$d1' and '$d2' and Str like 'Successful login%' and members.id=".$_SYSHCVOL['ARCH_DB'].".logs.IdMember" ;
+// Number of member who have logged
+$str="select count(distinct(members.id)) as cnt from members right join ".$_SYSHCVOL['ARCH_DB'].".logs on  members.id=".$_SYSHCVOL['ARCH_DB'].".logs.IdMember and ".$_SYSHCVOL['ARCH_DB'].".logs.type='Login' and ".$_SYSHCVOL['ARCH_DB'].".logs.created between '$d1' and '$d2' and ".$_SYSHCVOL['ARCH_DB'].".logs.Str like 'Successful login%' " ;
 echo "str=$str<br>" ;
 $rr=LoadRow($str) ;
 $NbMemberWhoLoggedToday=$rr->cnt ;
@@ -34,18 +37,19 @@ $NbMemberWhoLoggedToday=$rr->cnt ;
 $rr=LoadRow("select count(*) as cnt from messages where DateSent between '$d1' and '$d2' ") ;
 $NbMessageSent=$rr->cnt ;
 
+// Number of message read
 $rr=LoadRow("select count(*) as cnt from messages where WhenFirstRead between '$d1' and '$d2' ") ;
 $NbMessageRead=$rr->cnt ;
 
 
 if (IsLoggedIn()) {
 	echo "NbActiveMembers=",$NbActiveMembers,"<br>" ;
-	echo "NbMemberWhoLoggedToday=",$NbMemberWhoLoggedToday,"<br>" ;
-	echo "NbMessageRead=",$NbMessageRead,"<br>" ;
-	echo "NbMemberWithOneTrust=",$NbMemberWithOneTrust,"<br>" ;
-	echo "NbActiveMembers=",$NbActiveMembers,"<br>" ;
-	echo "NbMessageSent=",$NbMessageSent,"<br>" ;
-	echo "stat not updated";
+	echo "Nb Member With at least one positive comment=",$NbMemberWithOneTrust,"<br>" ;
+	echo "<br>between $d1 and $d2<br>" ;
+	echo "Nb Member have Logged Today=",$NbMemberWhoLoggedToday,"<br>" ;
+	echo "Nb Message Read today=",$NbMessageRead,"<br>" ;
+	echo "Nb Message Sent today= ",$NbMessageSent,"<br>" ;
+	echo "this is just a display, stat have not been updated";
 }
 else {
 	$str="INSERT INTO stats ( id , created , NbActiveMembers , NbMessageSent , NbMessageRead , NbMemberWithOneTrust , NbMemberWhoLoggedToday )VALUES (NULL ,CURRENT_TIMESTAMP , $NbActiveMembers , $NbMessageSent , $NbMessageRead , $NbMemberWithOneTrust , $NbMemberWhoLoggedToday ))" ;

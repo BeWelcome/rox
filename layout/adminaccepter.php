15f369faba0bf55ad53cb4b931a6b89e6cdb2597
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-21 17:45:49 +0000

    batch accepting
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1053 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/adminaccepter.php
+++ layout/adminaccepter.php
@@ -2,29 +2,43 @@
 require_once ("Menus.php");
 
 function ShowList($TData,$bgcolor="white",$title="") {
+	global $global_count ;
 	$max = count($TData);
 	$count = 0;
 	echo "\n<table width=\"60%\" bgcolor=$bgcolor>\n";
-	if ($title!="") echo "<th colspan=2 align=center>",$title," (",$max,")</th>\n" ;
+	if ($title!="") echo "<th colspan=4 align=center>",$title," (",$max,")</th>\n" ;
 	for ($ii = 0; $ii < $max; $ii++) {
 		$m = $TData[$ii];
 		$count++;
-		echo "<tr><td colspan=2>", LinkWithUsername($m->Username), "</td><td colspan=2>", $m->ProfileSummary, "</td>\n";
-		echo "<td rowspan=3>";
-		if ($m->Status == "Pending")
-			echo "<a href=\"adminaccepter.php?cid=", $m->id, "&action=accept\">accept</a><br>";
-		echo "<a href=\"adminaccepter.php?cid=", $m->id, "&action=reject\">reject</a><br>";
-		if ($m->Status == "Pending")
-			echo "<a href=\"adminaccepter.php?cid=", $m->id, "&action=needmore\">need more</a><br>";
-		echo "<a href=\"contactmember.php?cid=", $m->id, "\">contact</a><br>";
-		echo "<a href=\"updatemandatory.php?cid=", $m->id, "\">update mandatory</a>";
-		echo "</td>";
-		echo "<tr><td colspan=2>Name: ",$m->FirstName," <i>",$m->SecondName,"</i> <b>",$m->LastName,"</b></td>\n";
-		echo "<td colspan=2> <i>",$m->Email,"</i></td>" ;
+		echo "<input type=hidden name=IdMember_".$global_count." value=".$m->id.">" ;
+		echo "<tr><td colspan=2> <font size=5>",LinkWithUsername($m->Username),"</font></td><td colspan=2> <font size=4>",$m->FirstName," <i>",$m->SecondName,"</i> <b>",$m->LastName,"</b> </font>(<i>",$m->Email,"</i>)</td>\n";
+		echo "<tr><td colspan=4>", $m->ProfileSummary, "</td>\n";
 		echo "<tr><td>", $m->HouseNumber, "</td><td colspan=2>", $m->StreetName, "</td><td>", $m->Zip, "</td>\n";
 		echo "<tr><td colspan=4><font color=gray><b>", $m->countryname, " > ", $m->regionname, " > ", $m->cityname, "</b></font></td>\n";
-		echo "<tr><td colspan=4><font color=green><b><i>", $m->FeedBack, "</i></b></font></td><td></td>\n";
+		echo "<tr><td colspan=4><font color=green><b><i>", $m->FeedBack, "</i></b></font></td>\n";
+		echo "<tr><td>";
+		if ($m->Status == "Pending")
+		   echo "<input type=radio name=action_".$global_count." value=accept> accept<br>\n" ;
+//			echo "<a href=\"adminaccepter.php?cid=", $m->id, "&action=accept\">accept</a><br>";
+		   echo "<input type=radio name=action_".$global_count." value=reject> reject<br>" ;
+//		echo "<a href=\"adminaccepter.php?cid=", $m->id, "&action=reject\">reject</a><br>";
+		if ($m->Status == "Pending") {
+		   echo "<input type=radio name=action_".$global_count." value=needmore> need more<br>\n" ;
+		}
+		echo "</td><td colspan=3>" ;
+		if ($m->Status == "Pending") {
+		   echo "<textarea name=needmoretext_".$global_count." cols=60 rows=4>" ;
+		   echo "</textarea>\n" ;
+		}
+		echo "</td>" ;
+		  
+//			echo "<a href=\"adminaccepter.php?cid=", $m->id, "&action=needmore\">need more</a><br>";
+		echo "<tr><td colspan=5>";
+		echo "<a href=\"contactmember.php?cid=", $m->id, "\">contact</a> ";
+		echo "<a href=\"updatemandatory.php?cid=", $m->id, "\">update mandatory</a>";
+		echo "</td>";
 		echo "<tr><td colspan=5><hr></td>\n";
+		$global_count++ ;
 	}
 	echo "<tr><td align=left colspan=2>Total</td><td align=left colspan=2>$count</td>";
 	echo "\n</table><br>\n";
@@ -33,8 +47,11 @@ function ShowList($TData,$bgcolor="white",$title="") {
 function DisplayAdminAccepter($Taccepted, $Tmailchecking, $Tpending, $TtoComplete, $lastaction = "") {
 	global $countmatch;
 	global $title;
+	global $global_count;
 	$title = "Accept members";
 	global $AccepterScope;
+	
+	$global_count=0  ;
 
 	include "header.php";
 
@@ -45,11 +62,18 @@ function DisplayAdminAccepter($Taccepted, $Tmailchecking, $Tpending, $TtoComplet
 	DisplayHeaderShortUserContent($title . " : " . $lastaction);
 
 	echo " your Scope :", $AccepterScope;
+	
+	if (!IsAdmin()) {
+	  echo "temporarly disabled, under test" ;
+	  include "footer.php";
+	}
 
+	echo "<form name=adminaccepter action=adminaccepter.php>\n" ;
 	echo "<center>";
 
 	ShowList($Tpending,"#ffff66"," Members to accept");
 
+	echo "<br><input type=submit name=submit>\n" ;
 	echo "<hr><h3> Members who have to complete their profile</h3>";
 	ShowList($TtoComplete);
 
@@ -57,6 +81,9 @@ function DisplayAdminAccepter($Taccepted, $Tmailchecking, $Tpending, $TtoComplet
 	ShowList($Tmailchecking);
 
 	echo "</center>";
+	echo "<input type=hidden name=action value=batchaccept>" ;
+	echo "<input type=hidden name=global_count value=$global_count>" ;
+	echo "</form>" ;
 
 	include "footer.php";
 } // end of DisplayAdminAccepter($Taccepted,$Tmailchecking,$Tpending)

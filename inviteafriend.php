f90d958bc3fee2e4892b232d9f9ec2bf2fa347fa
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-23 19:09:23 +0000

    first install of invite a friend
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1112 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ inviteafriend.php
@@ -0,0 +1,60 @@
+<?php
+require_once "lib/init.php";
+require_once "layout/error.php";
+include "layout/inviteafriend.php";
+require_once "prepare_profile_header.php";
+
+MustLogIn() ; // member must login
+
+$IdMember = $_SESSION["IdMember"];
+$Email = GetParam("Email"); // find the email concerned 
+
+$m = prepare_profile_header($IdMember,"",0) ; // This is the profile of the member who is going to send the mail
+
+switch (GetParam("action")) {
+
+	case "Send" : // Send the mail
+		$MemberIdLanguage = GetDefaultLanguage($IdMember);
+		$subj = ww("MailInviteAFriendSubject", $membername,$_SESSION['Username']);
+		$urltosignup = "http://".$_SYSHCVOL['SiteName'] .$_SYSHCVOL['MainDir']. "signup.php" ;
+		$MessageFormatted=$Message ;
+		if (GetParam("JoinMemberPict")=="on") {
+	  	   $rImage=LoadRow("select * from membersphotos where IdMember=".$rr->IdSender." and SortOrder=0") ;
+	  	   $MessageFormatted="<html>\n<head>\n" ;
+	  	   $MessageFormatted.="<title>".$subj."</title>\n</head>\n" ;
+	  	   $MessageFormatted.="<body>\n" ;
+	  	   $MessageFormatted.="<table>\n" ;
+
+	  	   $MessageFormatted.="<tr><td>\n" ;
+	  	   $MessageFormatted.="<img alt=\"picture of ".$_SESSION['Username']."\" height=\"200px\" src=\"http://".$_SYSHCVOL['SiteName'].$m->Photo."\" />" ;
+
+	  	   $MessageFormatted.="</td>\n" ;
+	  	   $MessageFormatted.="<td>\n" ;
+	  	   $MessageFormatted.=ww("MailInviteAFriendText", $_SESSION['Username'], $Message, $urltosignup) ;
+	  	   $MessageFormatted.="</td>\n" ;
+	  	   $MessageFormatted.="</table>\n" ;
+	  	   $MessageFormatted.="</body>\n" ;
+	  	   $MessageFormatted.="</html>\n" ;
+	  
+	  	   $text=$MessageFormatted ;
+		}
+		else {
+	  	   $text = ww("YouveGotAMailText", $_SESSION['Username'], $MessageFormatted, $urltosignup);
+	 	}
+
+		$_SERVER['SERVER_NAME'] = "www.bewelcome.org"; // to force because context is not defined
+
+		if (!bw_mail($Email, $subj, $text, "", $_SYSHCVOL['MessageSenderMail'], $MemberIdLanguage, "html", "", "")) {
+		   die("\nCannot send message to ".$Email."<br>\n");
+		};
+
+		DisplayResults($m,ww("MailSentToFriend",$Email)) ;
+		LogStr("Sending a invite a friend mail to <b>".$Email."</b>","InviteAFriend") ;
+		exit(0) ;
+		break ;
+}
+
+
+DisplayForm($m) ;
+
+?>

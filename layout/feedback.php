8dbaca6fe3e5fff45237a1be0b253ee8ad64f332
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-23 13:16:10 +0000

    fixing misplaced include
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1093 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/feedback.php
+++ layout/feedback.php
@@ -16,7 +16,7 @@ function DisplayResults($Message) {
 	include "footer.php";
 } // end of DisplayResults
 
-function DisplayFeedback($tlist,$Message="",$IdCategory=0) {
+function DisplayFeedback($tlist,$IdCategory=0) {
 	global $title;
 	$title = ww('FeedbackPage');
 	include "header.php";
@@ -26,41 +26,35 @@ function DisplayFeedback($tlist,$Message="",$IdCategory=0) {
 	Menu2($_SERVER["PHP_SELF"]);
 
 	DisplayHeaderWithColumns(ww("ContactUs")); // Display the header
-	
-	if ($Message!="") {
-	    echo $Message ;
-	}
-	else {
 
-		 echo "<table>\n<form action=feedback.php>\n";
-		 $max = count($tlist);
-		 echo "<tr><td colspan=3>", ww("FeedBackDisclaimer"), "</td>\n";
-		 echo "<tr><td colspan=1>", ww("FeedBackChooseYourCategory"), "</td>";
-		 echo "\n<td><select name=IdCategory\n>";
+	 echo "<table>\n<form action=feedback.php method=post>\n";
+	 $max = count($tlist);
+	 echo "<tr><td colspan=3>", ww("FeedBackDisclaimer"), "</td>\n";
+	 echo "<tr><td colspan=1>", ww("FeedBackChooseYourCategory"), "</td>";
+	 echo "\n<td><select name=IdCategory\n>";
 
-		 for ($ii = 0; $ii < $max; $ii++) {
-		 	 echo "<option value=" . $tlist[$ii]->id;
-			 if ($IdCategory==$tlist[$ii]->id) echo " selected " ;
-			 echo  ">";
-			 echo ww("FeedBackName_" . $tlist[$ii]->Name);
-			 echo "</option>\n";
-		 }
-		 echo "</select>\n</td>\n";
-		 echo "<tr><td>", ww("FeedBackEnterYourQuestion"), "</td>";
-		 echo "<td><textarea name=FeedbackQuestion cols=70 rows=9>", "</textarea></td>\n";
-		 echo "<tr><td>", ww("FeedBackUrgentQuestion");
-		 echo " <input type=checkbox name=urgent></td>";
-		 if (!IsLoggedIn()) {
-		 	 echo "<td>", ww("FeedBackEmailNeeded");
-			 echo " <input type=text name=Email></td>\n";
-		 } else {
-		   	 echo "<td align=center>", ww("FeedBackIWantAnAnswer");
-			 echo " <input type=checkbox name=answerneededt></td>\n";
-		 }
-		 echo "<tr><td colspan=3 align=center><input type=submit name=submit value=submit></td>\n";
-		 echo "<input name=action type=hidden value=ask>\n" ;
-		 echo "</form>\n</table>\n";
-	}
+	 for ($ii = 0; $ii < $max; $ii++) {
+	 	 echo "<option value=" . $tlist[$ii]->id;
+		 if ($IdCategory==$tlist[$ii]->id) echo " selected " ;
+		 echo  ">";
+		 echo ww("FeedBackName_" . $tlist[$ii]->Name);
+		 echo "</option>\n";
+	 }
+	 echo "</select>\n</td>\n";
+	 echo "<tr><td>", ww("FeedBackEnterYourQuestion"), "</td>";
+	 echo "<td><textarea name=FeedbackQuestion cols=70 rows=9>", "</textarea></td>\n";
+	 echo "<tr><td>", ww("FeedBackUrgentQuestion");
+	 echo " <input type=checkbox name=urgent></td>";
+	 if (!IsLoggedIn()) {
+	 	 echo "<td>", ww("FeedBackEmailNeeded");
+		 echo " <input type=text name=Email></td>\n";
+	 } else {
+	   	 echo "<td align=center>", ww("FeedBackIWantAnAnswer");
+		 echo " <input type=checkbox name=answerneededt></td>\n";
+	 }
+	 echo "<tr><td colspan=3 align=center><input type=submit name=submit value=submit></td>\n";
+	 echo "<input name=action type=hidden value=ask>\n" ;
+	 echo "</form>\n</table>\n";
 
 	include "footer.php";
 }

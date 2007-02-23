830ba4e295e6153f3f593e5b7d010aaefaa7662f
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-23 19:09:08 +0000

    First install of invite a friend
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1111 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ layout/inviteafriend.php
@@ -0,0 +1,51 @@
+<?php
+require_once ("Menus.php");
+function DisplayForm($m,$JoinMemberPict="") {
+	global $title;
+	$title = ww('InviteAFriendPage');
+	include "header.php";
+
+	Menu1("", ww('MainPage')); // Displays the top menu
+
+	Menu2("inviteafriend.php", ww('MainPage')); // Displays the second menu
+
+	DisplayHeaderWithColumns("inviteafriend.php","",""); // Display the header
+
+	echo "<center>\n" ;
+
+	$iiMax = count($TData);
+	$CurrentCategory="" ;
+	echo "<table cellspacing=4 align=left>";
+	echo "<form method=post action=inviteafriend.php>\n" ;
+	echo "<input type=hidden name=action value=Send>" ;
+	echo "<tr><td>",ww("InviteAFriendRule"),"</td>\n" ;
+	echo "<tr><td>",ww("EmailOfYourFriend")," <input type=text value=\"",GetParam("Email"),"\">" ;
+	echo "<tr><td>","<textarea name=Message rows=10 cols=70>",ww("InviteAFriendStandardText"),"</textarea></td>\n" ;
+	echo "<tr><td>",ww("JoinMyPicture")," <input type=checkbox name=JoinMemberPict " ;
+	if ($JoinMemberPict=="on") echo "checked" ;
+	echo "></td>\n<tr><td align=center><input type=submit></td>\n" ;
+	echo "</table>\n";
+	echo "</center>\n" ;
+
+	include "footer.php";
+
+} // DisplayForm
+
+
+function DisplayResults($Message) {
+	global $title;
+	$title = ww('InviteAFriendPage');
+	include "header.php";
+
+	Menu1("inviteafriend.php", ww('MainPage')); // Displays the top menu
+
+	Menu2($_SERVER["PHP_SELF"]);
+
+	DisplayHeaderWithColumns(ww("InviteAFriendPage")); // Display the header
+	
+   echo $Message ;
+	include "footer.php";
+} // end of DisplayResults
+
+
+?>

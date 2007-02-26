b23ba772ce6ae832f8af737f27c7bdf1819bc64f
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-26 22:07:11 +0000

    layout
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1144 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/adminmandatory.php
+++ layout/adminmandatory.php
@@ -10,7 +10,7 @@ function ShowList($TData,$bgcolor="white",$title="") {
 		$m = $TData[$ii];
 		$count++;
 		echo "<tr><td colspan=1>", LinkWithUsername($m->Username), " (",fsince($m->created)," ",localdate($m->created),") </td><td colspan=3>", $m->ProfileSummary, "</td>\n";
-		echo "<tr><td colspan=4>OldName: ",$m->OldFirstName," <i>",$m->OldSecondName,"</i> <b>",$m->OldLastName,"</b></td>\n";
+		echo "<tr style=\"color:#c0c0c0;\"><td colspan=4>OldName: ",$m->OldFirstName," <i>",$m->OldSecondName,"</i> <b>",$m->OldLastName,"</b></td>\n";
 		echo "<td rowspan=3>";
 		if ($m->Status != "Active")
 			echo "<a href=\"adminmandatory.php?cid=", $m->IdMember, "&action=accept\">accept</a><br>";
@@ -20,8 +20,8 @@ function ShowList($TData,$bgcolor="white",$title="") {
 		echo "<a href=\"contactmember.php?cid=", $m->IdMember, "\">contact</a><br>";
 		echo "<a href=\"updatemandatory.php?cid=", $m->IdMember, "\">update mandatory</a>";
 		echo "</td>";
-		echo "<tr><td>Old Address: ", $m->OldHouseNumber, "</td><td colspan=2>", $m->OldStreetName, "</td><td>", $m->OldZip, "</td>\n";
-		echo "<tr><td colspan=4><font color=gray>Old Area <b>", $m->OldCountryName, " > ", $m->OldRegionName, " > ", $m->OldCityName, "</b></font></td>\n";
+		echo "<tr style=\"color:#c0c0c0;\"><td>Old Address: ", $m->OldHouseNumber, "</td><td colspan=2>", $m->OldStreetName, "</td><td>", $m->OldZip, "</td>\n";
+		echo "<tr style=\"color:#c0c0c0;\"><td colspan=4>Old Area :<b>", $m->OldCountryName, " > ", $m->OldRegionName, " > ", $m->OldCityName, "</b></td>\n";
 //		echo "<tr><td colspan=4><font color=green><b><i>", $m->FeedBack, "</i></b></font></td><td></td>\n";
 // new values
 		echo "<tr><td colspan=5>New Name: " ;

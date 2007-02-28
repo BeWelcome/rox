1f888f20eb461ef7878b58056189c056c71011d3
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-02-28 10:53:44 +0000

    adding the responsibles.php page
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1182 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ layout/responsibles.php
@@ -0,0 +1,39 @@
+<?php
+require_once ("Menus.php");
+
+function Displayresponsibles($TData) {
+	global $title;
+	$title = ww('ResponsiblesPage' . " " . $_POST['Username']);
+	include "header.php";
+
+	Menu1("", ww('MainPage')); // Displays the top menu
+
+	Menu2("members.php", ww('ResponsiblesPage')); // Displays the second menu
+
+	DisplayHeaderWithColumns(); // Display the header
+
+	$iiMax = count($TData);
+	echo "\n<table border=\"1\" rules=\"rows\">\n";
+	for ($ii = 0; $ii < $iiMax; $ii++) {
+		$m = $TData[$ii];
+		echo "<tr align=left valign=center>";
+		echo "<td align=center>";
+		if (($m->photo != "") and ($m->photo != "NULL")) {
+			echo "<div id=\"topcontent-profile-photo\">\n";
+            echo LinkWithPicture($m->Username,$m->photo) ;
+			echo "<br>" ;
+			echo "</div>";
+		}
+		echo "</td>";
+		echo "<td>", LinkWithUsername($m->Username), "</td>";
+		echo " <td>", $m->countryname, "</td> ";
+		echo "<td>";
+		echo $m->Description;
+		echo "</td>";
+		echo "</tr>\n";
+	}
+	echo "</table>\n";
+
+	include "footer.php";
+}
+?>

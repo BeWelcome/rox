8285036b0f1a22098c8c71dc24ecac61e6d730aa
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-03-02 18:21:41 +0000

    Trying to fix layout with IE
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1198 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/contactmember.php
+++ layout/contactmember.php
@@ -17,24 +17,35 @@ function DisplayContactMember($m, $Message = "", $iMes = 0, $Warning = "",$JoinM
 	// MAIN begin 3-column-part
 	echo "    <div id=\"main\">";
 	menumember("contactmember.php?cid=" . $m->id, $m->id, $m->NbComment);
-	echo "		<div id=\"columns-low\">";
 
-	ShowActions("<li><a href=\"todo.php\">Add to my list</a></li>\n<li><a href=\"todo.php\">View forum posts</a></li>\n"); // Show the Actions
+	$MenuAction = "";
+//	$MenuAction .= "               <li><a href=\"contactmember.php?cid=" . $m->id . "\">" . ww("ContactMember") . "</a></li>\n";
+	$MenuAction .= "               <li><a href=\"addcomments.php?cid=" . $m->id . "\">" . ww("addcomments") . "</a></li>\n";
+	$MenuAction .= "               <li><a href=\"todo.php\">".ww("ViewForumPosts")."</a></li>\n";
+	if (GetPreference("PreferenceAdvanced")=="Yes") {
+      if ($m->IdContact==0) {
+	   	  $MenuAction .= "<li><a href=\"mycontacts.php?IdContact=" . $m->id . "&action=add\">".ww("AddToMyNotes")."</a> </li>\n";
+	   }
+	   else {
+	   	  $MenuAction .= "<li><a href=\"mycontacts.php?IdContact=" . $m->id . "&action=view\">".ww("ViewMyNotesForThisMember")."</a> </li>\n";
+	   }
+	}
+	ShowActions($MenuAction); // Show the Actions
 	ShowAds(); // Show the Ads
 
 	// middle column
 	echo "      <div id=\"col3\"> \n"; 
-	echo "	    <div id=\"col3_content\" class=\"clearfix\"> \n"; 
-	echo "          <div id=\"content\"> \n";
+//	echo "	    <div id=\"col3_content\" class=\"clearfix\"> \n"; 
+//	echo "          <div id=\"content\"> \n";
 
 	// user content
 	echo "					<div class=\"info\">";
-
 	echo "					<div class=\"user-content\">";
 
+	// middle column
 
 	if ($Warning != "") {
-		echo "<br><br><table width=50%><tr><td><h4><font color=red>";
+		echo "<br><table width=50%><tr><td><h4><font color=red>";
 		echo $Warning;
 		echo "</font></h4></td></table>\n";
 	}
@@ -58,6 +69,24 @@ function DisplayContactMember($m, $Message = "", $iMes = 0, $Warning = "",$JoinM
 	echo "</td>";
 	echo "</table>\n";
 	echo "</form>";
+
+//	echo "					</div>\n";
+//	echo "				</div>\n";
+	
+echo "              <div class=\"clear\"></div>\n"; 
+echo "          </div>\n"; // end content
+echo "        </div>\n"; // end col3_content
+
+	// IE Column Clearing 
+echo "        <div id=\"ie_clearing\">&nbsp;</div>\n"; 
+	// End: IE Column Clearing 
+
+echo "      </div>\n"; // end col3
+	// End: MAIN 3-columns-part
+	
+echo "    </div>\n"; // end main
+
+	
 
 	include "footer.php";
 

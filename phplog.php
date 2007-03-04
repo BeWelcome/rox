40f427ea1fbb8d5431e60de2379ef85d35c76619
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-03-04 11:08:08 +0000

    phplogs works
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1226 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- phplog.php
+++ phplog.php
@@ -9,23 +9,24 @@
 
 // THis file display the last errors
 
-$NbLines = 10;
+$NbLines = 20;
 if (isset ($_GET["showerror"])) {
 	if ($_GET["showerror"] > 0)
 		$NbLines = $_GET["showerror"];
 }
 $filename = "/etc/httpd/logs/www.bewelcome.org-error_log";
 //$filename = "C:\wamp\logs\php_error.log" ;
-$ss = "tail --lines=" . $NbLines . " " . $filename;
-echo system($ss) ;
+//echo file_get_contents ($filename) ;
 echo "<br>" ;
-$sresult=htmlentities(system($ss)) ;
-$tt=explode("\n",$sresult) ;
+$tt=explode("\r",file_get_contents ($filename)) ;
+$iMax=count($tt) ;
 echo "\n<TABLE style='border:1px solid #cccccc;' cellPadding=3 cellSpacing=0 width=100% class=s>\n";
-echo "<TR><TH colspan=2 bgColor=#cccccc class=header>", $ss, "</TH>\n";
+echo "<TR><TH colspan=2 bgColor=#cccccc class=header>", $filename," (total=",$iMax, ", displaying ".$NbLines." last lines)</TH>\n";
 for ($count=0;$count<$NbLines;$count++) {
-  echo "<TR><TD>", $NbLine-$count , "</TD><TD bgColor=#ffff99 class=s>";
-  echo $tt[$count] ;
+  	 $indice=$iMax-$NbLines+$count ;
+	 if ($indice<0) continue ;
+  echo "<TR><TD>", $indice , "</TD><TD bgColor=#ffff99>";
+  echo htmlentities($tt[$indice]) ;
 //$sresult = htmlentities(system($ss));
 //echo str_replace("\n", "<br>", $sresult);
   echo "</TD>\n";

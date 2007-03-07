d43e19da1446f2e32c43d3b3a80543f9cbee5794
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-03-07 20:31:17 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1266 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- lib/FunctionsMessages.php
+++ lib/FunctionsMessages.php
@@ -8,22 +8,52 @@
 //------------------------------------------------------------------------------
 // ComputeSpamCheck read a message in the database and according to specific rules
 // set the SpamInfo  
-Function ComputeSpamCheck($IdMes) {
-	$rr = LoadRow("select messages.* from messages where id=" . $IdMes);
-	if (isset ($rr->id)) {
-		$Status = 'ToSend';
-		$SpamInfo = "";
-		// to do : implement spamchecking verification to update SpamInfo
-		// to do : implement spamchecking verification
 
-		$rPrefCheckMyMail = LoadRow("select *  from memberspreferences where IdMember=" . $rr->IdReceiver . " and IdPreference=4"); // PreferenceCheckMyMail --> IdPref=4
+// Rule is :
+// if sender has Flag NeverCheckSendMail : mail is always set to ToSend
+// if sender has Flag AlwayCheckSendMail : mail is always set to toCheck
+// if receiver has preference PreferenceCheckMyMail set to "Yes"  : mail is always set to toCheck
+// testing then badwords todo
+
+Function ComputeSpamCheck($IdMess) {
+	$Mes=LoadRow("select * from messages where id=".$IdMes) ;
+	if (isset ($Mes->id)) {
+	
+		$SpamInfo = "";
+
+		$CheckerComment=$Mes->$CheckerComment ;
+	    if (HasFlag("NeverCheckSendMail","",$Mes->IdSender)) {
+		      $Status = 'ToSend';
+			  $SpamInfo = "NoSpam";
+		      $CheckerComment.="Sent by member with NeverCheckSendMail \n" ;
+			  $str = "update messages set Status='".$Status."',CheckerComment='".$CheckerComment."',SpamInfo='" . $SpamInfo . "' where id=" . $Mes->id . " and Status!='Sent'";
+			  sql_query($str) ;
+			  LogStr("NeverCheckSendMail for message #".$IdMess." from <b>".fUsername($Mes->IdSender)."</b> to <b>".fUsername($Mes->IdReceiver)."</b>","AutoSpamCheck") ;
+			  return($Status) ;
+		}
+	    if (HasFlag("AlwayCheckSendMail ","",$Mes->IdSender)) {
+		      $Status = 'ToCheck';
+		      $CheckerComment.="Sent by member with AlwayCheckSendMail \n" ;
+			  $str = "update messages set Status='".$Status."',CheckerComment='".$CheckerComment."',SpamInfo='" . $SpamInfo . "' where id=" . $Mes->id . " and Status!='Sent'";
+			  sql_query($str) ;
+			  LogStr("AlwayCheckSendMail for message #".$IdMess." from <b>".fUsername($Mes->IdSender)."</b> to <b>".fUsername($Mes->IdReceiver)."</b>","AutoSpamCheck") ;
+			  return($Status) ;
+		}
+		$Status = 'ToSend';
+		$rPrefCheckMyMail = LoadRow("select *  from memberspreferences where IdMember=" . $Mes->IdReceiver . " and IdPreference=4"); // PreferenceCheckMyMail --> IdPref=4
 		if ($rPrefCheckMyMail->Value = 'Yes') { // if member has choosen CheckMyMail
+		    if ($SpamInfo == "")
+			   $SpamInfo = "NoSpam";
 			$Status = 'ToCheck';
+			$CheckerComment.="Member has ask for checking\n" ;
+			$str = "update messages set Status='".$Status."',CheckerComment='".$CheckerComment."',SpamInfo='" . $SpamInfo . "' where id=" . $Mes->id . " and Status!='Sent'";
+			sql_query($str);
+			LogStr("PreferenceCheckMyMail for message #".$IdMess." from <b>".fUsername($Mes->IdSender)."</b> to <b>".fUsername($Mes->IdReceiver)."</b>","AutoSpamCheck") ;
+			return($Status) ;
 		}
 		if ($SpamInfo == "")
 			$SpamInfo = "NoSpam";
-		$str = "update messages set Status=$Status,SpamInfo='" . $SpamInfo . "' where id=" . $IdMes . " and Status!='Sent'";
-		;
+		$str = "update messages set Status='".$Status."',CheckerComment='".$CheckerComment."',SpamInfo='" . $SpamInfo . "' where id=" . $Mes->id . " and Status!='Sent'";
 		sql_query($str);
 	}
 } // end of ComputeSpamCheck

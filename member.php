9b840f9392538abe67e326063658cc9c286a4c2b
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-03-11 15:54:13 +0000

    preparing flag for profiles if translated
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1283 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- member.php
+++ member.php
@@ -35,7 +35,17 @@ switch (GetParam("action")) {
 		exit (0);
 }
 
-$m = prepare_profile_header($IdMember,$wherestatus,$photorank) ; 
+$m = prepare_profile_header($IdMember,$wherestatus,$photorank) ;
+
+// Try to see how many language this members has used 
+$m->CountTrad=0 ;
+$m->Trad = array ();
+$str="SELECT DISTINCT (memberstrads.Idlanguage),languages.ShortCode FROM memberstrads,languages WHERE memberstrads.IdLanguage=languages.id and IdOwner=".$IdMember ; 
+$qry = mysql_query($str);
+while ($rr = mysql_fetch_object($qry)) {
+	array_push($m->Trad, $rr);
+	$m->CountTrad++ ;
+}
 
 // Try to load specialrelations and caracteristics belong to
 $Relations = array ();

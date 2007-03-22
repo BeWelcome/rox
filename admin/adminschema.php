1394e1f478257473f20088ca514adf26b5963ef5
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-03-22 21:44:25 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1323 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- admin/adminschema.php
+++ admin/adminschema.php
@@ -14,7 +14,7 @@ echo "<br><br><br><br><br><br><br>\n" ;
 $qry1 = sql_query($s1);
 while ($r1 = mysql_fetch_object($qry1)) {
 	echo "<b>", $r1->TABLE_NAME, "</b> <i>", $r1->TABLE_COMMENT, "</i><br>";
-	$s2 = "select COLUMN_NAME,COLUMN_COMMENT,DATA_TYPE from information_schema.COLUMNS where COLUMNS.TABLE_NAME='" . $r1->TABLE_NAME . "' and TABLE_SCHEMA='hcvoltest'";
+	$s2 = "select COLUMN_NAME,COLUMN_COMMENT,DATA_TYPE from information_schema.COLUMNS where COLUMNS.TABLE_NAME='" . $r1->TABLE_NAME . "' and TABLE_SCHEMA='BW_MAIN'";
 	$qry2 = sql_query($s2);
 	while ($r2 = mysql_fetch_object($qry2)) {
 		echo "&nbsp;&nbsp;&nbsp;<b>", $r2->COLUMN_NAME, "</b> ", $r2->DATA_TYPE, " <i>", $r2->COLUMN_COMMENT, "</i><br>\n";

9add891847bbe344020385fe559384640d81c90f
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-03-30 15:52:49 +0000

    joining region only if they are some for countries
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1393 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- membersbycountries.php
+++ membersbycountries.php
@@ -1,4 +1,11 @@
 <?php
+// Some informatin about geography
+// The way that cities are associated to regions
+// update cities set cities.IdRegion =
+//    (select id from regions where regions.feature_code='ADM1' and regions.admin1_code=cities.admin1_code and regions.IdCountry=cities.IdCountry) 
+//    where cities.IdRegion=0 LIMIT 10000
+
+
 require_once "lib/init.php";
 require_once "lib/FunctionsLogin.php";
 require_once "layout/error.php";
@@ -12,7 +19,7 @@ switch ($action) {
 }
 
 // prepare the countries list
-$str = "select members.Username as Username,countries.id as id,countries.Name as CountryName,regions.Name as RegionName,cities.Name as CityName  from countries,members,cities,regions where members.IdCity=cities.id and members.Status='Active' and cities.IdRegion=regions.id and countries.id=cities.IdCountry order by countries.id,regions.id,cities.id ";
+$str = "select members.Username as Username,countries.id as id,countries.Name as CountryName,regions.Name as RegionName,cities.Name as CityName  from (countries,members,cities) left join regions on (cities.IdRegion=regions.id) where members.IdCity=cities.id and members.Status='Active' and countries.id=cities.IdCountry order by countries.id,regions.id,cities.id ";
 $qry = mysql_query($str);
 $TList = array ();
 while ($rWhile = mysql_fetch_object($qry)) {

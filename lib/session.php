783e92af7a5893a4f2d59bba841d4482da190e8e
hkroger <hkroger@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-04-05 14:59:16 +0000

    session directory fix
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1456 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- lib/session.php
+++ lib/session.php
@@ -2,10 +2,14 @@
 
 function SetupSession()
 {
+	global $_SYSHCVOL;
  	global $MayBeDuplicate; // This string will be filled with someting in case a duplicate cookie is found
 	$MayBeDuplicate="";
+	
+	if (empty($_SYSHCVOL['SessionDirectory']))
+		bw_error("Setup SessionDirectory in config.php");
 
-	session_save_path("sessiondir") ; // creating a dedicated session directory
+	session_save_path($_SYSHCVOL['SessionDirectory']) ; // creating a dedicated session directory
 
 	session_cache_expire(30); // session will expire after 30 minutes
 	session_start();

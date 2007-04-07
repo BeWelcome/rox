e8c882b2869e5a9461951d7b23c5ffb9e076d058
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-04-07 08:35:21 +0000

    allowing to display members without region
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1465 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- members.php
+++ members.php
@@ -7,9 +7,9 @@ switch (GetParam("action")) {
 }
 $limitcount=Getparam("limitcount",200);
 if (IsLoggedIn()) {
-	$str = "select SQL_CACHE members.*,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,regions,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where cities.IdRegion=regions.id and countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' GROUP BY members.id order by members.LastLogin desc  limit ".$limitcount;
+	$str = "select SQL_CACHE members.*,cities.Name as cityname,IdRegion,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' GROUP BY members.id order by members.LastLogin desc  limit ".$limitcount;
 } else {
-	$str = "select SQL_CACHE members.*,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,regions,memberspublicprofiles,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where cities.IdRegion=regions.id and countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' and memberspublicprofiles.IdMember=members.id GROUP BY members.id order by members.LastLogin desc  limit ".$limitcount; 
+	$str = "select SQL_CACHE members.*,cities.Name as cityname,IdRegion,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,memberspublicprofiles,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' and memberspublicprofiles.IdMember=members.id GROUP BY members.id order by members.LastLogin desc  limit ".$limitcount; 
 }
 
 $TData = array ();
@@ -27,6 +27,14 @@ while ($rr = mysql_fetch_object($qry)) {
 		$rr->ProfileSummary = "";
 	}
 
+	if ($rr->IdRegion>0) { // let consider that in some case members can have a city without region 
+	   $rregion=LoadRow("select Name from regions where id=".$rr->IdRegion) ;
+	   $rr->regionname=$rregion->Name ;
+	}
+	else {
+	   $rr->regionname=ww("NoRegionDefined") ;
+	}
+	
 	array_push($TData, $rr);
 }
 

077c72e3fdee40166374f8e2e9cdfec9f63391d2
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-04-09 08:12:47 +0000

    working on it
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1482 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/adminmassmails.php
+++ layout/adminmassmails.php
@@ -10,43 +10,28 @@ function DisplayAdminMassMails($TData) {
 
 	Menu2("admin/adminmassmails.php", ww('MainPage')); // Displays the second menu
 
-	DisplayHeaderWithColumns($title); // Display the header
+	DisplayHeaderShortUserContent($title);// Display the header
 
 	$max = count($TData);
 	$max=0 ;
-	echo "<table>";
-	if ((GetParam(Username) == "") or (GetParam(Username2) != "")) {
-		echo "<tr><th>Username</th><th>type</th><th>Str</th><th>created</th><th>ip</th>\n";
-	} else {
-		echo "<tr><th colspan=4 align=center> Logs for ", LinkWithUsername(GetParam(Username)), "</th>\n";
-	}
-	for ($ii = 0; $ii < $max; $ii++) {
-		$logs = $TData[$ii];
-		echo "<tr>";
-		if ((GetParam(Username) == "") or (GetParam(Username2) != "")) {
-			echo "<td>";
-			echo "<a href=\"" . $_SERVER['PHP_SELF'] . "?Username=" . $logs->Username . "&type=" . $logs->Type . "\">" . $logs->Username . "</a>";
-			echo "</td>";
-		}
-		echo "<td>";
-		echo "<a href=\"" . $_SERVER['PHP_SELF'] . "?Username=" . GetParam(Username) . "&type=" . $logs->Type . "\">" . $logs->Type . "</a>";
-		//		echo $logs->Type;
-		echo "</td><td>";
-		echo $logs->Str;
-		echo "</td><td>$logs->created</td><td>&nbsp;&nbsp;&nbsp;";
-		echo "<a href=\"" . $_SERVER['PHP_SELF'] . "?Username=" . GetParam(Username) . "&ip=" . long2ip($logs->IpAddress) . "&type=" . GetParam(type) . "\">" . long2ip($logs->IpAddress) . "</a>";
-		echo "</td>\n";
-	}
-	echo "</table>\n";
-	echo "<hr>\n";
+	echo "<table><tr><td>Please write here in</td><td bgcolor=yellow>".LanguageName($_SESSION['IdLanguage'])."</td></table>";
+	echo "<br>" ;
+//	echo "<hr>\n";
 	echo "<table>\n";
-	echo "<form method=post action=adminlogs.php>\n";
+	echo "<form method=post action=adminmassmails.php>\n";
+	echo "<input type=hidden name=IdBroadcast value=",$TData->IdBroadcast,">\n" ;
 	echo "<tr><td>subject</td><td> <textarea name=subject  rows=1 cols=80>", GetParam(subject), "</textarea></td>";
 	echo "<tr><td>body</td><td> <textarea name=body rows=10 cols=80>", GetParam(body), "</textarea></td>";
 	echo "<tr><td>greetings</td><td> <textarea name=greetings rows=2 cols=80>", GetParam(greetings), "</textarea></td>";
-	echo "\n<tr><td colspan=3 align=center>";
-	echo "Send <input type=checkbox name=send> ";
-	echo "<input type=submit>";
+	echo "\n<tr><td colspan=2 align=center>";
+	echo "<input type=submit name=action value=find>";
+	if (empty($TData->IdBroadcast)) echo " <input type=submit name=action value=update>";
+	else echo " <input type=submit name=action value=update>";
+	echo "</td><td align=center>" ;
+   if (HasRight('MassMail','Send')) {
+	   echo "Send <input type=checkbox name=send> ";
+	   echo " <input type=submit name=action value=send>";
+	}
 	echo "</td> ";
 	echo "</form>\n";
 	echo "</table>\n";
@@ -69,4 +54,3 @@ function DisplayResults($TData,$Message) {
 
 }
 ?>
-

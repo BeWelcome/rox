a4a902264caeb463651d34d07840259ab054dd07
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-04-16 20:38:20 +0000

    test 2
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1513 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- members.php
+++ members.php
@@ -23,10 +23,10 @@ $next_rec = $eu + $limitcount;
 
 if (IsLoggedIn()) {
 	$str = "select SQL_CACHE members.*,cities.Name as cityname,IdRegion,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' GROUP BY members.id order by members.LastLogin desc  limit $eu,".$limitcount;
-	$rtot=LoadRow("select SQL_CACHE count(*) as cnt from from cities,countries,members where countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' GROUP BY members.id ";
+	$rtot=LoadRow("select SQL_CACHE count(*) as cnt from  cities,countries,members where countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' GROUP BY members.id ");
 } else {
 	$str = "select SQL_CACHE members.*,cities.Name as cityname,IdRegion,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,memberspublicprofiles,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' and memberspublicprofiles.IdMember=members.id GROUP BY members.id order by members.LastLogin desc  limit $eu,".$limitcount; 
-	$rtot=LoadRow("select SQL_CACHE count(*) as cnt from from cities,countries,members,memberspublicprofiles where countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' and memberspublicprofiles.IdMember=members.id GROUP BY members.id ";
+	$rtot=LoadRow("select SQL_CACHE count(*) as cnt from  cities,countries,members,memberspublicprofiles where countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' and memberspublicprofiles.IdMember=members.id GROUP BY members.id ");
 }
 
 $TData = array ();

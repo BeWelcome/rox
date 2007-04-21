26c9ba0544bdfd53338c70a0d6f36fe2f1ada7de
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-04-21 10:07:33 +0000

    creating a link page
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1550 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ links.php
@@ -0,0 +1,9 @@
+<?php
+require_once "lib/init.php";
+require_once "lib/FunctionsLogin.php";
+require_once "layout/error.php";
+require_once "layout/links.php";
+
+
+DisplayLinks(); // call the layout 
+?>

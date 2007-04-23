fbf9a5c508a3043f961d202cfe9c3bc1354a24ea
hkroger <hkroger@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-04-23 17:40:57 +0000

    merged swift to alpha
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@1569 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ lib/swift/Swift/FileException.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Swift File Exception
+ * Please read the LICENSE file
+ * @copyright Chris Corbyn <chris@w3style.co.uk>
+ * @author Chris Corbyn <chris@w3style.co.uk>
+ * @package Swift
+ * @license GNU Lesser General Public License
+ */
+ 
+/**
+ * Swift File Exception
+ * @package Swift
+ * @author Chris Corbyn <chris@w3style.co.uk>
+ */
+class Swift_FileException extends Exception
+{
+	
+}

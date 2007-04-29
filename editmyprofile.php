dfb5cd720046562d9e9443813d9b770e075c5be4
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-04-29 20:49:36 +0000

    in phase with profile_header move to lib
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@1609 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- editmyprofile.php
+++ editmyprofile.php
@@ -4,7 +4,6 @@ require_once "layout/error.php";
 require_once "lib/FunctionsLogin.php";
 require_once "lib/prepare_profile_header.php";
 
-die ("up to there") ;
 
 // Return the crypting criteraia according of IsHidden_* field of a checkbox
 function ShallICrypt($ss) {

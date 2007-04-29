a339e9a79d11bcfefe260294fbda78c820ab785c
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-04-29 09:25:36 +0000

    adding display of link cities regions
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@1585 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/profilepage_header.php
+++ layout/profilepage_header.php
@@ -44,7 +44,10 @@ echo "      </div>\n";  // close col1
 
 echo "      <div id=\"col3\"> \n"; 
 echo "        <div id=\"col3_content\" class=\"clearfix\"> \n";
-echo "				<div id=\"navigation-path\"><a href=\"membersbycountries.php\">", ww("country"), "</a> &gt; <a href=\"#\">",$m->countryname,"</a> &gt; <a href=\"#\">",$m->regionname,"</a> &gt; ",$m->cityname,"\n";
+echo "				<div id=\"navigation-path\"><a href=\"membersbycountries.php\">", ww("country"), "</a> &gt; " ;
+echo "<a href=regions.php?IdCountry=",$m->IdCountry,">",$m->countryname,"</a> &gt; " ;
+echo "<a href=\"cities.php?IdRegion=",$m->IdRegion,"\">",$m->regionname,"</a> &gt; ";
+echo "<a href=\"membersbycities.php?IdCity=",$m->IdCity,"\">",$m->cityname,"</a>\n";
 echo "		    	</div>\n";
 echo "<div id=\"topcontent-columns\">\n";
 echo "			<div id=\"profile-user-info\">\n";

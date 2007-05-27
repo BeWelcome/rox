cf7e209bf6590e363795e207e8fed28bc062611e
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-05-27 16:10:33 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1730 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- resendconfirmyourmail.php
+++ resendconfirmyourmail.php
@@ -7,7 +7,15 @@ require_once "layout/DisplayResendConfirmYourMail.php";
 
 // Find parameters
 
-if (HasRight("Accepter")) $UserName=GetStrParam("UserName") ; // accepter can force a specific username to receive its confirmmail again
+if (HasRight("Accepter")) { // accepter can force a specific username to receive its confirmmail again
+   $UserName=GetStrParam("UserName") ; // accepter can force a specific username to receive its confirmmail again
+	$ReadCrypted = "AdminReadCrypted"; // In this case the AdminReadCrypted will be used
+	$IsVolunteerAtWork = true;
+} else {
+	$IsVolunteerAtWork = false;
+	$ReadCrypted = "AdminReadCrypted"; // In this case the MemberReadCrypted will be used (only owner can decrypt)
+}
+
 
 if (empty($UserName)) {
 	$Username=fUsername($_SESSION["IdMember"]) ;
@@ -33,7 +41,10 @@ if (isset ($_COOKIE['MyBWusername'])) {
   LogStr("Signup on a computer previously used by  <b>".$_COOKIE['MyBWusername']."</b> ","resendconfirmyourmail");
 } 		
 // End of previous cookie was already there
-		
+
+$LastName = $ReadCrypted ($rr->LastName);
+$key = CreateKey($Username, $LastName, $_SESSION['IdMember'], "registration"); // compute a nearly unique key for cross checking
+
 $subj = ww("SignupSubjRegistration", $_SYSHCVOL['SiteName']);
 $urltoconfirm = $_SYSHCVOL['SiteName'] . $_SYSHCVOL['MainDir'] . "main.php?action=confirmsignup&username=$Username&key=$key&id=" . abs(crc32(time())); // compute the link for confirming registration
 $text = ww("SignupTextRegistrationAgain", $FirstName, $SecondName, $LastName, $_SYSHCVOL['SiteName'], $urltoconfirm, $urltoconfirm);

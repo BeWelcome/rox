2bd4271c331a11e08f33403721c378d6d124b508
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-05-28 16:37:28 +0000

    fixing empty search
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1737 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/quicksearch.php
+++ layout/quicksearch.php
@@ -12,18 +12,30 @@ function DisplayResults($TList, $searchtext = "") {
 
 	DisplayHeaderShortUserContent($title);
 	
+	$iiMax = count($TList);
+
 	echo "\n<br><center>\n";
 	echo "<table>\n";
-	echo "<tr valign=center>";
-	echo "<th align=left>", ww("Username"), "</th>";
-	echo "<th>", ww("ProfileSummary"), "</th>";
-	echo "<th>", ww('quicksearchresults', $searchtext), "</th>";
+	
+	if ($iiMax>0) { // only display results if they are found entries
+		 echo "<tr valign=center>";
+		 echo "<th align=left>", ww("Username"), "</th>";
+		 echo "<th>", ww("ProfileSummary"), "</th>";
+		 echo "<th>", ww('quicksearchresults', $searchtext), "</th>";
+	}
 
-	$iiMax = count($TList);
 	for ($ii = 0; $ii < $iiMax; $ii++) {
-		echo "<tr valign=center>";
-		echo "<td align=left>", LinkWithUsername($TList[$ii]->Username);
-		echo "</td>";
+		if (($ii==0) or ($TList[$ii]->Username!=$TList[$ii-1]->Username)) {  // don't display list with everytime the same username
+			 echo "<tr ><td colspan=2></td>";
+			 echo "<tr valign=center>";
+			 echo "<td align=left>" ;
+			 echo LinkWithUsername($TList[$ii]->Username);
+			 echo "<br>",$TList[$ii]->CountryName ;
+			 echo "</td>";
+		}
+		else {
+			 echo "<tr><td></td>" ;
+		}
 		echo "<td>";
 		if ($TList[$ii]->ProfileSummary > 0)
 			echo FindTrad($TList[$ii]->ProfileSummary);
@@ -32,6 +44,10 @@ function DisplayResults($TList, $searchtext = "") {
 		echo "</td>";
 	}
 	echo "</table>\n";
+	
+	if ($iiMax==0) {
+		echo ww("SorryNoresults",$searchtext) ;
+	}
 
 	echo "</center>\n";
 	require_once "footer.php";

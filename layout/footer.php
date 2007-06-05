3f7839b4a3dda4b5098e88b2de6e57370c3f52ad
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-05 16:50:01 +0000

    adding norvegian flag
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1814 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/footer.php
+++ layout/footer.php
@@ -39,6 +39,7 @@ DisplayFlag("pt","pt.png","Portuguese");
 DisplayFlag("hu","hu.png","Hungarian");
 DisplayFlag("lv","lv.png","Latvian");
 DisplayFlag("gr","gr.png","Greek");
+DisplayFlag("no","no.png","Norvegian");
 
 //if ($_SESSION['switchtrans']!='on') echo "<a href=\"",$langurl,"switchtrans=off\"><img border=0 height=10 src=\"images/showtransarray.gif\" alt=\"switch to translation mode\" width=16></a>&nbsp;";
 if ($_SESSION['switchtrans'] == 'on') {

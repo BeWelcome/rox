5edfbdf533048e84f84fee905f4a7b9c90031821
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-06 13:30:49 +0000

    Fix to remove dead code and to manage the StopBoring Me
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1840 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- login.php
+++ login.php
@@ -13,42 +13,6 @@ switch (GetParam("action")) {
 		Login(GetStrParam("Username"), GetStrParam("password"), $nextlink);
 		break;
 
-	case "confirmsignup" : // case a new signupper confirm his mail
-		$m = prepareProfileHeader(IdMember(GetParam("username"))," and Status='MailToConfirm' "); // pending members can edit their profile
-		if (isset ($m->id)) {
-
-			$key = CreateKey($m->Username, ReadCrypted($m->LastName), $m->id, "registration"); // retrieve the nearly unique key
-
-//			echo "key=", $key, "<br>";
-//			echo " GetParam(\"key\")=", GetParam("key"), "<br>";
-//			echo "ReadCrypted(\$m->LastName)=", ReadCrypted($m->LastName), "<br>";
-//			echo "\$m->Username=", $m->Username, "<br>";
-
-			if ($key != GetStrParam("key")) {
-				$errcode = "ErrorBadKey";
-				LogStr("Bad Key", "hacking");
-				DisplayError(ww($errcode));
-				exit (0);
-			}
-
-			if (GetParam("StopBoringMe",0)==1) { // Case in fact the member doesn't want to be signup, but want to be removed
-				 $str = "update members set Status='StopBoringMe' where id=" . $m->id;
-				 LogStr("While his mail was not yet confirmed, member has ask us to stop boring him with confirmation request","Signup") ;
-				 echo "OK, <b>",$m->Username,"</b> we will not send you this confirmation request anymore, thanks for visiting us" ;
-				 exit(0) ;
-			}
-
-
-			$str = "update members set Status='Pending' where id=" . $m->id;
-			sql_query($str);
-			if ($m->IdCity > 0) {
-				$rWhere = LoadRow("select cities.Name as cityname,regions.Name as regionname,countries.Name as countryname from cities,countries,regions where cities.IdRegion=regions.id and countries.id=cities.IdCountry and cities.id=" . $m->IdCity);
-			}
-			require_once "layout/editmyprofile.php";
-			$profilewarning = ww("YouCanCompleteProfAndWait", $m->Username);
-			DisplayEditMyProfile($m, "", "", 0, $rWhere->cityname, $rWhere->regionname, $rWhere->countryname, $profilewarning, array ());
-		}
-		exit (0);
 	case "logout" :
 		Logout("index.php");
 		exit (0);
--- main.php
+++ main.php
@@ -2,10 +2,12 @@
 require_once "lib/init.php";
 require_once "lib/FunctionsLogin.php";
 require_once "layout/error.php";
+require_once "lib/prepare_profile_header.php";
 
 switch (GetParam("action")) {
 	case "confirmsignup" : // case a new signupper confirm his mail
-		$m = LoadRow("select * from members where Username='" . GetParam("username") . "'");
+//		$m = prepareProfileHeader(IdMember(GetStrParam("username"))," and Status='MailToConfirm' "); // pending members can edit their profile
+		$m = LoadRow("select * from members where id=".IdMember(GetStrParam("username"))." and Status='MailToConfirm' "); // pending members can edit their profile
 
 		if (isset ($m->id)) {
 
@@ -29,12 +31,22 @@ switch (GetParam("action")) {
 								echo "\$m->Username=",$m->Username,"<br>";
 			*/
 
-			if ($key != GetParam("key")) {
+			if ($key != GetStrParam("key")) {
 				$errcode = "ErrorBadKey";
 				LogStr("Bad Key", "hacking");
 				DisplayError(ww($errcode));
 				exit (0);
 			}
+
+			if (GetParam("StopBoringMe",0)==1) { // Case in fact the member doesn't want to be signup, but want to be removed
+				 $str = "update members set Status='StopBoringMe' where id=" . $m->id;
+				 sql_query($str);
+				 LogStr("While his mail was not yet confirmed, member has ask us to stop boring him with confirmation request","StopBoringMe") ;
+				 echo "OK, <b>",$m->Username,"</b> we will not send you this confirmation request anymore, thanks for visiting us" ;
+				 die(0) ;
+			}
+
+
 			$str = "update members set Status='Pending' where id=" . $m->id; // The email is confirmed make the status Pending
 			sql_query($str);
 			$m->Status = "Pending";
@@ -48,7 +60,7 @@ if ($m->Status == "Pending") { // Members with Pending status can only update th
 	if ($m->IdCity > 0) {
 		$rWhere = LoadRow("select cities.Name as cityname,regions.Name as regionname,countries.Name as countryname from cities,countries,regions where cities.IdRegion=regions.id and countries.id=cities.IdCountry and cities.id=" . $m->IdCity);
 	}
-	require_once "layout/editmyprofile.php";
+	include "layout/editmyprofile.php";
 	$Message = ww("YouCanCompleteProfAndWait", $m->Username);
 	DisplayEditMyProfile($m, "", "", 0, $rWhere->cityname, $rWhere->regionname, $rWhere->countryname, $Message, array ());
 	exit (0);
@@ -58,7 +70,7 @@ if (IsLoggedIn()) {
 	$m = LoadRow("select * from members where id=" . $_SESSION['IdMember']);
 	$rr=LoadRow("select count(*) as cnt from mycontacts where IdMember=".$_SESSION['IdMember']);
 	$m->NbContacts=$rr->cnt;
-	require_once "layout/main.php";
+	include "layout/main.php";
 	DisplayMain($m);
 } else {
 	Logout("index.php");

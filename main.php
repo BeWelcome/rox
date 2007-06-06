7b81abfa1e7adcbb421f03d4e90b7668fbdf86b0
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-06 13:13:22 +0000

    adding right code at right place
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@1834 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- main.php
+++ main.php
@@ -5,7 +5,7 @@ require_once "layout/error.php";
 
 switch (GetParam("action")) {
 	case "confirmsignup" : // case a new signupper confirm his mail
-		$m = LoadRow("select * from members where Username='" . GetParam("username") . "'");
+		$m = prepareProfileHeader(IdMember(GetParam("username"))," and Status='MailToConfirm' "); // pending members can edit their profile
 
 		if (isset ($m->id)) {
 
@@ -29,12 +29,22 @@ switch (GetParam("action")) {
 								echo "\$m->Username=",$m->Username,"<br>";
 			*/
 
-			if ($key != GetParam("key")) {
+			if ($key != GetStrParam("key")) {
 				$errcode = "ErrorBadKey";
 				LogStr("Bad Key", "hacking");
 				DisplayError(ww($errcode));
 				exit (0);
 			}
+
+			if (GetParam("StopBoringMe",0)==1) { // Case in fact the member doesn't want to be signup, but want to be removed
+				 $str = "update members set Status='StopBoringMe' where id=" . $m->id;
+				 sql_query($str);
+				 LogStr("While his mail was not yet confirmed, member has ask us to stop boring him with confirmation request","StopBoringMe") ;
+				 echo "OK, <b>",$m->Username,"</b> we will not send you this confirmation request anymore, thanks for visiting us" ;
+				 die(0) ;
+			}
+
+
 			$str = "update members set Status='Pending' where id=" . $m->id; // The email is confirmed make the status Pending
 			sql_query($str);
 			$m->Status = "Pending";

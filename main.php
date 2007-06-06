837e331777f64f5680bea07ad1fa1c5442fd7d6d
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-06 13:50:26 +0000

    test
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@1842 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- main.php
+++ main.php
@@ -6,8 +6,8 @@ require_once "lib/prepare_profile_header.php";
 
 switch (GetParam("action")) {
 	case "confirmsignup" : // case a new signupper confirm his mail
-//		$m = prepareProfileHeader(IdMember(GetStrParam("username"))," and Status='MailToConfirm' "); // pending members can edit their profile
-		$m = LoadRow("select * from members where id=".IdMember(GetStrParam("username"))." and Status='MailToConfirm' "); // pending members can edit their profile
+		$m = prepareProfileHeader(IdMember(GetStrParam("username"))," and Status='MailToConfirm' "); // pending members can edit their profile
+//		$m = LoadRow("select * from members where id=".IdMember(GetStrParam("username"))." and Status='MailToConfirm' "); // pending members can edit their profile
 
 		if (isset ($m->id)) {
 

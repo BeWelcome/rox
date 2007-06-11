1ce0164b2c841d952dfe5628d73228de94c3ef6a
globetrotter_tt <globetrotter_tt@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-11 17:18:17 +0000

    *new layout for adminaccepter.php
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1925 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/adminaccepter.php
+++ layout/adminaccepter.php
@@ -5,61 +5,57 @@ function ShowList($TData,$bgcolor="white",$title="") {
 	global $global_count;
 	$max = count($TData);
 	$count = 0;
-	echo "            <table class=\"admin\" width=\"60%\" border=\"0\" bgcolor=$bgcolor>\n";
-	if ($title!="") echo "              <th colspan=4 align=center>\n",$title," (",$max,")\n";
+	
+	if ($title!="") echo "              <p class=\"highlight\">\n",("$max"),$title," \n";
 	for ($ii = 0; $ii < $max; $ii++) {
 		$m = $TData[$ii];
 		$count++;
-		echo "              </th>\n";
-		echo "              <input type=hidden name=IdMember_".$global_count." value=".$m->id.">\n";
-		echo "              <tr>\n";
-		echo "                <td colspan=2> <font size=5>",LinkWithUsername($m->Username),"</font> (",ww($m->Gender),")", " (",fsince($m->created)," ",localdate($m->created),")</td>\n";
-		echo "                <td colspan=2> <font size=4>",$m->FirstName," <i>",$m->SecondName,"</i> <b>",$m->LastName,"</b> </font>(<i>",$m->Email,"</i>)</td>\n";
-		echo "              </tr>\n";
-		echo "              <tr>\n";
-		echo "                <td colspan=4>", $m->ProfileSummary, "</td>\n";
-		echo "              </tr>\n";
-		echo "              <tr>\n";
-		echo "                <td>", $m->HouseNumber, "</td>\n";
-		echo "                <td colspan=2>", $m->StreetName, "</td>\n";
-		echo "                <td>", $m->Zip, "</td>\n";
-		echo "              </tr>\n";
-		echo "              <tr>\n";
-		echo "                <td colspan=4><font color=gray><b>", $m->countryname, " > ", $m->regionname, " > ", $m->cityname, "</b></font></td>\n";
-		echo "              </tr>\n";
-		echo "              <tr>\n";
-		echo "                <td colspan=4><font color=green><b><i>", str_replace("\n","<br>",$m->FeedBack), "</i></b></font></td>\n";
-		echo "              </tr>\n";
-		echo "              <tr>\n";
-		echo "                <td>\n";
+		echo "              \n";
+		$info_styles = array(0 => "          <div class=\"info\">\n", 1 => "          <div class=\" info highlight\">\n");
+		echo $info_styles[($ii%2)];
+		echo "             <input type=hidden name=IdMember_".$global_count." value=".$m->id.">\n";
+		echo "             <p> <font size=5>",LinkWithUsername($m->Username),"</font> (",ww($m->Gender),")", " (",fsince($m->created)," ",localdate($m->created),")</p>\n";
+		echo "             <p> <font size=4>",$m->FirstName," <i>",$m->SecondName,"</i> <b>",$m->LastName,"</b> </font>(<i>",$m->Email,"</i>)</p>\n";
+    // echo "          <h4>", ww('ProfileSummary'), "</h4>\n";	
+		// echo "          <p>", $m->ProfileSummary, "</p\n";
+		echo "             <h4>", ww('Address'), "</h4>\n";
+		echo "             <ul>\n";
+		echo "               <li>", $m->HouseNumber, ", ", $m->StreetName, "</li>\n";
+		echo "               <li>", $m->Zip, " ", $m->cityname, "</li>\n";
+		echo "               <li>", $m->regionname, "</li>\n";
+  	echo "               <li>", $m->countryname, "</li>\n";
+  	echo "             </ul>\n";
+  	echo "            <br />\n";
+  	echo "             <p><font color=green><b><i>", str_replace("\n","<br>",$m->FeedBack), "</i></b></font></p>\n";
+		echo "             <p>\n";
 		if ($m->Status == "Pending")
-		   echo "                <input type=radio name=action_".$global_count." value=accept> accept<br>\n";
-		   echo "                <input type=radio name=action_".$global_count." value=reject> reject<br>\n";
+		   echo "               <input type=radio name=action_".$global_count." value=accept> accept<br>\n";
+		   echo "               <input type=radio name=action_".$global_count." value=reject> reject<br>\n";
 		if ($m->Status == "Pending") {
-		   echo "                <input type=radio name=action_".$global_count." value=needmore> need more<br>\n";
+		   echo "               <input type=radio name=action_".$global_count." value=needmore> need more<br>\n";
 		}
-	    echo "                 <input type=radio name=action_".$global_count." value=nothing> nothing<br>\n";
-		echo "              </td>\n";
-		echo "              <td colspan=3>";
+	  echo "               <input type=radio name=action_".$global_count." value=nothing> nothing<br>\n";
+		echo "             </p>\n";
+		echo "             <p>";
 		if ($m->Status == "Pending") {
 		   echo "needmore aditional text for emailing to member<br>";
 		   echo "                <textarea name=needmoretext_".$global_count." cols=60 rows=4>";
 		   echo "</textarea>\n";
 		}
-		if ($m->Status == "MailToConfirm") {
-			 echo "<a href=\"../resendconfirmyourmail.php?Username=".$m->Username."\" onclick=\"return('Confirm you want to send again ? (beware not to spam members !) ');\">Send request for confirmation mail again</a>" ;
-		}
-		echo "</td>";
+		echo "</p>\n";
 		  
-		echo "<tr><td colspan=5>";
-		echo "<a href=\"".bwlink("contactmember.php?cid=". $m->id). "\">contact</a> ";
-		echo "<a href=\"".bwlink("updatemandatory.php?cid=". $m->id). "\">update mandatory</a>";
-		echo "</td>";
-		echo "<tr><td colspan=5><hr></td>\n";
+		echo "             <ul class=\"linkist\">";
+		if ($m->Status == "MailToConfirm") {
+			 echo "              <li><a href=\"../resendconfirmyourmail.php?Username=".$m->Username."\" onclick=\"return('Confirm you want to send again ? (beware not to spam members !) ');\">Send request for confirmation mail again</a></li>" ;
+		}
+		echo "               <li><a href=\"".bwlink("contactmember.php?cid=". $m->id). "\">contact</a></li>\n";
+		echo "               <li><a href=\"".bwlink("updatemandatory.php?cid=". $m->id). "\">update mandatory</a></li>\n";
+		echo "             </ul>\n";
+		echo "           </div>\n";
 		$global_count++;
 	}
-	echo "<tr><td align=left colspan=2>Total</td><td align=left colspan=2>$count</td>";
-	echo "\n</table><br>\n";
+	echo "        <div class=\"info\">\n";
+	echo "          <p>Total", $count, "</p>\n";
 } // end of ShowList
 
 function DisplayAdminAccepter($TData,$TNeedMore, $lastaction = "") {
@@ -81,8 +77,9 @@ function DisplayAdminAccepter($TData,$TNeedMore, $lastaction = "") {
 
 	DisplayHeaderShortUserContent($title . " : " . $lastaction);
 
-  echo "        <div class=\"info\">\n";
-	echo "          <p>your Scope :", $AccepterScope, "</p>\n";
+  echo "          <div class=\"info\">\n";
+	echo "            <p>your Scope :", $AccepterScope, "</p>\n";
+	echo "          </div>\n";
 	
 	if (!IsAdmin()) {
 	  echo "temporarly disabled, under test";
@@ -97,16 +94,17 @@ function DisplayAdminAccepter($TData,$TNeedMore, $lastaction = "") {
 	   	if ($tt[$ii]==$Status) $filterstatus.=" selected" ;
 	  	$filterstatus.=">$tt[$ii]</option>\n";
 	}	
-	$filterstatus.="          </select>\n" ;
+	$filterstatus.="          </select>  <input type=submit name=submit></p>\n" ;
 	
 	echo "          <form name=adminaccepter action=".bwlink("admin/adminaccepter.php").">\n";
 
-   ShowList($TData,"#ffff66"," Members with status ".$filterstatus);
-   echo "<br><input type=submit name=submit>\n";
+  ShowList($TData,"#ffff66"," Members with status ".$filterstatus);
+  echo "<div style=\"text-align: center\"><input type=submit name=submit></div></div>\n";
 
 	echo "<input type=hidden name=action value=batchaccept>";
 	echo "<input type=hidden name=global_count value=$global_count>";
 	echo "          </form>\n";
+	
 
 	include "footer.php";
 } // end of DisplayAdminAccepter($Taccepted,$Tmailchecking,$Tpending)

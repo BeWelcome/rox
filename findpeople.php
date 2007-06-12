aa214800751f56452022fde728ae23694e3e52bc
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-12 19:06:37 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@1949 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- findpeople.php
+++ findpeople.php
@@ -117,8 +117,8 @@ function buildresult() {
 	}
 
 	$str="select count(members.id) as cnt from ".$tablelist.$where." group by members.id" ;
-//	echo "For counting page limit<b>",$str,"</b>\n" ;
 	$rCount=LoadRow($str) ;
+	if (HasRight("Admin")) echo "For counting page limit<b>",$str,"</b> cnt=",$rCount->cnt,"\n" ;
 	
 	$str="select members.id as IdMember,members.BirthDate,members.HideBirthDate,members.Accomodation,members.Username as Username,members.LastLogin as LastLogin,cities.Name as CityName,countries.Name as CountryName,ProfileSummary,Gender,BirthDate from ".$tablelist.$where." group by members.id ".$OrderBy." limit ".$start_rec.",".$limitcount; ;
 

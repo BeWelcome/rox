4872fc581b9abd875b67beab26e90fdc50f0eae6
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-13 13:00:49 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1985 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- findpeople.php
+++ findpeople.php
@@ -28,13 +28,13 @@ function buildresult() {
 	
 	$nocriteria=true ;
 	$dblink="" ; // This will be used one day to query on another replicated database
-	$tablelist=$dblink."members,".$dblink."cities,".$dblink."countries,".$dblink."comments" ;
+	$tablelist=$dblink."members,".$dblink."cities,".$dblink."countries" ;
 	
 	if (GetStrParam("IncludeInactive"=="on")) {
-		 $where=" where comments.IdToMember=members.id and (members.Status='Active' or members.Status='ChoiceInActive' or members.Status='OutOfRemind')" ; // only active and inactive members
+		 $where=" where (members.Status='Active' or members.Status='ChoiceInActive' or members.Status='OutOfRemind')" ; // only active and inactive members
 	}
 	else {
-		 $where=" where comments.IdToMember=members.id and members.Status='Active'" ; // only active members
+		 $where=" where members.Status='Active'" ; // only active members
 	}
 	
 	
@@ -64,6 +64,13 @@ function buildresult() {
 	 	 	$where=$where." and memberstrads.Sentence like '%".addslashes($TextToFind)."%' and memberstrads.IdOwner=members.id" ;
 		 }
 	   	 $nocriteria=false ;
+	}
+
+// Process IdRegion parameter if any
+	if (GetParam("IdRegion","")!="") {
+	   $IdRegion=GetParam("IdRegion") ;
+	 	 $where=$where." and cities.IdRegion=".$IdRegion ;
+		 $nocriteria=false ;
 	}
 
 // Process Gender parameter if any
@@ -129,12 +136,10 @@ function buildresult() {
 	if ($nocriteria) {
 	   die("You must specify at least one criteria\n") ;
 	}
-
 	$str="select count(distinct members.id) as cnt from ".$tablelist.$where ;
 	$rCount=LoadRow($str) ;
 	if (HasRight("Admin")) echo "For counting page limit: <b>",$str,"</b> cnt=",$rCount->cnt,"<br>\n" ;
-	
-	$str="select count(comments.id) as NbComment,members.id as IdMember,members.BirthDate,members.HideBirthDate,members.Accomodation,members.Username as Username,members.LastLogin as LastLogin,cities.Name as CityName,countries.Name as CountryName,ProfileSummary,Gender,BirthDate from ".$tablelist.$where." group by members.id ".$OrderBy." limit ".$start_rec.",".$limitcount; ;
+	$str="select count(comments.id) as NbComment,members.id as IdMember,members.BirthDate,members.HideBirthDate,members.Accomodation,members.Username as Username,members.LastLogin as LastLogin,cities.Name as CityName,countries.Name as CountryName,ProfileSummary,Gender,BirthDate from (".$tablelist.") left join ".$dblink."comments on (members.id=comments.IdToMember) ".$where." group by members.id ".$OrderBy." limit ".$start_rec.",".$limitcount;
 
 	if (HasRight("Admin")) echo "<b>$str</b><br>" ;
 	$qry = sql_query($str);

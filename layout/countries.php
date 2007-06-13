467831cdd032c747f333b03de88205f72e9bf3fa
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-13 12:07:15 +0000

    realign
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1978 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/countries.php
+++ layout/countries.php
@@ -4,19 +4,21 @@ require_once ("menus.php");
 function DisplayCountries($TList) {
 	global $title;
 	$title = ww('Countries');
-	include "header.php";
+	require_once "header.php";
 
 	Menu1("countries.php", ww('Countries')); // Displays the top menu
 
 	Menu2($_SERVER["PHP_SELF"]);
 
 	DisplayHeaderWithColumns(ww('Countries')); // Display the header
+	echo "          <div class=\"info\">\n";
+	echo "            <ul class=\"floatbox\">\n";
 
 	echo "<ul>\n";
 
 	$iiMax = count($TList);
 	for ($ii = 0; $ii < $iiMax; $ii++) {
-		echo "<li>";
+		echo "              <li>";
 		echo "<a href=regions.php?IdCountry=";
 		echo $TList[$ii]->IdCountry, ">";
 		echo $TList[$ii]->country;
@@ -25,8 +27,9 @@ function DisplayCountries($TList) {
 		echo $TList[$ii]->cnt, ")</a>";
 		echo "</li>\n";
 	}
-	echo "</ul>\n";
-
-	include "footer.php";
+	echo "            </ul>\n";
+	echo "          </div>\n";
+  
+	require_once "footer.php";
 }
 ?>

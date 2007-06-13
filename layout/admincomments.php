e5f98ee2aca98e0725858e7e1323bf6ef5c62eb6
globetrotter_tt <globetrotter_tt@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-13 16:26:37 +0000

    * layout change of admincomments.php
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@1987 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/admincomments.php
+++ layout/admincomments.php
@@ -7,86 +7,86 @@ function ShowList($TData) {
 	$count = 0;
 	$tt = $_SYSHCVOL['LenghtComments'];
 	$max = count($tt);
-	echo "<div class=\"info\">\n";
-	echo "\n<table width=\"95%\">\n";
-	for ($iData = 0; $iData < $maxTData; $iData++) {
+	echo "          <div class=\"info highlight\">\n";
+		for ($iData = 0; $iData < $maxTData; $iData++) {
 		$c = $TData[$iData];
 		$count++;
-		echo "<tr><th colspan=2><div style=\"font-size:20px;\"> comment from ", LinkWithUsername($c->UsernameWriterMember), " to ", LinkWithUsername($c->UsernameReceiverMember), "</div>  <b>", $c->AdminAction, "</b></th>\n";
-
-		echo "<tr><td colspan=2";
-
-		echo "\n<form method=post action=admincomments.php>\n";
-		echo "<table valign=center style=\"font-size:12;\">\n";
-		$ttLenght = explode(",", $c->Lenght);
-		echo "<tr><td>";
-		echo "  <table valign=center style=\"font-size:12;\">\n";
-		for ($ii = 0; $ii < $max; $ii++) {
-			echo "  <tr><td>", ww("Comment_" . $tt[$ii]), "</td>";
-			echo "<td><input type=checkbox name=\"Comment_" . $tt[$ii] . "\"";
-			if (in_array($tt[$ii], $ttLenght))
-				echo " checked ";
-			echo "></td>\n";
-
-		}
-		echo "  </table>\n";
-		echo "</td>";
-
-		echo "<td>";
-		if ($c->AdminComment != "Checked")
-			echo "<a href=\"".bwlink("admin/admincomments.php?IdComment=". $c->id. "&action=Checked")."\">Checked</a><br><br>\n";
-		if (($c->AdminComment != "Checked") and (HasRight("Comments", "AdminComment")))
-			echo "<a href=\"".bwlink("admin/admincomments.php?IdComment=". $c->id. "&action=AdminCommentMustCheck")."\">Admin Comment Must Check</a><br><br>\n";
-		if (($c->AdminComment != "Checked") and (HasRight("Comments", "AdminAbuser")))
-			echo "<a href=\"".bwlink("admin/admincomments.php?IdComment=". $c->id. "&action=AdminAbuserMustCheck")."\">Admin Abuser Must Check</a><br><br>\n";
-		if (($c->AdminComment != "Checked") and (HasRight("Comments", "DeleteComment")))
-			echo "<a href=\"".bwlink("admin/admincomments.php?IdComment=". $c->id. "&action=del\" onclick=\"return('Confirm delete ?');")."\">del</a><br><br>\n";
-		echo "<a href=\"".bwlink("admin/admincomments.php?FromIdMember=" . $c->UsernameWriterMember )."&action=All\">Other comments written by ", $c->UsernameWriterMember, "</a><br><br>\n";
-		echo "<a href=\"".bwlink("admin/admincomments.php?ToIdMember=" . $c->UsernameReceiverMember )."&action=All\">Other comments written about ", $c->UsernameReceiverMember, "</a><br><br>\n";
-		echo "<a href=\"".bwlink("contactmember.php?cid=". $c->IdWriterMember)."\">contact writer (". $c->UsernameWriterMember.")</a><br><br>";
-		echo "<a href=\"".bwlink("contactmember.php?cid=". $c->IdReceiverMember)."\">contact receiver (". $c->UsernameReceiverMember.")</a>\n";
-		echo "</td>\n";
-
-		echo "<tr><td colspan=1>where<br><textarea name=TextWhere cols=70 rows=3>", $c->TextWhere, "</textarea></td>\n";
-
+		echo "\n";
+		echo "            <h3> comment from ", LinkWithUsername($c->UsernameWriterMember), " to ", LinkWithUsername($c->UsernameReceiverMember), "</h3>\n";
+		echo "            <p><strong>", $c->AdminAction, "</strong></p>\n";
+    echo "            <form method=post action=admincomments.php>\n";
+    echo "              <div class=\"subcolumns\">\n";
+		echo "                <div class=\"c50l\">\n";
+		echo "                  <div class=\"subcl\">\n";
+		
 		$QualityStyle = "background-color:lightgreen;";
 		if ($c->Quality == "Bad") {
-			$QualityStyle = "background-color:black;color:white;";
+			$QualityStyle = "background-color:red;color:white;";
 		}
 		if ($c->Quality == "Neutral") {
 			$QualityStyle = "background-color:lightgray;";
 		}
-		echo "<td rowspan=2 valign=center>Quality <select name=Quality style=\"", $QualityStyle, "\">\n";
-		echo "<option value=\"Neutral\" ";
+		echo "                    <p>\n";
+		echo "                      <select name=Quality style=\"", $QualityStyle, "\">\n";
+		echo "                        <option value=\"Neutral\" ";
 		if ($c->Quality == "Neutral")
 			echo "selected";
 		echo ">";
 		echo ww("CommentQuality_Neutral"), "</option>\n";
 
-		echo "<option value=\"Good\"";
+		echo "                        <option value=\"Good\"";
 		if ($c->Quality == "Good")
 			echo " selected ";
 		echo ">", ww("CommentQuality_Good"), "</option>\n";
 
-		echo "<option value=\"Bad\"";
+		echo "                        <option value=\"Bad\"";
 		if ($c->Quality == "Bad")
 			echo " selected ";
 		echo ">", ww("CommentQuality_Bad"), "</option>\n";
-		echo "</selected>\n";
-		echo "</td>\n";
-
-		echo "<tr><td colspan=2>comment:<br><textarea name=TextFree cols=70 rows=8>", $c->TextFree, "</textarea></td>\n";
-
-		echo "<tr><td align=center colspan=2><input type=hidden value=" . $c->id . " name=IdComment><input type=hidden value=" . $IdMember . " name=cid><input type=hidden name=action value=update><input type=submit value=update></td>\n";
-
-		echo "\n</table>";
-		echo "\n</form>";
-
-		echo "</td>";
-		echo "<tr><td colspan=2><hr></td>\n";
+		echo "                      </select>\n";
+		echo "                    </p>\n";
+		
+		$ttLenght = explode(",", $c->Lenght);
+		echo "                    <ul>\n";
+		for ($ii = 0; $ii < $max; $ii++) {
+			echo "                      <li><input type=checkbox name=\"Comment_" . $tt[$ii] . "\"";
+			if (in_array($tt[$ii], $ttLenght))
+				echo " checked ";
+			echo ">";
+			echo "&nbsp;", ww("Comment_" . $tt[$ii]), "</li>\n";
+		}
+		echo "                    </ul>\n";
+    echo "                  </div>\n";
+    echo "                </div>\n";
+    echo "                <div class=\"c50r\">\n";
+		echo "                  <div class=\"subcr\">\n";  
+		echo "                    <ul class=\"linklist\">\n";
+		if ($c->AdminComment != "Checked")
+			echo "                      <li><a href=\"".bwlink("admin/admincomments.php?IdComment=". $c->id. "&action=Checked")."\">Checked</a></li>\n";
+		if (($c->AdminComment != "Checked") and (HasRight("Comments", "AdminComment")))
+			echo "                      <li><a href=\"".bwlink("admin/admincomments.php?IdComment=". $c->id. "&action=AdminCommentMustCheck")."\">Admin Comment Must Check</a></li>\n";
+		if (($c->AdminComment != "Checked") and (HasRight("Comments", "AdminAbuser")))
+			echo "                      <li><a href=\"".bwlink("admin/admincomments.php?IdComment=". $c->id. "&action=AdminAbuserMustCheck")."\">Admin Abuser Must Check</a></li>\n";
+		if (($c->AdminComment != "Checked") and (HasRight("Comments", "DeleteComment")))
+			echo "                      <li><a href=\"".bwlink("admin/admincomments.php?IdComment=". $c->id. "&action=del\" onclick=\"return('Confirm delete ?');")."\">del</a></li>\n";
+		echo "                      <li><a href=\"".bwlink("admin/admincomments.php?FromIdMember=" . $c->UsernameWriterMember )."&action=All\">Other comments written by ", $c->UsernameWriterMember, "</a></li>\n";
+		echo "                      <li><a href=\"".bwlink("admin/admincomments.php?ToIdMember=" . $c->UsernameReceiverMember )."&action=All\">Other comments written about ", $c->UsernameReceiverMember, "</a></li>\n";
+		echo "                      <li><a href=\"".bwlink("contactmember.php?cid=". $c->IdWriterMember)."\">contact writer (". $c->UsernameWriterMember.")</a></li>\n";
+		echo "                      <li><a href=\"".bwlink("contactmember.php?cid=". $c->IdReceiverMember)."\">contact receiver (". $c->UsernameReceiverMember.")</a></li>\n";
+		echo "                    </ul>\n";
+		echo "                  </div>\n";
+    echo "                </div>\n";
+    echo "              </div>\n";
+  	echo "              <h4>Where ?</h4>\n";
+		echo "              <p><textarea name=TextWhere cols=50 rows=3>", $c->TextWhere, "</textarea></p\n";
+		echo "              <h4>Comment:</h4>\n";
+		echo "              <p><textarea name=TextFree cols=50 rows=8>", $c->TextFree, "</textarea></p\n";
+		echo "              <p align=\"center\"><input type=hidden value=" . $c->id . " name=IdComment><input type=hidden value=" . $IdMember . " name=cid><input type=hidden name=action value=update><input type=submit value=update></p>\n";
+    echo "              <br /><br />\n";		
+		echo "             </form>\n";
 	}
-	echo "<tr><td align=left >Total</td><td align=left>$count</td>";
-	echo "\n</table><br>\n";
+	echo "            <p><strong>Total number of comments:</strong> ", $count, "</p>\n";
+	echo "          </div>\n";
 } // end of ShowList
 
 function DisplayAdminComments($TData, $lastaction = "") {
@@ -102,15 +102,15 @@ function DisplayAdminComments($TData, $lastaction = "") {
 	Menu2("admincomments.php", ww('MainPage')); // Displays the second menu
 
 	DisplayHeaderShortUserContent($title . " : " . $lastaction);
-	echo " your Scope :", $AdminCommentsScope;
+	echo "          <div class=\"info\">\n";
+	echo "          <p> your Scope :", $AdminCommentsScope;
 	if (HasRight("Comments", "AdminAbuser"))
 		echo " <a href=\"".bwlink("admin/admincomments.php?action=AdminAbuser")."\">Comments to check by Admin Abuser</a>";
 	echo " <a href=\"".bwlink("admin/admincomments.php?action=All")."\">All Comments </a>";
-
-	echo "<center>";
+  echo "</p>\n";
+  echo "          </div>\n";
 	ShowList($TData);
-	echo "</center>";
-	echo "</div>\n";
+
 
 	require_once "footer.php";
 } // end of DisplayAdminAccepter($Taccepted,$Tmailchecking,$Tpending)

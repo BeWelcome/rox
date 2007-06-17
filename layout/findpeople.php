6c0039f251b558140d6b01f8e39848c1ab553dbb
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-17 08:09:45 +0000

    Adding CityName
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@2022 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/findpeople.php
+++ layout/findpeople.php
@@ -12,6 +12,7 @@ function ParamUrl() {
 	$strurl.="&IdGroup=".GetParam("IdGroup") ;
 	$strurl.="&TextToFind=".GetStrParam("TextToFind") ;
 	$strurl.="&IncludeInactive=".GetStrParam("IncludeInactive") ;
+	$strurl.="&CityName=".GetStrParam("CityName") ;
 	return($strurl) ;
 } // end of ParamUrl
 
@@ -215,12 +216,18 @@ function DisplayFindPeopleForm($TGroup,$TM,$maxpos) {
 	echo "                <li>\n";
 	echo "                  <p><strong class=\"small\">",ww("Username"),"</strong><br />\n";
 	echo "                  <input type=\"text\" name=\"Username\" size=\"30\" maxlength=\"30\" value=\"\"";
-	if ((GetParam("OrUsername",0)==1)and(IdMember($TextToFind)!=0)) { // in		 echo GetStrParam("TextToFind") ;
+	if ((GetParam("OrUsername",0)==1)and(IdMember($TextToFind)!=0)) { // in
+		 echo GetStrParam("TextToFind") ;
 	}
 	else {
 		 echo GetStrParam("Username") ;
 	}
 	echo " /></p>\n";
+	echo "                </li>\n";
+	echo "                <li>\n";
+	echo "                  <p><strong class=\"small\">",ww("CityName"),"</strong><br />\n";
+	echo "                  <input type=\"text\" name=\"CityName\" size=\"30\" maxlength=\"30\" value=\"",GetStrParam("CityName"),"\" />\n";
+	echo "                  </p>\n";
 	echo "                </li>\n";
 	echo "                <li>\n";
 	echo "                  <p><strong class=\"small\">",ww("Age"),"</strong><br />\n";
@@ -230,7 +237,8 @@ function DisplayFindPeopleForm($TGroup,$TM,$maxpos) {
 	echo "                <li>\n";
 	echo "                  <p><strong class=\"small\">",ww("TextToFind"),"</strong><br />\n";
 	echo "                  <input type=\"text\" name=\"TextToFind\" size=\"30\" maxlength=\"30\" value=\"\"" ;
-   if ((GetParam("OrUsername",0)==0)or(IdMember($TextToFind)==0)) { // if we were not comming from the quicksearch 	   echo GetStrParam("TextToFind") ;
+   if ((GetParam("OrUsername",0)==0)or(IdMember($TextToFind)==0)) { // if we were not comming from the quicksearch 
+	   echo GetStrParam("TextToFind") ;
 	}
 	echo " /></p>\n";
 	echo "                </li>\n";

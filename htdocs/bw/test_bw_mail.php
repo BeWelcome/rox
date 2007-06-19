82606be71784498d5e780aa39c0297f155b3d333
lemon339 <lemon339@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-19 14:44:02 +0000

    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@2069 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/test_bw_mail.php
+++ htdocs/bw/test_bw_mail.php
@@ -1,7 +1,7 @@
 <?php
 
 ini_set('display_errors', 1);
-ini_set( "error_reporting", E_ALL );
+ini_set( "error_reporting", E_NOTICE );
 
 //Load in the files we'll need
 require_once "swift/Swift.php";

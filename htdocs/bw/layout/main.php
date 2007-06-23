e919981855a5f82234f48d4c626bb883e63f4bbd
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-23 10:53:36 +0000

    Simplified main.php
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@2152 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/layout/main.php
+++ htdocs/bw/layout/main.php
@@ -22,68 +22,51 @@ function DisplayMain($me, $mlast,$TVisits,$newscount=0) {
 	if ($me->NbContacts>0) {
 	   $ListOfActions.= "<li><a href=\"mycontacts.php\">" . ww('DisplayAllContacts') . "</a></li>\n" ;
 	}
-	$ListOfActions.= VolMenu();
-	DisplayHeaderMainPage( "<br>&nbsp;&nbsp;&nbsp;". ww('MainPage'), "", $ListOfActions);
 
-	// open col3 (middle column)
-	echo "    <div id=\"col3\"> \n"; 
-	echo "      <div id=\"col3_content\" class=\"clearfix\"> \n"; 	
+	DisplayHeaderWithColumns( "<br>&nbsp;&nbsp;&nbsp;". ww('MainPage'), "", $ListOfActions);
+
+
+   echo "          <div class=\"info\">\n";
+	
 	
 	echo "<table>" ;
-	echo "<tr><td class=\"info\">" ;
+	echo "<tr><td>" ;
+	echo ww("RecentMember") ;
+	echo "</td>" ;
+	echo "<td colspan=3 align=left> " ;
+	echo ww("RecentVisitsOfyourProfile") ;
+	echo "</td>" ;
 
-	echo "		<div class=\"subcolumns main_preposts\">\n"; 
 // Display the last created members with a picture
 	$m=$mlast ;
-	echo "			  <div class=\"c33l\">\n"; 
-	echo "			    <div class=\"subcl\">\n"; 
-	echo "				<h3>",ww("RecentMember"),"</h3>\n"; 
-	echo "				<p class=\"floatbox\">";
-	echo LinkWithPicture($m->Username,$m->photo), LinkWithUsername($m->Username),"<br />",$m->countryname ;
-	echo "				</p>\n"; 
-	echo "			    </div>\n"; 
-	echo "			  </div>\n"; 
-	echo "			  <div class=\"c66r\">\n"; 
-	echo "			  <h3>",ww("RecentVisitsOfyourProfile"),"</h3>\n"; 
+	echo "<tr>" ;
+	echo "<td class=\"memberlist\">";
+   echo LinkWithPicture($m->Username,$m->photo);
+	echo "<br>" ;
+	echo LinkWithUsername($m->Username),"<br>" ;
+	echo $m->countryname, "</td> ";
 	
-	$DivForVisit[0]='c50l' ;
-	$DivForVisit[1]='c50r' ;
-	$DivForVisit[2]='c50r' ;
-// /*###   NEW   To be programmed: show the first visitor, then the second. !! Different div's (c50l, c50r)!  ###
-	for ($ii=0;$ii<count($TVisits);$ii++) {
-			$m=$TVisits[$ii] ;
-			echo "				  <div class=\"",$DivForVisit[$ii],"\">\n"; 
-			echo "				    <div class=\"subl\">\n"; 
-			echo "					<p class=\"floatbox\">";
-			echo LinkWithPicture($m->Username,$m->photo), LinkWithUsername($m->Username),"<br />",$m->countryname ;
-			echo "				</p>\n"; 
-			echo "					</div>\n"; 
-			echo "				  </div>\n"; 
-	} // end of for $ii on visits
-	/* 
-	echo "				  <div class=\"c50r\">\n"; 
-	echo "				    <div class=\"subcr\">\n"; 
-	echo "					 <p class=\"floatbox\"><img src=\"images/et.gif\" width=\"50\" height=\"50\" border=\"0\" alt=\"\" align=\"top\"  class=\"float_left framed\"><a href=\"#\" class=\"username\">maplefanta</a><br />from Oberschwanbach in USA:<br> <q>I love BeWelcome</q></p>		\n"; 
-	echo "					</div>\n"; 
-	echo "				  </div>\n"; 
-*/
-	echo "			  </div>\n"; 
-	echo "		</div>\n";
-
-// OLD DEACTIVATED Display the max last three visits
-/*
+// Display the max last three visits
 	for ($ii=0;$ii<count($TVisits);$ii++) {
 	$m=$TVisits[$ii] ;
-	echo "<td class=\"memberlist\" align=center>";
+	echo "<td class=\"memberlist\" align=left>";
    echo LinkWithPicture($m->Username,$m->photo);
 	echo "<br>" ;
 	echo LinkWithUsername($m->Username), "<br>";
 	echo $m->countryname, "</td> ";
 	  
 	} // end of for $ii on visits
-*/	
+	
+	echo "</table>" ;
+	
 
-	// news	
+
+	// dispplay the hello xx
+	echo ww("HelloUsername",LinkWithUsername($me->Username)) ;
+	
+	// news
+
+	echo "<br>" ;
 	echo "				<div class=\"subcolumns main_posts\">\n"; 
 	echo "				  <div class=\"c62l\">\n"; 
 	echo "				    <div class=\"subcl\">\n"; 
@@ -96,36 +79,16 @@ function DisplayMain($me, $mlast,$TVisits,$newscount=0) {
 	echo "				    </div>\n"; 
 	echo "				  </div>\n"; 
 	echo "\n"; 
-	echo "				  <div class=\"c38r\">\n"; 
-	echo "				    <div class=\"subcr\">\n"; 
-	echo "					<h3>Next visitors in town:</h3>\n"; 
-	echo "							 <ul>\n"; 
-	echo "							 	<li><a href=\"#\" class=\"username\">member1</a><span class=\"small grey\"> / 4 June 2007</span> </li>\n"; 
-	echo "							 	<li><a href=\"#\" class=\"username\">maplefanta</a><span class=\"small grey\"> / 14 June 2007</span> </li>\n"; 
-	echo "							 	<li><a href=\"#\" class=\"username\">autoseeker23</a><span class=\"small grey\"> / 23 June 2007</span> </li>\n"; 
-	echo "							 </ul>									\n"; 
-	echo "				    </div>\n"; 
-	echo "				  </div>\n"; 
-	echo "				</div>\n"; 
-	echo "				\n"; 
-/*	echo "				<p><a href=\"#\">",ww("MoreNews"),"</a></p>\n";  */
-	echo "</td>\n"; 
-	echo "</tr>\n"; 
-	echo "</table>\n";
 
-	
-	// news
-/* OLD DEACTIVATED
-	echo "<br><br>",ww("News"),"<br><br>" ;
+	/*
 	echo "<table cellspacing=5 cellspadding=5>" ;
 	for ($ii=$newscount;$ii>0;$ii--) {
 		echo "<tr><td><i>",ww("NewsTitle_".$ii),"</i></td><td><font color=gray>",newsdate("NewsTitle_".$ii),"</font></td>" ;
 		echo "<tr><td colspan=2>",ww("NewsText_".$ii),"<br> </td>" ;
 	}
 	echo "</table>" ;
-
-*/
 	
+	*/
 
 	require_once "footer.php";
 }

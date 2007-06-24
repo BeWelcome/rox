3f1497f04270c99d81fcab14f80fd9683b7ddd61
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-24 13:41:34 +0000

    iproving proposecity
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@2224 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- lib/FunctionsTools.php
+++ lib/FunctionsTools.php
@@ -226,7 +226,7 @@ function ProposeRegion($Id = 0, $IdCountry = 0, $form = "signup") {
 // or to CityName and preselected country if any 
 function ProposeCity($Id = 0, $IdRegion = 0,$form="signup",$CityName="",$IdCountry=0) {
 	$ss="\n<input type=hidden name=IdCity Value=0>\n";
-	if ($CityName!="") {
+	if (($CityName!="") and ($form!="findpeopleform")) {
 //	    $str = "select SQL_CACHE id,Name,OtherNames from cities where IdRegion=" . $IdRegion . " and ActiveCity='True' order by Name";
 		$str = "select SQL_CACHE cities.id,cities.Name,cities.OtherNames,regions.name as RegionName from (cities) left join regions on (cities.IdRegion=regions.id) where  cities.IdCountry=" . $IdCountry . " and ActiveCity='True' and cities.Name like '".$CityName."%' order by cities.population desc";
 	}

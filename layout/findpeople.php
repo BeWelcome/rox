e934a525d798681c9d15bc4e13f8593b17085193
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-24 13:51:00 +0000

    debugging
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@2229 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/findpeople.php
+++ layout/findpeople.php
@@ -214,7 +214,7 @@ function DisplayFindPeopleForm($TGroup,$TM,$maxpos=-1) {
 	if ($IdCountry!=0) {
 	   $IdCity=GetParam("IdCity",0) ;
 	   if ($IdCity==0) {
-	   	  $scity = ProposeCity($IdCity, -1, "findpeopleform",getcountryname($IdCity),$IdCountry);
+	   	  $scity = ProposeCity($IdCity, 0, "findpeopleform",getcountryname($IdCity),$IdCountry);
 	   }
 	   else {
 	   	  $scity = ProposeCity($IdCity, 0, "findpeopleform",getcountryname($IdCity),$IdCountry);
@@ -267,6 +267,7 @@ function DisplayFindPeopleForm($TGroup,$TM,$maxpos=-1) {
 	echo "                  </p>\n";
 	echo "                </li>\n";
 
+/*	
 	if (GetParam("IdCountry",0)!=0) {
 	   echo "                <li>\n";
 	   echo "                  <p><strong class=\"small\">",ww("City"),"</strong><br />\n";
@@ -274,6 +275,7 @@ function DisplayFindPeopleForm($TGroup,$TM,$maxpos=-1) {
 	   echo "                  </p>\n";
 	   echo "                </li>\n";
 	}
+	*/
 
 	echo "                <li>\n";
 	echo "                  <p><strong class=\"small\">",ww("Gender"),"</strong><br />\n";

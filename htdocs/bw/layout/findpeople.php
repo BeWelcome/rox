4095d11896a3a583e8fd95d55340857559abb77b
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-24 13:51:15 +0000

    debugging
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@2230 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/layout/findpeople.php
+++ htdocs/bw/layout/findpeople.php
@@ -213,7 +213,12 @@ function DisplayFindPeopleForm($TGroup,$TM,$maxpos=-1) {
 	$scountry = ProposeCountry($IdCountry, "findpeopleform");
 	if ($IdCountry!=0) {
 	   $IdCity=GetParam("IdCity",0) ;
-	   $scity = ProposeCity($IdCity, 0, "findpeopleform",getcountryname($IdCity),$IdCountry);
+	   if ($IdCity==0) {
+	   	  $scity = ProposeCity($IdCity, 0, "findpeopleform",getcountryname($IdCity),$IdCountry);
+	   }
+	   else {
+	   	  $scity = ProposeCity($IdCity, 0, "findpeopleform",getcountryname($IdCity),$IdCountry);
+	   }
 	}
 //echo "IdMember(GetStrParam(\"TextToFind\")=",IdMember(GetStrParam("TextToFind"));
 //echo " GetParam(\"OrUsername\",0)=",GetParam("OrUsername",0),"<br>\n" ;
@@ -262,6 +267,7 @@ function DisplayFindPeopleForm($TGroup,$TM,$maxpos=-1) {
 	echo "                  </p>\n";
 	echo "                </li>\n";
 
+/*	
 	if (GetParam("IdCountry",0)!=0) {
 	   echo "                <li>\n";
 	   echo "                  <p><strong class=\"small\">",ww("City"),"</strong><br />\n";
@@ -269,6 +275,7 @@ function DisplayFindPeopleForm($TGroup,$TM,$maxpos=-1) {
 	   echo "                  </p>\n";
 	   echo "                </li>\n";
 	}
+	*/
 
 	echo "                <li>\n";
 	echo "                  <p><strong class=\"small\">",ww("Gender"),"</strong><br />\n";

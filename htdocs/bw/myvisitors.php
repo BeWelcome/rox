2dd3252c3ee0d4902e7c38aa5b2a6df5001a6bbe
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-29 17:36:05 +0000

    change with profile summary
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@2346 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/myvisitors.php
+++ htdocs/bw/myvisitors.php
@@ -9,10 +9,10 @@ MustLogIn();
 // Find parameters
 $IdMember = $_SESSION['IdMember'];
 if (IsAdmin()) { // admin can alter other profiles
-	$IdMember = GetParam("cid", $_SESSION['IdMember']);
+	$IdMember = IdMember(GetStrParam("cid", $_SESSION['IdMember']));
 }
 
-$m = prepareProfileHeader($IdMember,"",0); // This is the profile of the contact which is going to be used
+$m = prepareProfileHeader($IdMember,"",0); // This is the profile of the member which is concerned by visits
 
 switch (GetParam("action")) {
 	case "del" : // todo
@@ -20,8 +20,14 @@ switch (GetParam("action")) {
 }
 
 $TData = array ();
+
+// this is with picture only
 $str = "select recentvisits.created as datevisite,members.Username,members.ProfileSummary,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment";
-$str .= " from cities,countries,regions,recentvisits,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where cities.IdRegion=regions.id and countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' and members.id=recentvisits.IdVisitor and recentvisits.IdMember=" . $IdMember . " and members.status='Active' GROUP BY members.id order by recentvisits.created desc";
+$str .= " from cities,countries,regions,recentvisits,members,membersphotos where membersphotos.IdMember=members.id and membersphotos.SortOrder=0 and cities.IdRegion=regions.id and countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' and members.id=recentvisits.IdVisitor and recentvisits.IdMember=" . $IdMember . " and members.status='Active' GROUP BY members.id order by recentvisits.created desc";
+
+// regardless pictures
+$str = "select recentvisits.created as datevisite,members.Username,members.ProfileSummary,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment";
+$str .= " from (cities,countries,regions,recentvisits,members) left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where cities.IdRegion=regions.id and countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' and members.id=recentvisits.IdVisitor and recentvisits.IdMember=" . $IdMember . " and members.status='Active' GROUP BY members.id order by recentvisits.created desc";
 $qry = sql_query($str);
 while ($rr = mysql_fetch_object($qry)) {
 	if ($rr->Comment > 0) {

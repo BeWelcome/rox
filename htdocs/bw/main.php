796518764d4058620993526675e28f6a8162c6e0
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-06-30 14:03:48 +0000

    using profilesvisits
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@2362 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/main.php
+++ htdocs/bw/main.php
@@ -73,8 +73,8 @@ if (IsLoggedIn()) {
 	include "layout/main.php";
 
 	$TVisits=array() ;
-   $str = "select recentvisits.created as datevisite,members.Username,members.ProfileSummary,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment";
-   $str .= " from cities,countries,regions,recentvisits,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where cities.IdRegion=regions.id and countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' and members.id=recentvisits.IdVisitor and recentvisits.IdMember=" . $_SESSION["IdMember"] . " and members.Status='Active' GROUP BY members.id order by recentvisits.created desc limit 3";
+   $str = "select profilesvisits.updated as datevisite,members.Username,members.ProfileSummary,cities.Name as cityname,regions.Name as regionname,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment";
+   $str .= " from cities,countries,regions,profilesvisits,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where cities.IdRegion=regions.id and countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' and members.id=profilesvisits.IdVisitor and profilesvisits.IdMember=" . $_SESSION["IdMember"] . " and members.Status='Active' GROUP BY members.id order by profilesvisits.updated desc limit 3";
    $qry = sql_query($str);
   	while ($rr = mysql_fetch_object($qry)) {
 	  if ($rr->Comment > 0) {

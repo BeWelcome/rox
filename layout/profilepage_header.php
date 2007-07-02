931fa868659faab9ce135565a665013976d0c2c4
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-07-02 20:08:50 +0000

    fix
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@2426 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/profilepage_header.php
+++ layout/profilepage_header.php
@@ -110,8 +110,8 @@ function DisplayProfilePageHeader( $m )
 		echo "              <img src=\"images/icon_castle.gif\" class=\"float_left\" title=\"", ww("CanOfferCityTour"),"\" width=\"30\" height=\"30\" alt=\"icon_castle\" />\n"; 
 	if (strstr($m->TypicOffer, "dinner")
 		echo "              <img src=\"images/icon_food.gif\" class=\"float_left\" title=\"", ww("CanOfferDinner"),"\" width=\"30\" height=\"30\" alt=\"icon_food\" />\n";
-	if ($m->CanHostWeelChair=="Yes")
-		echo "              <img src=\"images/wheelchair.gif\" class=\"float_left\" title=\"", ww("CanHostWeelChairYes"),"\" width=\"30\" height=\"30\" alt=\"wheelchair\" />\n";
+	if (strstr($m->TypicOffer, "CanHostWeelChair")
+		echo "              <img src=\"images/wheelchair.gif\" class=\"float_left\" title=\"", ww("CanHostWeelChair"),"\" width=\"30\" height=\"30\" alt=\"wheelchair\" />\n";
 
 	echo "<table>";
 	echo "<tr>";	

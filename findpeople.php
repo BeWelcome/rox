1665af1626f9a478f5b1474a769e9414f18fa2fe
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-07-04 17:07:01 +0000

    fix
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@2465 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- findpeople.php
+++ findpeople.php
@@ -45,7 +45,7 @@ function buildresult() {
 				if ($set!="") $set.="," ;
 				$set=$set."'".$value."'" ;
       }
-			$where.=" and TypicOffer FIND_IN_SET(".$set.")" ;
+			$where.=" and  FIND_IN_SET(".$set.",TypicOffer)" ;
 	} 	
 // Process Username parameter if any
 	if (GetStrParam("Username","")!="") {

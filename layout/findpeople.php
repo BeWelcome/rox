a9456c2f50187761caaf23ec82c14233d0ce014c
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-07-04 20:50:08 +0000

    fix
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@2474 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/findpeople.php
+++ layout/findpeople.php
@@ -13,7 +13,7 @@ function ParamUrl() {
 	$strurl.="&TextToFind=".GetStrParam("TextToFind") ;
 	$strurl.="&IncludeInactive=".GetStrParam("IncludeInactive") ;
 	$strurl.="&CityName=".GetStrParam("CityName") ;
-	$strurl.="&TypicOffer=".GetStrParam("TypicOffer[]") ;
+	$strurl.="&TypicOffer=".GetArrayParam("TypicOffer") ;
 	return($strurl) ;
 } // end of ParamUrl
 
@@ -201,7 +201,7 @@ function DisplayFindPeopleForm($TGroup,$TM,$maxpos=-1) {
 	echo "        <h1>", $title, " </h1>\n";
 	echo "      </div>\n";
 
-	echo "TypicOffer[]=",GetStrParam("TypicOffer[]","empty"),"<br>" ;
+	echo "TypicOffer[]=",print_r(GetArrayParam("TypicOffer[]","empty")),"<br>" ;
 	
 	menufindmembers("findpeople.php" . $menutab, $title);
 

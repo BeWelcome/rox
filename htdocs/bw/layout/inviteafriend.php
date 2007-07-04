c5bad3594bba833320c60d7d3a90a0aa72f8928f
fake51 <fake51@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-07-04 20:15:47 +0000

    Proofing the html coding - attributes weren't encoded right
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@2470 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/layout/inviteafriend.php
+++ htdocs/bw/layout/inviteafriend.php
@@ -16,20 +16,20 @@ function DisplayForm($m,$JoinMemberPict="") {
 	// TODO: check the meaning of the next row. $TData is not defined
 	$iiMax = count($TData);
 	$CurrentCategory="";
-	echo "<form method=post action=inviteafriend.php>\n";
-	echo "<input type=hidden name=action value=Send>\n";
+	echo "<form method=\"post\" action=\"inviteafriend.php\">\n";
+	echo "<input type=\"hidden\" name=\"action\" value=\"Send\">\n";
 	echo "<p>",ww("InviteAFriendRule",$m->FullName),"</p>\n";
-	echo "<p>",ww("EmailOfYourFriend")," <input type=text name=Email value=\"",GetStrParam("Email"),"\"></p>\n";
+	echo "<p>",ww("EmailOfYourFriend")," <input type=\"text\" name=\"Email\" value=\"",GetStrParam("Email"),"\"></p>\n";
 	if (IsPublic($_SESSION["IdMember"])) {
-		 echo "<p>","<textarea name=Message rows=20 cols=80>",str_replace("<br />","\n",ww("InviteAFriendStandardText","<a href=\"http://www.bewelcome.org/member.php?cid=".$_SESSION["Username"]."\">".$_SESSION["Username"]."</a>")),"</textarea></p>\n";
+		 echo "<p>","<textarea name=\"Message\" rows=\"20\" cols=\"80\">",str_replace("<br />","\n",ww("InviteAFriendStandardText","<a href=\"http://www.bewelcome.org/member.php?cid=".$_SESSION["Username"]."\">".$_SESSION["Username"]."</a>")),"</textarea></p>\n";
 	}
 	else {
-		 echo "<p>","<textarea name=Message rows=20 cols=80>",str_replace("<br />","\n",ww("InviteAFriendTextPrivateProf","<a href=\"http://www.bewelcome.org/member.php?cid=".$_SESSION["Username"]."\">".$_SESSION["Username"]."</a>")),"</textarea></p>\n";
+		 echo "<p>","<textarea name=\"Message\" rows=\"20\" cols=\"80\">",str_replace("<br />","\n",ww("InviteAFriendTextPrivateProf","<a href=\"http://www.bewelcome.org/member.php?cid=".$_SESSION["Username"]."\">".$_SESSION["Username"]."</a>")),"</textarea></p>\n";
 	}
-	echo "<p><input type=checkbox name=JoinMemberPict ";
+	echo "<p><input type=\"checkbox\" name=\"JoinMemberPict\" ";
 	if ($JoinMemberPict=="on") echo "checked";
 	echo "> ",ww("JoinMyPicture")," </p>\n";
-	echo "<p align=center><input type=submit></p>\n";
+	echo "<p align=\"center\"><input type=\"submit\"></p>\n";
 	echo "</div>\n";
 
 	require_once "footer.php";

5e0b762d2de5b7cac76c9992108a6d39635ee995
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-07-04 16:28:34 +0000

    starting to add TypicOffer has filter
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@2453 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/findpeople.php
+++ layout/findpeople.php
@@ -13,7 +13,7 @@ function ParamUrl() {
 	$strurl.="&TextToFind=".GetStrParam("TextToFind") ;
 	$strurl.="&IncludeInactive=".GetStrParam("IncludeInactive") ;
 	$strurl.="&CityName=".GetStrParam("CityName") ;
-	$strurl.="&CanHostWeelChair=".GetStrParam("CanHostWeelChair") ;
+	$strurl.="&TypicOffer=".GetStrParam("TypicOffer") ;
 	return($strurl) ;
 } // end of ParamUrl
 
@@ -309,10 +309,17 @@ function DisplayFindPeopleForm($TGroup,$TM,$maxpos=-1) {
 	echo "                  </p>\n";
 	echo "                </li>\n";
 	echo "                <li>\n";
-	echo ww("MustHostWheelChair") ;
-	echo " <input name=CanHostWeelChair type=checkbox" ;
-	if (GetStrParam("CanHostWeelChair")=="on") echo " checked" ;
-	echo ">" ;
+	echo ww("WhoOfferTypicOffer")," \n" ;
+	echo " <select name=TypicOffer[] multiple>" ;
+
+	$TabTypicOffer = sql_get_set("members", "TypicOffer");
+
+	for ($ii=0;$ii<count($TabTypicOffer);$ii++) {
+			echo "<option value=\"".$ii."\" ;
+			if ($TypicOffer[$ii]=="on") echo " checked " ;
+			echo ">",ww("Fiter_".$TabTypicOffer[$ii],"</option>" ;
+	}
+	echo "</select>" ;
 	echo "                </li>\n";
 	
 	echo "              </ul>\n";

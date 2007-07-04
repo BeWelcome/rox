e82a14f48bf6a6800276b2382572f53e0e7eee34
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-07-04 17:15:04 +0000

    fix
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@2468 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- layout/findpeople.php
+++ layout/findpeople.php
@@ -201,7 +201,7 @@ function DisplayFindPeopleForm($TGroup,$TM,$maxpos=-1) {
 	echo "        <h1>", $title, " </h1>\n";
 	echo "      </div>\n";
 
-	echo "TypicOffer=",GetParamStr("TypicOffer","empty"),"<br>" ;
+	echo "TypicOffer=",GetStrParam("TypicOffer","empty"),"<br>" ;
 	
 	menufindmembers("findpeople.php" . $menutab, $title);
 

b7b7808b9be67dd193a378c5f5eb753f8a6b3444
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-07-10 14:07:10 +0000

    fix (from test)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@2510 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- lib/FunctionsTools.php
+++ lib/FunctionsTools.php
@@ -651,24 +651,20 @@ function fFullName($m) {
 
 //------------------------------------------------------------------------------
 function GetPreference($namepref,$idm=0) {
-	$IdMember=0;
-   if ($idm==0) {
+	$IdMember=$idm;
+  if ($idm==0) {
 	   if ($_SESSION['IdMember']!="") $IdMember=$_SESSION['IdMember'];
-	   
-	}
-	else {
-		$IdMember=$idm;
 	}
 	if ($IdMember==0) {
-	   $rr=LoadRow("select DefaultValue SQL_CACHE from preferences where codeName='".$namepref."'");
+	   $rr=LoadRow("select SQL_CACHE DefaultValue from preferences where codeName='".$namepref."'");
 	   return($rr->DefaultValue);
 	}
 	else {
 	   $rr = LoadRow("select SQL_CACHE Value from memberspreferences,preferences where preferences.codeName='$namepref' and memberspreferences.IdPreference=preferences.id and IdMember=" . $IdMember);
 	   if (isset ($rr->Value))
-		  $def = $rr->Value;
-		else {
-	   	  $rr=LoadRow("select DefaultValue SQL_CACHE from preferences where codeName='".$namepref."'");
+		  	$def = $rr->Value;
+		 else {
+	   	  $rr=LoadRow("select SQL_CACHE DefaultValue from preferences where codeName='".$namepref."'");
 	   	  if (isset($rr->DefaultValue))
 	      	return($rr->DefaultValue);
 	      else
@@ -682,7 +678,9 @@ function GetPreference($namepref,$idm=0) {
 //------------------------------------------------------------------------------
 // function GetDefaultLanguage return the default language of member $IdMember 
 function GetDefaultLanguage($IdMember=0) {
-	return(GetPreference("PreferenceLanguage",$IdMember));
+	$res=GetPreference("PreferenceLanguage",$IdMember) ;
+	if (empty($res)) return (0) ;
+	return($res);
 } // end of GetDefaultLanguage
 
 //------------------------------------------------------------------------------
@@ -701,8 +699,8 @@ function GetEmail($IdMemb = 0) {
 
 //------------------------------------------------------------------------------
 // function GetEmail return the email of member $IdMember (or current member if 0) 
-function LanguageName($IdLanguage) {
-	$rr = LoadRow("select SQL_CACHE EnglishName,ShortCode from languages where id=" . $IdLanguage);
+function LanguageName($IdLanguage=0) {
+	$rr = LoadRow("select SQL_CACHE EnglishName,ShortCode from languages where id=" . $IdLanguage." /* in LanguageName*/);
 	return ($rr->EnglishName);
 } // end of LanguageName
 
@@ -783,24 +781,24 @@ function FlagLanguage($IdLang=-1,$title="") {
  * this function write data in php_log
  * according to member right (Debug) this function will also display error on screen 
  */
-function bw_error( $errortext ) {
-   $serr="" ;
-	$tt=time() ; // save a timestant which will be used in the log to retrieve error reference
+function bw_error( $errortext, $showalways = false ) {
+   	$serr="";
+	$tt=time(); // save a timestant which will be used in the log to retrieve error reference
 	if (isset($_SESSION["Username"])) {
-	   $serr="[".$tt."] bw_error for :".$_SESSION["Username"]." :\n" ;
+	   $serr="[".$tt."] bw_error for :".$_SESSION["Username"]." :\n";
 	}
 	else {
-	   $serr="[".$tt."] bw_error for unknownmember :\n" ;
+	   $serr="[".$tt."] bw_error for unknownmember:\n";
 	} 
-	$serr.=$_SERVER["PHP_SELF"] ;
-	if ($_SERVER["QUERY_STRING"]!="") $serr=$serr."?".$_SERVER["QUERY_STRING"] ;
-	$serr.="\n" ; 
+	$serr.=$_SERVER["PHP_SELF"];
+	if ($_SERVER["QUERY_STRING"]!="") $serr=$serr."?".$_SERVER["QUERY_STRING"];
+	$serr.="\n"; 
 
-   error_log($serr.$errortext) ;
-	if (HasRigh("Debug")) {
-	   die("System error: ".$serr."<br>");
+   	error_log($serr.$errortext);
+	if (HasRight("Debug") || $showalways) {
+	   die("System error: ".$serr.": ".$errortext."<br>");
 	}
-	die("System error, please report about the following bug: timestamp: [".$tt."]");
+	die("System error, please report the following timestamp along the error: [".$tt."]");
 } // end of bw error
 
 
@@ -816,7 +814,6 @@ function bw_error( $errortext ) {
 // the quality can be set. default = 85
 // this function returns the thumb filename or null
 
-// modified by Fake51
 // $mode specifies if the new image is based on a cropped and resized version of the old, or just a resized
 // $mode = "square" means a cropped version
 // $mode = "ratio" means merely resized
@@ -824,7 +821,6 @@ function getthumb($file, $max_x, $max_y,$quality = 85, $thumbdir = 'thumbs',$mod
 {
 	// TODO: analyze MIME-TYPE of the input file (not try / catch)
 	// TODO: error analysis of wrong paths
-	// TODO: dynamic prefix (now: /th/)
 	
 	if (empty($file))
 		return null;
@@ -934,47 +930,47 @@ function getthumb($file, $max_x, $max_y,$quality = 85, $thumbdir = 'thumbs',$mod
 // function MyPict() return the path of the picture for the member
 function MyPict($paramIdMember=0) {
   if ($paramIdMember==0) {
-		 $IdMember=$_SESSION["IdMember"] ;
+		 $IdMember=$_SESSION["IdMember"];
 	}
 	else {
-		 $IdMember=$paramIdMember ;
+		 $IdMember=$paramIdMember;
 	}
 	
-   if ($IdMember==0) return(DummyPict()) ; 
+   if ($IdMember==0) return(DummyPict()); 
 
 	$rr = LoadRow("select SQL_CACHE * from membersphotos where IdMember=" . $IdMember . " and SortOrder=0");
-	if (isset($rr->FilePath)) return($rr->FilePath) ;
+	if (isset($rr->FilePath)) return($rr->FilePath);
 	else {
 	  $rr = LoadRow("select SQL_CACHE * from members where id=" . $IdMember);
-	  return(DummyPict($rr->Gender,$rr->HideGender)) ;
+	  return(DummyPict($rr->Gender,$rr->HideGender));
 	}
 } // end of MyPict
 
 //------------------------------------------------------------------------------
 // THis function return true if the member is in the status list
-// for example $Status="Active,ActiveHidden" ;
+// for example $Status="Active,ActiveHidden";
 function CheckStatus($Status,$paramIdMember=0) {
   if ($paramIdMember==0) {
-		 $IdMember=$_SESSION["IdMember"] ;
+		 $IdMember=$_SESSION["IdMember"];
 	}
 	else {
-		 $IdMember=$paramIdMember ;
+		 $IdMember=$paramIdMember;
 	}
-   if ($IdMember==0) return(False) ;
+   if ($IdMember==0) return(False);
 	
-	$tt=explode(",",$Status) ;
-	$rr=LoadRow("select SQL_CACHE * from members where id=".$IdMember) ;
-	if (in_array($rr->Status,$tt)) return (true) ;
-	return (false) ;
+	$tt=explode(",",$Status);
+	$rr=LoadRow("select SQL_CACHE * from members where id=".$IdMember);
+	if (in_array($rr->Status,$tt)) return (true);
+	return (false);
 } // end of LogVisit
 
 //------------------------------------------------------------------------------
 // THis function return a picture according to member gender if (any)
 function DummyPict($Gender="IDontTell",$HideGender="Yes") {
-  if ($HideGender=="Yes") return ($_SYSHCVOL['IMAGEDIR'] . "/et.jpg") ;
-  if ($Gender=="male") return ($_SYSHCVOL['IMAGEDIR'] . "/et_male.jpg") ; 
-  if ($Gender=="female") return ($_SYSHCVOL['IMAGEDIR'] . "/et_female.jpg") ; 
-  return ($_SYSHCVOL['IMAGEDIR'] . "/et.gif") ; 
+  if ($HideGender=="Yes") return ($_SYSHCVOL['IMAGEDIR'] . "/et.jpg");
+  if ($Gender=="male") return ($_SYSHCVOL['IMAGEDIR'] . "/et_male.jpg"); 
+  if ($Gender=="female") return ($_SYSHCVOL['IMAGEDIR'] . "/et_female.jpg"); 
+  return ($_SYSHCVOL['IMAGEDIR'] . "/et.gif"); 
 } // end of DummyPict
 
 
@@ -982,5 +978,5 @@ function DummyPict($Gender="IDontTell",$HideGender="Yes") {
 // to solve the double name for this function 
 // todo really solve this problem (only one name shall rename)
 //function prepareProfileHeader($IdMember,null,$photorank) {
-//   prepare_profile_header($IdMember,null,$photorank) ;
+//   prepare_profile_header($IdMember,null,$photorank);
 //}

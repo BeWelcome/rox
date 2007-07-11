99c344bdb52dbe3a3b650bcdb16da8e04facce8d
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-07-11 17:24:47 +0000

    last fix for update one preference
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@2520 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/mypreferences.php
+++ htdocs/bw/mypreferences.php
@@ -2,6 +2,7 @@
 require_once "lib/init.php";
 require_once "lib/prepare_profile_header.php";
 require_once "layout/error.php";
+require_once "layout/mypreferences.php";
 
 MustLogIn();
 
@@ -9,7 +10,7 @@ $IdMember = $_SESSION['IdMember'];
 $photorank = 0; // Alway use picture 0 on preference page 
 
 if (HasRight(Admin)) { // Admin will have access to any member right thru cid
-	$IdMember = IdMember(GetParam("cid", $_SESSION['IdMember']));
+	$IdMember = IdMember(GetStrParam("cid", $_SESSION['IdMember']));
 }
 
 // Try to load the member
@@ -27,7 +28,7 @@ switch (GetParam("action")) {
 		$countinsert = 0;
 		$countupdate = 0;
 		while ($rWhile = mysql_fetch_object($qry)) { // browse all preference
-			$Value = GetParam($rWhile->codeName);
+			$Value = GetStrParam($rWhile->codeName);
 			if ($Value != "") {
 				$rr = LoadRow("select memberspreferences.id as id from memberspreferences,preferences where IdMember=" . $IdMember . " and IdPreference=preferences.id and preferences.codeName='" . $rWhile->codeName . "'");
 				if (isset ($rr->id)) {
@@ -45,7 +46,7 @@ switch (GetParam("action")) {
 		LogStr("updating/inserting " . $countupdate . "/" . $countinsert . " preferences", "Update Preference");
 
 		$rPublicPref = LoadRow("select * from memberspublicprofiles where IdMember=" . $IdMember);
-		if (GetParam(PreferencePublicProfile) == "Yes") {
+		if (GetStrParam(PreferencePublicProfile) == "Yes") {
 			if (!isset ($rPublicPref->id)) {
 				$str = "insert into memberspublicprofiles(IdMember,created,type) values(" . $IdMember . ",now(),'normal')";
 				sql_query($str);
@@ -60,6 +61,33 @@ switch (GetParam("action")) {
 		}
 		
 		if (isset($_SESSION["stylesheet"])) unset($_SESSION["stylesheet"]) ; // clean the style sheet cache
+
+
+		break;
+		
+	case "UpdateOne" : //this is supposed to be called via a link mypreferences.php?action=UpdateOne&IdPreference=1&NewValue=fr for example (force french language)
+		$rPref = LoadRow("select * from preferences where id=".GetParam("IdPreference"));
+		$IdMember=$_SESSION["IdMember"] ;
+		$IdPreference=GetStrParam("IdPreference") ;
+		$Value=GetStrParam("NewValue") ;
+		
+		$rr = LoadRow("select memberspreferences.id as id from memberspreferences,preferences where IdMember=" . $IdMember . " and IdPreference=preferences.id and preferences.id=" . $IdPreference );
+		if (isset ($rr->id)) {
+					$str = "update memberspreferences set Value='" . addslashes($Value) . "' where id=" . $rr->id;
+					sql_query($str);
+					LogStr("updating one preference " . $rPref->codeName . "To Value <b>/" . $Value . " </b>", "Update Preference");
+		} else {
+					$str = "insert into memberspreferences(IdPreference,IdMember,Value,created) values(" . $IdPreference . "," . $IdMember . ",'" . addslashes($Value) . "',now() )";
+					sql_query($str);
+					LogStr("inserting one preference " . $rPref->codeName . "To Value <b>/" . $Value . " </b>", "Update Preference");
+		}
+
+		$m = prepareProfileHeader($IdMember,"",0);
+		
+		DisplayOneUpdate($m,$rPref->codeName, $Value) ; // call the display
+		 
+
+		
 
 
 		break;
@@ -79,6 +107,5 @@ $m = prepareProfileHeader($IdMember,"",0);
 // Load wether its inside the public profiles	
 $m->TPublic = LoadRow("select * from memberspublicprofiles where IdMember=" . $IdMember);
 
-require_once "layout/mypreferences.php";
 DisplayMyPreferences($TPref, $m, $IdMember); // call the layout
 ?>

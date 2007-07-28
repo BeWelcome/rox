e34fe79c04ac9b67e5735757882f638d007f20c0
guaka <guaka@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-07-28 15:51:22 +0000

    Stub for profile import functionality * parses XML data (get it from Java importer and put it in the same dir) * shows list of friends
    I hope I'm using more or less the right subdir and filename
    
    
    
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@2635 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ htdocs/bw/import/importer.php
@@ -0,0 +1,56 @@
+<?php
+
+# (c) 2007 Kasper Souren
+# GPL (please add the whole header)
+
+# PHP5, so we're using the DOM extension
+# http://www.php.net/manual/en/ref.dom.php
+
+
+/*
+ I don't know too much about the BW framework, and I don't have
+ the thing installed yet. So this will just be some hackerish stuff 
+ to get started at least.
+   20070728 gka
+*/
+
+
+$dom = get_profile();
+echo "<h1>Profile importer</h1>";
+show_friends($dom);
+
+
+
+function get_profile() {
+
+  # the profile XML data should come from the Java importer
+  # see http://bevolunteer.org/wiki/Java_importer
+
+  $doc = new DOMDocument();
+  $doc->load('csprofile.xml');
+  # echo $doc->saveXML();
+
+  # in case we'll have a DTD one day:
+  # if (false and $doc->validate()) 
+  #  echo "valid XML";
+
+  return $doc;	
+}
+
+	
+
+
+function show_friends($dom) {
+  $params = $dom->getElementsByTagName('friend');
+
+  foreach ($params as $param) {
+    # echo $param->getElementsByTagName('member')->nodeValue;
+    echo $param->nodeValue . '<br><br>';
+  }
+}
+
+
+
+
+
+?>

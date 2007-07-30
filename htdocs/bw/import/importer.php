d528ac046ed3ac90c67544f164ba69f20bced571
guaka <guaka@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-07-30 16:59:44 +0000

    * adding some PHPDoc stuff * adding license * adding BW headers
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@2650 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/import/importer.php
+++ htdocs/bw/import/importer.php
@@ -1,11 +1,38 @@
 <?php
 
-# (c) 2007 Kasper Souren
-# GPL (please add the whole header)
+/**
+ * Short description for file
+ *
+ * Long description for file (if any)...
+ *
+ * PHP version 5
+ *
+ * LICENSE: 
+
+   Foobar is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   Foobar is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ *
+ * @author     Kasper Souren <kasper.souren@gmail.com>
+ * @copyright  2007 Kasper Souren
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @version    CVS: $Id:$
+ * @link       http://www.bevolunteer.org/wiki/Java_importer
+ */
+
 
 # PHP5, so we're using the DOM extension
 # http://www.php.net/manual/en/ref.dom.php
-
 
 /*
  I don't know too much about the BW framework, and I don't have
@@ -13,6 +40,12 @@
  to get started at least.
    20070728 gka
 */
+
+
+require_once "../lib/init.php";
+require_once "../lib/FunctionsLogin.php";
+require_once "../layout/error.php";
+require_once "../lib/prepare_profile_header.php";
 
 
 $dom = get_profile();

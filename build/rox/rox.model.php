dd43a3e1635b9f6193c3473216645b63f14e5ce1
steinwinde <steinwinde@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-08-06 23:22:09 +0000

    Basic app for Rox, which is supposed to provide header, footer, sidebars etc. for all pages; at the same time provider for frontpage, and some semi static pages like the about page.
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@2681 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ build/rox/rox.model.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * rox model
+ *
+ * @package rox
+ * @author Felix van Hove <fvanhove@gmx.de>
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License Version 2
+ */
+class Rox extends PAppModel {
+    protected $dao;
+    
+    // supported languages for translations; basis for flags in the footer
+	// TODO: these were taken from a list in BW footer template; but eg. "ge"
+	// does not work; the list itself should be fetched from DB
+	private $_langs = array(
+	    'en', 'fr', 'esp', 'de', 'it', 'ru', 'espe', 'pl', 'tr', 'lt', 'nl', 'dk',
+	    'cat', 'cat', 'fi', 'pt', 'hu', 'lv', 'gr', 'no', 'srp', 'bg', 'br', 'ge'
+		);
+    
+    
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    /**
+     * set defaults
+     */
+    public function loadDefaults() {
+        if (!isset($_SESSION['lang']) || !file_exists(SCRIPT_BASE.'text/'.$_SESSION['lang'])) {
+            $_SESSION['lang'] = 'en';
+        }
+        PVars::register('lang', $_SESSION['lang']);
+        
+        $loc = array();
+        require SCRIPT_BASE.'text/'.PVars::get()->lang.'/base.php';
+        setlocale(LC_ALL, $loc);
+        require SCRIPT_BASE.'text/'.PVars::get()->lang.'/page.php';
+
+        return true;
+    }
+    
+    public function getLangNames() {
+        
+        $l =  '';
+		foreach ($this->_langs as $lang) {
+		    $l .= '\'' . $lang . '\',';
+		}
+		$l = substr($l, 0, (strlen($l)-1));
+		
+        $query = '
+SELECT `EnglishName`, `ShortCode`
+FROM `languages`
+WHERE `ShortCode` in (' . $l . ')
+		';
+        $result = $this->dao->query($query);
+        
+        $langNames = array();
+        while ($row = $result->fetch(PDB::FETCH_OBJ)) {
+            $langNames[$row->ShortCode] = $row->EnglishName;
+        }
+        return $langNames;
+    }
+    
+}
+?>
---
+++ build/rox/rox.view.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * rox view
+ *
+ * @package rox
+ * @author Felix van Hove <fvanhove@gmx.de>
+ * @license http://opensource.org/licenses/gpl-license.php GNU General Public License Version 2
+ */
+class RoxView extends PAppView {
+    private $_model;
+            
+    public function __construct(Rox $model) {
+        $this->_model = $model;
+    }
+
+    public function passthroughCSS($req) {
+        $loc = PApps::getBuildDir().'rox/'.$req;
+        if (!file_exists($loc))
+            exit();
+        $headers = apache_request_headers();
+        // Checking if the client is validating his cache and if it is current.
+        if (isset($headers['If-Modified-Since']) && (strtotime($headers['If-Modified-Since']) == filemtime($loc))) {
+            // Client's cache IS current, so we just respond '304 Not Modified'.
+            header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($loc)).' GMT', true, 304);
+        } else {
+            // File not cached or cache outdated, we respond '200 OK' and output the image.
+            header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($loc)).' GMT', true, 200);
+            header('Content-Length: '.filesize($loc));
+        }
+        header('Content-type: text/css');
+        @copy($loc, 'php://output');
+        exit();
+    }
+
+    public function aboutpage() 
+    {
+        require TEMPLATE_DIR.'apps/rox/about.php';
+    }
+    public function globalhelppage() 
+    {
+        require TEMPLATE_DIR.'apps/rox/help.php';
+    }
+    public function startpage() 
+    {
+        require TEMPLATE_DIR.'apps/rox/startpage.php';
+    }
+
+    public function topMenu() {
+        require TEMPLATE_DIR.'apps/rox/topmenu.php';
+    }
+    
+    public function footer() {
+        $flagList = $this->buildFlagList();
+        require TEMPLATE_DIR.'apps/rox/footer.php';
+    }
+    
+    private function buildFlagList() {
+        
+        $langurl = $_SERVER['REQUEST_URI'] . "?";
+		if ($_SERVER['QUERY_STRING'] != "") {
+			$QS = explode('&', $_SERVER['QUERY_STRING']);
+			for ($ii = 0; $ii < count($QS); $ii++) {
+				if (strpos($QS[$ii], "lang=") === false)
+					$langurl = $langurl . $QS[$ii] . "&";
+			}
+		}
+		
+		$pair = $this->_model->getLangNames();
+		$flaglist = '';
+		foreach($pair as $abbr => $title) {
+		    $png = $abbr.'.png';
+		    if ($_SESSION['lang'] == $abbr) {
+		        $flaglist .= "<span><a href=\"" . $langurl . "lang=" . $abbr .
+		        "\"><img src=\"/bw/images/flags/" . $png . "\" alt=\"" . $title . 
+		        "\" title=\"" . $title . "\"></img></a></span>\n";
+		    }
+		    else {
+		        $flaglist .= "<a href=\"" . $langurl . "in=" . $abbr . 
+		        "\"><img src=\"/bw/images/flags/" . $png . 
+		        "\" alt=\"" . $title . "\" title=\"" . $title . "\"></img></a>\n";
+		    }
+		}
+		
+		return $flaglist;
+    }
+    
+}
+?>

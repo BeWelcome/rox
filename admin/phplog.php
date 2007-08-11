2586044e3307d9604297dba8bad5e99f1a245470
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-08-11 14:10:53 +0000

    adding files
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@2703 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ admin/phplog.php
@@ -0,0 +1,36 @@
+<?php require_once("../lib/init.php"); ?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+<head>
+<title>Last error log</title>
+</head>
+<body>
+<?php
+
+require_once("../lib/init.php");
+
+if (!HasRight("Debug")) die("You miss Debug Right") ;
+
+// THis file display the last errors
+
+$NbLines = GetParam("NbLines",100);
+$filename = "/etc/httpd/logs/www.bewelcome.org-error_log";
+
+echo "tail --lines=".$NbLines." <b>",$filename,"</b><br>" ;
+$t=array() ;
+exec("tail --lines=".$NbLines." ".$filename,$t) ;
+$max=count($t) ;
+for ($ii=0;$ii<$max;$ii++) {
+	 $ss=str_replace("\n","<br>",$t[$ii]) ;
+  	 echo htmlentities($ss),"<br>\n";
+}
+echo "Current date=<b>[",date("D M j G:i:s Y"),"]</b><br>"  ;
+echo "<form>" ;
+echo "NbLines : <input type=text Name=NbLines value=\"".$NbLines."\"> <input type=submit id=submit>\n</form>\n" ;
+echo "</body></html>";
+
+exit (0);
+?>
+

a629787461fadcdd3c26017f1a78cf7110f6fe1d
hkroger <hkroger@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-08-11 15:37:12 +0000

    removed maintenance (not used, not relevant)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@2712 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- maintenance/mysql/updates/191_forums_threads.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `forums_threads` CHANGE `first_postid` `first_postid` INT( 10 ) UNSIGNED NULL ;
-ALTER TABLE `forums_threads` CHANGE `last_postid` `last_postid` INT( 10 ) UNSIGNED NULL ;

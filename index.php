6ecc07f87a02da6419108b5fe346330a3dda58c7
guaka <guaka@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-08-16 21:45:50 +0000

    * Moving some small parts from documentation/*.txt into README * adding comments to index.php (is legacy/deprecated) * INSTALL++
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@2753 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+/* 
+ *  This path should not be accessible through a browser.
+ *  This file is deprecated and is just here for legacy reasons.
+ */
+
 header("HTTP/1.0 301 Moved Permanently");
 header("Location: htdocs/");
 header("Connection: close");

dd30ff4b975603cdfc833703535c099736bc166a
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-08-17 20:38:11 +0000

    improving spam procesing
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@2766 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/layout/adminchecker.php
+++ htdocs/bw/layout/adminchecker.php
@@ -1,29 +1,4 @@
 <?php
-
-/*
-
-Copyright (c) 2007 BeVolunteer
-
-This file is part of BW Rox.
-
-BW Rox is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-Foobar is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, see <http://www.gnu.org/licenses/> or 
-write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, 
-Boston, MA  02111-1307, USA.
-
-*/
-
-
 require_once ("menus.php");
 function DisplayMessages($TMess, $lastaction = "") {
 	global $countmatch;
@@ -35,7 +10,10 @@ function DisplayMessages($TMess, $lastaction = "") {
 
 	Menu2($_SERVER["PHP_SELF"]);
 
-	DisplayHeaderShortUserContent($title);
+
+	$MenuAction  = "          <li><a href=\"".$_SERVER["PHP_SELF"]."\">Admin Checkers</a></li>\n";
+	$MenuAction .= "          <li><a href=\"".$_SERVER["PHP_SELF"]."?action=PendingSpammers\">Pending Spammers</a></li>\n";
+	DisplayHeaderShortUserContent( $title , $MenuAction );
 
    echo "          <div class=\"info highlight\">\n";
 	
@@ -89,7 +67,11 @@ function DisplayMessages($TMess, $lastaction = "") {
 		echo $rr->SpamInfo;
 		echo "</td>";
 	}
-	echo "<tr><td colspan=5 align=center><input type=submit id=submit name=submit value=submit></td>";
+	echo "<tr><td colspan=5 align=center><input type=submit name=submit value=submit></td>";
+	if (GetStrParam("IdSender","") !="") {
+	   echo "<input type=submit name=IdSender value=".GetStrParam("IdSender").">" ;
+	} 
+
 	echo "</form>";
 	echo "\n</table><br>\n";
 	
@@ -100,4 +82,57 @@ function DisplayMessages($TMess, $lastaction = "") {
 
 	require_once "footer.php";
 
-} // DisplayAdminRights($username,$rightname,$TRights,$TRightsVol,$rright,$lastaction,$scope) {
+} // DisplayMessages() 
+
+function DisplayPendingMayBeSpammers($TMess, $lastaction = "") {
+	global $countmatch;
+	global $title;
+	$title = "Pending May Be Spammers";
+	require_once "header.php";
+
+	Menu1(); // Displays the top menu
+
+	Menu2($_SERVER["PHP_SELF"]);
+
+	DisplayHeaderShortUserContent($title);
+
+   echo "          <div class=\"info highlight\">\n";
+	
+	if ($lastaction != "") {
+		echo "$lastaction<br>";
+	}
+
+	$max = count($TMess);
+	$count = 0;
+
+	echo "<center>\n";
+	echo "<table width=100% style=\"font-size:11;\">\n";
+	if ($max == 0) {
+		echo "<tr><td align=center>No pending messages to check</td>";
+	} else {
+		echo "\n<tr><th>Sender</th><th>Nb Pending</th><th>Action</th><th>SpamInfo</th>";
+	}
+
+	for ($ii = 0; $ii < $max; $ii++) {
+		$rr = $TMess[$ii];
+		$count++;
+		echo "<tr>";
+		echo "<td>";
+		echo LinkWithUsername($rr->Username_sender);
+		echo "</td>";
+		echo "<td>";
+		echo "$rr->cnt";
+		echo "</td>";
+		echo "<td align=left>";
+		echo "<a href=\"",$_SERVER["PHP_SELF"],"?action=view&IdSender=$rr->IdSender\">view spam(?) messages</a>" ;
+		echo "</td>";
+	}
+	echo "\n</table><br>\n";
+	
+
+	echo "</center>";
+	echo "</div>" ;
+
+	require_once "footer.php";
+
+} // DisplayPendingMayBeSpammers

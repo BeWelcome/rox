b277f4fb46804e108e46f6b03baaeeaf30c8a68c
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-08-27 14:12:46 +0000

    Big Fix on updatemandatory to avoid confusing what member wants to hide and what he doesn't want Now the previous value of IsCrypted is correctly kept by UpdtateMandatory
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@2809 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/updatemandatory.php
+++ htdocs/bw/updatemandatory.php
@@ -151,9 +151,9 @@ switch (GetParam("action")) {
 			    $IdAddress=mysql_insert_id();
 				LogStr("Doing a mandatoryupdate on <b>" . $Username . "</b> creating address", "updatemandatory");
 			}
-			$m->FirstName = ReplaceInCrypted($FirstName, $m->FirstName, $m->id,IsCrypted($m->FirstName));
-			$m->SecondName = ReplaceInCrypted($SecondName, $m->SecondName, $m->id,IsCrypted($m->SecondName));
-			$m->LastName = ReplaceInCrypted($LastName, $m->LastName, $m->id,IsCrypted($m->LastName));
+			$m->FirstName = ReplaceInCrypted($FirstName, $m->FirstName, $m->id,IsCryptedValue($m->FirstName));
+			$m->SecondName = ReplaceInCrypted($SecondName, $m->SecondName, $m->id,IsCryptedValue($m->SecondName));
+			$m->LastName = ReplaceInCrypted(stripslashes($LastName), $m->LastName, $m->id,IsCryptedValue($m->LastName));
 
 			$str = "update members set FirstName=" . $m->FirstName . ",SecondName=" . $m->SecondName . ",LastName=" . $m->LastName . ",Gender='" . $Gender . "',HideGender='" . $HideGender . "',BirthDate='" . $DB_BirthDate . "',HideBirthDate='" . $HideBirthDate . "',IdCity=" . $IdCity . " where id=" . $m->id;
 			sql_query($str);

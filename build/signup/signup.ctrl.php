8517021f861f60bfcf63df25f11e2e44f20925f2
steinwinde <steinwinde@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-08-29 19:01:50 +0000

    Cleaning up, adding GPL header, removing debug code, adding comments.
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@2815 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/signup/signup.ctrl.php
+++ build/signup/signup.ctrl.php
@@ -1,10 +1,31 @@
 <?php
+/*
+
+Copyright (c) 2007 BeVolunteer
+
+This file is part of BW Rox.
+
+BW Rox is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BW Rox is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/> or 
+write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, 
+Boston, MA  02111-1307, USA.
+
+*/
 /**
  * signup controller
  *
  * @package signup
  * @author Felix van Hove <fvanhove@gmx.de>
- * @license http://opensource.org/licenses/gpl-license.php GNU General Public License Version 2
  */
 class SignupController extends PAppController {
 
@@ -30,6 +51,8 @@ class SignupController extends PAppController {
         parent::__construct();
         $this->_model = new Signup();
         $this->_view =  new SignupView($this->_model);
+        
+        // TODO: does this still make sense?
         if (!PModules::moduleLoaded('user')) {
             throw new PException('Require module "user"!');
         }
@@ -63,18 +86,19 @@ class SignupController extends PAppController {
         
         switch($request[1]) {
             
-            // FIXME: remove
+            // stub for debugging
             case 'test':
-                $this->_model->test();
                 PPHP::PExit();
                 break;
                 
-            // register form
-            default:
             case 'register':
+                
+                // TODO: why check here? of course we need it, but
+                // why can't we take it for granted the module is loaded?
                 if (!PModules::moduleLoaded('mail')) {
                     throw new PException('Module "mail" not found!');
                 }
+                
                 // start output buffering to save all to content
                 ob_start();
                 $this->_view->registerForm();

b19cf2020bf617df38c1d20c54ed59dc34fda004
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-09-05 14:18:36 +0000

    Boring fix for being HTML strict
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@2855 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/layout/faq.php
+++ htdocs/bw/layout/faq.php
@@ -39,7 +39,10 @@ function DisplayFaq($TFaq) {
 			
 			if (HasRight("Faq") > 0)
 				echo "[<a href=\"faq.php?action=insert&IdCategory=", $TFaq[$ii]->IdCategory, "\">insert new faq in this category</a>]\n";
-			if ($IdFaq==0) echo " <h3>", ww($TFaq[$ii]->CategoryName), "</h3><ul>\n";
+			if ($IdFaq==0) {
+				 if ($ii>0) echo "</ul><br/>\n";
+				 echo " <h3>", ww($TFaq[$ii]->CategoryName), "</h3>\n<ul>\n";
+			}
 		}
 
 		$Q = ww("FaqQ_" . $TFaq[$ii]->QandA);
@@ -53,11 +56,18 @@ function DisplayFaq($TFaq) {
 				echo " [<a href=\"faq.php?action=edit&IdFaq=", $TFaq[$ii]->id, "\">edit</a>]\n";
 		}
 //		echo " <a href=\"" . $_SERVER["PHP_SELF"] . "?IdFaq=", $TFaq[$ii]->id, "\">", $Q, "</a>";
-		if ($IdFaq==0) echo " <a href=\"faq_" . $TFaq[$ii]->QandA."_".$_SESSION["lang"].".php\">", $Q, "</a></li></br>\n";
+		if ($IdFaq==0) {
+		   if (IsLoggedIn()) {
+  	   		  echo " <a href=\"faq.php?IdFaq=".$TFaq[$ii]->id."\">", $Q, "</a></li>\n";
+		   }
+		   else {
+		   	  echo " <a href=\"faq_" . $TFaq[$ii]->QandA."_".$_SESSION["lang"].".php\">", $Q, "</a></li>\n";
+		   }
+		}
 
-	}
-	if ($IdFaq==0) echo "</ul></br>\n";
+	} // end of for $ii
 
+	if ($IdFaq==0) echo "</ul><br/>\n";
 
 	// Display the list of the answers
 	for ($ii = 0; $ii < $iiMax; $ii++) {
@@ -65,12 +75,14 @@ function DisplayFaq($TFaq) {
 		if ($IdFaq==0) echo " <h3>", ww($TFaq[$ii]->CategoryName), "</h3>";
 		$Q = ww("FaqQ_" . $TFaq[$ii]->QandA);
 		$A = ww("FaqA_" . $TFaq[$ii]->QandA);
-		if ($IdFaq==0) echo "<h4><a name=", $TFaq[$ii]->id, "></a> ", $Q, "</h4>\n";
+		if ($IdFaq==0) {
+		   echo "<h4><a name=\"", $TFaq[$ii]->id, "\"></a> ", $Q, "</h4>\n";
+		}
 		echo "<p>", str_replace("\n", "", $A), "</p>\n";
 	}
 	
 	if (IsAdmin()) {
-	   echo "<br><p><a href=\"faq.php?action=rebuildextraphpfiles\">rebuild extra php files</a></p>" ; 
+	   echo "<br/><p><a href=\"faq.php?action=rebuildextraphpfiles\">rebuild extra php files</a></p>" ; 
 	}
 
 	include "footer.php";
@@ -96,18 +108,18 @@ function DisplayFaqWiki($TFaq) {
 
 		if ($LastCat != $TFaq[$ii]->CategoryName) {
 			$LastCat = $TFaq[$ii]->CategoryName;
-			echo "<br>";
-			echo "'''==", ww($TFaq[$ii]->CategoryName), "=='''<br>\n";
+			echo "<br/>";
+			echo "'''==", ww($TFaq[$ii]->CategoryName), "=='''<br/>\n";
 		}
 
 		$A = ww("FaqA_" . $TFaq[$ii]->QandA);
 		$Q = ww("FaqQ_" . $TFaq[$ii]->QandA);
-		echo "==", $Q, "==<br>\n";
-		echo "", $A, "<br>\n";
-		echo "<br>";
+		echo "==", $Q, "==<br/>\n";
+		echo "", $A, "<br/>\n";
+		echo "<br/>";
 
 	}
-	echo "<br>";
+	echo "<br/>";
 
 	include "footer.php";
 } // end of DisplayFaqWiki
@@ -124,15 +136,15 @@ function DisplayEditFaq($Faq, $TCategory) {
 
 	DisplayHeaderShortUserContent("Editing FAQ#" . $Faq->id . " (" . $Faq->QandA . ")"); // Display the header
 
-	echo "<center>\n<b>Beware</b> edit Faq only apply to english Faq. For other languages, use AdminWords<br><br>\n";
+	echo "<center>\n<b>Beware</b> edit Faq only apply to english Faq. For other languages, use AdminWords<br/><br/>\n";
 
 	echo "<form method=post action=faq.php>\n";
 	echo "<table width=\"90%\">\n";
-	echo "<input type=hidden Name=IdFaq value=", $Faq->id, ">\n";
+	echo "<input type=hidden Name=\"IdFaq\# value=", $Faq->id, ">\n";
 	echo "<input type=hidden Name=action value=update>\n";
 	echo "<tr><td colspan=2>";
 	echo "Category  ";
-	echo "<select Name=IdCategory>\n";
+	echo "<select Name=\"IdCategory\">\n";
 	for ($ii = 0; $ii < count($TCategory); $ii++) {
 		echo "<option value=" . $TCategory[$ii]->id;
 		if ($TCategory[$ii]->id == $Faq->IdCategory)
@@ -140,7 +152,7 @@ function DisplayEditFaq($Faq, $TCategory) {
 		echo ">", ww($TCategory[$ii]->Description), "</option>\n";
 	}
 	echo "</select>\n";
-	echo "\nStatus :<select name=Status>\n";
+	echo "\nStatus :<select name=\"Status\">\n";
 	echo "<option value=\"Active\" ";
 	if ($Faq->Active == "Active")
 		echo " selected ";

093dbd9ff2d7de84d44e7a18e1cab1102bef9cce
tobixen <tobixen@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-09-09 22:34:43 +0000

    bugfix; group.HasMembers referenced as group.HasMember
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@2868 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/admin/admingroups.php
+++ htdocs/bw/admin/admingroups.php
@@ -43,7 +43,7 @@ switch (GetParam("action")) {
 		   		echo "group ",GetStrParam("Name"), " allready exist" ;
 		   		break ;
 			}
-			$str = "insert into groups(Picture,MoreInfo,HasMember,Type,Name) values('" . GetStrParam("Picture") . "','". GetStrParam("MoreInfo") . "','" . GetParam("HasMember") . "','" . GetParam("Type") . "','" . GetParam("Name") . "')";
+			$str = "insert into groups(Picture,MoreInfo,HasMembers,Type,Name) values('" . GetStrParam("Picture") . "','". GetStrParam("MoreInfo") . "','" . GetParam("HasMember") . "','" . GetParam("Type") . "','" . GetParam("Name") . "')";
 			sql_query($str);
 			$IdGroup = mysql_insert_id();
 			$str = "insert into words(code,ShortCode,IdLanguage,Sentence,updated,IdMember) values('Group_" . GetStrParam("Name"). "','en',0,'" . addslashes(GetStrParam("Group_")) . "',now(),".$_SESSION['IdMember'].")";

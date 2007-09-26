eab573760eba09fc9c851e05cd4c81fd4a9a2161
globetrotter_tt <globetrotter_tt@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-09-26 20:22:56 +0000

    bigger searchfield, removed fieldset
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@2943 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/apps/rox/topmenu.php
+++ templates/apps/rox/topmenu.php
@@ -21,38 +21,53 @@ write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA  02111-1307, USA.
 
 */
+/*
+
+Copyright (c) 2007 BeVolunteer
+
+This file is part of BW Rox.
+
+BW Rox is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BW Rox is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/> or 
+write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, 
+Boston, MA  02111-1307, USA.
+
+*/
 $words = new MOD_words();
 ?>
 
 <!-- #nav: main navigation -->
 <div id="nav">
-	<div id="nav_main">
-	    <ul>
-		
-			<li><a href="bw/main.php"><span>Home</span></a></li>
-			<li><a href="bw/member.php?cid=<?php echo isset($_SESSION['username']) ? $_SESSION['username'] : ''; ?>"><span>My Account</span></a></li>
-			<li<?php echo ($currentTab == 'searchmembers') ? ' class="active"' : ''; ?>><a href="searchmembers/index"><span>Find Members</span></a></li>
-			<li<?php echo ($currentTab != 'searchmembers') ? ' class="active"' : ''; ?>><a href="forums"><span>Community</span></a></li>
-			<li><a href="bw/groups.php"><span>Groups</span></a></li>
-			<li><a href="bw/aboutus.php"><span>Get Answers</span></a></li>
-
-			<!-- #nav_flowright: This part of the main navigation floats to the right. The items have to be listed in reversed order to float properly-->			
-			<span id="nav_flowright">
-		    <li>
-		      <form action="quicksearch.php" id="form-quicksearch">
-		          <fieldset id="fieldset-quicksearch">
-		          Search 
-		          <input type="text" name="searchtext" size="10" maxlength="30" id="text-field" />
-		          <input type="hidden" name="action" value="quicksearch" />
-		          <input type="image" src="styles/YAML/images/icon_go.gif" id="submit-button" />
-		        </fieldset>
-		      </form>
-		    </li>
-			</span>
-			<!-- #nav_flowright: end -->
-			
-	    </ul>
-	</div>
+  <div id="nav_main">
+    <ul>
+      <li><a href="bw/main.php"><span>Home</span></a></li>
+      <li><a href="bw/member.php?cid=<?php echo isset($_SESSION['username']) ? $_SESSION['username'] : ''; ?>"><span>My Account</span></a></li>
+      <li><a href="bw/main.php"><span>My Messages</span></a></li>
+      <li<?php echo ($currentTab == 'searchmembers') ? ' class="active"' : ''; ?>><a href="searchmembers/index"><span>Find Members</span></a></li>
+      <li<?php echo ($currentTab != 'searchmembers') ? ' class="active"' : ''; ?>><a href="forums"><span>Community</span></a></li>
+      <!--<li><a href="bw/aboutus.php"><span>Get Answers</span></a></li> -->
+    </ul>
+    
+      <!-- #nav_flowright: This part of the main navigation floats to the right. The items have to be listed in reversed order to float properly-->			
+    <div id="nav_flowright">
+      <form action="quicksearch.php" id="form-quicksearch">	        
+        <input type="text" name="searchtext" size="15" maxlength="30" id="text-field" value="Search..." />
+        <input type="hidden" name="action" value="quicksearch" />
+        <input type="image" src="styles/YAML/images/icon_go.gif" id="submit-button" />
+      </form>
+    </div> <!-- nav_flowright -->
+      
+  </div>
 </div>
 <!-- #nav: - end -->
 

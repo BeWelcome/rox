cb8fafcb57d177dbe9011ee12693226cb53e65d9
globetrotter_tt <globetrotter_tt@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-10-05 22:56:22 +0000

    forum rules -> translation
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@3013 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/apps/forums/rules.php
+++ templates/apps/forums/rules.php
@@ -1,51 +1,67 @@
-<h3>BeWelcome Forum Charter</h3>
+<?php
+/*
 
-<h4>This Charter</h4>
+Copyright (c) 2007 BeVolunteer
 
+This file is part of BW Rox.
+
+BW Rox is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BW Rox is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/> or 
+write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, 
+Boston, MA  02111-1307, USA.
+
+*/
+
+$words = new MOD_words(); ?>
+
+<h3><?php echo $words->getformatted('ForumRulesCharterHeadline1'); ?></h3>
+
+<h4><?php echo $words->getformatted('ForumRulesCharterHeadline2'); ?></h4>
 <ol>
-<li>Everybody is allowed to talk about making BeWelcome a better website and a stronger community. If you have any issues with BW, or want to make constructive suggestions, you are more than welcome to talk about them here on the forum. In some cases it might be better to send us directly a message through the “contact us” form (in case of a problem with your personal account for example). But in other cases it might be better to start a discussion here on the forum to see if there what other members feel. Once there is a consensus amongst the other readers of this forum, you could then contact us directly with the results of the discussion.<br />
-Everybody can discuss new features, since we believe that good ideas should not be kept a secret. If other communities like the features discussed here, let them use it! It can only make Hospitality Exchange stronger. However please remember that because you want something, it doesn’t necessarily mean we can provide it.</li>
-
-<li>Honesty is an important value on this forum, but what you believe to be the truth might not be what others believe. Therefore we promise to make it visible to everybody if and when the forum moderators edit or delete a post. In the rare case that we have to put somebody in premoderation for repeated breaking of the rules, we will inform that person.</li>
-
-<li>Everybody should try to keep this forum as clean as possible.We will proved a search function (through the use of tags) so it is easier for you to find something. Try to post in the correct categories and don’t post the same message twice (so-called crossposting)</li>
-
-<li>Moderators are individuals whose job it is to look after the running of the forums from day to day. They have the power to edit or delete posts and lock, unlock, move, delete and split topics in the forum they moderate. Generally moderators are there to prevent people going off-topic or posting abusive or offensive material.</li>
-
-<li>Refrain from posting any material which is knowingly false and/or defamatory, inaccurate, abusive, vulgar, hateful, harassing, obscene, profane, oriented, threatening, invasive of a person's privacy, or otherwise violative of any law. This means do not post personal data about other people without their consent. It is absolutely fine to disagree with another poster; we only ask that you do so without resorting to rudeness. We also require that posts be in your own words -- i.e., no cutting and pasting of other people's material, copyrighted or</li>
-
-<li>If you see a message or reply that violates our rules of conduct, contact us with the forum category and the title of the post -- we'll look into it. We would prefer that you NOT add further comments to an objectionable message. </li>
-
+  <li><?php echo $words->getformatted('ForumRulesCharterText1'); ?></li>
+  <li><?php echo $words->getformatted('ForumRulesCharterText2'); ?></li>
+  <li><?php echo $words->getformatted('ForumRulesCharterText3'); ?></li>
+  <li><?php echo $words->getformatted('ForumRulesCharterText4'); ?></li>
+  <li><?php echo $words->getformatted('ForumRulesCharterText5'); ?></li>
+  <li><?php echo $words->getformatted('ForumRulesCharterText6'); ?></li>
 </ol>
 
+<h3><?php echo $words->getformatted('ForumRulesHeadline1'); ?></h3>
 
-<h3>BeWelcome Forum Rules</h3>
-
-<h3>By posting in the forum, you agree to the following:</h3>
-
-<p>(a) You have read and accepted the rules of the forum of BeWelcome, which are as follows:</p>
+<h4><?php echo $words->getformatted('ForumRulesHeadline2'); ?></h4>
+<p><?php echo $words->getformatted('ForumRulesText1'); ?></p>
 
 <ol>
-  <li>No personal attacks or flaming.</li>
-  <li>The forum is NOT a public trial. NO hanging people out to dry publicly (this is PURELY in relation to persons - any general discussions of problems or abuse is allowed)</li>
-  <li>No spamming, no crossposting</li>
-  <li>No posting personal info on other people</li>
-  <li>Heed any and all advice and requests from moderators</li>
+  <li><?php echo $words->getformatted('ForumRulesText2'); ?></li>
+  <li><?php echo $words->getformatted('ForumRulesText3'); ?></li>
+  <li><?php echo $words->getformatted('ForumRulesText4'); ?></li>
+  <li><?php echo $words->getformatted('ForumRulesText5'); ?></li>
+  <li><?php echo $words->getformatted('ForumRulesText6'); ?></li>
 </ol>
 
-<p>(b) You have read and agreed to follow the guidelines of the forum, which are as follows:</p>
+<p><?php echo $words->getformatted('ForumRulesText7'); ?></p>
 
 <ol>
-  <li>No trolling</li>
-  <li>Keep it clean - some people are more sensitive than others, respect that.</li>
-  <li>Keep the forum in order - post in the right category</li>
-  <li>Keep it readable</li>
-<ol>
+  <li><?php echo $words->getformatted('ForumRulesText8'); ?></li>
+  <li><?php echo $words->getformatted('ForumRulesText9'); ?></li>
+  <li><?php echo $words->getformatted('ForumRulesText10'); ?></li>
+  <li><?php echo $words->getformatted('ForumRulesText11'); ?></li>
+</ol>
 
-<p>(c) You accept that the forum of BeWelcome is moderated, and breaking the rules or failing to follow the guidelines will result in your postings being moderated.</p>
+<p><?php echo $words->getformatted('ForumRulesText12'); ?></p>
 
-<p>(d) That in cases of repeated problems, the moderators can submit all your posts to moderation (in which case they will have to be approved individually before being allowed on the board), and that in extreme cases moderators have as a last option the possibility to ban a member from the forum.</p>
+<p><?php echo $words->getformatted('ForumRulesText13'); ?></p>
 
-<p>(e) That moderators are the authority in the forum and hence have the last word in any and all disputes in the forum.</p>
+<p><?php echo $words->getformatted('ForumRulesText14'); ?></p>
 
-<p>(f) That moderators have the option to lock or delete threads if they are breaking the rules, getting out of control, outdated, crosspostings or in other ways at odds with the forum.</p> 
+<p><?php echo $words->getformatted('ForumRulesText15'); ?></p> 

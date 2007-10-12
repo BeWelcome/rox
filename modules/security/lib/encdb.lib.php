7008e8bc57d5cd64779b4018a7862694520eae8b
steinwinde <steinwinde@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-10-12 23:15:43 +0000

    Added missing GPL header, added an assert in a test.
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@3064 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- modules/security/lib/encdb.lib.php
+++ modules/security/lib/encdb.lib.php
@@ -1,4 +1,26 @@
 <?php
+/*
+
+Copyright (c) 2007 BeVolunteer
+
+This file is part of BW Rox.
+
+BW Rox is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BW Rox is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/> or 
+write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, 
+Boston, MA  02111-1307, USA.
+
+*/
 /**
  * All r/w operations on potentially encrypted fields in database
  * should get initiated here.
@@ -121,6 +143,7 @@ WHERE `id`=' . $id;
         $val_m = $this->readEncryptedMemberValue($id);
         $val_a = $this->readEncryptedAdminValue($id);
         assert(strcmp($val_m, $val_a) == 0);
+        assert(strcmp($val_m, $s) == 0);
         
         echo "<h1>Finished test encdb.lib.php successfully.</h1>";
     }

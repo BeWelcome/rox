ae9da26cc12eb4406cba5a202a909a1002ca918a
jaliste <jaliste@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-11-09 18:00:27 +0000

    Fixing #137
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@3204 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/apps/rox/rightContentOut.php
+++ templates/apps/rox/rightContentOut.php
@@ -6,7 +6,7 @@ $words = new MOD_words();
 <p><?php $words->get('IndexPageWord18', '<a
 > href="/bw/lostpassword.php">', '</a>'); ?></a>
 </p>
-<script type="text/javascript">document.login.Username.focus();</script>
+<script type="text/javascript">document.getElementById("login-u").focus();</script>
 <h3><?php $words->get('SignupNow'); ?></h3>
 <p><?php $words->getFormatted('IndexPageWord17', '<a
 > href="/bw/signup.php">', '</a>'); ?></p>

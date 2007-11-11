c4d8395c13be826f02e133a9726fe029e7d566af
matrixpoint <matrixpoint@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-11-11 18:59:54 +0000

    test 3
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@3222 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/lib/prepare_profile_header.php
+++ htdocs/bw/lib/prepare_profile_header.php
@@ -26,9 +26,9 @@ function prepareProfileHeader($IdMember,$wherestatus="",$photorank=0) {
 
 // temporary testing by matrixpoint to find out where photos are:
 $return_var;
-echo system('find . -name wukk_1169999660.square.80x80.jpg -print0', $return_var);
+echo system('find . -name wukk_1169999660.square.80x80.jpg -print', $return_var);
 echo $return_var;
-passthru('find . -name wukk_1169999660.square.80x80.jpg -print0', $return_var);
+passthru('find . -name wukk_1169999660.square.80x80.jpg -print', $return_var);
 echo $return_var;
 
 

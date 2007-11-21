b315a2ced14025aa7499151641ab86e752e55e9e
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-11-21 16:30:56 +0000

    This module is a temporary one which allow to use old BW function inside ROW. It includes the files in (old)BW/LIB. Of course, one by one these functions will need to be re-engeenered
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@3272 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ modules/old_bw_func/lib/old_bw_func.lib.php
@@ -0,0 +1,78 @@
+<?php
+/*
+
+Copyright (c) 2007 BeVolunteer
+
+This file is part of BW Rox.
+
+BW Rox is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BW Rox is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/> or 
+write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, 
+Boston, MA  02111-1307, USA.
+
+*/
+/**
+ * This module define various standars way to produce various links, according to
+ * provide parameters.
+ * Typically link to profile with username
+ * or link with the picture of the username
+ * It is VERY IMPORTANT According to understand that according to the parameters the shape of the link
+ * may look different. This is very useful to see quickly and graphically teh status of a member for example 
+ * @author JeanYves 
+ */
+
+ class MOD_old_bw_func {
+    
+
+    /**
+     * Singleton instance
+     * 
+     * @var MOD_old_bw_func
+     * @access private
+     */
+    private static $_instance;
+    
+    public function __construct()
+    {
+    
+        $db = PVars::getObj('config_rdbms');
+        if (!$db) {
+            throw new PException('DB config error!');
+        }
+        $dao = PDB::get($db->dsn, $db->user, $db->password);
+        $this->dao =& $dao;
+    }
+    
+    /**
+     * singleton getter
+     * 
+     * @param void
+     * @return PApps
+     */
+    public static function get()
+    {
+        if (!isset(self::$_instance)) {
+            $c = __CLASS__;
+            self::$_instance = new $c;
+        }
+        return self::$_instance;
+    }
+} // end of MOD_old_bw_func
+
+$dir="../htdocs/bw/lib/" ;
+require_once($dir."FunctionsTools.php");
+require_once($dir."session.php");
+require_once($dir."bwdb.php");
+require_once($dir."lang.php");
+require_once("../htdocs/bw/layout/layouttools.php");
+?>

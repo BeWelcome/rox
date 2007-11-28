d0e9ded9be3956623e8a90dfc756d2bed4954236
matrixpoint <matrixpoint@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-11-28 17:25:25 +0000

    Maximum number of results increased to 100.
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@3309 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/searchmembers/searchmembers.model.php
+++ build/searchmembers/searchmembers.model.php
@@ -183,7 +183,7 @@ public function searchmembers(&$vars) {
 	$TMember=array() ;
 
 	$limitcount=$this->GetParam($vars, "limitcount",10); // Number of records per page
-	if($limitcount > 50) $limitcount = 50;
+	if($limitcount > 100) $limitcount = 100;
 	$vars['limitcount'] = $limitcount;
 
 	$start_rec=$this->GetParam($vars, "start_rec",0); // Number of records per page

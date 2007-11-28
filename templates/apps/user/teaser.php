3277f48d67509ede749937832ed6a213109881d0
matrixpoint <matrixpoint@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-11-28 23:07:50 +0000

    Layout code added to new Rox version of change password.
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@3313 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/apps/user/teaser.php
+++ templates/apps/user/teaser.php
@@ -1,12 +1,12 @@
 <?php
+$User = APP_User::login();
+
 $words = new MOD_words();
 ?>
 
 <div id="teaser" class="clearfix">
-
-<div id="teaser_l1"> 
-
+<h1><?php echo $words->getFormatted('ChangePasswordTitle'); ?></h1>
+<table><td valign="top" spacing="5px"><img src="images/info.gif"></td><td><?php echo $words->getFormatted('ChangePasswordIntro'); ?></td></table>
 </div>
-<!--<div id="teaser_r"> 
-</div>-->
-</div>
+
+

c4e7548c426175ee3dae1877b355932c87e535d9
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-11-29 21:10:30 +0000

    Commit just to have the automatic script to run
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@3325 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/lib/tbinit.php
+++ htdocs/bw/lib/tbinit.php
@@ -18,7 +18,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, see <http://www.gnu.org/licenses/> or 
 write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, 
-Boston, MA  02111-1307, USA.
+Boston, MA  02111-1307,  USA. 
 
 */
 if (!version_compare(phpversion(), '5.1.0', '>='))

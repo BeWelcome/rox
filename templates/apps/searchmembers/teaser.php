ad177a21bdf9ab5ec9a45c0735035bd244b89940
micha <micha@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-12-04 02:25:10 +0000

    removed disturbing exclamation mark in teaser of searchmembers
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@3364 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/apps/searchmembers/teaser.php
+++ templates/apps/searchmembers/teaser.php
@@ -6,5 +6,5 @@ $words = new MOD_words();
 <div id="teaser" class="clearfix">
 <h1><?php echo $words->getFormatted('searchmembersTitle'); ?></h1>
 
-<table><td valign="top" spacing="5px"><img src="images/info.gif"></td><td><?php echo $words->getFormatted('searchmembersIntro'); ?></td></table>
+<table><td><?php echo $words->getFormatted('searchmembersIntro'); ?></td></table>
 </div>

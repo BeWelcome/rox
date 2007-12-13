a813a55bf71470e13a083e638210cd807865597b
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-12-13 09:56:21 +0000

    #248 #ix bad links and bad header in invite a friend
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/alpha@3460 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/layout/inviteafriend.php
+++ htdocs/bw/layout/inviteafriend.php
@@ -33,22 +33,20 @@ function DisplayForm($m,$JoinMemberPict="") {
 
 	Menu2("inviteafriend.php", ww('MainPage')); // Displays the second menu
 
-	DisplayHeaderShortUserContent("inviteafriend.php","",""); // Display the header
+	DisplayHeaderShortUserContent(ww("InviteAFriendPage"),"",""); // Display the header
 
 	echo "<div class=\"info\">\n";
 
-	// TODO: check the meaning of the next row. $TData is not defined
-	$iiMax = count($TData);
 	$CurrentCategory="";
 	echo "<form method=\"post\" action=\"inviteafriend.php\">\n";
 	echo "<input type=\"hidden\" name=\"action\" value=\"Send\">\n";
 	echo "<p>",ww("InviteAFriendRule",$m->FullName),"</p>\n";
 	echo "<p>",ww("EmailOfYourFriend")," <input type=\"text\" name=\"Email\" value=\"",GetStrParam("Email"),"\"></p>\n";
 	if (IsPublic($_SESSION["IdMember"])) {
-		 echo "<p>","<textarea name=\"Message\" rows=\"20\" cols=\"80\">",str_replace("<br />","\n",ww("InviteAFriendStandardText","<a href=\"http://www.bewelcome.org/member.php?cid=".$_SESSION["Username"]."\">".$_SESSION["Username"]."</a>")),"</textarea></p>\n";
+		 echo "<p>","<textarea name=\"Message\" rows=\"20\" cols=\"80\">",str_replace("<br />","\n",ww("InviteAFriendStandardText","<a href=\"http://www.bewelcome.org/bw/member.php?cid=".$_SESSION["Username"]."\">".$_SESSION["Username"]."</a>")),"</textarea></p>\n";
 	}
 	else {
-		 echo "<p>","<textarea name=\"Message\" rows=\"20\" cols=\"80\">",str_replace("<br />","\n",ww("InviteAFriendTextPrivateProf","<a href=\"http://www.bewelcome.org/member.php?cid=".$_SESSION["Username"]."\">".$_SESSION["Username"]."</a>")),"</textarea></p>\n";
+		 echo "<p>","<textarea name=\"Message\" rows=\"20\" cols=\"80\">",str_replace("<br />","\n",ww("InviteAFriendTextPrivateProf","<a href=\"http://www.bewelcome.org/bw/member.php?cid=".$_SESSION["Username"]."\">".$_SESSION["Username"]."</a>")),"</textarea></p>\n";
 	}
 	echo "<p><input type=\"checkbox\" name=\"JoinMemberPict\" ";
 	if ($JoinMemberPict=="on") echo "checked";

72fd5086f0c05b2d7ff7c1cb1bd14ac7c86f0ce3
steinwinde <steinwinde@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2007-12-28 17:16:33 +0000

    Code beautifying, see revision 3548.
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@3563 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/page.php
+++ templates/page.php
@@ -40,12 +40,15 @@ MOD_user::updateSessionOnlineCounter();    // update session environment
     <base id="baseuri" href="<?php echo $Env->baseuri; ?>" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="verify-v1" content="NzxSlKbYK+CRnCfULeWj0RaPCGNIuPqq10oUpGAEyWw=" />
-<?
-echo "  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n";
-if (empty($meta_description)) $meta_description=$words->getFormatted("default_meta_description") ;
-echo "  <meta name=\"description\" content=\"",$meta_description,"\" />\n" ;
-if (empty($meta_keyword)) $meta_keyword=$words->getFormatted("default_meta_keyword") ;
-echo "  <meta name=\"keywords\" content=\"",$meta_keyword,"\" />\n" ;
+<?php
+if (empty($meta_description)) {
+    $meta_description = $words->getFormatted("default_meta_description");
+}
+echo "    <meta name=\"description\" content=\"",$meta_description,"\" />\n";
+if (empty($meta_keyword)) {
+    $meta_keyword = $words->getFormatted("default_meta_keyword");
+}
+echo "    <meta name=\"keywords\" content=\"",$meta_keyword,"\" />\n";
 ?>
     <link rel="shortcut icon" href="bw/favicon.ico" />
     <link rel="stylesheet" href="styles/YAML/main.css" type="text/css" />

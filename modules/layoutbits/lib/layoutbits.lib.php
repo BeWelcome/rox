099d42f78d47e0df944dd44f8a28a91dd05cdbf9
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-01-07 03:21:41 +0000

    send missing files (layoutbits module)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@3690 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ modules/layoutbits/lib/layoutbits.lib.php
@@ -0,0 +1,263 @@
+<?php
+/*
+
+Copyright (c) 2007 BeVolunteer
+
+This file is part of BW Rox.
+
+BW Rox is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BW Rox is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/> or 
+write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, 
+Boston, MA  02111-1307, USA.
+
+*/
+/**
+ * Collection of functions that create elements for a page.
+ * 
+ * An example for its use:
+ * $layoutbits = MOD_layoutbits::get();  // get the singleton instance
+ * $id = $geo->getCityID($cityname);
+ * 
+ * @author Andreas (bw/cs:lemon-head)
+ */
+class MOD_layoutbits
+{
+    /**
+     * Singleton instance
+     * 
+     * @var MOD_layoutbits
+     * @access private
+     */
+    private static $_instance;
+    
+	private function __construct()
+    {
+        $db = PVars::getObj('config_rdbms');
+        if (!$db) {
+            throw new PException('DB config error!');
+        }
+        $dao = PDB::get($db->dsn, $db->user, $db->password);
+        $this->dao =& $dao;
+    }
+    
+    /**
+     * singleton getter
+     * 
+     * @param void
+     * @return PApps
+     */
+    public static function get()
+    {
+        if (!isset(self::$_instance)) {
+            $c = __CLASS__;
+            self::$_instance = new $c;
+        }
+        return self::$_instance;
+    }
+    
+    
+    
+
+    
+    /**
+     * function LinkWithPicture build a link with picture and Username to the member profile.
+     * Optional parameter status can be used to alter the link.
+     * Old version found in FunctionsTools.php
+     *
+     * @param string $username
+     * @param string $pic alternative picture path
+     * @param string $mode can be used to enable 'map_style'
+     * @return string html-formatted link with picture
+     */
+    public static function linkWithPicture($username, $picfile="", $mode="")
+    {
+        $words = new MOD_words();
+        
+        if(!is_file(getcwd().$picfile)) {
+            // get the usual profile pic
+            $sql_result = self::get()->dao->query(
+                'SELECT SQL_CACHE Gender, HideGender '.
+                'FROM members '.
+                "WHERE Username='$username'"
+            );
+            $row = $sql_result->fetch(PDB::FETCH_OBJ);
+            $picfile = self::_dummyPic($row->Gender,$row->HideGender) ;
+        }
+        
+        $thumburl = self::_getThumb($picfile, 100, 100);
+        if ($thumburl === null) $thumburl = "";
+        
+        if ($mode == 'map_style') {
+            // TODO: why return a window with "$username" ??
+            return
+                '<a '.
+                    'href="javascript:newWindow('."'".$username."'".')" '.
+                    'title="' . $words->getBuffered('SeeProfileOf', $username).'"'.
+                '><img '.
+                    'class="framed" '.
+                    'style="float: left; margin: 4px" '.
+                    'src="'.$thumburl.'" '.
+                    'height="50px" '.
+                    'width="50px" '.
+                    'alt="Profile" '.
+                '/></a>'
+            ;
+        } else {
+            return
+                '<a '.
+                    'href="bw/member.php?cid='.$username.'" '.
+                    'title="'.$words->getBuffered('SeeProfileOf', $username).'" '.
+                '><img '.
+                    'class="framed" '.
+                    'src="'.$thumburl.'" '.
+                    'height="50px" '.
+                    'width="50px" '.
+                    'alt="Profile" '.
+                '/></a>'
+            ;
+        }
+    }
+    
+    /**
+     * 
+     * Thumbnail creator. (by markus5, Markus Hutzler 25.02.2007)
+     * tested with GD Version: bundled (2.0.28 compatible)
+     * with GIF Read Support: Enabled
+     * with JPG Support: Enabled
+     * with PNG Support: Enabled
+     *
+     * this function creates a thumbnail of a JPEG, GIF or PNG image
+     * file: path (with /)!!!
+     * max_x / max_y delimit the maximal size. default = 100 (it keeps the ratio)
+     * the quality can be set. default = 85
+     * this function returns the thumb filename or null
+     *
+     * modified by Fake51
+     * $mode specifies if the new image is based on a cropped and resized version of the old, or just a resized
+     * $mode = "square" means a cropped version
+     * $mode = "ratio" means merely resized
+     */
+    private static function _getThumb(
+                                    $file,
+                                    $max_x, $max_y,
+                                    $quality = 85,
+                                    $thumbdir = 'thumbs',
+                                    $mode = 'square'
+                                )
+    {
+        // TODO: analyze MIME-TYPE of the input file (not try / catch)
+        // TODO: error analysis of wrong paths
+        // TODO: dynamic prefix (now: /th/)
+    
+        if($file == "") return null;
+        
+        $filename = basename($file);
+        $filename_noext = substr($filename, 0, strrpos($filename, '.'));
+        $filepath = getcwd()."/bw/memberphotos";
+        $wwwpath = PVars::getObj('env')->baseuri."bw/memberphotos";
+        
+        $thumbfile = $filename_noext.'.'.$mode.'.'.$max_x.'x'.$max_y.'.jpg';
+        
+        // look if thumbnail already exists
+        if(is_file("$filepath/$thumbdir/$thumbfile")) return "$wwwpath/$thumbdir/$thumbfile";
+        
+        // look if original file exists
+        if (!is_file("$filepath/$filename")) return null;
+        
+        // TODO: bw_error("get_thumb: no file found");
+        
+        // look if thumbnail directory exists
+        if(!is_dir("$filepath/$thumbdir")) return null;
+        
+        // TODO: bw_error("get_thumb: no directory found");
+        
+        ini_set("memory_limit",'64M'); //jeanyves increasing the memory these functions need a lot
+        
+        // read image - try different image types
+        $image = false;
+        if (!$image) $image = @imagecreatefromjpeg("$filepath/$filename");
+        if (!$image) $image = @imagecreatefrompng("$filepath/$filename");
+        if (!$image) $image = @imagecreatefromgif("$filepath/$filename");
+        
+        // look if reading the image was successful
+        if($image == false) return null;
+    
+        // calculate ratio
+        $size_x = imagesx($image);
+        $size_y = imagesy($image);
+        
+        if($size_x == 0 or $size_y == 0){
+            bw_error("bad image size (0)");
+        }
+        
+        switch($mode){
+            case "ratio":
+                if (($max_x / $size_x) >= ($max_y / $size_y)){
+                    $ratio = $max_y / $size_y;
+                } else {
+                    $ratio = $max_x / $size_x;
+                }
+                $startx = 0;
+                $starty = 0;
+                break;
+            default:
+                if ($size_x >= $size_y){
+                    $startx = ($size_x - $size_y) / 2;
+                    $starty = 0;
+                    $size_x = $size_y;
+                } else {
+                    $starty = ($size_y - $size_x) / 2;
+                    $startx = 0;
+                    $size_y = $size_x;
+                }
+                
+                if ($max_x >= $max_y){
+                    $ratio = $max_y / $size_y;
+                } else {
+                    $ratio = $max_x / $size_x;
+                }
+                break;
+        }
+        
+        $th_size_x = $size_x * $ratio;
+        $th_size_y = $size_y * $ratio;
+        
+        // creating thumb
+        $thumb = imagecreatetruecolor($th_size_x,$th_size_y);
+        imagecopyresampled($thumb,$image,0,0,$startx,$starty,$th_size_x,$th_size_y,$size_x,$size_y);
+        
+        // try to write the new image
+        imagejpeg($thumb, "$filepath/$thumbdir/$thumbfile", $quality);
+        return "$wwwpath/$thumbdir/$thumbfile";
+    }
+    
+    
+    /**
+     * This function return a picture according to member gender if (any).
+     * It is used when no personal picture is found.
+     *
+     * @param string $Gender
+     * @param string $HideGender
+     * @return string path+filename of the dummy picture
+     */
+    private static function _dummyPic($Gender="IDontTell",$HideGender="Yes")
+    {
+        if ($HideGender=="Yes") return ('/memberphotos/et.jpg');
+        else if ($Gender=="male") return ('/memberphotos/et_male.jpg');
+        else if ($Gender=="female") return ('/memberphotos/et_female.jpg');
+        else return ('/memberphotos/et.gif');
+    }
+}
+
+?>
+

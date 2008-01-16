5d6c2993046963a297c16b35da6e441027652f75
micha <micha@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-01-16 16:29:40 +0000

    moving 3752, 3754 from alpha to production // temporal unconference startpage
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@3758 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/apps/rox/teaser.php
+++ templates/apps/rox/teaser.php
@@ -25,91 +25,51 @@ $words = new MOD_words();
 ?>
 
 <div id="teaser" class="clearfix">
-<div id="teaser_index"> 
+<div id="teaser_index" style="background: transparent;"> 
 
-<div id="teaser_l1"> 
-<?php	
+<div id="teaser_l1" class="float_left" style="width: 65%; margin-right: 10px; padding-right: 10px; padding-bottom: 20px;"> 
 
-			   
-// Random teaser content generation
-$chKey = rand(2,8); //case 1 is deactivated as showing random user pics doesn't work yet
-
-
-function slogan($slogan, $author) {
-    echo "<h1><em>" . $slogan . "</em></h1>\n";
-    echo "<h2>" . $author . "</h2>\n";
-    //This is slightly different now. TODO: use <blockquote> and some css
-}
-	
-switch ($chKey) {
-    case 1:
-         echo "          <div class=\"subcolumns\">\n"; 
-	 // Display the last created members with a picture
-	 $m = $mlastpublic;
-	 echo "                    <div class=\"c75l\">\n"; 
-	 echo "<h1>", $words->get('IndexPageWord2a'),"</h1>\n"; // Needs to be something like "Go, travel the world!"
-	 echo "                    <div class=\"c50l\">\n"; 
-	 echo "                      <div class=\"subl\">\n"; 
-	 echo "<h2>", $words->get('IndexPageWord1a'),"</h2>\n"; // Needs to be something like "Some are tired of discovering the world only in front of their TV:"
-	 echo "                      </div>\n"; 
-	 echo "                    </div>\n"; 
-	 echo "                    <div class=\"c50l\">\n"; 
-	 echo "                    <div class=\"c50l\">\n"; 
-	 echo "                      <div class=\"subl\">\n"; 
-	 echo "                          <p class=\"floatbox UserpicFloated\">";
-	 echo LinkWithPicture($m->Username,$m->photo), LinkWithUsername($m->Username),"<br />",$m->countryname ;
-	 echo "                          </p>\n"; 
-	 echo "                      </div>\n"; 
-	 echo "                      </div>\n"; 
-	 echo "                    <div class=\"c50r\">\n"; 
-	 echo "                      <div class=\"subr\">\n"; 
-	 echo "                          <p class=\"floatbox UserpicFloated\">";
-	 echo LinkWithPicture($m->Username,$m->photo), LinkWithUsername($m->Username),"<br />",$m->countryname ;
-	 echo "                          </p>\n"; 
-	 echo "                      </div>\n"; 
-	 echo "                      </div>\n";  
-	 echo "                    </div>\n"; 
-	 echo "                    </div>\n"; 
-         
-	 echo "                    <div class=\"c25l\">\n"; 
-	 echo "                      <div class=\"subl\">\n"; 
-	 echo "                          <p class=\"floatbox\">";
-	 echo "                          </p>\n"; 
-	 echo "                      </div>\n"; 
-	 echo "                    </div>\n"; 
-	 echo "          </div>\n"; 
-         
-	 break;
-     case 2:
-         echo "<h2>", $words->get('IndexPageWord1'),"</h2>\n";
-	 //why does <h2> come first here?
-	 echo "<h1>", $words->get('IndexPageWord2'),"</h1>\n";
-	 break;
-     case 3:
-         echo "<h2>", $words->get('IndexPageWord1b'),"</h2>\n";
-	 //why does <h2> come first here?
-	 echo "<h1>", $words->get('IndexPageWord2'),"</h1>\n";
-	 break;
-     case 4:
-	 slogan($words->get('slogan_Pathsaremadebywalking'), "Franz Kafka (1883 - 1924)");
-	 break;
-     case 5:
-         slogan($words->get('slogan_Theworldisabook'), "Saint Augustin (354 - 430)");
-	 break;
-     case 6:
-	 slogan($words->get('slogan_Donttellme'), "Muhammad (570 - 632)");
-	 break;
-     case 7:
-         slogan($words->get('slogan_Travellingislikeflirting'), "Advertisement");
-	 //This should not be "Advertisement", guaka 14112007
-	 break;
-     case 8:
-         slogan($words->get('slogan_maupassant'), "Guy de Maupassant (French writer, 1850 - 1893)");
-	 break;
-}
-?>
+     <h1><?php echo $words->get('IndexPageSpecial') ?></h1>
+	 <h2><a href="http://blogs.bevolunteer.org/"><?php echo $words->get('IndexPageSpecialLink1') ?> <em>&raquo;</em></a>&nbsp;&nbsp; <a href="http://www.bewelcome.org/forums"><?php echo $words->get('IndexPageSpecialLink2') ?> <em>&raquo;</em></a></h2>
 </div>
-<!--<div id="teaser_r"> 
-</div>-->
+
+<div id="teaser_r1" class="float_right" style="width: 30%; padding-bottom: 20px; text-align: right;"> 
+
+<h2><a href="http://www.flickr.com/photos/22828233@N05/tags/unconference"><?php echo $words->get('IndexPageSpecial3') ?></a></h2>
+<?php
+require_once("phpFlickr/phpFlickr.php");
+// Create new phpFlickr object
+$f = new phpFlickr("ae8eecd853e96099423763f64ed51857");
+/*$f->enableCache(
+    "db",
+    "mysql://[DB User]:[DB Password]@[DB Server]/[DB Name]"
+); */
+
+$i = 0;
+    // Find the NSID of the username inputted via the form
+    $person = $f->people_findByUsername('be.welcome');
+    
+    // Get the friendly URL of the user's photos
+    $photos_url = $f->urls_getUserPhotos($person['id']);
+    
+    // Get the user's first 36 public photos
+    $photos = $f->people_getPublicPhotos($person['id'], NULL, 2);
+    
+    // Loop through the photos and output the html now
+    foreach ((array)$photos['photo'] as $photo) {
+        echo "<a href=$photos_url$photo[id]>";
+        echo "<img class='framed' style='margin: 5px' border='0' alt='$photo[title]' ".
+            "src=" . $f->buildPhotoURL($photo, "Square") . ">";
+        echo "</a>";
+        $i++;
+        // If it reaches the sixth photo, insert a line break
+        if ($i % 4 == 0) {
+            echo "<br>\n";
+        }
+    }
+
+?>
+
+</div>
 </div>
 </div>

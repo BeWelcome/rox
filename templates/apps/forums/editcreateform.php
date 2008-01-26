c46b9fa64478566c30b383c3c71be441dd608a76
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-01-26 01:14:41 +0000

    #258, #358 - restore and improve TinyMCE configuration in forum (editcreateform.php)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@3826 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/apps/forums/editcreateform.php
+++ templates/apps/forums/editcreateform.php
@@ -43,46 +43,18 @@ if (!isset($vars['tags']) || !$vars['tags']) {
 tinyMCE.srcMode = '';
 tinyMCE.baseURL = http_baseuri+'script/tiny_mce';
 tinyMCE.init({
-    
+    theme_advanced_resize_horizontal : false,
     mode: "exact",
     elements: "topic_text",
     theme: "advanced",
     relative_urls:false,
     convert_urls:false,
-    /*
-    theme_advanced_buttons1: "bold,italic,underline,strikethrough,link,bullist,separator,justifyleft,justifycenter,justifyfull,bullist,numlist,forecolor,backcolor,charmap",
+    theme_advanced_buttons1: "bold,italic,underline,strikethrough,link,separator,bullist,numlist,blockquote,separator,justifyleft,justifyright,justifycenter,justifyfull,forecolor,backcolor,charmap",
     theme_advanced_buttons2: "",
     theme_advanced_buttons3: "",    
     theme_advanced_toolbar_location: 'top',
     theme_advanced_statusbar_location: 'bottom',
     theme_advanced_resizing: true
-    */
-    
-    /*plugins : "safari,spellchecker,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,pagebreak,imagemanager,filemanager",*/
-    theme_advanced_buttons1_add_before : "save,newdocument,separator",
-    theme_advanced_buttons1_add : "fontselect,fontsizeselect",
-    theme_advanced_buttons2_add : "separator,insertdate,inserttime,preview,separator,forecolor,backcolor",
-    theme_advanced_buttons2_add_before: "cut,copy,paste,pastetext,pasteword,separator,search,replace,separator",
-    theme_advanced_buttons3_add_before : "tablecontrols,separator",
-    theme_advanced_buttons3_add : "emotions,iespell,media,advhr,separator,print,separator,ltr,rtl,separator,fullscreen",
-    theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,spellchecker,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak,|,insertfile,insertimage",
-    theme_advanced_toolbar_location : "top",
-    theme_advanced_toolbar_align : "left",
-    theme_advanced_statusbar_location : "bottom",
-    content_css : "/example_data/example_full.css",
-    plugin_insertdate_dateFormat : "%Y-%m-%d",
-    plugin_insertdate_timeFormat : "%H:%M:%S",
-    external_link_list_url : "example_data/example_link_list.js",
-    external_image_list_url : "example_data/example_image_list.js",
-    flash_external_list_url : "example_data/example_flash_list.js",
-    template_external_list_url : "example_data/example_template_list.js",
-    theme_advanced_resize_horizontal : false,
-    theme_advanced_resizing : true,
-    apply_source_formatting : true,
-    /*
-    spellchecker_languages : "+English=en,Danish=da,Dutch=nl,Finnish=fi,French=fr,German=de,Italian=it,Polish=pl,Portuguese=pt,Spanish=es,Swedish=sv"
-    */
-    
 });
 //-->
 </script>
@@ -144,7 +116,7 @@ if ($allow_title) { // New Topic
 <p></p>
 <div class="row">
 <label for="topic_text"><?php echo $formText['label_text']; ?></label><br />
-<textarea name="topic_text" cols="60" rows="15" id="topic_text"><?php echo isset($vars['topic_text']) ? $vars['topic_text'] : ''; ?></textarea>
+<textarea name="topic_text" cols="70" rows="15" id="topic_text"><?php echo isset($vars['topic_text']) ? $vars['topic_text'] : ''; ?></textarea>
 </div>
 <p></p>
 

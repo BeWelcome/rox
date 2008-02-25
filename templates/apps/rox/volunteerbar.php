09b09377949c0f6e49315d5043680ef419c21bce
philipp <philipp@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-02-25 08:06:48 +0000

    adding links to volunteer page in userbar on main page and volunteerbar - ready for alpha
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@4175 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/apps/rox/volunteerbar.php
+++ templates/apps/rox/volunteerbar.php
@@ -42,6 +42,9 @@ Boston, MA  02111-1307, USA.
         '<h3>'.$words->getFormatted("VolunteerAction").'</h3>'.
         '<ul class="linklist">'
     ;
+	 echo
+		'<a href="volunteer">'. $words->get("Volunteerpage") . '</a></strong></li>';
+	
     $array_of_items =
         array(
             array(

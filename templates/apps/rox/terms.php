30c00226eb2f3703eca77e2cfe32c5b917122e63
globetrotter_tt <globetrotter_tt@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-02-29 19:19:59 +0000

    #410 :  Wrong Chars (�) at the "About BeWelcome?" site
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@4230 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/apps/rox/terms.php
+++ templates/apps/rox/terms.php
@@ -16,8 +16,8 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, see <http://www.gnu.org/licenses/> or 
-write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, 
+along with this program; if not, see <http://www.gnu.org/licenses/> or
+write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA  02111-1307, USA.
 
 */
@@ -31,31 +31,31 @@ $words = new MOD_words(); ?>
 As part of the registration process, you will designate a password, subject to our password security requirements. It is your responsibility to keep this password confidential. If you find out that your password or account is being used by someone without your consent, or you discover any other breach of security, you agree to notify us promptly without delay.</p>
 
 <h4>2. Privacy</h4>
-<p>We are committed to safeguarding your privacy. Thus we understand your wish and respect your right to privacy and personal information. However, by submitting your photograph and any other personal information, you allow any member of BeWelcome to view and use this information. If you do not wish to keep your personal picture or information private vis-à-vis other members of BeWelcome, you shall choose to hide it accordingly. We reserve the right to collect further personal information from you that we do not post on BeWelcome. Under normal circumstances we will not transfer your personal information to unaffiliated third parties without your consent. However, we reserve the right to transfer such information without your consent in the following cases: to prevent an emergency, to protect or enforce our legal rights, to protect or enforce legal rights of a third party or in pursuance to a court order or subpoena or as otherwise required or permitted by law. This also applies to messages you send through BeWelcome. While these messages are normally only read by respective senders and addressees, you agree to disclose those messages to volunteers for spam monitoring. BeVolunteer may use the email address you provided upon registration in order to send you regular newsletters, which, however, you can unsubscribe from. You further agree to your visits of other members profiles being logged and displayed to them or logged for administrative or technical purposes.</p>
+<p>We are committed to safeguarding your privacy. Thus we understand your wish and respect your right to privacy and personal information. However, by submitting your photograph and any other personal information, you allow any member of BeWelcome to view and use this information. If you do not wish to keep your personal picture or information private vis-&agrave;-vis other members of BeWelcome, you shall choose to hide it accordingly. We reserve the right to collect further personal information from you that we do not post on BeWelcome. Under normal circumstances we will not transfer your personal information to unaffiliated third parties without your consent. However, we reserve the right to transfer such information without your consent in the following cases: to prevent an emergency, to protect or enforce our legal rights, to protect or enforce legal rights of a third party or in pursuance to a court order or subpoena or as otherwise required or permitted by law. This also applies to messages you send through BeWelcome. While these messages are normally only read by respective senders and addressees, you agree to disclose those messages to volunteers for spam monitoring. BeVolunteer may use the email address you provided upon registration in order to send you regular newsletters, which, however, you can unsubscribe from. You further agree to your visits of other members profiles being logged and displayed to them or logged for administrative or technical purposes.</p>
 
 <h4>3. Liability</h4>
-<p>We offer a platform for registered members to arrange hospitality exchange and to use tools to plan and record their travels. Even though we hope to meet your requirements, we do not warrant any information provided on the site to be accurate and reliable at any relevant time, to meet your expectations, to offer error-free, uninterrupted and fast service. We shall not be responsible or liable in case of a dispute with one of our members. You thus exempt us herewith from any claims or liabilities whatsoever arising from or in connection with our service. This Service is provided by BeWelcome and BeVolunteer, the organization behind BeWelcome (registrated in France at 'la préfecture de Rennes' with the number W3530003536) on an "AS-IS" and "AS AVAILABLE" basis and BeVolunteer reserves the right to modify, suspend or discontinue the Service, in its sole discretion, at any time and without notice. You agree that BeVolunteer is not and will not be liable to you for any modification, suspension or discontinuance of the Service.</p>
+<p>We offer a platform for registered members to arrange hospitality exchange and to use tools to plan and record their travels. Even though we hope to meet your requirements, we do not warrant any information provided on the site to be accurate and reliable at any relevant time, to meet your expectations, to offer error-free, uninterrupted and fast service. We shall not be responsible or liable in case of a dispute with one of our members. You thus exempt us herewith from any claims or liabilities whatsoever arising from or in connection with our service. This Service is provided by BeWelcome and BeVolunteer, the organization behind BeWelcome (registrated in France at 'la pr&eacute;fecture de Rennes' with the number W3530003536) on an &quot;AS-IS&quot; and &quot;AS AVAILABLE&quot; basis and BeVolunteer reserves the right to modify, suspend or discontinue the Service, in its sole discretion, at any time and without notice. You agree that BeVolunteer is not and will not be liable to you for any modification, suspension or discontinuance of the Service.</p>
 
 <h4>4. General Conduct</h4>
 <p>You agree that you shall not</p>
 <ul>
   <li>(a) interfere with or disrupt (or attempt to interfere with or disrupt) this Service or servers, through, including, but not limited to, the use of viruses, cancel bots, Trojan horses, harmful code, flood pings, denial-of-service attacks, packet or IP spoofing, forged routing or electronic mail address information or similar methods or technology</li>
-  
-  <li>(b) provide any information to BeWelcome that is false or misleading, that would otherwise hide your identity or that you do not have the right to disclose</li>
-  
-  <li>(c) violate any applicable law or regulation. Since BeVolunteer (the legal organization behind BeWelcome) is registrated in France at 'la préfecture de Rennes' (number W3530003536), French law is applicable</li>
-  
-  <li>(d) violate the rights of any third party, including, but not limited to, intellectual property, privacy, publicity or contractual rights</li>
-  
-  <li>(e) collect, store or abuse any information you obtain through our Service including any information about any other user for any unauthorized purposes</li>
-  
-  <li>(f) use our Services to transmit, distribute, post or submit any information concerning any other person or entity, including, but not limited to, personal contact information or credit, debit, calling card or account numbers</li>
-  
-  <li>(g) molest, "stalk" or harass any other user of our Services</li>
-  
-  <li>(h) use our Services for any commercial purpose especially in connection with the distribution of unsolicited commercial email ("Spam") or advertisements</li>
 
-  <li>(i) assist any third party in doing any of the foregoing</li> 
+  <li>(b) provide any information to BeWelcome that is false or misleading, that would otherwise hide your identity or that you do not have the right to disclose</li>
+
+  <li>(c) violate any applicable law or regulation. Since BeVolunteer (the legal organization behind BeWelcome) is registrated in France at 'la pr&eacute;fecture de Rennes' (number W3530003536), French law is applicable</li>
+
+  <li>(d) violate the rights of any third party, including, but not limited to, intellectual property, privacy, publicity or contractual rights</li>
+
+  <li>(e) collect, store or abuse any information you obtain through our Service including any information about any other user for any unauthorized purposes</li>
+
+  <li>(f) use our Services to transmit, distribute, post or submit any information concerning any other person or entity, including, but not limited to, personal contact information or credit, debit, calling card or account numbers</li>
+
+  <li>(g) molest, "stalk" or harass any other user of our Services</li>
+
+  <li>(h) use our Services for any commercial purpose especially in connection with the distribution of unsolicited commercial email (&quot;Spam&quot;) or advertisements</li>
+
+  <li>(i) assist any third party in doing any of the foregoing</li>
 </ul>
 
 <p>You agree to defend, indemnify, and hold harmless BeWelcome and BeVolunteer, its directors and volunteers, from and against any claims, liabilities, damages, losses, and expenses, including, but not limited to, reasonable legal and expert fees, arising out of or in any way connected with your access to or use of the Services, or your violation of these Terms.</p>

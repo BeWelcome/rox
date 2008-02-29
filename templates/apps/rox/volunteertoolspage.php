a920fac47e2d01fc57a39175899684e6e81f9ac5
matrixpoint <matrixpoint@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-02-29 12:34:30 +0000

    #196. moving to production.
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@4229 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/apps/rox/volunteertoolspage.php
+++ templates/apps/rox/volunteertoolspage.php
@@ -21,7 +21,7 @@ switch($currentSubPage) {
 	break;
 	
 	case 'tasks':
-		echo	"<iframe src=\"http://www.bevolunteer.org/trac/query?status=new&status=assigned&status=reopened&show_on_bw=1&order=priority\" width=\"100%\" height=\"600\" frameborder=\"0\" name=\"ToolsFrame\"></iframe>"; 
+		echo	"<iframe src=\"http://www.bevolunteer.org/trac/query?status=new&status=assigned&status=reopened&group=status&type=volunteer+task&order=priority\" width=\"100%\" height=\"600\" frameborder=\"0\" name=\"ToolsFrame\"></iframe>"; 
 	break;	
 
 	case 'features':
@@ -45,18 +45,41 @@ switch($currentSubPage) {
 	break;	
 	
 	default:
+	
+	?>
+	<h3><?php echo $words->get('VolunteerCoordinators'); ?></h3>
+	<ul class="floatbox">
+		<li class="userpicbox_big float_left"><h4><a href="user/kiwiflave"><img src="http://www.bewelcome.org/memberphotos/thumbs/kiwiflave_1171823734.square.80x80.jpg" class="framed float_left" style="height:70px; width: 70px;">Florian</a><br /></h4>
+		</li>
+		<li class="userpicbox_big float_left">
+			<h4>
+				<a href="user/claudiaab"><img src="http://www.bewelcome.org/memberphotos/thumbs/claudiaab_1169485927.square.80x80.jpg" class="framed float_left" style="height:70px; width: 70px;">Claudia</a><br />
+			</h4>
+		</li>
+		<li class="userpicbox_big float_left">
+			<p>
+				<?php echo $words->get('VolunteerCoordinatorsText'); ?>
+			</p>
+       </li>
+	</ul>
+<?
+	
 		echo "<div class=\"subcolumns\">\n";
 		echo "  <div class=\"c50l\">\n";
 		echo "  <div class=\"subcl\">\n";
 		
 
 		echo "<div class=\"info\">\n";
+
+
 		echo "<h3><a href=\"volunteer/forum\" title=\"" . $words->get('VolunteerToolsTipForum') . "\" >" . $words->get('VolunteerTools_BVForum') . "</a> <a href=\"http://www.bevolunteer.org/forum\" target =\"blank\" title=\"" . $words->get('VolunteerToolsTipForumExt') . "\" >[ext]</a></h3>";
 		echo "<p>",$words->get("VolunteerTools_BvForumText"),"</p>";
 		echo "<h3><a href=\"volunteer/trac\" title=\"" . $words->get('VolunteerToolsTipTrac') . "\" >" . $words->get('VolunteerTools_Trac') . "</a> <a href=\"http://bevolunteer.org/trac/\" target =\"blank\" title=\"" . $words->get('VolunteerToolsTipForumExt') . "\" >[ext]</a></h3>";	
 		echo "<p>",$words->get("VolunteerTools_TracText"),"</p>";
 		echo "<h3><a href=\"volunteer/otrs\" title=\"" . $words->get('VolunteerToolsTipOTRS') . "\" >" . $words->get('VolunteerTools_OTRS') . "</a> <a href=\"http://www.bevolunteer.org/otrs\" target =\"blank\" title=\"" . $words->get('VolunteerToolsTipForumExt') . "\" >[ext]</a></h3>";	
 		echo "<p>",$words->get("VolunteerTools_OTRSText"),"</p>";
+		echo "<h3><a href=\"volunteer/blogs\" title=\"" . $words->get('VolunteerToolsTipBlogs') . "\" >" . $words->get('VolunteerTools_Blogs') . "</a> <a href=\"http://blogs.bevolunteer.org/\" target =\"blank\" title=\"" . $words->get('VolunteerToolsTipForumExt') . "\" >[ext]</a></h3>";
+		echo "<p>",$words->get("VolunteerTools_BlogsText"),"</p>";
 		echo "</div>\n";
 		
 		echo "</div>\n";
@@ -66,10 +89,10 @@ switch($currentSubPage) {
         echo "<div class=\"subcr\">\n";
 		
 		echo "<div class=\"info\">\n";
-		echo "<h3><a href=\"volunteer/blogs\" title=\"" . $words->get('VolunteerToolsTipBlogs') . "\" >" . $words->get('VolunteerTools_Blogs') . "</a> <a href=\"http://blogs.bevolunteer.org/\" target =\"blank\" title=\"" . $words->get('VolunteerToolsTipForumExt') . "\" >[ext]</a></h3>";
-		echo "<p>",$words->get("VolunteerTools_BlogsText"),"</p>";
 		echo "<h3><a href=\"volunteer/mailman\" title=\"" . $words->get('VolunteerToolsTipMailman') . "\" >" . $words->get('VolunteerTools_Mailman') . "</a> <a href=\"http://bewelcome.org/mailman/listinfo/\" target =\"blank\" title=\"" . $words->get('VolunteerToolsTipForumExt') . "\" >[ext]</a></h3>";	
 		echo "<p>",$words->get("VolunteerTools_MailmanText"),"</p>";
+		echo "<h3>", $words->get("VolunteerTools_Chat"),"</h3>";	
+		echo "<p>",$words->get("VolunteerTools_ChatText"),"</p>";
 		echo "<h3>", $words->get("VolunteerTools_Test"),"</h3>";	
 		echo "<p>",$words->get("VolunteerTools_TestText"),"</p>";
 		echo "<h3>", $words->get("VolunteerTools_Alpha"),"</h3>";	

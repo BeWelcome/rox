df1816a8e2094803e0d6d3fda6194934fb452815
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-03-29 00:24:59 +0000

    #478 - activate the RoxLauncher on test.bw - yeah! (this happens by commit of htdocs/index.php)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@4423 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/index.php
+++ htdocs/index.php
@@ -22,8 +22,9 @@ Boston, MA  02111-1307, USA.
 
 */
 
-if (!version_compare(phpversion(), '5.1.0', '>='))
+if (!version_compare(phpversion(), '5.1.0', '>=')) {
     die('Only for PHP version 5.1.0 or greater!');
+}
 
 // Find out whether the scripts reside in a subdir or not
 $script_base = dirname($_SERVER['SCRIPT_FILENAME']);
@@ -35,8 +36,10 @@ if (file_exists($script_base.'/base.xml')) {
     die('File "base.xml" not found!');
 }
 
+
 ini_set('display_errors', 1);
 ini_set('allow_url_fopen', 1);
+
 
 /**
  * The directory where the script resides
@@ -49,132 +52,13 @@ define('SCRIPT_BASE', $script_base);
 define('HTDOCS_BASE', dirname(__FILE__).'/');
 ini_set('error_log', SCRIPT_BASE.'errors.log');
 error_reporting(E_ALL);
+
+
+
 try {
-    /**
-     * load base.xml data
-     */
-    require_once SCRIPT_BASE.'inc/base.inc.php';
-    /**
-     * load libraries
-     */
-    require_once SCRIPT_BASE.'lib/libs.php';
-    $S = PSurveillance::get();
-    /**
-     * environment check
-     */
-    require_once SCRIPT_BASE.'inc/env_check.inc.php';
-    
-    // TODO: this case distinction is needed for now to make us independent of MOD_env.
-    if (is_file(SCRIPT_BASE.'modules/env/lib/env.lib.php')) {
-        PSurveillance::setPoint('loading_modules');
-        // load modules
-        $Mod = PModules::get();
-        $Mod->setModuleDir(SCRIPT_BASE.'modules');
-        $Mod->loadModules();
-        PSurveillance::setPoint('modules_loaded');
-        
-        // force configuration with MOD_env
-        MOD_env::get();
-    } else if (is_file(SCRIPT_BASE.'inc/config.inc.php')) {
-        // configuration
-        require_once SCRIPT_BASE.'inc/config.inc.php';
-    } else {
-        echo '<pre>
-    No config file found. 
-     
-    Please create a file 
-    <b>'.SCRIPT_BASE.'inc/config.inc.php</b> 
-    as a modified copy of 
-    '.SCRIPT_BASE.'inc/config.inc.php.example 
-        </pre>';
-        PPHP::PExit();
-    }
-    
-    /**
-     * defaults
-     */
-    require_once SCRIPT_BASE.'inc/defaults.inc.php';
-    PSurveillance::setPoint('base_loaded');
-    
-    if (defined ('SESSION_NAME'))
-        ini_set ('session.name', SESSION_NAME);
-    ini_set ('session.use_trans_sid', 0);
-    ini_set ('url_rewrite.tags', '');
-    ini_set ('session.hash_bits_per_character', 6);
-    ini_set ('session.hash_function', 1);
-    session_cache_expire(3600); // Set session lenght to one hour
-    session_start();
-    if (empty ($_COOKIE[session_name ()]) ) {
-        PVars::register('cookiesAccepted', false);
-    } else {
-        PVars::register('cookiesAccepted', true);
-    }
-    PVars::register('queries', 0);
-    
-    // this is the old place where the modules were loaded    
-    // Now this happens a bit earlier, which is needed for MOD_env.
-    if(!is_file(SCRIPT_BASE.'modules/env/lib/env.lib.php')) {
-        PSurveillance::setPoint('loading_modules');
-        // load modules
-        $Mod = PModules::get();
-        $Mod->setModuleDir(SCRIPT_BASE.'modules');
-        $Mod->loadModules();
-        PSurveillance::setPoint('modules_loaded');
-    }
-    
-    $Apps = PApps::get();
-    $Apps->build();
-    // process includes
-    $includes = $Apps->getIncludes();
-    if ($includes) {
-        foreach ($includes as $inc) {
-            require_once $inc;
-        }
-    }
-
-    if (!function_exists('PPckup')) {
-        throw new PException('No default pickup!');
-    } 
-    
-    PSurveillance::setPoint('loading_apps');
-
-    // rox
-	$Rox = new RoxController;
-    $PH = PPostHandler::get();
-
-    $request = PRequest::get()->request;
-    // class not set
-    if (!isset($request[0])) {
-        $class = PPckup();
-    } else {
-        $app = translate($request[0]);
-        // Does the class exist? Maybe with first letter uppercased?
-        if (!$app = PApps::getAppName($app)) {
-            $class = PPckup();
-        } else {
-            $class = $app;
-        }
-    }
-    
-    // set the default page title
-    // this should happen before the applications can overwrite it.
-    // TODO: maybe there's a better place for this.
-    PVars::getObj('page')->title='BeWelcome';
-    
-    $App = new $class;
-    $App->index();
-    
-    // TODO: Why is that?
-    $Rox->buildContent();
-    PSurveillance::setPoint('apps_loaded');
-    
-    if (PVars::getObj('page')->output_done) {
-        // output already happened, or not planned
-    } else {
-        $D = new PDefaultController;
-        $D->output();
-    }
-    
+    require_once SCRIPT_BASE.'roxlauncher/roxlauncher.php';
+    $launcher = new RoxLauncher();
+    $launcher->launch();
 } catch (PException $e) {
     header('Content-type: application/xml; charset=utf-8');
     echo $e;
@@ -184,5 +68,7 @@ try {
     echo "\n{$e->getFile()} ({$e->getLine()})";
     exit();
 }
+
 session_write_close();
+
 ?>

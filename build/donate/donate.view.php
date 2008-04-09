5ed314aec3f4b90264c53337e82975012f3d8d2e
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-04-09 01:13:47 +0000

    #395 - repair donations application that I killed with Rox update.
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@4503 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ build/donate/donate.view.php
@@ -0,0 +1,34 @@
+<?php
+
+class DonateView extends PAppView
+{
+    /**
+     * Loading Simple Teaser - just needs defined title
+     *
+     * @param void
+     */
+    public function ShowSimpleTeaser($title)
+    {
+        require TEMPLATE_DIR.'apps/rox/teaser_simple.php';
+    }
+
+    public function donate($sub = false,$TDonationArray = false)
+    {
+        require TEMPLATE_DIR.'apps/rox/donate.php';
+    }
+    
+    public function donateBar()
+    {
+        require TEMPLATE_DIR.'apps/rox/userbar_donate.php';
+    }
+    
+}
+
+
+
+
+
+
+
+
+?>

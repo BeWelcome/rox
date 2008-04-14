b50e4d89a4d52d7b581a3f250dcf53c2d74146f8
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-04-14 20:29:59 +0000

    more stuff in "tools"... (necessary for new members application)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@4552 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- tools/mvc-base/mvc-base.model.php
+++ tools/mvc-base/mvc-base.model.php
@@ -15,12 +15,37 @@ class RoxModelBase extends PAppModel
     }
     
     
+    public function bulkLookup_assoc($query_string)
+    {
+        $rows = array();
+        if (!$sql_result = $this->dao->query($query_string)) {
+            // sql problem
+        } else while ($row = $sql_result->fetch(PDB::FETCH_ASSOC)) {
+            $rows[] = $row;
+        }
+        return $rows;
+    }
+    
+    
     public function singleLookup($query_string)
     {
         if (!$sql_result = $this->dao->query($query_string)) {
             // sql problem
             return false;
         } else if (!$row = $sql_result->fetch(PDB::FETCH_OBJ)) {
+            // nothing found
+            return false;
+        } else {
+            return $row;
+        }
+    }
+        
+    public function singleLookup_assoc($query_string)
+    {
+        if (!$sql_result = $this->dao->query($query_string)) {
+            // sql problem
+            return false;
+        } else if (!$row = $sql_result->fetch(PDB::FETCH_ASSOC)) {
             // nothing found
             return false;
         } else {

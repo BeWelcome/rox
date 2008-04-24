75d9947bd696e62ea59d1732f0a4086b0b5307b9
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-04-24 20:20:20 +0000

    remove debug echos in roxlauncher/roxloader.php
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@4620 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- roxlauncher/roxloader.php
+++ roxlauncher/roxloader.php
@@ -132,7 +132,6 @@ class RoxLoader
         if (!is_array($files)) {
             // do nothing, leave $ini_settings empty
         } else foreach ($files as $filename) {
-            echo $filename.'<br>';
             if (is_file($filename)) {
                 $sections = parse_ini_file($filename, true);
                 foreach ($sections as $section => $contents) {

12f4ffd56fb7e2cb1dafb0e774c41862f52c671e
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-05-06 15:38:27 +0000

    #556 - simulate missing native php functions - una mas
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@4707 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- roxlauncher/ptlauncher.php
+++ roxlauncher/ptlauncher.php
@@ -53,7 +53,8 @@ abstract class PTLauncher
      */
     protected function simulateMissingFunctions()
     {
-        foreach (scandir(SCRIPT_BASE.'roxlauncher/missingfunctions') as $file) {
+        foreach (scandir(SCRIPT_BASE.'roxlauncher/missingfunctions') as $filename) {
+			$file = SCRIPT_BASE.'roxlauncher/missingfunctions/'.$filename;
             if (is_file($file)) {
                 $functionname = basename($file, '.php');
                 if (!function_exists($functionname)) {

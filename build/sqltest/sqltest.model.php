36c0e424bd42cc11803487c236379bc412cb0a8e
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-05-17 12:54:31 +0000

    tool to check for unsynced records in user and members tables: little pimp-up
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@4792 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/sqltest/sqltest.model.php
+++ build/sqltest/sqltest.model.php
@@ -23,7 +23,7 @@ FROM user
         );
     }
     
-    public function getAll()
+    public function analyse()
     {
         $all = array();
         foreach ($this->getBWMembers() as $m) {
@@ -34,7 +34,28 @@ FROM user
             if (!isset($all[$u->handle])) $all[$u->handle] = new stdClass;
             $all[$u->handle]->u = $u;
         }
-        return $all;
+        $res = new stdClass();
+        $res->all = $all;
+        
+        $res->orphan_total = array();
+        $res->orphan_m = array();
+        $res->orphan_u = array();
+        $res->id_mismatch = array();
+        
+        foreach ($all as $username => $x) {
+            if (!isset($x->u) && !isset($x->m)) {
+                $res->orphan_total[] = $x;
+            } else if (!isset($x->u)) {
+                $res->orphan_m[] = $x->m;
+            } else if (!isset($x->m)) {
+                $res->orphan_u[] = $x->u;
+            } else if ($x->u->id != $x->m->id) {
+                $res->id_mismatch[] = $x;
+            }
+        }
+        
+        
+        return $res;
     }
 }
 
--- build/sqltest/sqltest.page.php
+++ build/sqltest/sqltest.page.php
@@ -5,8 +5,35 @@ class SqltestPage extends RoxPageView
 {
     protected function column_col3()
     {
-        $all = $this->model->getAll();
-        foreach ($all as $username => $x) {
+        $result = $this->model->analyse();
+        
+        echo '<h3>BW members without a BW member record</h3>';
+        
+        echo '<table><tr><th>m->id</th><th>m->handle</th></tr>';
+        foreach ($result->orphan_m as $m) {
+            echo '<tr><td>'.$m->id.'</td><td>'.$m->Username.'</td></tr>';
+        }
+        echo '</table>';
+        
+        echo '<h3>TB users without a BW member record</h3>';
+        
+        echo '<table><tr><th>u->id</th><th>u->handle</th></tr>';
+        foreach ($result->orphan_u as $u) {
+            echo '<tr><td>'.$u->id.'</td><td>'.$u->handle.'</td></tr>';
+        }
+        echo '</table>';
+        
+        echo '<h3>id mismatch in users / members table</h3>';
+        
+        echo '<table><tr><th>m->id</th><th>u->id</th><th>username</th></tr>';
+        foreach ($result->id_mismatch as $x) {
+            echo '<tr><td>'.$x->m->id.'</td><td>'.$x->u->id.'</td><td>'.$x->m->Username.'</td></tr>';
+        }
+        echo '</table>';
+        
+        echo '<h3>All</h3>';
+        
+        foreach ($result->all as $username => $x) {
             if (!isset($x->u) && !isset($x->m)) {
                 echo '<br>??';
             }

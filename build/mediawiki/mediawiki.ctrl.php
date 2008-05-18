1311ad137c7cd6363a60617a4828e1f408d0dd08
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-05-18 16:06:36 +0000

    mediawiki inclusion. Greetings, Kasper and Andreas.
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@4808 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ build/mediawiki/mediawiki.ctrl.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Hello universe controller
+ *
+ * @package hellouniverse
+ * @author Andreas (lemon-head)
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License (GPL)
+ * @version $Id$
+ */
+class MediawikiController extends RoxControllerBase
+{
+    /**
+     * decide which page to show.
+     * This method is called automatically
+     */
+    public function index($args = false)
+    {
+        $request = $args->request;
+        $page = new MediawikiPage();
+        $mwiki_title = isset($args->request[1]) ? $args->request[1] : 'Main_Page';
+        $mwiki_title = str_replace(" ", "_", $page->mwiki_title); 
+        switch (isset($request[0]) ? $request[0] : '') {
+            case 'ocswiki':
+                $page->base_url = 'http://www.opencouchsurfing.org/w/';
+                break;
+            case 'hitchwiki':
+                $page->base_url = 'http://en.hitchwiki.org/';
+                break;
+            case 'bvwiki':
+            default:
+                $page->base_url = 'http://www.bevolunteer.org/wiki/';
+        }
+        $page->base_url .= 'index.php?title='.$mwiki_title;
+        $page->inclusion_url = $page->base_url .'&action=render';
+        $page->history_url = $page->base_url .'&action=history';
+        $page->edit_url = $page->base_url .'&action=edit';
+        $page->headline = str_replace("_", " ", $mwiki_title);
+        return $page;
+    }
+}
+
+
+?>0
---
+++ build/mediawiki/mediawiki.page.php
@@ -0,0 +1,42 @@
+<?php
+
+
+/**
+ * Hello universe page.
+ * This is a base class for other pages in the same application.
+ * We redefine the methods of RoxPageView to configure this page.
+ * Here we make some more decorations.
+ *
+ * @package hellouniverse
+ * @author Andreas (lemon-head)
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License (GPL)
+ * @version $Id$
+ */
+class MediawikiPage extends RoxPageView
+{
+    protected function teaserHeadline()
+    {
+        echo $this->headline . " - " . $this->wikiname;
+    }
+    
+    
+    /**
+     * content of the middle column - this is the most important part
+     */
+    protected function columnsArea()
+    {
+        // get the translation module
+        $words = $this->getWords();
+        
+        $contents = file_get_contents($this->inclusion_url);
+        //TODO: replace URLs, add edit and historz linkz, caching, fix redirect, css
+        echo $contents;
+        
+    }
+
+}
+
+
+
+
+?>

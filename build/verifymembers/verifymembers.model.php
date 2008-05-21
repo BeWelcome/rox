36039080bf97dcc7da519e0d36c0e39f2d688f94
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-05-21 20:45:59 +0000

    This is a test
    @Andreas : could you tell me why the http://localhost/verifymembers don't results in anything ?
    
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@4821 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ build/verifymembers/verifymembers.model.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Gallery model
+ * 
+ * @package about
+ * @author The myTravelbook Team <http://www.sourceforge.net/projects/mytravelbook>
+ * @copyright Copyright (c) 2005-2006, myTravelbook Team
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License (GPL)
+ * @version $Id$
+ */
+class VerifyMembersModel extends PAppModel
+{
+}
+
+
+
+
+?>

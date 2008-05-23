cb2b5742545b5d8fe9445e627d6ee3a4ec2568c9
micha <micha@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-05-23 13:58:48 +0000

    Changing the online-application to the new framework-base by lemonhead
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@4843 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/online/online.ctrl.php
+++ build/online/online.ctrl.php
@@ -1,50 +1,40 @@
 <?php
-/**
- * Gallery controller
- *
- * @package gallery
- * @author The myTravelbook Team <http://www.sourceforge.net/projects/mytravelbook>
- * @copyright Copyright (c) 2005-2006, myTravelbook Team
+/** 
+ * Online Controller
+ * 
+ * @package online
+ * @author lupochen
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License (GPL)
  * @version $Id$
  */
 
-require_once("../htdocs/bw/lib/rights.php") ; // Requiring BW right
 
-class OnlineController extends PAppController {
-    private $_model;
-    private $_view;
+class OnlineController extends RoxControllerBase
+{
 
-    public function __construct() {
-        parent::__construct();
-        $this->_model = new Online();
-        $this->_view  = new OnlineView($this->_model);
+    public function index()
+    {
+        $request = PRequest::get()->request;
+        $model = new OnlineModel();
+        
+        if (!isset($request[1])) {
+            // normal chat page
+            $page = new OnlinePage();
+        } else switch($request[1]) {
+            case 'online':
+            // nothing yet
+            default:
+                $page = new OnlinePage();
+        }
+        $page->model = $model;
+        PVars::getObj('page')->output_done = true;
+        return $page;
     }
-
-    public function __destruct() {
-        unset($this->_model);
-        unset($this->_view);
+    
+    protected function createOnlineOtherPage($args)
+    {
+        return new OnlineOtherPage();
     }
-
-    public function index() {
-
-    /* teaser content */
-        ob_start();
-        $this->_view->Teaser();
-        $str = ob_get_contents();
-        $P = PVars::getObj('page');
-        $P->teaserBar .= $str;
-        ob_end_clean();;
-
-    /* page content */
-        ob_start();
-        $this->_view->ShowOnline() ;
-        $str = ob_get_contents();
-        ob_end_clean();
-        $P = PVars::getObj('page');
-        $P->content .= $str;
-
-   } // end of index
-
+    
 }
 ?>

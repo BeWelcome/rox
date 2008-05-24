7244886cbb1f467fd0b5c22e617ed630f54cfc20
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-05-24 21:29:01 +0000

    minor trying to fix a warning in errorlog ($mlastpublic not initialized)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@4852 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/layout/menus.php
+++ htdocs/bw/layout/menus.php
@@ -572,7 +572,12 @@ function DisplayHeaderIndexPage($TitleTopContent = "") {
 		case 1:
 			echo "		<div class=\"subcolumns\">\n"; 
 			// Display the last created members with a picture
-			$m=$mlastpublic ;
+			if (!isset($mlastpublic)) {
+			   $m=$mlastpublic = mysql_fetch_object(mysql_query("select SQL_CACHE members.*,cities.Name as cityname,IdRegion,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,memberspublicprofiles,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' and memberspublicprofiles.IdMember=members.id GROUP BY members.id order by members.LastLogin desc limit 1")); 
+			}
+			else {
+				 $m=$mlastpublic ;
+			}
 			echo "			  <div class=\"c75l\">\n"; 
 				echo "<h1>", ww("IndexPageWord2a"),"</h1>\n"; // Needs to be something like "Go, travel the world!"
 				echo "			  <div class=\"c50l\">\n"; 

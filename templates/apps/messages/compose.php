aeb5196417c4ef3e23479ed8fb6cf4061ca35f42
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-05-27 14:29:06 +0000

    messages application, templates were missing (don't know where needed)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@4933 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ templates/apps/messages/compose.php
@@ -0,0 +1,41 @@
+
+
+
+<h3>Message to <a href="bw/member.php?cid=<?=$receiver_username ?>"><?=$receiver_username ?></a></h3>
+
+<form method="POST" action="<?=$page_url ?>">
+    <?=$callback_tag ?>
+    
+    <?php if ($receiver_username) { ?>
+    <input type="hidden" name="receiver_id" value="<?=$receiver_id ?>"/>
+    <?php } else { ?>
+    <p>To: <input name="receiver_username"/></p>
+    <?php } ?>
+    
+    <p>
+        <textarea name="text" rows="15" cols="80"><?=$text ?></textarea>
+    </p>
+    
+    <p>
+        I confirm that I have read the
+        <a href="http://www.bevolunteer.org/wiki/index.php/Spam_Info_Page">Infos about Spam</a>
+        and agree with them.
+    </p>
+    
+    <p>
+        <input type="checkbox" name="agree_spam_policy" id="IamAwareOfSpamCheckingRules">
+        <label for="IamAwareOfSpamCheckingRules">I agree</label>
+    </p>
+    
+    <p>
+        <input type="checkbox" name="attach_picture" id="JoinMemberPict"<?=$attach_picture ?>/>
+        <label for="JoinMemberPict">Attach my profile picture</label>
+    </p>
+    
+    <p>
+        <input type="submit" value="send"/>
+    </p>
+
+</form>
+        
+
---
+++ templates/apps/messages/compose_warning.php
@@ -0,0 +1,8 @@
+
+<h3>There were problems in your sent data.</h3>
+<table>
+  <tr><th>Field name</th><th>Problem</th></tr>
+  <?php foreach ($memory->problems as $key => $value) { ?>
+  <tr><td><?=$key ?></td><td><?=$value ?></td></tr>
+  <?php } ?>
+</table>

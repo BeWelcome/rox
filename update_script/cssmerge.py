f6fff89b4871a2d794792261776592bbb5fd9130
guaka <guaka@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-05-28 12:01:04 +0000

    a bit of work on paths, needs some more, disabling the cssmerge stuff for now
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@4966 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- update_script/cssmerge.py
+++ update_script/cssmerge.py
@@ -8,6 +8,7 @@ here. Right now it's just a convenient place to store the script.
 """
 
 import re
+import os
 
 filename = "bw_yaml.css";
 lines = file(filename).readlines()
@@ -18,9 +19,17 @@ for l in lines:
     if matches and not l.find('/*') > -1:
         print matches.groups()
         import_file = matches.groups()[0]
+        import_dir = os.path.dirname(import_file)
         for import_line in file(import_file).readlines():
-            if import_line.find('@charset "UTF-8"') > 0:
+            if import_line.find('@charset "UTF-8"') > 0: # should become re.sub
+                print "Skip ". import_line
+                pass
+            elif import_line.find('url(') > 0:
+                # TODO: relative paths should be fixed
                 print import_line
+                url_matches = re.search('url\(["\s]*(.*?)[\s"]*\)', import_line)
+                if url_matches:
+                    print url_matches.groups()
             else:
                 out_file.write(import_line)
     else:

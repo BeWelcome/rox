27309b4917d3160c29ba4e625a209b9f988334cc
guaka <guaka@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-05-28 11:18:57 +0000

    script for removing bw_yaml.css imports
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@4963 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ htdocs/styles/YAML/cssmerge.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+Merge CSS files.
+
+This is checked into BW Rox trunk, but not actually executed from
+here. Right now it's just a convenient place to store the script.
+"""
+
+import re
+
+filename = "bw_yaml.css";
+lines = file(filename).readlines()
+out_file = file(filename + ".compact", 'w')
+
+for l in lines:
+    matches = re.search("@import url\((.*)\)", l)
+    if matches and not l.find('/*') > -1:
+        import_file = matches.groups()[0]
+        for import_line in file(import_file).readlines():
+            out_file.write(import_line)
+    else:
+        out_file.write(l)
+
+# at some point we could use cssutils to compress the CSS
+# see http://code.google.com/p/cssutils/
+
+out_file.close()
+

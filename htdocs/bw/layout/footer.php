a2140d406b067519e0242138c59d25bf425fb13a
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-06-02 19:42:56 +0000

    adding basque language
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@5034 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/layout/footer.php
+++ htdocs/bw/layout/footer.php
@@ -67,6 +67,7 @@ DisplayFlag("bg","bg.png","Bulgarian");
 DisplayFlag("br","br.png","Portuguese(bra)");
 DisplayFlag("ge","ge.png","Georgian");
 DisplayFlag("ar","sa.png","Arabic");
+DisplayFlag("basque","basque.png","Basque");
 
 //if ($_SESSION['switchtrans']!='on') echo "<a href=\"",$langurl,"switchtrans=off\"><img border=0 height=10 src=\"images/showtransarray.gif\" alt=\"switch to translation mode\" width=16></a>&nbsp;";
 if (array_key_exists('switchtrans', $_SESSION) and $_SESSION['switchtrans'] == 'on') {

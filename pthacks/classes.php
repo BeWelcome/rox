a15f18a931f58ca8aab6b7d4f780235cc38bb57d
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-06-20 22:55:09 +0000

    #596 needs the pthacks folder.
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5236 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ pthacks/classes.php
@@ -0,0 +1,22 @@
+<?php
+
+
+/**
+ * This class is necessary to cheat on PlatformPT
+ * It allows us to have our own autoload mechanism, instead of using the PT one.
+ */
+class Classes
+{
+    private static $_sim_classes;
+    
+    static function set($sim_classes) {
+        self::$_sim_classes = $sim_classes;
+    }
+    
+    static function get() {
+        return self::$_sim_classes;
+    }
+}
+
+
+?>

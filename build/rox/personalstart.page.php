9615eaa10808bd478b3e88c83cee17fe8645c070
micha <micha@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-06-21 15:19:47 +0000

    update on messages-widget. Make it alpha/production ready: #591
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5249 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/rox/personalstart.page.php
+++ build/rox/personalstart.page.php
@@ -68,14 +68,19 @@ class MailboxWidget_Personalstart extends MailboxWidget_Received
     {
         extract(get_object_vars($message));
         // print_r($message);
-        echo '<a href="bw/member.php?cid='.$senderUsername.'">'.$senderUsername.'</a>: ';
-        echo $Message;
+        echo '<a class="float_right" href="bw/contactmember.php?action=reply&cid='.$senderUsername . '&iMes='.$i_row .'">';
+        echo '<img src="images/icons/icons1616/icon_reply.png"></a> ';
+        echo '<a href="bw/member.php?cid='.$senderUsername.'">'.$senderUsername.': </a> ';
+        if (strlen($Message) >= 50)
+            echo substr($Message, 0, 50).'... ';
+        else 
+            echo $Message;
     }
     
     
     protected function showBetweenListItems($prev_item, $item, $i_row)
     {
-        $time_difference = $item->unixtime_created - $prev_item->unixtime_created;
+        /*$time_difference = $item->unixtime_created - $prev_item->unixtime_created;
         $seconds = $time_difference % 60;
         $time_difference = (int)($time_difference/60);
         $minutes = $time_difference % 60;
@@ -83,7 +88,8 @@ class MailboxWidget_Personalstart extends MailboxWidget_Received
         $hours = $time_difference % 24;
         $time_difference = (int)($time_difference/24);
         $days = $time_difference;
-        echo '<div style="color:#ccc;">'.$days.' days and '.$hours.':'.$minutes.':'.$seconds.' between messages</div>';
+        echo '<div style="color:#ccc;">'.$days.' days and '.$hours.':'.$minutes.':'.$seconds.' between messages</div>';*/
+        echo '<p class="small">'.MOD_layoutbits::ago($item->unixtime_created).'</p>';
     }
 }
 

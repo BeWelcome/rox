032152fe4e99871b57a6189cbca80285d6045183
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-06-21 16:23:16 +0000

    changes to myphotos.php  * #598, trying to fix the redirect loop  * some other things I don't know about. If alpha myphotos breaks, we revert.
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@5256 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/myphotos.php
+++ htdocs/bw/myphotos.php
@@ -27,54 +27,75 @@ require_once "lib/FunctionsLogin.php";
 require_once "layout/myphotos.php";
 require_once "lib/prepare_profile_header.php";
 
-// test if is logged, if not logged and forward to the current page
-if (GetParam("PictForMember","")!="") {
-		$SortPict=GetParam("PictNum",0)	;			  
-		$Photo=LoadRow("select membersphotos.*,Username from membersphotos,members,memberspublicprofiles where members.id=".IdMember(GetParam("PictForMember"))." and members.id=memberspublicprofiles.IdMember and members.id=membersphotos.IdMember and membersphotos.SortOrder=".$SortPict);
 
-		if (!isset($Photo->id)) {
-		   $Photo=LoadRow("select membersphotos.*,Username from membersphotos,members,memberspublicprofiles where members.id=".IdMember("admin")." and members.id=memberspublicprofiles.IdMember and members.id=membersphotos.IdMember and membersphotos.SortOrder=0");
-		}
-		$fpath=$Photo->FilePath;
-//		echo "readlink=",readlink("memberphotos"),"<br />";
-//		$fpath=str_replace("/memberphotos/","/var/www/upload/images/",$Photo->FilePath);
-//		$ff=fopen($fpath, 'rb');
-//		if (!$ff) die ("cant open file ".$fpath);
-//		fpassthru($ff);
-//       fclose($ff);
-		echo $_SYSHCVOL['SiteName'].$fpath;
-		exit(0);
+// test if is logged, if not logged and forward to the current page
+if (GetStrParam("PictForMember","")!="") {
+    $SortPict=GetParam("PictNum",0)	;			  
+    $Photo=LoadRow("SELECT membersphotos.*,Username from membersphotos,members,memberspublicprofiles where members.id=".IdMember(GetStrParam("PictForMember"))." and members.id=memberspublicprofiles.IdMember and members.id=membersphotos.IdMember and membersphotos.SortOrder=".$SortPict);
+    
+    if (!isset($Photo->id)) {
+        $Photo=LoadRow("SELECT membersphotos.*,Username from membersphotos,members,memberspublicprofiles where members.id=".IdMember("admin")." and members.id=memberspublicprofiles.IdMember and members.id=membersphotos.IdMember and membersphotos.SortOrder=0");
+    }
+    $fpath = $Photo->FilePath;
+    echo $_SYSHCVOL['SiteName'].$fpath;
+    exit(0);
 } 
 
 // test if is logged, if not logged and forward to the current page
 // exeption for the people at confirm signup state
-if ((!IsLoggedIn()) and (GetParam("action") != "confirmsignup") and (GetParam("action") != "update")) {
-	$ItsAPendingMember=false ;
-	if (isset ($_SESSION['IdMember'])) { // if there is a IdMember in session (this can because of a memebr in pending state
-	   $m = prepareProfileHeader($_SESSION['IdMember']," and (Status='Pending')"); // pending members can edit their profile
-	   $ItsAPendingMember= ($m->Status=="Pending") ;
-	}
-	if (! $ItsAPendingMember) { // A pending member will be allowed to edit his picture
-	   APP_User::get()->logout();
-	   header("Location: " . $_SERVER['PHP_SELF']);
-	   exit (0);
-	}
+if (
+    !IsLoggedIn() &&
+    GetParam("action") != "confirmsignup" &&
+    GetParam("action") != "update"
+) {
+    $ItsAPendingMember=false ;
+    if (isset ($_SESSION['IdMember'])) {
+        // if there is a IdMember in session (this can because of a memebr in pending state
+        $m = prepareProfileHeader($_SESSION['IdMember']," and (Status='Pending')"); // pending members can edit their profile
+        $ItsAPendingMember = ($m->Status=="Pending") ;
+    }
+    if (! $ItsAPendingMember) { // A pending member will be allowed to edit his picture
+        MustLogIn();
+    }
 }
 
 if (!isset ($_SESSION['IdMember'])) {
-	$errcode = "ErrorMustBeIndentified";
-	DisplayError(ww($errcode));
-	exit (0);
+    $errcode = "ErrorMustBeIndentified";
+    DisplayError(ww($errcode));
+    exit (0);
 }
 
 // Find parameters
 $IdMember = $_SESSION['IdMember'];
-if ((IsAdmin())or(CanTranslate(GetParam("cid", $_SESSION['IdMember'])))) { // admin or CanTranslate can alter other profiles 
-	$IdMember = GetParam("cid", $_SESSION['IdMember']);
+if ((IsAdmin())or(CanTranslate(IdMember(GetStrParam("cid", $_SESSION['IdMember']))))) { // admin or CanTranslate can alter other profiles 
+	$IdMember = IdMember(GetStrParam("cid", $_SESSION['IdMember']));
 }
 
 // manage picture photorank (swithing from one picture to the other)
 $photorank = GetParam("photorank", 0);
+
+// recomputes and updates the databases SortOrder fields
+// and returns the users' rows of memberphotos
+function fix_sort_order() {
+  global $IdMember;
+
+  // First recompute order of pictures
+  $TData = array ();
+  $str = "select * from membersphotos where membersphotos.IdMember=" . $IdMember . " order by SortOrder asc";
+  $qry = sql_query($str);
+  for ($i=0; $rr = mysql_fetch_object($qry); $i ++) { // Fix Sort numbers
+    if ($rr->SortOrder != $i) {
+      $str = "update membersphotos set SortOrder=" . $i . " where id=" . $rr->id;
+      //				echo "str=$str<br />";
+      sql_query($str);
+      $rr->SortOrder = $i;
+    }
+
+    array_push($TData, $rr);
+  }
+
+  return $TData;
+}
 
 switch (GetParam("action")) {
 	case "update" :
@@ -87,17 +108,7 @@ switch (GetParam("action")) {
 	
 	case "moveup" :
 		// First recompute order of pictures
-		$TData = array ();
-		$ii = 0;
-		$str = "select * from membersphotos where membersphotos.IdMember=" . $IdMember . " order by SortOrder asc";
-		$qry = sql_query($str);
-		while ($rr = mysql_fetch_object($qry)) { // Fix Sort numbers
-			array_push($TData, $rr);
-			$str = "update membersphotos set SortOrder=" . $ii . " where id=" . $rr->id . " and IdMember=" . $IdMember;
-			sql_query($str);
-			$ii++;
-		}
-		$max = $ii;
+	        $TData = fix_sort_order();
 		$iPos = GetParam("iPos");
 		if ($iPos > 0) { // if not to the up picture
 			$str = "update membersphotos set SortOrder=" . $TData[$iPos -1]->SortOrder . " where id=" . $TData[$iPos]->id . " and IdMember=" . $IdMember;
@@ -109,22 +120,9 @@ switch (GetParam("action")) {
 		break;
 
 	case "movedown" :
-		// First recompute order of pictures
-		$TData = array ();
-		$ii = 0;
-		$str = "select * from membersphotos where membersphotos.IdMember=" . $IdMember . " order by SortOrder asc";
-		$qry = sql_query($str);
-		while ($rr = mysql_fetch_object($qry)) { // Fix Sort numbers
-			array_push($TData, $rr);
-			$str = "update membersphotos set SortOrder=" . $ii . " where id=" . $rr->id;
-			//				echo "str=$str<br />";
-			sql_query($str);
-			$TData[$ii]->SortOrder = $ii;
-			$ii++;
-		}
-		$max = $ii;
+	        $TData = fix_sort_order();
 		$iPos = GetParam("iPos");
-		if (($iPos +1) < $max) { // if not to the up picture
+		if ($iPos < count($TData) - 1) { // if it is not already the last picture
 			$str = "update membersphotos set SortOrder=" . $TData[$iPos +1]->SortOrder . " where id=" . $TData[$iPos]->id . " and IdMember=" . $IdMember;
 			sql_query($str);
 			$str = "update membersphotos set SortOrder=" . $TData[$iPos]->SortOrder . " where id=" . $TData[$iPos +1]->id . " and IdMember=" . $IdMember;
@@ -137,6 +135,7 @@ switch (GetParam("action")) {
 		//			echo "str=$str<br />";
 		sql_query($str);
 		LogStr("delete picture #" . GetParam("IdPhoto"), "update profile");
+		fix_sort_order();
 
 		break;
 
@@ -147,10 +146,8 @@ switch (GetParam("action")) {
 
 		LogStr("Upload of file <i>" . $_FILES[userfile][name] . "</i> " . $_FILES[userfile][size] . " bytes", "upload photo");
 		$filename = $_FILES[userfile][name];
-		$ext = strtolower(strstr($filename, "."));
+		$ext = strtolower(strrchr($filename, ".")); //everything after last occurrence of .
 
-		//			echo "ext=$ext<br />";
-		//			echo "filename=$filename<br />";
 		// test format of file
 		if (($ext != ".jpg") and ($ext != ".png")) {
 			$errcode = "ErrorBadPictureFormat";
@@ -174,16 +171,9 @@ switch (GetParam("action")) {
 		//			echo "fname=",$fname,"<br />";
 
 		if (@copy($_FILES[userfile][tmp_name], $_SYSHCVOL['IMAGEDIR'] ."/". $fname)) { // try to copy file with its real name
-			$str = "insert into membersphotos(FilePath,IdMember,created,SortOrder,Comment) values('" . "/memberphotos/" . $fname . "'," . $IdMember . ",now(),-1," . InsertInMTrad(GetStrParam("Comment")) . ")";
+			$str = "insert into membersphotos(FilePath,IdMember,created,SortOrder,Comment) values('" . "/memberphotos/" . $fname . "'," . $IdMember . ",now(),-1," . NewInsertInMTrad(GetStrParam("Comment"),"membersphotos.Comment",0) . ")";
 			sql_query($str);
-			$ii=0;
-		    $str = "select * from membersphotos where membersphotos.IdMember=" . $IdMember . " order by SortOrder asc";
-			$qry = sql_query($str);
-			while ($rr = mysql_fetch_object($qry)) { // Fix Sort numbers
-				  $str = "update membersphotos set SortOrder=" . $ii . " where id=" . $rr->id . " and IdMember=" . $IdMember;
-				  sql_query($str);
-				  $ii++;
-			}
+			fix_sort_order();
 		} else {
 			echo "failed to copy " . $_FILES[userfile][tmp_name] . " to " . $_SYSHCVOL['IMAGEDIR'] . $fname;
 		}
@@ -193,7 +183,7 @@ switch (GetParam("action")) {
 
 	case "updatecomment";
 		$rr = LoadRow("select Comment,id from membersphotos where IdMember=" . $IdMember . " and id=" . GetParam("IdPhoto"));
-		ReplaceInMTrad(GetStrParam("Comment"), $rr->Comment, $IdMember);
+		NewReplaceInMTrad(GetStrParam("Comment"),"membersphotos.Comment",$rr->id, $rr->Comment, $IdMember);
 		LogStr("Updating comment for picture #" . $rr->id, "update profile");
 		break;
 

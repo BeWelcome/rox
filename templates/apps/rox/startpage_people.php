5cdce679bb60458f197cd029bdd3aa16abe700c3
globetrotter_tt <globetrotter_tt@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-06-22 17:22:15 +0000

    #561 - showing the pictures less chaotic (or was it wanted like that, micha ? )
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5276 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/apps/rox/startpage_people.php
+++ templates/apps/rox/startpage_people.php
@@ -16,8 +16,8 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, see <http://www.gnu.org/licenses/> or 
-write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, 
+along with this program; if not, see <http://www.gnu.org/licenses/> or
+write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA  02111-1307, USA.
 
 */
@@ -26,23 +26,21 @@ $words = new MOD_words();
 
   <div class="index_row2">
       <div class="info">
-        <div class="floatbox" style="margin-bottom: 10px">
-            <h3><?php  echo $words->get('IndexPeopleThat');?></h3>
+        <h3><?php  echo $words->get('IndexPeopleThat');?></h3>
+        <div class="floatbox">
 
 <?php for ($ii=0;$ii<count($members);$ii++) {
         $m=$members[$ii] ;
 ?>
-        <div class="float_left" style="padding: 12px"> 
-            <p class="floatbox UserpicFloated">
-                <?php echo MOD_layoutbits::PIC_50_50($m->Username,'',$style='float_left framed') ?>
-                <?php echo '<a href="bw/member.php?cid='.$m->Username.'">'.$m->Username.'</a>' ?>
-                <br />
-                <?php echo $m->countryname; ?>
-            </p> 
-        </div>
+            <div class="float_left" style="padding-right: 15px">
+                <p class="center">
+                    <span class="username"><?php echo '<a href="bw/member.php?cid='.$m->Username.'">'.$m->Username.'</a>' ?></span><br />
+                    <?php echo MOD_layoutbits::PIC_50_50($m->Username,'',$style='framed') ?><br />
+                    <span class="small grey"><?php echo $m->countryname; ?></span>
+                </p>
+            </div> <!-- float_left -->
 <?php } ?>
-            
         </div> <!-- floatbox -->
         <p><?=$words->get('IndexPeopleThat_ManyMore','<a href="searchmembers">','</a>')?></p>
       </div> <!-- info index -->
-  </div>
+  </div> <!-- index row2 -->

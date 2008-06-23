b327ee0a3d02d9b437fc1b9dd86b385327a3c007
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-06-23 16:48:51 +0000

    remove messages widget from PersonalStartpage
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5282 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/rox/personalstart.page.php
+++ build/rox/personalstart.page.php
@@ -40,11 +40,11 @@ class PersonalStartpage extends RoxPageView
     protected function column_col3()
     {
         // echo '<h3>Your messages</h3>';
-        
+        /*
         $inbox_widget = new MailboxWidget_Personalstart;
         $inbox_widget->model = new MessagesModel;
         $inbox_widget->items_per_page = 4;
-        
+        */
         // $inbox_widget->render();
         // echo '<a href="bw/mymessages.php">more...</a>';
         
@@ -55,7 +55,7 @@ class PersonalStartpage extends RoxPageView
     }
 }
 
-
+/*
 class MailboxWidget_Personalstart extends MailboxWidget_Received
 {
     protected function showItems()
@@ -80,18 +80,18 @@ class MailboxWidget_Personalstart extends MailboxWidget_Received
     
     protected function showBetweenListItems($prev_item, $item, $i_row)
     {
-        /*$time_difference = $item->unixtime_created - $prev_item->unixtime_created;
-        $seconds = $time_difference % 60;
-        $time_difference = (int)($time_difference/60);
-        $minutes = $time_difference % 60;
-        $time_difference = (int)($time_difference/60);
-        $hours = $time_difference % 24;
-        $time_difference = (int)($time_difference/24);
-        $days = $time_difference;
-        echo '<div style="color:#ccc;">'.$days.' days and '.$hours.':'.$minutes.':'.$seconds.' between messages</div>';*/
+        // $time_difference = $item->unixtime_created - $prev_item->unixtime_created;
+        // $seconds = $time_difference % 60;
+        // $time_difference = (int)($time_difference/60);
+        // $minutes = $time_difference % 60;
+        // $time_difference = (int)($time_difference/60);
+        // $hours = $time_difference % 24;
+        // $time_difference = (int)($time_difference/24);
+        // $days = $time_difference;
+        // echo '<div style="color:#ccc;">'.$days.' days and '.$hours.':'.$minutes.':'.$seconds.' between messages</div>';
         echo '<p class="small">'.MOD_layoutbits::ago($item->unixtime_created).'</p>';
     }
 }
-
+*/
 
 ?>

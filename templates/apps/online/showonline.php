009d0da6e5e4795693d67088861398e7a1def8e8
micha <micha@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-02 09:59:49 +0000

    replacing
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@5340 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/apps/online/showonline.php
+++ templates/apps/online/showonline.php
@@ -28,30 +28,43 @@ if (!isset($vars['errors']) || !is_array($vars['errors'])) {
     $vars['errors'] = array();
 }
 
-$canSeeGuestLastActivity=HasRight("Debug","ShowLastActivity") ;
-
 $words = new MOD_words();
 $styles = array( 'highlight', 'blank' ); // alternating background for table rows
 $iiMax = count($TMembers)
 ?>
 <p><?php echo $words->getFormatted("WeAreTotNumber",$TotMembers); ?></p>
+
 <table class="full">
+<?php if ($TMembers != false) { ?>
     <tr>
         <th><?php echo $words->getFormatted("Username"); ?></th>
         <th><?php echo $words->getFormatted("Location"); ?></th>
         <th><?php echo $words->getFormatted("ProfileSummary"); ?></th>
     </tr>
 <?php
+}
   for ($ii = 0; $ii < $iiMax; $ii++) {
     $m = $TMembers[$ii];
 ?>
     <tr class="<?php echo $styles[$ii%2]; ?>">
         <td align="center"><a class="username" href="bw/member.php?cid=<?php echo $m->Username; ?>"><?php echo $m->Username; ?></a><br />
-            <img class="framed" src="<?php echo MOD_layoutbits::smallUserPic_username($m->Username); ?>" alt="avatar" width="80px" height="80px" />
+            <?php echo MOD_layoutbits::PIC_50_50($m->Username); ?>
         </td>
         <td><?php echo $m->countryname; ?></td>
         <td><?php echo $words->mTrad($m->ProfileSummary); ?></td>
-        <td></td>
+        <td><?php
+                // Deactivated on our servers. Only used for testing locally.
+                /*
+                if (IsAdmin()) {
+                    echo $m->NbSec," sec ";
+                }
+                //  echo $m->ProfileSummary;
+                if (IsAdmin()) {
+                    echo $m->lastactivity;
+                } 
+                */
+            ?>
+        </td>
     </tr>
 <?php
   }
@@ -59,7 +72,9 @@ $iiMax = count($TMembers)
 </table>
 
 <?php
-  if ($canSeeGuestLastActivity) {
+                // Deactivated on our servers. Only used for testing locally.
+                /*
+  if (IsAdmin()) {
      $iiMax = count($TGuests);
 ?>
     <table class="full">
@@ -78,7 +93,8 @@ $iiMax = count($TMembers)
 ?>
     </table>
 <?php
-  } // end of canSeeGuestLastActivity
+  }
+                */
 
   if (!APP_User::login()) {
      echo "<p>",$words->getFormatted("OnlinePrivateProfilesAreNotDisplayed"), "</p>\n";

d3274d0568961762daa99d101b1ee6e02c11d456
guaka <guaka@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-03 19:52:11 +0000

    bw/findpeople*.php -> redir to searchmembers
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5385 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/findpeople.php
+++ htdocs/bw/findpeople.php
@@ -22,32 +22,5 @@ Boston, MA  02111-1307, USA.
 
 */
 require_once "lib/init.php";
-require_once "lib/findpeople.php";
-require_once "layout/findpeople.php";
-
-
-// rebuild the group list
-$str = "select SQL_CACHE * from groups";
-$qry = sql_query($str);
-$TGroup = array ();
-while ($rr = mysql_fetch_object($qry)) {
-	array_push($TGroup, $rr);
-}
-
-$TList=array() ;
-
-switch (GetParam("action")) {
-
-
-	case "" : // initial form displayed
-		 DisplayFindPeopleForm($TGroup,$TList,-1) ;
-		 break ;
-
-	case "Find" : // Compute and Show the results
-	case ww("FindPeopleSubmit") : // Compute and Show the results
-		 $TList=buildresult() ;
-		 DisplayFindPeopleForm($TGroup,$TList,$rCount->cnt) ;
-		 break ;
-}
-
+redirect("searchmembers");
 ?>

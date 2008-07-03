29d7f5af85afc6d6716e3bfd0523da8a1d506d19
guaka <guaka@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-03 19:01:15 +0000

    bw/impressum.php -> redirect
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5375 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/impressum.php
+++ htdocs/bw/impressum.php
@@ -23,41 +23,5 @@ Boston, MA  02111-1307, USA.
 */
 
 require_once "lib/init.php";
-require_once "layout/menus.php";
-
-	global $title;
-	$title = ww('Impressum');
-	require_once "layout/header.php";
-
-	Menu1("", ""); // Displays the top menu
-
-	Menu2("inviteafriend.php", ww('MainPage')); // Displays the second menu
-
-	DisplayHeaderShortUserContent(ww("Impressum")); // Display the header
-
-
-echo "	<div id=\"col3\">\n";
-echo "		<div id=\"col3_content\" class=\"clearfix\" >\n";
-echo "			<div class=\"info\">\n";
-echo "				<p>Anbieterkennzeichung nach &sect;6 Teledienstgesetz (information provider identification according to &sect;6 Teledienstgesetz) :<br />\n";
-echo "				<br />\n";
-echo "				BeVolunteer<br />\n";
-echo "				c/o Jean-Yves Hegron<br />\n";
-echo "				19 rue de Paris<br />\n";
-echo "				35500 Vitre<br />\n";
-echo "				France<br />\n";
-echo "				<br />\n";
-echo "				" . ww("SignupEmail") . ": info@bevolunteer.org<br /> </p>\n";
-echo "				<br />";
-echo "				<p>The layout is based on <a href=\"http://www.yaml.de/\">YAML</a> &copy; 2005-2006 by <a href=\"http://www.highresolution.info\">Dirk Jesse</a></p>";
-echo "			</div> <!-- #info: - end -->\n";
-echo "		</div> <!-- #col3_content: - end -->\n";
-echo "		<div id=\"ie_clearing\">&nbsp;</div>\n";
-echo "	</div> ";
-echo "<!-- #col3: - End -->\n";
-
-	require_once "layout/footer.php";
-
-
-
+redirect("impressum");
 ?>

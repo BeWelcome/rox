cb0f6e4918631c8b6e42abb74edc12905ad44ee6
guaka <guaka@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-03 23:20:31 +0000

    clearer?
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5413 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/misc/pages.php
+++ templates/misc/pages.php
@@ -1,49 +1,25 @@
 <?php
-if (!is_array($pages) || count($pages) == 0)
+  /* what is this supposed to do?   It should probably become a widget. */
+
+if (!is_array($pages) || count($pages) == 0) {
     return false;
+}
 ?>
 <div class="pages">
     <ul>
-        <li>
-<?php
-if ($currentPage != 1) {
-?>
-            <a href="<?=sprintf($request, ($currentPage - 1))?>">&lt;&lt;</a>
-<?php
-} else {
-?>
-                &lt;&lt;
-<?php
-}
-?>
-        </li>
-<?php
-foreach ($pages as $page) {
-	if (!is_array($page)) {
+        <li><?php if ($currentPage != 1) { ?><a href="<?=sprintf($request, ($currentPage - 1))?>">&lt;&lt;</a><?php } else { ?>&lt;&lt;<?php } ?></li>
+        <?php
+        foreach ($pages as $page) {
+            if (!is_array($page)) {
 		echo '<li class="sep">...</li>';
-        continue;
-	}
-    if (!isset($page['current'])) {
-        echo '<li><a href="'.sprintf($request, $page['pageno']).'">';
-        echo $page['pageno'];
-        echo '</a></li>';
-    } else {
-    	echo '<li class="current">'.$page['pageno'].'</li>';
-    }
-}
-?>
-        <li>
-<?php
-if ($currentPage != $maxPage) {
-?>
-            <a href="<?=sprintf($request, ($currentPage + 1))?>">&gt;&gt;</a>
-<?php
-} else {
-?>
-                &gt;&gt;
-<?php
-}
-?>
-        </li>
+                continue;
+            }
+            if (!isset($page['current'])) {
+                echo '<li><a href="'.sprintf($request, $page['pageno']).'">'.$page['pageno'].'</a></li>';
+            } else {
+                echo '<li class="current">'.$page['pageno'].'</li>';
+            }
+          } ?>
+        <li><?php if ($currentPage != $maxPage) { ?><a href="<?=sprintf($request, ($currentPage + 1))?>">&gt;&gt;</a><?php } else {?> &gt;&gt; <?php } ?></li>
     </ul>
 </div>

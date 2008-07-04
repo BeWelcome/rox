b1d1e88023e67ae959330e9dd69e9ac0fa279f3e
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-04 20:30:38 +0000

    removing test change
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@5434 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/lib/tbinit.php
+++ htdocs/bw/lib/tbinit.php
@@ -29,9 +29,6 @@ if (file_exists($script_base.'/base.xml')) {
     $script_base = str_replace('\\', '/', $script_base).'/';
 } elseif (file_exists($script_base.'/../base.xml')) {
     $script_base = str_replace('\\', '/', realpath($script_base.'/..')).'/';
-} elseif (file_exists('.././base.xml')) { // Jean-Yves experiment for mail bot
-    $script_base = "../." ;
-    $script_base = str_replace('\\', '/', realpath($script_base.'/..')).'/';
 } else {
     die('in tbinit.php File "base.xml" not found! \$script_base/base.xml=['.$script_base."base.xml] Current directory=".getcwd()."\n" );
 }

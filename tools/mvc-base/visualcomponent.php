c6e50e2cec93cd840914c0acfd8f622f19d73571
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-05 20:38:00 +0000

    new fancy mechanic for template inclusion without creating a method
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5472 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- tools/mvc-base/visualcomponent.php
+++ tools/mvc-base/visualcomponent.php
@@ -18,13 +18,63 @@ class VisualComponent extends RoxComponentBase
         return true;
     }
     
-    function __call($methodname, $args)
-    {
-        echo '
-            Please implement<br>
-            '.get_class($this).'<br>
-            ::'.$methodname.'()
-        '; 
+    function __call($methodname, $args) {
+        $parameters_method = 'parameters_'.$methodname;
+        if (method_exists($this, $parameters_method)) {
+            $parameters = call_user_func_array($parameters_method, $args);
+            extract($parameters);
+        }
+        $classname = get_class($this);
+        do {
+            $template_prefix = $this->getTemplatePrefix($classname);
+            if (is_file($templatefile = $template_prefix.$methodname.'.php')) {
+                if (is_file($helperfile = $template_prefix.$methodname.'.helper.php')) {
+                    include $helperfile;
+                }
+                include $templatefile;
+                break;
+            }
+        } while ($classname = get_parent_class($classname));
+        
+        if (is_file($templatefile)) {
+            return true;
+        } else {
+            echo '<pre style="background:white; border:1px solid grey; color:black; overflow:auto; font-size:12px; padding:3px; font-weight:normal; line-height:normal; letter-spacing:0px;">
+<span style="color:grey">Please implement</span>
+'.get_class($this).'
+::'.$methodname.'()
+
+<span style="color:grey">or create a file</span>
+"'.$templatefile.'"
+
+</pre>';
+            return false;
+        }
+    }
+    
+    protected function getTemplatePrefix($classname = false) {
+        if (!$classname) $classname = get_class($this);
+        return SCRIPT_BASE.'build/'.$this->getAppname($classname).'/templates/'.$classname.'.';
+    }
+    
+    protected function getAppname($classname) {
+        $file = ClassLoader::whereIsClass(get_class($this));
+        if (!is_string($file)) {
+            return 'yummydummy';
+        } else if (!is_file($file)) {
+            return 'yummygummy';
+        } else {
+            // using a heuristic to guess which is the correct application directory name
+            $subdirs = split('[/\\]', dirname($file));
+            $subdir_count = count($subdirs);
+            if ($subdirs[$subdir_count-2] != 'build') {
+                array_pop($subdirs);
+            }
+            if ($subdirs[$subdir_count-2] != 'build') {
+                return 'yummytummy';
+            }
+            return end($subdirs);
+        }
     }
 }
 

2584568e85d4e703e93f55eb6d572659792558c7
guaka <guaka@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-05 20:31:46 +0000

    removing index.php
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5470 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/bw/bw.ctrl.php
+++ build/bw/bw.ctrl.php
@@ -24,6 +24,7 @@ class BwController extends RoxControllerBase
                                "cities.php" => "home",
                                "aboutus.php" => "about",
                                "main.php" => "main",
+                               "index.php" => "index",
                                "missions.php" => "about/missions",
                                "login.php" => "login#login-widget",
                                );

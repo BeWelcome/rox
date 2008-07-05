d9fe835b7afa9472fdb3b040e314301ef07e2164
guaka <guaka@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-05 18:04:47 +0000

    fix translation links in bw footer
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5456 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/apps/rox/footer.php
+++ templates/apps/rox/footer.php
@@ -44,18 +44,19 @@ if (MOD_right::get()->hasRight("Words", PVars::get()->lang)) {
     echo "<a href='bw/admin/adminwords.php?showtransarray=1&amp;pagetotranslate=" . $pagetotranslate . "' target='_blank'><img height='11px' width='16px' src='bw/images/switchtrans.gif' alt='go to current translation list for " . $_SERVER['PHP_SELF'] . "' title='go to current translation list for " . $_SERVER['PHP_SELF'] . "' /></a>\n";
 
     $request_string = implode('/',PVars::__get('request'));
+    $rox_tr = PVars::getObj("env")->baseuri . "rox/tr_mode";
     switch ($words->getTrMode()) {
         case 'translate':
             ?>
-    <a href="rox/tr_mode/browse/<?php echo $request_string ?>">browse</a>
+    <a href="<?=$rox_tr?>/browse/<?php echo $request_string ?>">browse</a>
     <strong>translate</strong>
-    <a href="rox/tr_mode/edit/<?php echo $request_string ?>">edit</a>
+    <a href="<?=$rox_tr?>/edit/<?php echo $request_string ?>">edit</a>
             <?php
             break;
         case 'edit':
             ?>
-    <a href="rox/tr_mode/browse/<?php echo $request_string ?>">browse</a>
-    <a href="rox/tr_mode/translate/<?php echo $request_string ?>">translate</a>
+    <a href="<?=$rox_tr?>/browse/<?php echo $request_string ?>">browse</a>
+    <a href="<?=$rox_tr?>/translate/<?php echo $request_string ?>">translate</a>
     <strong>edit</strong>
             <?php
             break;
@@ -63,8 +64,8 @@ if (MOD_right::get()->hasRight("Words", PVars::get()->lang)) {
         case 'browse':
             ?>
     <strong>browse</strong>
-    <a href="rox/tr_mode/translate/<?php echo $request_string ?>">translate</a>
-    <a href="rox/tr_mode/edit/<?php echo $request_string ?>">edit</a>
+    <a href="<?=$rox_tr?>/translate/<?php echo $request_string ?>">translate</a>
+    <a href="<?=$rox_tr?>/edit/<?php echo $request_string ?>">edit</a>
             <?php
             break;
     }

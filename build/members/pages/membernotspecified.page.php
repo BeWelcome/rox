3a5af89b69181c8c9c50b76d0b09da6ea544e972
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-06 13:38:33 +0000

    moving pages to subdirectory
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5480 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ build/members/pages/membernotspecified.page.php
@@ -0,0 +1,32 @@
+<?php
+
+
+class MembersMembernotspecifiedPage extends RoxPageView
+{
+    protected function teaserHeadline()
+    {
+        echo '
+        Members - No username specified.';
+    }
+    
+    protected function leftSidebar()
+    {
+        echo '
+        unclickable black';
+    }
+    
+    protected function column_col3()
+    {
+        echo '
+        <p>We have so many members!!!<br> Which one do you want?</p>
+        <ul>
+        <li><a href="">The dangerous rabbit.</a></li>
+        <li><a href="">The old lady.</a></li>
+        <li><a href="">The bus driver.</a></li>
+        <li><a href="">Santa Claus.</a></li>
+        </ul>';
+    }
+}
+
+
+?>
---
+++ build/members/pages/mustlogin.page.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class MembersMustloginPage extends RoxPageView
+{
+    protected function column_col3()
+    {
+        $loginWidget = $this->layoutkit->createWidget('LoginFormWidget');
+        $loginWidget->render();
+    }
+}
+
+
+?>

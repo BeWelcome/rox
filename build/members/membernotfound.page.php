aed67f38c062966a14f37fde99ec6d5d05230d1b
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-06 13:36:03 +0000

    page to show if member not found
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5479 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ build/members/membernotfound.page.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class MembersMembernotfoundPage extends MemberPage
+{
+    protected function column_col3()
+    {
+        echo "
+        Did not find this member. We are not amused.";
+    }
+}
+
+
+?>

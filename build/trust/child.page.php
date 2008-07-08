9acc4bbd3eec63e2df7e74b6cf87cf3152fbd9ce
philipp <philipp@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-08 14:27:45 +0000

    stub for a trust application to calculate trust values for members (based on hellouniverse)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5505 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ build/trust/child.page.php
@@ -0,0 +1,8 @@
+<?php
+
+
+class HellouniverseChildPage extends HellouniverseSimplePage
+{}
+
+
+?>
---
+++ build/trust/trust.model.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Trust model
+ * 
+ * @package trust
+ * @author Philipp (philipp)
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License (GPL)
+ * @version $Id$
+ */
+class TrustModel extends PAppModel
+{
+}
+
+
+
+
+?>

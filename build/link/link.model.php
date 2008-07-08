ae9c0854adfb4faedfe524080d25333174238b66
philipp <philipp@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-08 14:18:23 +0000

    stub for a link application to handle connections between members (based on hellouniverse)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5504 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ build/link/link.model.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Link model
+ * 
+ * @package link
+ * @author Philipp (philipp)
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License (GPL)
+ * @version $Id$
+ */
+class LinkModel extends PAppModel
+{
+}
+
+
+
+
+?>

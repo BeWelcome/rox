6fc3c6fd01c87c390b71a7e4dc04481aee7fdfa2
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-10 03:01:13 +0000

    i18n for login widget on production. using brute copy, merging sucks
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@5533 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/login/loginform.widget.php
+++ build/login/loginform.widget.php
@@ -8,49 +8,63 @@ class LoginFormWidget extends RoxWidget
     public function render()
     {
         $layoutkit = $this->layoutkit;
-        $words = $layoutkit->getWords();
+        $words = $this->words;
+        $ww = $this->ww;
         $formkit = $layoutkit->formkit;
         
         $callback_tag = $formkit->setPostCallback('LoginController', 'loginCallback');
         $mem_recovery_tag = $formkit->setMemForRecovery();
         
-        $req = implode('/', PRequest::get()->request);
+        $url = PVars::getObj('env')->baseuri . implode('/', PRequest::get()->request);
+        if (!empty($_SERVER['QUERY_STRING'])) {
+            $url .= '?'.$_SERVER['QUERY_STRING'];
+        }
         
-        if (APP_User::loggedIn()) {
+        $logged_in = APP_User::loggedIn();
+        
+        if ($logged_in) {
             // already logged in
             // why show this form?
             ?>
             <div style="border:1px solid grey;">
-              <p>You are already logged in!</p>
-              <p><a href="logout">logout</a></p>
-            </div>
-            <?php 
-        } else {
-            ?>
-            <div class="info" id="login-widget">
-            <h3>Login</h3>
-            <form method="post" action="<?=$req ?>">
-              <?=$callback_tag ?>
-              <?=$mem_recovery_tag ?>
-              
-              <p>
-                <label for="login-u">Username</label>
-                <input type="text" id="login-u" name="u" />
-              </p>
-              <p>
-                <label for="login-p">Password</label>
-                <input type="password" id="login-p" name="p" />
-              </p>
-              <p>
-                <input type="submit" value="Login" class="button"/>
-              </p>
-              <p>Forgot your password? Get a new one <a href="lostpassword.php">here</a> !</p>
-              <h3>Sign up now</h3>
-              <p><a href="signup.php">Create a profile</a> without obligations. Joining and using the network is free!</p>
-            </form>
-            <script type="text/javascript">document.getElementById("login-u").focus();</script>
+              <p><?=$ww->LoginformAlreadyLogged($_SESSION['Username']) ?></p>
+              <p><a href="logout"><?=$ww->Logout ?></a></p>
             </div>
             <?php
+            // for translators, we want links for all the translations,
+            // because otherwise they will never see the login form words.
+            // we make this happen with a little trick :)
+            ob_start();
+            $ww = $this->wwsilent;
+        }
+        ?>
+        <div class="info" id="login-widget">
+        <h3><?=$ww->Login ?></h3>
+        <form method="post" action="<?=$url ?>">
+          <?=$callback_tag ?>
+          <?=$mem_recovery_tag ?>
+          
+          <p>
+            <label for="login-u"><?=$ww->Username ?></label>
+            <input type="text" id="login-u" name="u" />
+          </p>
+          <p>
+            <label for="login-p"><?=$ww->Password ?></label>
+            <input type="password" id="login-p" name="p" />
+          </p>
+          <p>
+            <input type="submit" value="Login" class="button"/>
+          </p>
+          <p><?=$ww->LoginformForgetPassword('<a href="bw/lostpassword.php">', '</a>') ?></p>
+          <h3><?=$ww->SignupNow ?></h3>
+          <p><?=$ww->IndexPageWord17('<a href="bw/signup.php">', '</a>') ?></p>
+        </form>
+        <script type="text/javascript">document.getElementById("login-u").focus();</script>
+        </div>
+        <?php
+        if ($logged_in) {
+            ob_end_clean();
+            echo $words->flushBuffer();
         }
     }
 }

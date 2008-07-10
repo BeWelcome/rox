0b5b59ed5fdc431da130ad8ac5ca4aecab3cd7a4
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-10 21:25:40 +0000

    Preparing translatable versions
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5539 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/about/templates/bod.php
+++ build/about/templates/bod.php
@@ -50,56 +50,55 @@ $words = new MOD_words();
 	
 <h3><?php echo $words->get('BoD_TheBoardMembers'); ?></h3>
 <ul class="floatbox">
-    <li class="userpicbox_big float_left"><h4><a href="user/thorgal67"><img src="http://www.bewelcome.org/memberphotos/thumbs/thorgal67_1187438461.square.80x80.jpg" class="framed float_left" style="height:70px; width: 70px;">Frank</a><br /><span class="small">Executive</span></h4>
+    <li class="userpicbox_big float_left"><h4><a href="user/thorgal67"><img src="http://www.bewelcome.org/memberphotos/thumbs/thorgal67_1187438461.square.80x80.jpg" class="framed float_left" style="height:70px; width: 70px;">Frank</a><br /><span class="small"><?php echo $words->get('BoD_RoleOfThorgal67'); ?></span></h4>
     <p>
     <a href="javascript:;" id="infocol1" onclick="new Effect.BlindUp('info1', {duration: .3}); $('infocol1', 'infoexp1').each(function(el){ Effect.toggle(el, 'appear', {duration: 0}) })" onfocus="blur();" style="Display: none;"><< hide info</a> <a id="infoexp1" href="javascript:;" onclick="new Effect.BlindDown('info1', {duration: .3}); $('infocol1', 'infoexp1').each(function(el){ Effect.toggle(el, 'appear', {duration: 0}) })" onfocus="blur();"><?php echo $words->get('ShowInfoLink'); ?> >></a>
     </p>
     <div id="info1" style="Display: none;">
         <p>
-        Frank is a translator (English/Italian), graduate in journalism and attending photography workshops. He started travelling from a very young age, first in Europe, later in North and Central America and South-East Asia. Frank firmly believes that meeting locals is what makes a trip so worthwhile and he likes to give back as a volunteer.
+		 <?php echo $words->get('BoD_TheBoardThorgal67_1'); ?>
         </p>
         <p>
-        He is also one of the founders of BeWelcome and BeVolunteer and feels that committing his work and enthusiasm to the project is much more rewarding than just donating money. Frank has followed the development of hospitality exchange networks from the very beginning and has extensively contributed to them as a host, guest and local volunteer.
+		 <?php echo $words->get('BoD_TheBoardThorgal67_2'); ?>
         </p>
     </div>
     </li>
     <li class="userpicbox_big float_left">
         <h4>
-        <a href="user/pietshah"><img src="http://www.bewelcome.org/memberphotos/thumbs/pietshah_1169955258.square.80x80.jpg" class="framed float_left" style="height:70px; width: 70px;">Pierre-Charles</a><br /><span class="small">Secretary</span>
+        <a href="user/pietshah"><img src="http://www.bewelcome.org/memberphotos/thumbs/pietshah_1169955258.square.80x80.jpg" class="framed float_left" style="height:70px; width: 70px;">Pierre-Charles</a><br /><span class="small"><?php echo $words->get('BoD_RoleOfPietshah'); ?></span>
         </h4>
         <p>
         <a href="javascript:;" id="infocol3" onclick="new Effect.BlindUp('info3', {duration: .3}); $('infocol3', 'infoexp3').each(function(el){ Effect.toggle(el, 'appear', {duration: 0}) })" onfocus="blur();" style="Display: none;"><< <?php echo $words->get('HideInfoLink'); ?></a> <a id="infoexp3" href="javascript:;" onclick="new Effect.BlindDown('info3', {duration: .3}); $('infocol3', 'infoexp3').each(function(el){ Effect.toggle(el, 'appear', {duration: 0}) })" onfocus="blur();"><?php echo $words->get('ShowInfoLink'); ?> >></a>
         </p>
         <div id="info3" style="Display: none;">
-            <p>
-            Pierre-Charles is studying architecture and currently lives in Caracas, Venezuela. He sees life like a journey: dreaming, reading a book, inventing new ways of working, meeting new people, receiving travelers at home, running the roads hitch-hiking or quietly living in a far isolated village.</p>
-
-<p>He traveled mostly in Europe (motherland) and South America (heart land), and will probably continue to jump from one to the other for a while, between investigations about an integral architecture and social revolutions.</p>
-
-<p>Pierre-Charles participated in the Hospitality movements from their beginning, and soon enjoyed traveling during his free time and receiving foreigners at home, making it a way of life. Co-founder of BeWelcome, he helps in various areas and mostly tries to maintain and develop the structure of BeVolunteer.
-            </p>
+        <p>
+		 <?php echo $words->get('BoD_TheBoardPietshah_1'); ?>
+        </p>
+        <p>
+		 <?php echo $words->get('BoD_TheBoardPietshah_2'); ?>
+        </p>
         </div>
     </li>
     <li class="userpicbox_big float_left">
         <h4>
-        <a href="user/claudiaab"><img src="http://www.bewelcome.org/memberphotos/thumbs/claudiaab_1169485927.square.80x80.jpg" class="framed float_left" style="height:70px; width: 70px;">Claudia</a><br /><span class="small">Vice Secretary</span>
+        <a href="user/claudiaab"><img src="http://www.bewelcome.org/memberphotos/thumbs/claudiaab_1169485927.square.80x80.jpg" class="framed float_left" style="height:70px; width: 70px;">Claudia</a><br /><span class="small"><?php echo $words->get('BoD_RoleOfClaudiaab'); ?></span>
         </h4>
         <p>
         <a href="javascript:;" id="infocol4" onclick="new Effect.BlindUp('info4', {duration: .3}); $('infocol4', 'infoexp4').each(function(el){ Effect.toggle(el, 'appear', {duration: 0}) })" onfocus="blur();" style="Display: none;"><< <?php echo $words->get('HideInfoLink'); ?></a> <a id="infoexp4" href="javascript:;" onclick="new Effect.BlindDown('info4', {duration: .3}); $('infocol4', 'infoexp4').each(function(el){ Effect.toggle(el, 'appear', {duration: 0}) })" onfocus="blur();"><?php echo $words->get('ShowInfoLink'); ?> >></a>
         </p>
         <div id="info4" style="Display: none;">
-            <p>
-Claudia is teaching English and Latin at secondary school in Aachen, Germany. She has always loved travelling and immersing herself into foreign cultures. Even with her parents they preferred to get in contact with locals so the idea of hospitality exchange came very natural to her. Claudia travelled a lot in Europe, but also to the U.S., Vietnam and India. Together with her boyfriend Frank she is now planning a trip to South Africa.   
-            </p>
-            <p>
-            As volunteer for BeWelcome Claudia enjoys to contribute to the project as it enables others to benefit from hospitality exchange, too. Moreover, she got to know wonderful people through volunteering - some of whom have become valuable friends.
-            </p>
+        <p>
+		 <?php echo $words->get('BoD_TheBoardClaudiaab_1'); ?>
+        </p>
+        <p>
+		 <?php echo $words->get('BoD_TheBoardClaudiaab_2'); ?>
+        </p>
         </div>    
     </li>
     <li class="userpicbox_big float_left">
     <div>
         <h4>
-            <a href="user/tgoorden"><img src="https://www.bewelcome.org/bw/memberphotos/thumbs/tgoorden_1179743953.square.80x80.jpg" class="framed float_left" style="height:70px; width: 70px;">Thomas</a><br /><span class="small">Board Member</span>
+            <a href="user/tgoorden"><img src="https://www.bewelcome.org/bw/memberphotos/thumbs/tgoorden_1185739855.square.80x80.jpg" class="framed float_left" style="height:70px; width: 70px;">Thomas</a><br /><span class="small"><?php echo $words->get('BoD_RoleOfTgoorden'); ?></span>
         </h4>
     <p>
     <a href="javascript:;" id="infocol5" onclick="new Effect.BlindUp('info5', {duration: .3}); $('infocol5', 'infoexp5').each(function(el){ Effect.toggle(el, 'appear', {duration: 0}) })" onfocus="blur();" style="Display: none;"><< <?php echo $words->get('HideInfoLink'); ?></a> <a id="infoexp5" href="javascript:;" onclick="new Effect.BlindDown('info5', {duration: .3}); $('infocol5', 'infoexp5').each(function(el){ Effect.toggle(el, 'appear', {duration: 0}) })" onfocus="blur();"><?php echo $words->get('ShowInfoLink'); ?> >></a>
@@ -107,13 +106,17 @@ Claudia is teaching English and Latin at secondary school in Aachen, Germany. Sh
     </div>
     <div id="info5" style="Display: none;">
         <p>
+		 <?php echo $words->get('BoD_TheBoardTgoorden_1'); ?>
+        </p>
+        <p>
+		 <?php echo $words->get('BoD_TheBoardTgoorden_2'); ?>
         </p>
     </div>    
     </li>
     <li class="userpicbox_big float_left">
     <div>
         <h4>
-            <a href="user/jeanyves"><img src="http://www.bewelcome.org/memberphotos/thumbs/jeanyves_1167996233.square.100x100.jpg" class="framed float_left" style="height:70px; width: 70px;">JeanYves</a><br /><span class="small">Treasurer</span>
+            <a href="user/jeanyves"><img src="http://www.bewelcome.org/memberphotos/thumbs/jeanyves_1167996233.square.100x100.jpg" class="framed float_left" style="height:70px; width: 70px;">JeanYves</a><br /><span class="small"><?php echo $words->get('BoD_RoleOfJeanYves'); ?></span>
         </h4>
     <p>
     <a href="javascript:;" id="infocol5" onclick="new Effect.BlindUp('info5', {duration: .3}); $('infocol5', 'infoexp5').each(function(el){ Effect.toggle(el, 'appear', {duration: 0}) })" onfocus="blur();" style="Display: none;"><< <?php echo $words->get('HideInfoLink'); ?></a> <a id="infoexp5" href="javascript:;" onclick="new Effect.BlindDown('info5', {duration: .3}); $('infocol5', 'infoexp5').each(function(el){ Effect.toggle(el, 'appear', {duration: 0}) })" onfocus="blur();"><?php echo $words->get('ShowInfoLink'); ?> >></a>
@@ -121,10 +124,10 @@ Claudia is teaching English and Latin at secondary school in Aachen, Germany. Sh
     </div>
     <div id="info5" style="Display: none;">
         <p>
-            JeanYves, husband and father of three children, graduated in electricity and automation and is specialist in database design. He currently works in a sensor analysis laboratory, previous work areas are as diverse as nuclear and navigation systems, airports, pharmacy and military. The most "exotic" destinations JeanYves has visited so far include Japan, Azerbaijan and Southern Arabia.
+		 <?php echo $words->get('BoD_TheBoardJeanyves_1'); ?>
         </p>
         <p>
-            As co-founder and key programmer of the project BeWelcome JeanYves loves to contribute to hospitality and cultural sharing and he highly appreciates the unexpected ideas and new concepts you encounter when looking across personal borders. 
+		 <?php echo $words->get('BoD_TheBoardJeanyves_2'); ?>
         </p>
     </div>    
     </li>

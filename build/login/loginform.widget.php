78dd16d069e17f8b0b2ce2df2992f86080144e1d
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-10 01:05:16 +0000

    show translations links for login form, even when logged in.
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5525 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/login/loginform.widget.php
+++ build/login/loginform.widget.php
@@ -20,7 +20,9 @@ class LoginFormWidget extends RoxWidget
             $url .= '?'.$_SERVER['QUERY_STRING'];
         }
         
-        if (APP_User::loggedIn()) {
+        $logged_in = APP_User::loggedIn();
+        
+        if ($logged_in) {
             // already logged in
             // why show this form?
             ?>
@@ -28,33 +30,41 @@ class LoginFormWidget extends RoxWidget
               <p><?=$ww->LoginformAlreadyLogged ?></p>
               <p><a href="logout"><?=$ww->Logout ?></a></p>
             </div>
-            <?php 
-        } else {
-            ?>
-            <div class="info" id="login-widget">
-            <h3><?=$ww->Login ?></h3>
-            <form method="post" action="<?=$url ?>">
-              <?=$callback_tag ?>
-              <?=$mem_recovery_tag ?>
-              
-              <p>
-                <label for="login-u"><?=$ww->Username ?></label>
-                <input type="text" id="login-u" name="u" />
-              </p>
-              <p>
-                <label for="login-p"><?=$ww->Password ?></label>
-                <input type="password" id="login-p" name="p" />
-              </p>
-              <p>
-                <input type="submit" value="Login" class="button"/>
-              </p>
-              <p><?=$ww->LoginformForgetPassword('<a href="lostpassword.php">', '</a>') ?> = Did you forget your password? Get a new one <a href="lostpassword.php">here</a>!</p>
-              <h3><?=$ww->SignupNow ?></h3>
-              <p><a href="signup.php"><?=$ww->IndexPageWord17 ?></p>
-            </form>
-            <script type="text/javascript">document.getElementById("login-u").focus();</script>
-            </div>
             <?php
+            // for translators, we want links for all the translations,
+            // because otherwise they will never see the login form words.
+            // we make this happen with a little trick :)
+            ob_start();
+            $ww = $this->wwsilent;
+        }
+        ?>
+        <div class="info" id="login-widget">
+        <h3><?=$ww->Login ?></h3>
+        <form method="post" action="<?=$url ?>">
+          <?=$callback_tag ?>
+          <?=$mem_recovery_tag ?>
+          
+          <p>
+            <label for="login-u"><?=$ww->Username ?></label>
+            <input type="text" id="login-u" name="u" />
+          </p>
+          <p>
+            <label for="login-p"><?=$ww->Password ?></label>
+            <input type="password" id="login-p" name="p" />
+          </p>
+          <p>
+            <input type="submit" value="Login" class="button"/>
+          </p>
+          <p><?=$ww->LoginformForgetPassword('<a href="lostpassword.php">', '</a>') ?> = Did you forget your password? Get a new one <a href="lostpassword.php">here</a>!</p>
+          <h3><?=$ww->SignupNow ?></h3>
+          <p><a href="signup.php"><?=$ww->IndexPageWord17 ?></p>
+        </form>
+        <script type="text/javascript">document.getElementById("login-u").focus();</script>
+        </div>
+        <?php
+        if ($logged_in) {
+            ob_end_clean();
+            $words->flushBuffer();
         }
     }
 }

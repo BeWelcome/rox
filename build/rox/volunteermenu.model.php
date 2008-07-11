95df325075ae534edaca434ed0e68edd9b22f4ae
lemon-head <lemon-head@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-11 12:41:02 +0000

    fix for counters for accepter etc. plus a bit of cleanup.
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5544 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/rox/volunteermenu.model.php
+++ build/rox/volunteermenu.model.php
@@ -16,18 +16,26 @@ class VolunteermenuModel extends PAppModel
     {
         $R = MOD_right::get();
         $AccepterScope = mysql_real_escape_string($R->RightScope('Accepter'));
-        if (($AccepterScope == "\"All\"") or ($AccepterScope == "All") or ($AccepterScope == "'All'")) {
-           $InScope = " /* All countries */";
+        if (count(explode('All', $AccepterScope)) > 1) {
+            $InScope = " /* All countries */";
         } else {
-          $InScope = "AND countries.id IN (" . $AccepterScope . ")";
+            $InScope = "AND countries.id IN ($AccepterScope)";
         }
-        $query = '
-SELECT SQL_CACHE COUNT(*) AS cnt
-FROM members, countries, cities
-WHERE members.Status=\'Pending\'
-AND cities.id=members.IdCity
-AND countries.id=cities.IdCountry ' . $InScope;
-        $result = $this->dao->query($query);
+        $result = $this->dao->query(
+            "
+SELECT SQL_CACHE
+    COUNT(*) AS cnt
+FROM
+    members,
+    countries,
+    cities
+WHERE
+    members.Status = 'Pending'  AND
+    cities.id = members.IdCity  AND
+    countries.id = cities.IdCountry
+    $InScope
+            "
+        );
         $record = $result->fetch(PDB::FETCH_OBJ);
         return $record->cnt;
     }
@@ -43,18 +51,26 @@ AND countries.id=cities.IdCountry ' . $InScope;
         // FIXME: this if clause indicates a problem, doesn't it???
         // But you need database access to solve it.
         $AccepterScope = mysql_real_escape_string($AccepterScope);
-        if (($AccepterScope == "\"All\"") or ($AccepterScope == "All") or ($AccepterScope == "'All'")) {
-           $InScope = " /* All countries */";
+        if (count(explode('All', $AccepterScope)) > 1) {
+            $InScope = " /* All countries */";
         } else {
-          $InScope = "AND countries.id IN (" . $AccepterScope . ")";
+            $InScope = "AND countries.id IN (" . $AccepterScope . ")";
         }
-        $query = '
-SELECT SQL_CACHE COUNT(*) AS cnt
-FROM pendingmandatory, countries, cities
-WHERE pendingmandatory.Status=\'Pending\'
-AND cities.id=pendingmandatory.IdCity
-AND countries.id=cities.IdCountry ' . $InScope;
-        $result = $this->dao->query($query);
+        $result = $this->dao->query(
+            "
+SELECT SQL_CACHE
+    COUNT(*) AS cnt
+FROM
+    pendingmandatory,
+    countries,
+    cities
+WHERE
+    pendingmandatory.Status = 'Pending'   AND
+    cities.id = pendingmandatory.IdCity   AND
+    countries.id = cities.IdCountry
+    $InScope
+            "
+        );
         $record = $result->fetch(PDB::FETCH_OBJ);
         return $record->cnt;
     }
@@ -73,25 +89,28 @@ AND countries.id=cities.IdCountry ' . $InScope;
         $where="" ;
         if ($GroupScope!='"All"') {
             $tt=explode(",",$GroupScope) ;
-            $where="(" ;
-            foreach ($tt as $Scope) {
-                if ($where!="(") {
-                    $where.="," ;
-                }
-                $where=$where.$Scope;
-            }
-            $where=" AND `groups`.`Name` IN " .$where.")" ;
+            $where=" AND groups.Name IN (".implode(', ', $tt).")" ;
         }
-        $query = 'SELECT SQL_CACHE COUNT(*) AS cnt FROM `membersgroups`,`groups` where `membersgroups`.`Status`="WantToBeIn" and `groups`.`id`=`membersgroups`.`IdGroup`'.$where ;
-//   die($query) ;
-        $result = $this->dao->query($query);
+        $result = $this->dao->query(
+            "
+SELECT SQL_CACHE
+    COUNT(*) AS cnt
+FROM
+    membersgroups,
+    groups
+WHERE
+    membersgroups.Status = 'WantToBeIn'  AND
+    groups.id = membersgroups.IdGroup
+    $where
+            "
+        );
         $record = $result->fetch(PDB::FETCH_OBJ);
         if (isset($record->cnt)) {
-                     return $record->cnt;
-                }
-                else {
-                     return(0) ;
-                }
+            return $record->cnt;
+        }
+        else {
+            return(0) ;
+        }
     } // end of getNumberPersonsToAcceptedInGroup
 
     /**
@@ -100,12 +119,17 @@ AND countries.id=cities.IdCountry ' . $InScope;
      */
     public function getNumberMessagesToBeChecked()
     {
-        $query = '
-SELECT COUNT(*) AS cnt
-FROM messages
-WHERE Status=\'ToCheck\'
-AND messages.WhenFirstRead=\'0000-00-00 00:00:00\'';
-        $result = $this->dao->query($query);
+        $result = $this->dao->query(
+            "
+SELECT
+    COUNT(*) AS cnt
+FROM
+    messages
+WHERE
+    Status = 'ToCheck'  AND
+    messages.WhenFirstRead = '0000-00-00 00:00:00'
+            "
+        );
         $record = $result->fetch(PDB::FETCH_OBJ);
         return $record->cnt;
     }
@@ -116,14 +140,21 @@ AND messages.WhenFirstRead=\'0000-00-00 00:00:00\'';
      */
     public function getNumberSpamToBeChecked()
     {
-        $query = '
-SELECT COUNT(*) AS cnt
-FROM messages, members AS mSender, members AS mReceiver
-WHERE mSender.id=IdSender
-AND messages.SpamInfo=\'SpamSayMember\'
-AND mReceiver.id=IdReceiver
-AND mSender.Status=\'Active\'';
-        $result = $this->dao->query($query);
+        $result = $this->dao->query(
+            "
+SELECT
+    COUNT(*) AS cnt
+FROM
+    messages,
+    members AS mSender,
+    members AS mReceiver
+WHERE
+    mSender.id        = IdSender         AND
+    messages.SpamInfo = 'SpamSayMember'  AND
+    mReceiver.id      = IdReceiver       AND
+    mSender.Status    = 'Active'
+            "
+        );
         $record = $result->fetch(PDB::FETCH_OBJ);
         return $record->cnt;
     }

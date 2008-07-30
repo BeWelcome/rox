a8a672e4f867b3be8fac6636be0b4f2001317cec
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-07-30 16:57:47 +0000

    // Adding a reference language (the first one used, default to english) for each title thread
    This is to be able to see if the current forums_threads.title can be used instead of searching in forum_trads
    
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5615 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/lib/dbupdate.php
+++ htdocs/bw/lib/dbupdate.php
@@ -437,6 +437,11 @@ CHANGE `id` `id` INT( 10 ) UNSIGNED NOT NULL AUTO_INCREMENT
 // Adding a reference language (the first one used, default to english) for each post
 	$updates[] = "ALTER TABLE `forums_posts` 	ADD `IdFirstLanguageUsed` INT NOT NULL DEFAULT '0'
 	COMMENT 'This is the id of the first language used for this post, which allows to consider the \"reference language\" for it'";
+
+// Adding a reference language (the first one used, default to english) for each post
+	$updates[] = "ALTER TABLE `forums_threads` 	ADD `IdFirstLanguageUsed` INT NOT NULL DEFAULT '0'
+	COMMENT 'This is the id of the first language used for this thread title, which allows to consider the \"reference language\" for this forum title'";
+
     $res = mysql_query( "SELECT version FROM dbversion" );
     
     if (empty($res)) {

b1dff8c594c4ca8ab7a3f6a92a154e347365abc0
guaka <guaka@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-08-09 18:06:36 +0000

    more permissions
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5697 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- setup/fix-permission.sh
+++ setup/fix-permission.sh
@@ -1,1 +1,4 @@
+# a bit crude, but good and simple enough for local installations
+chmod a+w htdocs/bw/memberphotos/
+chmod a+w data
 for i in build modules tools pthacks; do touch pthacks/autoload.cache.ini; chmod a+w pthacks/autoload.cache.ini; done

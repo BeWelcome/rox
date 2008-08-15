ec8341c5a4a086acfe8f05c0deab28d887d74180
micha <micha@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-08-15 20:30:30 +0000

    quickfix to move 5748 to production //reverting in trunk
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5756 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/rox/publicstart.page.php
+++ build/rox/publicstart.page.php
@@ -7,79 +7,89 @@ class PublicStartpage extends RoxPageView
     {
         require TEMPLATE_DIR . 'shared/roxpage/body_index.php';
     }
-    
+
     protected function getStylesheets() {
         $stylesheets[] = 'styles/minimal_index.css';
         return $stylesheets;
     }
-    
+
     protected function getStylesheetPatches()
     {
         $stylesheet_patches[] = 'styles/YAML/patches/patch_2col_left_seo.css';
         return $stylesheet_patches;
     }
-    
+
+    protected function includeScriptfiles()
+    {
+        $stylesheets = parent::includeScriptfiles();
+    }
+
     protected function teaserContent() {
-        require TEMPLATE_DIR.'apps/rox/teaser.php';
-    }
-    
-    protected function getPageTitle() {
-        $words = new MOD_words();
-        if (isset($_SESSION['Username'])) {
-            return $words->getFormatted('WelcomeUsername',$_SESSION['Username']);
-        } else {
-            // this should not happen actually!
-            return $words->getFormatted('WelcomeGuest');
-        }
-    }
-    
-    protected function column_col1()
-    {
-        // should be invisible anyway
-        echo 'left column';
-    }
-    
-    protected function column_col2()
-    {
         $request = PRequest::get()->request;
         if(!isset($request[0])) {
             $redirect_url = false;
+            require TEMPLATE_DIR.'apps/rox/teaser.php';
         } else if ($request[0]=='login') {
             $redirect_url = implode('/', array_slice($request, 1));
             if (!empty($_SERVER['QUERY_STRING'])) {
                 $redirect_url .= '?'.$_SERVER['QUERY_STRING'];
             }
+            $login_widget = $this->createWidget('LoginFormWidget');
+            $login_widget->render();
+        } else {
+            require TEMPLATE_DIR.'apps/rox/teaser.php';
+        }
+    }
+
+    protected function getPageTitle() {
+        if (isset($_SESSION['Username'])) {
+            return 'Welcome, '.$_SESSION['Username'].'!';
+        } else {
+            return 'Be Welcome!';
+        }
+    }
+
+    protected function column_col1()
+    {
+        // should be invisible anyway
+        echo 'left column';
+    }
+
+    protected function column_col2()
+    {
+
+    }
+
+    protected function column_col3() {
+        $members = $this->model->getMembersStartpage(2);
+        $request = PRequest::get()->request;
+        if(!isset($request[0])) {
+            $redirect_url = false;
+            require 'templates/startpage.php';
+        } else if ($request[0]=='login') {
         } else {
             $redirect_url = false;
+            require 'templates/startpage.php';
         }
-        
-        /*
-        $User = new UserController;
-        $User->displayLoginForm($redirect_url);
-        */
-        
-        $login_widget = $this->createWidget('LoginFormWidget');
-        $login_widget->render();
     }
-    
-    protected function column_col3() {
-        $flagList = $this->_buildFlagList();
-        $members = $this->model->getMembersStartpage(7);
-        require TEMPLATE_DIR.'apps/rox/startpage.php';
-        require TEMPLATE_DIR.'apps/rox/startpage_people.php';
-    }
-    
+
     protected function getColumnNames ()
     {
-        return array('col2', 'col3');
+        return array('col3');
     }
-    
-    protected function quicksearch()
-    {
-        PPostHandler::setCallback('quicksearch_callbackId', 'SearchmembersController', 'index');
+
+    protected function topnav() {
+        parent::topnav();
+        require 'templates/_languageselector.helper.php';
+        $languageSelectorDropDown = _languageSelectorDropDown();
+        $words = new MOD_words();
+        echo '<div class="grey">'.$languageSelectorDropDown.'</div>';
     }
-    
-    
+    protected function quicksearch() {
+        $login_widget = $this->createWidget('LoginFormWidget');
+        $login_widget->render(true);
+    }
+
 }
 
 

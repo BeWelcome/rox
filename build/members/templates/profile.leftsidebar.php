2029d2d24d959bd9996384a78b2b117db649c9d2
guaka <guaka@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-08-19 01:34:55 +0000

    improving profile.leftsidebar.php
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5805 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/members/templates/profile.leftsidebar.php
+++ build/members/templates/profile.leftsidebar.php
@@ -1,17 +1,17 @@
 <h3><?=$words->get('Actions')?></h3>
 <ul class="linklist" >
   <li class="icon contactmember16" >
-    <A href="contactmember.php?cid=<?=$member->id?>" ><?=$words->get('ContactMember');?></a>
+    <A href="bw/contactmember.php?cid=<?=$member->id?>" ><?=$words->get('ContactMember');?></a>
   </li>
   <li class="icon addcomment16" >
-    <A href="addcomments.php?cid=<?=$member->id?>" ><?=$words->get('addcomments');?></a>
+    <A href="bw/addcomments.php?cid=<?=$member->id?>" ><?=$words->get('addcomments');?></a>
   </li>
   <li class="icon forumpost16" >
-    <A href="http://localhost/bw-trunk-new/htdocs/forums/member/<?=$member->Username?>" ><?=$words->get('ViewForumPosts', 7);?>View Forum Posts</a>
+    <A href="forums/member/<?=$member->Username?>" ><?=$words->get('ViewForumPosts', 7);?></a>
   </li>
 </ul>
-<h3><?=$words->get('MyRelations');?></H3>
-<ul class="linklist" >
+<h3><?=$words->get('MyRelations');?></h3>
+<ul class="linklist">
 	<?php 
 		$relations = $member->relations;
 		foreach ($relations as $rel) {
@@ -26,12 +26,4 @@
     <br />
   </li>
   <?php } ?>
-  <!--<li>
-    <A href="http://localhost/bw-trunk-new/htdocs/bw/member.php?cid=henri"  title="See profile henri" >
-      <IMG class="framed"  src="http://localhost/bw-trunk-new/htdocs/bw/"  height="50px"  width="50px"  alt="Profile" >
-    </a>
-    <br />
-    <A href="http://localhost/bw-trunk-new/htdocs/bw/member.php?cid=henri" >henri</a>
-    <br />
-  </li>-->
 </ul>

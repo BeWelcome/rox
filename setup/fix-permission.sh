ac7b8e196a0e7fad3e261abfb007149f28fbe017
guaka <guaka@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-08-19 00:57:29 +0000

    also fix autoload.cache.ini for extensions
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5801 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- setup/fix-permission.sh
+++ setup/fix-permission.sh
@@ -1,7 +1,7 @@
 # a bit crude, but good and simple enough for local installations
 chmod -R a+w htdocs/bw/memberphotos/
 chmod -R a+w data
-for i in build modules tools pthacks; do 
+for i in build modules tools pthacks extensions extensions/minimal extensions/newskin extensions/otherskin; do 
 	touch $i/autoload.cache.ini $i/alias.cache.ini;
 	chmod a+w $i/autoload.cache.ini $i/alias.cache.ini;
 done

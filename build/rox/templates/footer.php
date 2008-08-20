83a90081dae9a6b57db006255e851cb08d107080
guaka <guaka@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-08-20 22:13:26 +0000

    removing quotes, moving translator block to helper
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5857 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/rox/templates/footer.php
+++ build/rox/templates/footer.php
@@ -27,45 +27,9 @@ require_once "footer.helper.php";
 <div id="footer">
   <p class="center"><?php echo $words->get('ToChangeLanguageClickFlag'); ?></p>
   <div id="flags" class="center">
-  <?php 
-echo $languageSelector;
+    <?php echo $languageSelector; ?>
 
-if (MOD_right::get()->hasRight("Words", PVars::get()->lang)) {
-    $pagetotranslate = $_SERVER['PHP_SELF'];
-    if ($pagetotranslate { 0 } == "/") {
-        // funky array stuff
-        $pagetotranslate { 0 } = "_";
-    }
-    echo "<a href='bw/admin/adminwords.php?showtransarray=1&amp;pagetotranslate=" . $pagetotranslate . "' target='_blank'><img height='11px' width='16px' src='bw/images/switchtrans.gif' alt='go to current translation list for " . $_SERVER['PHP_SELF'] . "' title='go to current translation list for " . $_SERVER['PHP_SELF'] . "' /></a>\n";
-
-    $request_string = implode('/',PVars::__get('request'));
-    $rox_tr = PVars::getObj("env")->baseuri . "rox/tr_mode";
-    switch ($words->getTrMode()) {
-        case 'translate':
-            ?>
-    <a href="<?=$rox_tr?>/browse/<?php echo $request_string ?>">browse</a>
-    <strong>translate</strong>
-    <a href="<?=$rox_tr?>/edit/<?php echo $request_string ?>">edit</a>
-            <?php
-            break;
-        case 'edit':
-            ?>
-    <a href="<?=$rox_tr?>/browse/<?php echo $request_string ?>">browse</a>
-    <a href="<?=$rox_tr?>/translate/<?php echo $request_string ?>">translate</a>
-    <strong>edit</strong>
-            <?php
-            break;
-        default:
-        case 'browse':
-            ?>
-    <strong>browse</strong>
-    <a href="<?=$rox_tr?>/translate/<?php echo $request_string ?>">translate</a>
-    <a href="<?=$rox_tr?>/edit/<?php echo $request_string ?>">edit</a>
-            <?php
-            break;
-    }
-}
-?>
+    <?php echo $translator_block; ?>
   </div>
 
   <p class="center">
@@ -78,7 +42,7 @@ if (MOD_right::get()->hasRight("Words", PVars::get()->lang)) {
   </p>
   <div class="floatbox">
   <p class="float_left">
-     &copy; 2007-2008 <strong>BeWelcome</strong> - "<?php echo $words->get('TheHospitalityNetwork'); ?>"
+     &copy; 2007-2008 <strong>BeWelcome</strong> - <?php echo $words->get('TheHospitalityNetwork'); ?>
   </p>
   <p class="float_right">
      running on <a href="http://bevolunteer.org/trac">BW Rox <?php echo $versionInfo; ?></a>

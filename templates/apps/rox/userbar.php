584ac090049d2338dd53d40953e7d2d92f44e865
micha <micha@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-08-21 01:38:12 +0000

    adding blog entry link to userbar.php
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@5874 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/apps/rox/userbar.php
+++ templates/apps/rox/userbar.php
@@ -11,11 +11,11 @@ if ($ToggleDonateBar) {
 } 
 ?>
 
-<h3>Actions</h3>
+<h3><?=$words->get('Actions')?></h3>
 <ul class="linklist">
 <li><a href="invite"><?php echo $words->get('InviteAFriendPage') ?></a></li>
 <li><a href="bw/editmyprofile.php"><?php echo $words->get('EditMyProfile') ?></a></li>
-<li><a href="bw/mycontacts.php"><?php echo $words->get('DisplayAllContacts') ?></a></li>
-<li><a href="volunteer"><?php echo $words->get('VolunteerpageLink') ?></a></li>
+<li><img src="images/icons/page_white_star.png" alt="" /> <a href="blog/create"><?=$words->get('Blog_CreateEntry')?></a></li>
+<li><a href="bw/mycontacts.php"><?=$words->get('DisplayAllContacts')?></a></li>
+<li><a href="volunteer"><?=$words->get('VolunteerpageLink')?></a></li>
 </ul>
-		   

0b0799c0abf9c847917e1b51268c82274a06bbae
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-10-26 09:42:12 +0000

    Counting the pending and the NeedMore in the whoisonline
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@6286 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- modules/user/lib/user.lib.php
+++ modules/user/lib/user.lib.php
@@ -352,7 +352,7 @@ FROM
     online
 WHERE
     online.updated > DATE_SUB( NOW(), INTERVAL $interval minute )  AND
-    online.Status = 'Active'
+    (online.Status = 'Active' or online.Status = 'Pending' online.Status = 'NeedMore')
             "
         );
         $record = $result->fetch(PDB::FETCH_OBJ);

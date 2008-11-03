f0e5d540bf1c3f394d4dbd84b514a6c664b8be63
micha <micha@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-11-03 15:17:02 +0000

    Added setlocationPage (will be merged to updatemandatory once it's ready) in members-app. connected to #475,#315. /commit 2 of 2
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@6383 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ build/members/templates/updatemandatory.column_col3.helper.php
@@ -0,0 +1,66 @@
+<?php
+
+// TODO: Move to other spot and share with SignupPage
+    function buildBirthYearOptions($selYear = 0) {
+
+        $old_member_born = date('Y') - 100;
+        $young_member_born = date('Y') - SignupModel::YOUNGEST_MEMBER;
+
+        $out = '';
+        for ($i=$young_member_born; $i>$old_member_born; $i--) {
+            if (!empty($selYear) && $selYear == $i) {
+                $out .= "<option value=\"$i\" selected=\"selected\">$i</option>";
+            } else {
+                $out .= "<option value=\"$i\">$i</option>";
+            }
+        }
+        return $out;
+    }
+
+
+$callback_tag = $this->layoutkit->formkit->setPostCallback('MembersController', 'updateMandatoryCallback');
+$member = $this->member;
+$m->firstname = MOD_crypt::MemberReadCrypted($member->FirstName,'');
+$m->secondname = MOD_crypt::MemberReadCrypted($member->SecondName,'');
+$m->lastname = MOD_crypt::MemberReadCrypted($member->LastName,'');
+$m->geonameid = $member->IdCity;
+$m->street = MOD_crypt::MemberReadCrypted($member->street);
+$m->housenumber = MOD_crypt::MemberReadCrypted($member->HouseNumber);
+$m->zip = MOD_crypt::MemberReadCrypted($member->Zip);
+$m->birthday = date("d",strtotime($member->BirthDate));
+$m->birthmonth = date("m",strtotime($member->BirthDate));
+$selYear = date("Y",strtotime($member->BirthDate));
+$birthYearOptions = buildBirthYearOptions($selYear);
+$m->gender = $member->Gender;
+
+echo 'HALLO'.$member->street;
+var_dump($member->address);
+// values from previous form submit
+if (!$mem_redirect = $this->layoutkit->formkit->getMemFromRedirect()) {
+    // this is a fresh form
+    foreach ($m as $key => $value) {
+        $vars[$key] = $value;
+    }
+    $Geo = new GeoModel;
+    if (isset($vars['geonameid']) && !isset($vars['geonamename']))
+        $vars['geonamename'] = $Geo->getDataById($vars['geonameid'])->name;
+        $vars['geonamecountry'] = '';
+    if (!isset($vars['errors']))
+        $vars['errors'] = array();
+} else {
+    $vars = $mem_redirect->post;
+}
+
+$lang = $this->model->get_profile_language();
+$profile_language = $lang->id;
+$profile_language_code = $lang->ShortCode;
+$words = $this->getWords();
+
+$messengers = $member->messengers();
+$website = $member->WebSite;
+
+$groups = $member->get_group_memberships();
+
+var_dump($member);
+
+?>

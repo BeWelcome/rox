df6dc1a6292c8ec893d3b7b5ab1e00e5115cdf50
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-11-07 21:05:46 +0000

    disabling quickly linkpath_render
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@6425 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/layout/profilepage_header.php
+++ htdocs/bw/layout/profilepage_header.php
@@ -295,7 +295,7 @@ echo "      </div>\n";
 // var_dump($m->id);
 // var_dump(strcmp($m->id,$SESSION["IdMember"]));
 	if ((isset($_SESSION["IdMember"]))and(strcmp($m->id,$_SESSION["IdMember"]) != 0)) {
-		linkpath_render($_SESSION["IdMember"],$m->id,'profile-picture-linkpath');
+//		linkpath_render($_SESSION["IdMember"],$m->id,'profile-picture-linkpath');
     }
 
 

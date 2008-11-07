f057d2bbf2e75f80d52a006bcdaab11d3adad52c
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-11-07 13:14:16 +0000

    Trying to fix ticket #687
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@6412 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/layout/profilepage_header.php
+++ htdocs/bw/layout/profilepage_header.php
@@ -22,6 +22,15 @@ Boston, MA  02111-1307, USA.
 
 */
 
+/**
+* get information about the connection between members
+*
+**/
+	function linkpath_render($fromID,$toID,$cssID) {
+        $linkwidget = new LinkSinglePictureLinkpathWidget();
+        $linkwidget->render($fromID,$toID,$cssID);
+	}
+
 
 
 function DisplayProfilePageHeader( $m,$profilewarning="" )

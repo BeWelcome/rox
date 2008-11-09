7494ce314c9eaf4685d4b1b8df802a1d10fc0b68
micha <micha@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-11-09 21:37:39 +0000

    taking back some changes in trips /pass through (#678)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@6465 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/trip/templates/singletrip.php
+++ build/trip/templates/singletrip.php
@@ -23,12 +23,11 @@ if (isset($trip_data[$trip->trip_id])) {
         if ($blog->blog_start) {
             ?>
             <h2 class="trip_date"><?php echo date("M d", strtotime($blog->blog_start)) ?><br />
-            <span style="font-size: 14px;"><?php echo date("Y", strtotime($blog->blog_start)) ?></span>
-            </h2>
+            <span style="font-size: 14px;"><?php echo date("Y", strtotime($blog->blog_start)) ?></span></h2>
             <!--<div class="calendar calendar-icon-<?php echo date("m", strtotime($blog->blog_start)) ?>">
               <div class="calendar-day"><?php echo date("j", strtotime($blog->blog_start)) ?></div>
             </div> -->
-<?php
+            <?php
 		}
 ?>
 <!-- End of contents for left subtemplate -->
@@ -40,126 +39,21 @@ if (isset($trip_data[$trip->trip_id])) {
       <!-- Contents for right subtemplate -->
 <?php
         echo '<h3 class="borderless">';
+        echo '<a href="blog/'.$trip->handle.'/'.$blogid.'">'.$blog->blog_title.'</a><br />';
 		if ($blog->name) {
-			echo '<span id="b-geo'.$blogid.'" style="font-size: 14px;">'.$blog->name.'</span>';
-            if ($isOwnTrip) {
-            ?> <a href="#" id="b-geo-edit<?=$blogid?>" class="button">Set location</a> 
-            <?php } ?>
-            <a href="#" onclick="javascript: map_<?=$trip->trip_id?>.setCenter(new GLatLng(<?=$blog->latitude?>, <?=$blog->longitude?>), 8); return false;"><img src="styles/YAML/images/iconsfam/map.png" alt="Trip Map & Details"> </a><br /><?php
-        }
-        echo '<a href="blog/'.$trip->handle.'/'.$blogid.'" id="b-title'.$blogid.'">'.$blog->blog_title.'</a><br />';
+			echo '<span style="font-size: 14px;">'.$blog->name.'</span>';
+		}
         echo '</h3>';
 		if ($blog->blog_text) {
-			if (!$isOwnTrip && strlen($blog->blog_text) > 400) {
+			if (strlen($blog->blog_text) > 400) {
 				$blogtext = substr($blog->blog_text, 0, 400);
-				$blogtext_add = '...<br /><a href="blog/'.$trip->handle.'/'.$blogid.'">'.$words->get('ReadMore').'...</a>';
+				$blogtext .= '...<br /><a href="blog/'.$trip->handle.'/'.$blogid.'">'.$words->get('ReadMore').'...</a>';
 			} else {
 				$blogtext = $blog->blog_text;
 			}
-			echo '<div id="b-text'.$blogid.'">'.$blogtext.'</div>';
-			echo isset($blogtext_add) ? '<p id="id="b-text'.$blogid.'">'.$blogtext.'</p>' : '';
-		} elseif ($isOwnTrip) {
-            echo '<div id="b-text'.$blogid.'">click here to add text!</div>';
-        }
-        if ($isOwnTrip) {
-            echo '<p><a href="trip/'.$trip->trip_id.'/subedit/'.$blogid.'" class="button">Edit</a></p>';
-        }
+			echo '<p>'.$blogtext.'</p>';
+		}
 ?>
-<?php
-	if ($isOwnTrip) {
-?>
-
-<script type='text/javascript' src='script/lightview.js'></script>
-<script type="text/javascript">
-// TESTING LIGHTVIEW
-// document.observe('dom:loaded', function() {
-  // Lightview custom event for the demos, see the documentation for more examples.
-  // $('b-geo-edit<?=$blogid?>').observe('lightview:opened', function(event) {
-    // new Effect.Pulsate($('lightview').down('.lv_Caption'), { pulses: 3 });
-  // });
-
-  // Ajax Form Demo
-  // $('b-geo-edit<?=$blogid?>').observe('click', function(event) {
-    // event.stop();
-    // $('geoajaxform').observe('submit', submitGeoForm);
-	// Lightview.show({
-	  // href: '#geo-entry',
-	  // rel: 'inline',
-	  // title: '<?=$words->get('label_search_location')?>',
-	  // options: {
-	    // autosize: true,
-	  // }
-	// });
-  // });
-  
-  // });
-
-
-    function setGeo(item,geonameid) {
-        var add = '?item='+ item +'&geoid=' + geonameid;
-        new Ajax.Request('blog/ajax/post/'+add,
-          {
-            method:'get',
-            onSuccess: function(transport){
-              var response = transport.responseText || "no response text";
-              alert("Success! \n\n" + response);
-            },
-            onFailure: function(){ alert('Something went wrong...') }
-          })
-        $('b-geo'+item).hide();
-        }
-    function setMap(geonameid,a,b,c,d,e,f,g,h) {
-        }
-    new Ajax.InPlaceEditor('b-geo<?=$blogid?>', 'blog/ajax/post/', {
-            onComplete: function(form, value) {
-                    Lightview.show({
-                        href: 'geo/suggestLocation/'+ value,
-                        rel: 'ajax',
-                        options: {
-                          title: 'results',
-                          menubar: false,
-                          topclose: true,
-                          autosize: false,
-                          width: 600,
-                          height: 300,
-                          ajax: {
-                            parameters: form.serialize // the parameters from the form
-                          }
-                        }
-                      })
-                // return '?item=<?=$blogid?>&geoid=' + decodeURIComponent(value)
-                // return '?s=' + decodeURIComponent(value)
-            },
-            externalControl: 'b-title-edit',
-            formClassName: 'inplaceeditor-form-big',
-            cols: '25',
-            ajaxOptions: {method: 'get'},
-            savingText: 'Searching...',
-            externalControl: 'b-geo-edit<?=$blogid?>',
-            externalControlOnly: true
-        })
-
-    new Ajax.InPlaceEditor('b-title<?=$blogid?>', 'blog/ajax/post/', {
-            callback: function(form, value) {
-                return '?item=<?=$blogid?>&title=' + decodeURIComponent(value)
-            },
-            externalControl: 'b-title-edit',
-            formClassName: 'inplaceeditor-form-big',
-            cols: '25',
-            ajaxOptions: {method: 'get'}
-        })
-
-    new Ajax.InPlaceEditor('b-text<?=$blogid?>', 'blog/ajax/post/', {
-            callback: function(form, value) {
-                return '?item=<?=$blogid?>&text=' + decodeURIComponent(value)
-            },
-            externalControl: 'b-text-edit',
-            rows: '5',
-            cols: '55',
-            ajaxOptions: {method: 'get'}
-        })
-</script>
-<?php } ?>
 <!-- End of contents for right subtemplate -->
     </div>
   </div>
@@ -169,37 +63,14 @@ if (isset($trip_data[$trip->trip_id])) {
 			
 	}
 	echo '</ul>';
+
+
 ?>
 
 <?php
 	if ($isOwnTrip) {
 ?>
 <script type="text/javascript">
-
-  
-function submitGeoForm(event) {
-  // block default form submit
-  event.stop();
-  
-  params = Form.serialize('geoajaxform');
-  search = params['search'].value;
-  
-  Lightview.show({
-    href: 'geo/suggestLocation/'+ search,
-    rel: 'ajax',
-    options: {
-      title: 'results',
-	  menubar: false,
-	  topclose: true,
-	  autosize: false,
-	  width: 600,
-	  height: 300,
-      ajax: {
-        parameters: params // the parameters from the form
-      }
-    }
-  });
-}
 
 Sortable.create('triplist', {
 	onUpdate:function(){
@@ -241,10 +112,3 @@ else {
 <?php
     }
 ?>
-<div id="geo-entry">
-    <form id="geoajaxform">
-        <input type="text" id="geo-search" name="search"  />
-        <input type="submit" id="geosubmit" value="<?=$words->get('label_search_location')?>" class="button" />
-    </form>
-</div>
-<link rel="stylesheet" href="styles/lightview.css" type="text/css"/>   

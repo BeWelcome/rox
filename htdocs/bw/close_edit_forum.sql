d97fb290a0a5b4e48a4af793aca630b0c57df10b
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-11-16 13:22:43 +0000

    Adding a sql script which will be called by a cron to disable the forum editing for post older than 30 minutes and already replied
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@6522 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ htdocs/bw/close_edit_forum.sql
@@ -0,0 +1,15 @@
+/*
+This script is run each 30 minutes by a cron and results in closing edit possibility for the post which have been already repiled to
+*/
+
+
+update  `forums_posts` , forums_threads
+set OwnerCanStillEdit='No'
+WHERE forums_threads.id = forums_posts.id
+AND last_postid != forums_posts.id
+AND `OwnerCanStillEdit` = 'Yes' and create_time<date_sub(now(),interval 30 minute) and  create_time>date_sub(now(),interval 90 minute)
+  
+/* proceed only post in the last hour */
+;
+
+insert into `BW_ARCH`.`logs`(`IdMember`,`Str`,`Type`,`created`,`IpAddress`) values(1,'Disabling Edit for forums post older than 20 minutes which have been replied','cron_task',now(),2130706433) ;

01774300a2f82d044037cb91d041f8cd205ba135
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2008-11-17 12:47:09 +0000

    fix for tag #949 (the new topic should apply to current selected tag)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@6541 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/forums/templates/userbar.php
+++ build/forums/templates/userbar.php
@@ -4,7 +4,16 @@ $words = new MOD_words();
 
            <h3><?php echo $words->getFormatted('Actions'); ?></h3>
            <ul class="linklist">
-	        <li class="icon fam_commentadd"><a href="forums/new"><?php echo $words->getBuffered('ForumNewTopic'); ?></a><?php echo $words->flushBuffer(); ?></li>
+					 <?php 
+//	        echo "<li class=\"icon fam_commentadd\"><a href=\"forums/new\"" ;
+        $request = PRequest::get()->request;
+        $uri = implode('/', $request);
+        $uri = rtrim($uri, '/').'/';
+
+	        echo "<li class=\"icon fam_commentadd\"><a href=\"",$uri,"new\"" ;
+					echo ">" ;
+					echo $words->getBuffered('ForumNewTopic'); 
+					?></a><?php echo $words->flushBuffer(); ?></li>
 	        <li><a href="forums/rules"><?php echo $words->get('ForumRulesShort'); ?></a></li>
 	        <li><a href="http://www.bevolunteer.org/wiki/forumdoc"><?php echo $words->get('ForumLinkToDoc'); ?></a></li>
 	        <?php  if (isset($_SESSION["IdMember"])) echo "<li><a href=\"forums/subscriptions\">",$words->get('forum_YourSubscription'),"</a></li>"; ?>

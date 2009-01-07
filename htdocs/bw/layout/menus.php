48a58dd081af323d16377234ad60cb52bd6cdae1
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-01-07 10:58:53 +0000

    Adding some extra parameters (comments) in query to figure which page created sql error when yahoo tries to  index the pages
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@6672 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/layout/menus.php
+++ htdocs/bw/layout/menus.php
@@ -578,7 +578,7 @@ function DisplayHeaderIndexPage($TitleTopContent = "") {
 			echo "		<div class=\"subcolumns\">\n"; 
 			// Display the last created members with a picture
 			if (!isset($mlastpublic)) {
-			   $m=$mlastpublic = mysql_fetch_object(mysql_query("select SQL_CACHE members.*,cities.Name as cityname,IdRegion,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,memberspublicprofiles,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' and memberspublicprofiles.IdMember=members.id GROUP BY members.id order by members.LastLogin desc limit 1")); 
+			   $m=$mlastpublic = mysql_fetch_object(mysql_query("select SQL_CACHE members.*,cities.Name as cityname,IdRegion,countries.Name as countryname,membersphotos.FilePath as photo,membersphotos.Comment from cities,countries,memberspublicprofiles,members left join membersphotos on membersphotos.IdMember=members.id and membersphotos.SortOrder=0 where countries.id=cities.IdCountry and cities.id=members.IdCity and status='Active' and memberspublicprofiles.IdMember=members.id GROUP BY members.id order by members.LastLogin desc limit 1 /* search laspublic in menus.php */")); 
 			}
 			else {
 				 $m=$mlastpublic ;

f972341c8e67cf36fa89b15fff9903943d538fd6
micha <micha@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-01-13 14:02:41 +0000

    hotfix for shouts and gallery
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@6727 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/gallery/gallery.view.php
+++ build/gallery/gallery.view.php
@@ -106,10 +106,10 @@ class GalleryView extends PAppView {
     }
     public function latestGallery($statement, $userHandle = false, $type = 'gallery') 
     {
-        $request = PRequest::get()->request;
         require 'templates/latestgallery.php';
         $shoutsCtrl = new ShoutsController;
-        $shoutsCtrl->shoutsList('trip', $request[3]);
+        $request = PRequest::get()->request;
+        $shoutsCtrl->shoutsList('gallery', $request[3]);
     }
     public function allGalleries($galleries) 
     {

441b35cf0715620c5cb7ade1e10fdf5afc592917
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-01-16 13:45:01 +0000

    Identifying the object IdLoggedMembers
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@6748 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- templates/shared/roxpage/topnav.php
+++ templates/shared/roxpage/topnav.php
@@ -1,5 +1,5 @@
 <ul>
-  <li><img src="styles/YAML/images/icon_grey_online.png" alt="onlinemembers" /> <a href="online"><?php echo $words->getBuffered('NbMembersOnline', $who_is_online_count); ?></a><?php echo $words->flushBuffer(); ?></li>
+  <li><img src="styles/YAML/images/icon_grey_online.png" alt="onlinemembers" /> <a href="online" id="IdLoggedMembers"><?php echo $words->getBuffered('NbMembersOnline', $who_is_online_count); ?></a><?php echo $words->flushBuffer(); ?></li>
   <?php if ($logged_in) { ?>
   <li><img src="styles/YAML/images/icon_grey_mail.png" alt="mymessages"/><a href="bw/mymessages.php"><?php echo $words->getBuffered('Mymessages'); ?></a><?php echo $words->flushBuffer(); ?></li>
   <li><img src="styles/YAML/images/icon_grey_pref.png" alt="mypreferences"/><a href="bw/mypreferences.php"><?php echo $words->getBuffered('MyPreferences'); ?></a><?php echo $words->flushBuffer(); ?></li>

d9fdb88fe43ceb02c7bba3bf53c4e8653545afc3
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-01-19 19:41:24 +0000

    trying to restore the forum search bar
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@6785 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/forums/templates/teaser.php
+++ build/forums/templates/teaser.php
@@ -35,5 +35,17 @@ $words = new MOD_words();
     }
 ?>
             </select>
+				<!-- Google froum search bar -->
+        <p><?php echo $words->getFormatted('ForumSearch'); ?></p>
+        <form action="forums/search" id="cse-search-box">
+            <div>
+                <input type="hidden" name="cx" value="003793464580395137050:n7s_x10-itw" />
+                <input type="hidden" name="cof" value="FORID:9" />
+                <input type="hidden" name="ie" value="UTF-8" />
+                <input type="text" name="q" size="31" />
+                <input type="submit" name="sa" value="Search" />
+            </div>
+        </form>
+        <script type="text/javascript" src="http://www.google.com/coop/cse/brand?form=cse-search-box&lang=en"></script>
     </div> <!-- float_right -->
 </div> <!-- teaser -->

c8e4dcf5af5ddf14e2ffc48adab79744f888a297
globetrotter_tt <globetrotter_tt@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-01-26 20:17:31 +0000

    #486 (grox-groups): textarea would be more appropriate for comments than input field
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@6914 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/groups/groupmembersettings.page.php
+++ build/groups/groupmembersettings.page.php
@@ -13,8 +13,8 @@ class GroupMemberSettingsPage extends GroupsBasePage
         $redirected = $formkit->mem_from_redirect;
         if (is_object($redirected))
         {
-            $resultmsg = (($redirected->result) ? "<p>{$words->get('GroupMemberSettingsUpdated')}</p>" : "<p>{$words->get('GroupMemberSettingsNotUpdated')}</p>");
-            $problemmsg = (($redirected->problems) ? "<p>{$words->get('GroupMemberSettingsProblems')}</p>" : '');
+            $resultmsg = (($redirected->result) ? "<p class=\"note\">{$words->get('GroupMemberSettingsUpdated')}</p>" : "<p class=\"note\">{$words->get('GroupMemberSettingsNotUpdated')}</p>");
+            $problemmsg = (($redirected->problems) ? "<p class=\"error\">{$words->get('GroupMemberSettingsProblems')}</p>" : '');
         }
 
         if (!APP_user::isBWLoggedIn('NeedMore,Pending'))
@@ -33,16 +33,17 @@ class GroupMemberSettingsPage extends GroupsBasePage
         <?= $callbacktag; ?>
             <input type='hidden' name='member_id' value='<?= $this->member->id ;?>' />
             <input type='hidden' name='group_id' value='<?= $membershipinfo->IdGroup ;?>' />
-            <label for="comment">Membership comment</label>
-            <input id="comment" type="text" value="<?= (($membershipinfo->Comment != '' ) ? htmlspecialchars($words->mTrad($membershipinfo->Comment)) : '' ); ?>" name="membershipinfo_comment" /><br />
-            <label for="acceptgroupmail">Accept mail from the group</label>
-            <span id="acceptgroupmail">
-                <label for="no_option">No</label>
-                <input id='no_option' type="radio" value="no" name="membershipinfo_acceptgroupmail" <?= (($membershipinfo->IacceptMassMailFromThisGroup == 'no') ? 'checked ' : '' ); ?>/>
-                <label for="yes_option">Yes</label>
-                <input id='yes_option' type="radio" value="yes" name="membershipinfo_acceptgroupmail" <?= (($membershipinfo->IacceptMassMailFromThisGroup == 'yes') ? 'checked ' : '' ); ?>/>
-            </span><br />
-        
+            <div class="row">
+                <label for="comment"><?= $words->get('GroupsMemberComments') ;?></label><br />
+                <textarea id="comment" name="membershipinfo_comment" cols="60" rows="5" class="long" ><?= (($membershipinfo->Comment != '' ) ? htmlspecialchars($words->mTrad($membershipinfo->Comment)) : '' ); ?></textarea>
+            </div> <!-- row -->
+            <div class="row">
+                <label for="acceptgroupmail"><?= $words->get('GroupsMemberAcceptMail') ;?>:  </label>
+                <input id='no_option' type="radio" value="no" name="membershipinfo_acceptgroupmail" <?= (($membershipinfo->IacceptMassMailFromThisGroup == 'no') ? 'checked="checked" ' : '' ); ?>/>
+                <label for="no_option"><?= $words->get('no') ;?></label>
+                <input id='yes_option' type="radio" value="yes" name="membershipinfo_acceptgroupmail" <?= (($membershipinfo->IacceptMassMailFromThisGroup == 'yes') ? 'checked="checked" ' : '' ); ?>/>
+                <label for="yes_option"><?= $words->get('yes') ;?></label>
+            </div> <!-- row -->
             <input type='submit' value='<?= $words->get('GroupsUpdateMemberSettings') ;?>' />
         </form>
         <?php
@@ -50,7 +51,7 @@ class GroupMemberSettingsPage extends GroupsBasePage
     }
     
     protected function getSubmenuActiveItem() {
-        return 'settings';
+        return 'membersettings';
     }
 }
 

0e6780508e42c4ed35d00b9fbd47dc629fdc4bcc
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-01-26 09:37:41 +0000

    debug tracks to understand why no volunteers are found in locations
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@6897 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/places/places.model.php
+++ build/places/places.model.php
@@ -76,11 +76,13 @@ class Places extends PAppModel {
 * @IdLocation is the geoname id where the members is volunteering
 */
 	public function getVolunteersOfPlace($IdLocation) {
-        $query = sprintf("SELECT members.ProfileSummary,members.BirthDate,membersgroups.Comment as VolComment,
+        $query = "SELECT members.ProfileSummary,members.BirthDate,membersgroups.Comment as VolComment,
 				 	members.HideBirthDate,members.Accomodation,username,cities.name AS city FROM members,cities,countries,groups, groups_locations,membersgroups
                  WHERE `members`.`Status`='Active' AND groups.Name='BewelcomeLV' AND groups_locations.IdGroupMembership=membersgroups.id 
 								 AND membersgroups.IdMember=members.id AND membersgroups.IdGroup=groups.id AND members.IdCity=cities.id AND cities.IdCountry=countries.id 
-                 AND groups_locations.IdLocation='%d'",$this->dao->escape($IdLocation));
+                 AND groups_locations.IdLocation='".$this->dao->escape($IdLocation)."'";
+				MOD_log::get()->write("place.model.php::getVolunteersOfPlace[".$query."]" ,"debug") ;
+
         return $this->getMembersAll($query);
         } // end of getVolunteersOfPlace
     

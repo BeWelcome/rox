0157b03c37fb2d3bfc7717ba78749115fa985c81
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-01-26 10:00:55 +0000

    removing tracks and fxing the test for empty(volunteers)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@6904 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/places/places.view.php
+++ build/places/places.view.php
@@ -40,7 +40,7 @@ class PlacesView extends PAppView {
         $forums = '';
         $wiki = new WikiController();
         $wikipage = str_replace(' ', '', ucwords($countryinfo->name));
-				MOD_log::get()->write("place.view.php::displayPlacesInfo[".$countryinfo->name."]<br /> count(\$volunteers)=".count($volunteers)  ,"debug") ;
+//				MOD_log::get()->write("place.view.php::displayPlacesInfo[".$countryinfo->name."]<br /> count(\$volunteers)=".count($volunteers)  ,"debug") ;
 				
         require 'templates/countryInfo.php';
     }

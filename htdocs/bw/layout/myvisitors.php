ce4d127535e40e8cb3224a842c26b7eb83a84957
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-02-08 11:16:32 +0000

    pic fix
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@6988 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/layout/myvisitors.php
+++ htdocs/bw/layout/myvisitors.php
@@ -40,44 +40,33 @@ function DisplayMyVisitors($TData, $m) {
 	menumember("myvisitors.php?cid=" . $m->id, $m);
 	// Prepare the $MenuAction for ShowAction()  
 	$MenuAction = "";
-	$MenuAction .= "          <li class=\"icon contactmember16\"><a href=\"contactmember.php?cid=" . $m->id . "\">" . ww("ContactMember") . "</a></li>\n";
-	$MenuAction .= "          <li class=\"icon addcomment16\"><a href=\"addcomments.php?cid=" . $m->id . "\">" . ww("addcomments") . "</a></li>\n";
-	// $MenuAction .= "          <li class=\"icon forumpost16\"><a href=\"todo.php\">".ww("ViewForumPosts")."</a></li>\n";
 
+	// Contact notes could grow into something that the safety team can use. This is how I implemented it on CS.  20080330 guaka
 	if (GetPreference("PreferenceAdvanced")=="Yes") {
-      if ($m->IdContact==0) {
-	   	  $MenuAction .= "          <li class=\"icon mylist16\"><a href=\"mycontacts.php?IdContact=" . $m->id . "&amp;action=add\">".ww("AddToMyNotes")."</a> </li>\n";
+	  if ($m->IdContact==0) {
+	   	  $MenuAction .= "<li class=\"icon mylist16\"><a href=\"mycontacts.php?IdContact=" . $m->id . "&amp;action=add\">".ww("AddToMyNotes")."</a> </li>\n";
 	   }
 	   else {
-	   	  $MenuAction .= "          <li class=\"icon mylist16\"><a href=\"mycontacts.php?IdContact=" . $m->id . "&amp;action=view\">".ww("ViewMyNotesForThisMember")."</a> </li>\n";
-	   }
-	}
-
-	if (GetPreference("PreferenceAdvanced")=="Yes") {
-      if ($m->IdRelation==0) {
-	   	  $MenuAction .= "        <li class=\"icon myrelations16\"><a href=\"myrelations.php?IdRelation=" . $m->id . "&amp;action=add\">".ww("AddToMyRelations")."</a> </li>\n";
-	   }
-	   else {
-	   		$MenuAction .= "        <li class=\"icon myrelations16\"><a href=\"myrelations.php?IdRelation=" . $m->id . "&amp;action=view\">".ww("ViewMyRelationForThisMember")."</a> </li>\n";
+	   	  $MenuAction .= "<li class=\"icon mylist16\"><a href=\"mycontacts.php?IdContact=" . $m->id . "&amp;action=view\">".ww("ViewMyNotesForThisMember")."</a> </li>\n";
 	   }
 	}
 
 	if ($CanBeEdited) {
-		$MenuAction .= "          <li><a href=\"editmyprofile.php?cid=" . $m->id . "\">".ww("TranslateProfileIn",LanguageName($_SESSION["IdLanguage"]))." ".FlagLanguage(-1,$title="Translate this profile")."</a> </li>\n";
+		$MenuAction .= "<li><a href=\"editmyprofile.php?cid=" . $m->id . "\">".ww("TranslateProfileIn",LanguageName($_SESSION["IdLanguage"]))." ".FlagLanguage(-1,$title="Translate this profile")."</a> </li>\n";
 	}
 
 	$VolAction=ProfileVolunteerMenu($m); // This will receive the possible vol action for this member
 	
 	$SpecialRelation="" ;
-//special relation should be in col1 (left column) -> function ShowActions needs to be changed for this 
-  $Relations=$m->Relations;
-  $iiMax=count($Relations);
-  if ($iiMax>0) { // if member has declared confirmed relation
-     for ($ii=0;$ii<$iiMax;$ii++) {
-	  	  $SpecialRelation=$SpecialRelation."<li>". LinkWithPicture($Relations[$ii]->Username,$Relations[$ii]->photo)."<br>".LinkWithUsername($Relations[$ii]->Username);
-	  	  $SpecialRelation=$SpecialRelation."<br>".$Relations[$ii]->Comment."</li>\n" ;
-  	  }
-  } // end if member has declared confirmed relation	
+	//special relation should be in col1 (left column) -> function ShowActions needs to be changed for this 
+	$Relations=$m->Relations;
+	$iiMax=count($Relations);
+	if ($iiMax>0) { // if member has declared confirmed relation
+	    for ($ii=0;$ii<$iiMax;$ii++) {
+	        $SpecialRelation=$SpecialRelation."<li>". LinkWithPicture($Relations[$ii]->Username,$Relations[$ii]->photo)."<br>".LinkWithUsername($Relations[$ii]->Username);
+		$SpecialRelation=$SpecialRelation."<br>".$Relations[$ii]->Comment."</li>\n" ;
+	    }
+	} // end if member has declared confirmed relation	
 	
 	ShowLeftColumn($MenuAction,$VolAction,$SpecialRelation); // Show the Actions
 	ShowAds(); // Show the Ads
@@ -97,21 +86,25 @@ function DisplayMyVisitors($TData, $m) {
 		echo "<tr align=left>";
 		echo "<td valign=center align=center>";
 //		if (($rr->photo != "") and ($rr->photo != "NULL")) {
-			echo "<div id=\"topcontent-profile-photo\">\n";
-			echo LinkWithPicture($rr->Username,$rr->photo),"\n";
-			echo "</div>";
+		echo "<div id=\"topcontent-profile-photo\">\n";
+        echo MOD_layoutbits::PIC_50_50($m->Username) ;
+//		echo LinkWithPicture($rr->Username,$rr->photo),"\n";
+		echo "</div>";
 //		}
 		echo  LinkWithUsername($rr->Username), "</td>";
-		echo " <td valign=center>", $rr->countryname, "</td> ";
+		echo  " <td valign=center>" ;
+		echo  "<a href=\"../country/".$rr->CountryCode."\">",$rr->countryname,"</a>" ;
+		echo  "<br><a href=\"../place/".$rr->CountryCode."/".$rr->RegionName."\">",$rr->RegionName,"</a>" ;
+		echo  "<br><a href=\"../place/".$rr->CountryCode."/".$rr->RegionName."/".$rr->cityname."\">",$rr->cityname,"</a>" ;
+		echo "</td> ";
 		echo "<td valign=center>";
-//		if ($rr->ProfileSummary > 0)
-			echo $rr->ProfileSummary;
+		//		if ($rr->ProfileSummary > 0)
+		echo ww("MemberSince",$rr->MemberSince),"<br />" ;
+		echo $rr->ProfileSummary;
 
-		echo "</td>";
-		echo "<td>";
+		echo "</td><td>";
 		echo $rr->datevisite;
-		echo "</td>";
-		echo "</tr>";
+		echo "</td></tr>";
 	}
 	echo "</table>";
 

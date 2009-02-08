639914ecbad3c4bace82764ff735acb787636551
globetrotter_tt <globetrotter_tt@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-02-08 20:22:52 +0000

    #486 (rox-groups):  featured groups (adding missing template)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@7003 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ build/groups/templates/groupsfeatured.column_col3.php
@@ -0,0 +1,52 @@
+<div id="groups">
+    <div>
+        <h3><? echo $this->words->get('GroupsFeaturedGroups'); ?></h3>
+        <form action="groups/featured" method="GET">
+            <input type="hidden" name="GroupsFeaturedInput" value="" id="GroupsFeaturedInput" />
+        </form>
+        <?php
+        if ($this->search_result)
+        {
+            $name_order = (($this->result_order == "nameasc") ? 'namedesc' : 'nameasc');
+            $member_order = (($this->result_order == "membersasc") ? 'membersdesc' : 'membersasc');
+            $created_order = (($this->result_order == "createdasc") ? 'createddesc' : 'createdasc');
+            $category_order = (($this->result_order == "categoryasc") ? 'categorydesc' : 'categoryasc');
+            echo <<<HTML
+            <h4>Order by:</h4>
+            <p class="grey">
+            <a class="grey" href="groups/featured?GroupsFeaturedInput={$this->search_terms}&amp;Order={$name_order}&Page={$this->result_page}">Group name</a>
+            |
+            <a class="grey" href="groups/featured?GroupsFeaturedInput={$this->search_terms}&amp;Order={$member_order}&Page={$this->result_page}">Number of Members</a>
+            |
+            <a class="grey" href="groups/featured?GroupsFeaturedInput={$this->search_terms}&amp;Order={$created_order}&Page={$this->result_page}">Date of creation</a>
+HTML;
+// Categories link disabled until we have categories
+//            |
+//            <a class="grey" href="groups/search?GroupsSearchInput={$this->search_terms}&amp;Order={$category_order}&Page={$this->result_page}">Category</a>
+?>
+    </div> <!-- row -->           
+<?
+
+            foreach ($this->search_result as $group_data) : ?>
+                <div class="groupinfo">
+                    <img class="framed float_left"  width="60px" alt="group" src="<?= ((strlen($group_data->Picture) > 0) ? "groups/thumbimg/{$group_data->getPKValue()}" : 'images/icons/group.png' ) ?>"/>
+                    <h4><a href="groups/<?=$group_data->id ?>"><?=$group_data->Name ?></a></h4>
+                    <ul>
+                        <li><?= $words->get('GroupsMemberCount');?>: <?=$group_data->getMemberCount(); ?></li>
+                        <li><?= $words->get('GroupsDateCreation');?>: <?=$group_data->created; ?></li>
+                        <li><?= $words->get('GroupsNewForumPosts');?>: <?=$group_data->getNewForumPosts; ?></li>
+                    </ul>
+                </div> <!-- groupinfo -->
+            <?php endforeach ; 
+        }
+        else
+        {
+            echo <<<HTML
+            <div>
+            {$words->get('GroupSearchNoResults')}
+            </div>
+</div>
+</div>
+HTML;
+        }
+        ?>

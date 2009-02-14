2d008f32272fc94cd08f53d7034e47322a00b3ea
globetrotter_tt <globetrotter_tt@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-02-14 20:18:12 +0000

    #468 (grox-groups): fix for my last commit
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@7042 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/groups/templates/groupssearch.column_col3.php
+++ build/groups/templates/groupssearch.column_col3.php
@@ -15,15 +15,15 @@
             $member_order = (($this->result_order == "membersasc") ? 'membersdesc' : 'membersasc');
             $created_order = (($this->result_order == "createdasc") ? 'createddesc' : 'createdasc');
             $category_order = (($this->result_order == "categoryasc") ? 'categorydesc' : 'categoryasc');
-            echo <<<HTML
+            ?>
             <h4><?= $words->get('GroupsSearchOrder');?></h4>
             <p class="grey">
-            <a class="grey" href="groups/search?GroupsSearchInput={$this->search_terms}&amp;Order={$name_order}&Page={$this->result_page}">Group name</a>
+            <a class="grey" href="groups/search?GroupsSearchInput=<?=$this->search_terms;?>&amp;Order=<?=$name_order;?>&Page=<?=$this->result_page; ?>"><?= $words->get('GroupsOrderName'); ?></a>
             |
-            <a class="grey" href="groups/search?GroupsSearchInput={$this->search_terms}&amp;Order={$member_order}&Page={$this->result_page}">Number of Members</a>
+            <a class="grey" href="groups/search?GroupsSearchInput=<?=$this->search_terms;?>&amp;Order=<?=$member_order;?>&Page=<?$this->result_page; ?>"><?= $words->get('GroupsOrderMembers'); ?></a>
             |
-            <a class="grey" href="groups/search?GroupsSearchInput={$this->search_terms}&amp;Order={$created_order}&Page={$this->result_page}">Date of creation</a>
-HTML;
+            <a class="grey" href="groups/search?GroupsSearchInput=<?=$this->search_terms;?>&amp;Order=<?$created_order;?>&Page=<?$this->result_page;?>"><?= $words->get('GroupsOrderDate'); ?></a>
+<?
 // Categories link disabled until we have categories
 //            |
 //            <a class="grey" href="groups/search?GroupsSearchInput={$this->search_terms}&amp;Order={$category_order}&Page={$this->result_page}">Category</a>

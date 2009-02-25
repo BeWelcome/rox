4fefa03328055cee3f10e2b31b697df59d597000
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-02-25 21:38:22 +0000

    Adding a link depending on Local Vol right to allow to create directly a local vol message
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@7112 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/places/templates/cityInfo.php
+++ build/places/templates/cityInfo.php
@@ -16,6 +16,9 @@ $words = new MOD_words();
 <?php 
 echo '<h3>',$words->get('localvolunteers'),'</h3>'; 
 require 'localvolunteerslist.php'; 
+if ((MOD_right::get()->HasRight('ContactLocation','$cityinfo->IdCity')) or (MOD_right::get()->HasRight('ContactLocation','All'))) {
+	echo " <a href=\"contactlocal/preparenewmessage/".$cityinfo->IdCity."\" title=\" preprare a local volunteer message for this area\">write a local vol message</a>" ;
+}
 ?>
 <h3><?php echo $words->get('members'); ?></h3>
 <?php require 'memberlist.php'; ?>
--- build/places/templates/countryInfo.php
+++ build/places/templates/countryInfo.php
@@ -11,6 +11,9 @@ $words = new MOD_words();
 ?></h2>
 <h3><?php echo $words->get('localvolunteers'); ?></h3>
 <?php require 'localvolunteerslist.php'; ?>
+if ((MOD_right::get()->HasRight('ContactLocation','$countryinfo->IdCountry')) or (MOD_right::get()->HasRight('ContactLocation','All'))) {
+	echo " <a href=\"contactlocal/preparenewmessage/".$countryinfo->IdCountry."\" title=\" prepare a local volunteer message for this country\">write a local vol message</a>" ;
+}
 <h3><?php echo $words->get('members'); ?></h3>
 <?php require 'memberlist.php'; ?>
 

b65e36e644510ee7433308b650226dd6e3196370
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-02-26 08:29:12 +0000

    Adding contactlocal feature
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@7117 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ build/contactlocal/contactlocal.ctrl.php
@@ -0,0 +1,156 @@
+<?php
+
+/**
+ * contactlocals controller
+ *
+ * @package ContactlocalsController
+ * @author JeanYves
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License (GPL)
+ * @version $Id$
+ * The purpose of this package is to allow, according to his right,
+ * a local volunteers to contact the members in his scope
+ */
+require_once("../htdocs/bw/lib/rights.php") ; // Requiring BW right 
+// TODO: use the MyTB right.. (MOD_right)
+// no, not for now
+class ContactlocalController extends RoxControllerBase
+{
+
+    public function __construct() {
+        parent::__construct();
+
+    }
+    /**
+     * decide which page to show.
+     * This method is called automatically
+     */
+    public function index($args=false)
+    {
+        $User = APP_User::login(); // The user must be logged in
+
+        $request = $args->request;
+        $model = new ContactlocalsModel;
+
+        
+        if (!isset($_SESSION['IdMember'])) {
+            $page = new MessagesMustloginPage();
+            $page->setRedirectURL(implode('/',$request));
+        		return $page;
+        } 
+//        print_r($args->post);
+		if (!MOD_right::get()->HasRight("ContactLocation")) {
+			$page = new ContactlocalsPage("","MissRight","ContactLocation");
+		}
+		else {
+			// look at the request.
+			switch (isset($request[1]) ? $request[1] : false) {
+				case 'listall':
+					$page = new ContactlocalsPage("","listall",$model->LoadList(""));
+					break;
+				case 'addlocation':
+					$model->AddLocation($args->post) ;
+					$data->IdMess=$model->IdMess ;
+					$tt=$model->LoadList($data->IdMess) ;
+					$data->localmessage=$tt[0] ;
+					$data->PossibleLocations=$model->GetAllowedLocation() ;
+					$data->PossibleLanguages=$model->GetMemberLanguages() ;
+					$page = new ContactlocalsPage("","preparenewmessage",$data);
+					break ;
+				case 'updatetranslation':
+					$model->UpdateTranslation($args->post) ;
+					$data->IdMess=$model->IdMess ;
+					$tt=$model->LoadList($data->IdMess) ;
+					$data->localmessage=$tt[0] ;
+					$data->PossibleLocations=$model->GetAllowedLocation() ;
+					$data->PossibleLanguages=$model->GetMemberLanguages() ;
+					$page = new ContactlocalsPage("","preparenewmessage",$data);
+					break ;
+					
+				case 'deletetranslation':
+					$model->DelTranslation($args->post) ;
+					$data->IdMess=$model->IdMess ;
+					$tt=$model->LoadList($data->IdMess) ;
+					$data->localmessage=$tt[0] ;
+					$data->PossibleLocations=$model->GetAllowedLocation() ;
+					$data->PossibleLanguages=$model->GetMemberLanguages() ;
+					$page = new ContactlocalsPage("","preparenewmessage",$data);
+					break ;
+					
+				case 'addtranslation':
+					$model->AddTranslation($args->post) ;
+					$data->IdMess=$model->IdMess ;
+					$tt=$model->LoadList($data->IdMess) ;
+					$data->localmessage=$tt[0] ;
+					$data->PossibleLocations=$model->GetAllowedLocation() ;
+					$data->PossibleLanguages=$model->GetMemberLanguages() ;
+					$page = new ContactlocalsPage("","preparenewmessage",$data);
+					break ;
+					
+				case 'dellocation':
+					$model->DelLocation($args->post) ;
+					$data->IdMess=$model->IdMess ;
+					$tt=$model->LoadList($data->IdMess) ;
+					$data->localmessage=$tt[0] ;
+					$data->PossibleLocations=$model->GetAllowedLocation() ;
+					$data->PossibleLanguages=$model->GetMemberLanguages() ;
+					$page = new ContactlocalsPage("","preparenewmessage",$data);
+					break ;
+				case 'preparenewmessage':
+					$data->PossibleLocations=$model->GetAllowedLocation() ;
+					$data->PossibleLanguages=$model->GetMemberLanguages() ;
+					$data->IdLocation=0 ;
+					if (isset($request[2])) {
+						$data->IdLocation=$request[2] ;
+						$data->rrLocation=$model->GetLocation($data->IdLocation) ;
+					}
+					$page = new ContactlocalsPage("","preparenewmessage",$data);
+					break ;
+				case 'delete':
+					$data->IdMess=$request[2] ;
+					$model->DeleteMessage($data->IdMess) ;
+					$page = new ContactlocalsPage("","",$model->LoadList()); // Without error
+					break;
+				case 'modify':
+					$data->IdMess=$request[2] ;
+					$tt=$model->LoadList($data->IdMess) ;
+					$data->localmessage=$tt[0] ;
+					$data->PossibleLocations=$model->GetAllowedLocation() ;
+					$data->PossibleLanguages=$model->GetMemberLanguages() ;
+					$page = new ContactlocalsPage("","preparenewmessage",$data);
+					break ;
+				case 'testnotification':
+					$data->IdMess=$request[2] ;
+					$ss=$model->Trigger($data->IdMess) ;
+					$page = new ContactlocalsPage($ss,"",$model->LoadList()); // Without error
+					break ;
+				case 'trigger':
+					$data->IdMess=$request[2] ;
+					$ss=$model->Trigger($data->IdMess,true) ;
+					$page = new ContactlocalsPage($ss,"",$model->LoadList()); // Without error
+					break ;
+				case 'recordnewmessage':
+					$model->recordnewmessage($args->post) ;
+					$data->IdMess=$model->IdMess ;
+					$tt=$model->LoadList($data->IdMess) ;
+					$data->localmessage=$tt[0] ;
+					$data->PossibleLocations=$model->GetAllowedLocation() ;
+					$data->PossibleLanguages=$model->GetMemberLanguages() ;
+					$page = new ContactlocalsPage("","preparenewmessage",$data);
+					break ;					
+					
+				case false:
+				default :
+				case '':
+					// no request[1] was specified
+					$page = new ContactlocalsPage("","",$model->LoadList()); // Without error
+					break;
+			}
+		} // end of else not has right
+        // return the $page object,
+        // so the framework can call the "$page->render()" function.
+        return $page;
+    }
+} // end of ContactlocalsController
+
+
+?>
---
+++ build/contactlocal/templates/listall.php
@@ -0,0 +1,106 @@
+<?php
+/*
+
+Copyright (c) 2007 BeVolunteer
+
+This file is part of BW Rox.
+
+BW Rox is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BW Rox is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/> or
+write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA.
+
+*/
+// get current request
+$request = PRequest::get()->request;
+
+if (!isset($vars['errors']) || !is_array($vars['errors'])) {
+    $vars['errors'] = array();
+}
+
+$list=$this->_data ; // Retrieve the data to display (set by the controller)
+
+$words = new MOD_words();
+$styles = array( 'highlight', 'blank' ); // alternating background for table rows
+$iiMax = count($list) ; // This retrieve the number of polls
+?>
+
+<table class="full">
+
+<?php if ($list != false) { ?>
+    <tr>
+        <th>status</th>
+        <th>author</th>
+        <th>title and text</th>
+        <th>locations</th>
+        <th>Action</th>
+    </tr>
+<?php } ?>
+
+<?php
+for ($ii = 0; $ii < $iiMax; $ii++) {
+    $p = $list[$ii];
+	$ChosenLocations=$p->ChosenLocations ;
+    ?>
+    <tr class="<?=$styles[$ii%2] ?>">
+        <td>
+            <h4><?=$p->Status ?></h4>
+        </td>
+        <td>
+            <?
+            if (!empty($p->IdCreator)) {
+                echo MOD_layoutbits::PIC_50_50($p->CreatorUsername) ;
+                echo "<br />" ;
+                echo "<a class=\"username\" href=\"bw/member.php?cid=",$p->CreatorUsername,"\">",$p->CreatorUsername,"</a>" ;
+				echo "<br/><i>",$p->PurposeDescription,"</i>" ;
+            }
+            ?>
+        </td>
+        <td><? echo "<b>",$words->fTrad($p->IdTitleText),"</b><br/>",$words->fTrad($p->IdMessageText); ?></td>
+        <td>
+<?php
+	foreach ($ChosenLocations as $loc) {
+?>
+	<?=$loc->Choice?><br />
+<?php
+	}
+?>
+		</td>
+        <td>
+		<?php
+		// Local Vol coord with All right can modify the message parameters
+		// Owner can modify it too if it is in the ToApproveSTatus
+		if ((($_SESSION["IdMember"]==$p->IdCreator)and($p->Status=='ToApprove')) or (MOD_right::get()->HasRight("ContactLocation","All"))) {
+			echo "<a href=\"contactlocal/modify/".$p->IdMess."\">Modify</a>" ;
+		}
+		// Local Vol coord with All right can delete the message
+		// Owner can delete it too if it is in the ToApproveSTatus
+		if ((($_SESSION["IdMember"]==$p->IdCreator)and($p->Status=='ToApprove')) or (MOD_right::get()->HasRight("ContactLocation","All"))) {
+			echo "<br /><a href=\"contactlocal/delete/".$p->IdMess."\" onclick=\"return confirm('Are you sure you want to delete this message ?')\">Delete</a>" ;
+		}
+		// Local Vol coord with All right can approve the message
+		// Owner can approve it too if it is in the ToApproveSTatus and if he has scope CanApprove
+		if ((($_SESSION["IdMember"]==$p->IdCreator)and($p->Status=='ToApprove')) or (MOD_right::get()->HasRight("ContactLocation","All"))) {
+			echo "<br /><a href=\"contactlocal/testnotification/".$p->IdMess."\">test</a>" ;
+		}
+		// Local Vol coord with All right can approve the message
+		// Owner can approve it too if it is in the ToApproveSTatus and if he has scope CanApprove
+		if ((($_SESSION["IdMember"]==$p->IdCreator)and($p->Status=='ToApprove')and (MOD_right::get()->HasRight("ContactLocation","CanTrigger"))) or (MOD_right::get()->HasRight("ContactLocation","All"))) {
+			echo "<br /><a href=\"contactlocal/trigger/".$p->IdMess."\" onclick=\"return confirm('Are you sure you want to trigger this message ?')\">trigger</a>" ;
+		}
+		?></td>
+    </tr>
+    <?php
+}
+?>
+</table>

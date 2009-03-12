21451a8cb0e8994f94d1720242f2d2f8297e1d20
micha <micha@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-03-12 17:16:07 +0000

    We have to adjust the layoutbits a little to show notifications  properly (#1107)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@7223 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- modules/layoutbits/lib/layoutbits.lib.php
+++ modules/layoutbits/lib/layoutbits.lib.php
@@ -460,10 +460,13 @@ class MOD_layoutbits
      */
     public function ago($timestamp)
     {
+        // test if the given timestamp could be a unix timestamp, otherwise try to make it one
+        if (!is_int($timestamp)) $timestamp = strtotime($timestamp);
         $words = new MOD_words();
         $difference_in_seconds = time() - $timestamp;
         $period_in_seconds = 1;
-
+        $difference = time() - $timestamp;
+/*
         foreach (array(
             'second' => 60,
             'minute' => 60,
@@ -483,16 +486,16 @@ class MOD_layoutbits
         // if nothing helped
         $difference_in_decades = round($difference_in_seconds / $period_in_seconds);
         return $difference_in_decades.' '.$words->get(($difference_in_decades > 1) ? 'decades' : 'decade').' '.$words->get('ago');
-        /*
-        $periods = array($words->get('second'), $words->get('minute'),$words->get('hour'), $words->get('day'), $words->get('week'), $words->get('month'), $words->get('years'), $words->get('decade'));
+*/
+        $periods = array('second','minute','hour','day','week','month','year','decade');
         $lengths = array("60","60","24","7","4.35","12","10");
         for($j = 0; $difference >= $lengths[$j]; $j++)
-        $difference /= $lengths[$j];
+            $difference /= $lengths[$j];
         $difference = round($difference);
         if($difference != 1) $periods[$j].= "s";
-        $text = "$difference $periods[$j] ago";
+        $text = $difference.' '.$words->get($periods[$j]).' '.$words->get('ago');
         return $text;
-        */
+        
     }
 
 

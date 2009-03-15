0c5c1fe58bfac899649c63c292148d6a22001073
globetrotter_tt <globetrotter_tt@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-03-15 17:38:27 +0000

    Ticket #474 (Messages in rox): removing text from the message body - should be empty by default
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@7251 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/messages/compose.page.php
+++ build/messages/compose.page.php
@@ -3,7 +3,7 @@
 
 /**
  * Page for writing a new message
- * 
+ *
  * @package messages
  * @author Andreas (lemon-head)
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License (GPL)
@@ -20,20 +20,20 @@ class ComposeMessagePage extends MessagesBasePage
         $layoutkit = $this->layoutkit;
         $words = $layoutkit->getWords();
         $model = $this->getModel();
-        
+
         $page_url = PVars::getObj('env')->baseuri . implode('/', PRequest::get()->request);
-        
+
         $formkit = $layoutkit->formkit;
         $callback_tag = $formkit->setPostCallback('MessagesController', 'sendMessageCallback');
-        
+
         // defaults
-        $text = 'type your message';
+        $text = '';
         $attach_picture = '';
-        
+
         if ($message = $this->message) {
             $receiver = $model->getMember($message->receiverUsername);
             $text = $message->Message;
-            $attach_picture = ($message->JoinMemberPict == 'yes' ? ' checked' : ''); 
+            $attach_picture = ($message->JoinMemberPict == 'yes' ? ' checked' : '');
         } else if (!$receiver = $this->receiver) {
             // can't show message...
             echo 'no receiver set.';
@@ -41,7 +41,7 @@ class ComposeMessagePage extends MessagesBasePage
         }
         $receiver_username = $receiver->Username;
         $receiver_id = $receiver->id;
-        
+
         if (!$memory = $formkit->getMemFromRedirect()) {
             // no memory
             // echo 'no memory';
@@ -55,7 +55,7 @@ class ComposeMessagePage extends MessagesBasePage
                     $attach_picture = ' checked';
                 }
             }
-            
+
             if ($memory->expired) {
                 ?>your session has expired while you were typing. Try again?<?php
             } else if ($memory->already_sent_as) {
@@ -68,24 +68,24 @@ class ComposeMessagePage extends MessagesBasePage
                     ?>looks like you've already sent this message, but system can't find it. Send again?<?php
                 }
             }
-            
+
             // problems from previous form
             if (is_array($memory->problems)) {
                 require_once 'templates/compose_warning.php';
             }
         }
-        
+
         require_once 'templates/compose.php';
     }
-    
+
     protected function getFieldValues()
     {
         $field_values = array(
             'message_id' => 0,
-            'receiver_id' => $this->_recipient->id, 
+            'receiver_id' => $this->_recipient->id,
             'text' => 'type something'
         );
-        
+
         return $field_values;
     }
 }

e0ee77fb345c9a46fb2f3355500e6e776fbba87a
micha <micha@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-03-29 11:18:00 +0000

    replacing
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@7335 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/link/templates/adminbar.php
+++ build/link/templates/adminbar.php
@@ -2,7 +2,7 @@
 <div id="blog-userbar" class="vert-infobar box">
     <h2><?=$words->get('linkAdminbar')?></h2>
     <p>
-        <a href="link/update"><?=$words->get('linkUpdate')?></a>
+        <a href="link/update">Udpate links</a>
     </p>
     <div class="clear"></div>
 </div>

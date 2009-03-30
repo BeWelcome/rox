89c84825500b7837d7381d2463a4ae099b5b9ecd
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-03-30 11:22:36 +0000

    pure cosmetic merge
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@7359 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/forums/templates/board.php
+++ build/forums/templates/board.php
@@ -75,8 +75,8 @@ if ($navichain_items = $boards->getNaviChain()) {
 
 	$number = $boards->getTotalThreads(); 
 	if ($number == 0) {
-		$this->page->SetMetaRobots("NOINDEX, NOFOLLOW") ;
 		echo $words->getFormatted("Found0Threads");
+		$this->page->SetMetaRobots("NOINDEX, NOFOLLOW") ;
 	} else if ($number == 1) {
 		echo $words->getFormatted("Found1Threads");
 	} else {

7cf4bcb5de720009572f617caa1e172af551841c
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-04-13 13:32:31 +0000

    Fix for a log when a group is updated
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@7522 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- htdocs/bw/groups.php
+++ htdocs/bw/groups.php
@@ -66,10 +66,10 @@ switch (GetParam("action")) {
 		}
 		//			echo "str=$str<br>";
 		sql_query($str);
-		LogStr("update profile in Group <b>", wwinlang("Group_" . $TGroup->Name, 0), "</b> with comment " . GetStrParam('Comment'), "Group");
+		LogStr("update profile in Group <b>". wwinlang("Group_" . $TGroup->Name, 0). "</b> with comment " . GetStrParam('Comment'), "Group");
 		break;
 	case "ShowMembers" :
-		$TGroup = LoadRow("select *,groups.id as IdGroup from groups where id=" . GetParam("IdGroup"));
+		$TGroup = LoadRow("select *,groups.id as IdGroup from groups where id=" . GetParam("IdGroup")." /* groups.php show members */");
 		$rr=LoadRow("select count(*) as NbThread from forums_threads where IdGroup=" . GetParam("IdGroup")) ;
 		$TGroup->NbThread =$rr->NbThread  ;
 		$Tlist = array ();

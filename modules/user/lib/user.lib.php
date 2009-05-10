00828f5b32ecd71dad727177640f6b7f77b2f4b1
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-05-10 20:56:15 +0000

    revert
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@7672 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- modules/user/lib/user.lib.php
+++ modules/user/lib/user.lib.php
@@ -338,12 +338,6 @@ WHERE
         }
         $dao = PDB::get($db->dsn, $db->user, $db->password);
         $localDao =& $dao;
-
-
-		// Added by JeanYves to be able to manage in a dynamic way the changes in Param Table
-		$result = $localDao->query("SELECT * FROM  `params` limit 1");
-        $_SESSION["Param"] = $result->fetch(PDB::FETCH_OBJ);
-
         if (isset($_SYSHCVOL['WhoIsOnlineDelayInMinutes'])) {
             $interval = $_SYSHCVOL['WhoIsOnlineDelayInMinutes'];
         } else {
@@ -402,9 +396,9 @@ WHERE
         $localDao =& $dao;
 
 
-//		// Added by JeanYves to be able to manage in a dynamic way the changes in Param Table
-//		$result = $localDao->query("SELECT * FROM  `params` limit 1");
-//        $_SESSION["Param"] = $result->fetch(PDB::FETCH_OBJ);
+				// Added by JeanYves to be able to manage in a dynamic way the changes in Param Table
+				$result = $localDao->query("SELECT * FROM  `params` limit 1");
+        $_SESSION["Param"] = $result->fetch(PDB::FETCH_OBJ);
 
         $ip_string="196.168.1.1";
         // prior to any updates, the entry in the table guestsonline 

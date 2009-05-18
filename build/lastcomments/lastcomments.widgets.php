b771e165da9c652a2c22a7216a31f59eabf23678
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-05-18 21:08:55 +0000

    adding recent comments page
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@7720 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ build/lastcomments/lastcomments.widgets.php
@@ -0,0 +1,55 @@
+<?php
+
+
+//------------------------------------------------------------------------------------
+/**
+ * This widget shows the forum for a group page
+ *
+ */
+class CommentsWidget  // extends ForumBoardWidget
+{
+    public function render()
+    {
+        echo 'Comment of the dat';
+    }
+    
+    public function setGroup($group)
+    {
+        // extract information from the $group object
+    }
+}
+
+//------------------------------------------------------------------------------------
+/**
+ * This widget shows a list of members with pictures.
+ */
+class GroupMemberlistWidget  // extends MemberlistWidget?
+{
+    private $_group;
+    
+    public function render()
+    {
+        $memberships = $this->_group->getMembers();
+        for ($i = 0; $i < 6 && $i < count($memberships); $i++)
+        {
+            ?>
+            <div class="groupmembers center float_left">                
+                <?=MOD_layoutbits::PIC_50_50($memberships[$i]->Username) ?>
+                <a href="people/<?=$memberships[$i]->Username ?>"><?=$memberships[$i]->Username ?></a>               
+            </div>
+            <?php
+        }
+    }
+    
+    public function setGroup($group)
+    {
+        // extract memberlist information from the $group object
+        $this->_group = $group;
+    }
+}
+
+
+
+
+
+?>

a0698125715e0d817d80dd867ab9da13d21fe678
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-05-22 19:54:20 +0000

    some fixes for comments vote
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@7743 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/lastcomments/templates/lastcomments.php
+++ build/lastcomments/templates/lastcomments.php
@@ -48,10 +48,10 @@
 		<td>
 		<?php
 		if (empty($c->IdCommentHasVote)) { // If there is not yet any vote from teh current member for this comment
-			echo "<a  href=\"lastcomments/vote/",$c->IdComment," title=\"".$words->getFormatted("VoteCommentIsSignificantExplanation")."\">",$words->getFormatted("VoteCommentIsSignificant"),"</a>" ; 
+			echo "<a  href=\"lastcomments/vote/",$c->IdComment," title=\"".$words->getBuffered("VoteCommentIsSignificantExplanation")."\">",$words->getBuffered("VoteCommentIsSignificant"),"</a>" ; 
 		}
 		else {
-			echo "<a  href=\"lastcomments/voteremove/",$c->IdComment," title=\"".$words->getFormatted("VoteCommentIsSignificantExplanation")."\">",$words->getFormatted("VoteCommentIsSignificantRemove"),"</a>" ; 
+			echo "<a  href=\"lastcomments/voteremove/",$c->IdComment," title=\"".$words->getBuffered("VoteCommentIsSignificantExplanation")."\">",$words->getBuffered("VoteCommentIsSignificantRemove"),"</a>" ; 
 		}
 		?>
 		

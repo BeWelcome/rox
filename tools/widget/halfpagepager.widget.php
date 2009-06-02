fde36e644ac9c92706f3e19eaedbf6f24d676e0e
fake51 <fake51@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-06-02 21:18:01 +0000

    adding new pager widget
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@7896 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ tools/widget/halfpagepager.widget.php
@@ -0,0 +1,100 @@
+<?php
+
+class HalfPagePager extends RoxWidget
+{
+
+    const block_links = 2;
+
+    /**
+     * outputs a list of list links, to reflect paging
+     *
+     * @access public
+     */
+    public function render()
+    {
+        if ($this->pages < 2)
+        {
+            return;
+        }
+        echo "<div class='pages'>\n";
+        echo "<ul>\n";
+        if ($this->active_page > 1)
+        {
+            echo "<li>{$this->pager->outputLink(1, '&lt;&lt;', $this->getWords()->getSilent('PagerToFirstPage'))}</li>\n";
+            echo "<li>{$this->pager->outputLink($this->active_page - 1, '&lt;',$this->getWords()->getSilent('PagerToFirstPage'))}</li>\n";
+        }
+        else
+        {
+            echo "<li>&lt;&lt;</li>\n";
+            echo "<li>&lt;</li>\n";
+        }
+        for ($i = 1; $i <= self::block_links && $i <= $this->pages; $i++)
+        {
+            $this->outputPage($i);
+        }
+
+        if ($this->pages > self::block_links)
+        {
+            $end_range = $this->pages - (self::block_links - 1);
+            if ($end_range <= (self::block_links + 1))
+            {
+                $end_range = self::block_links + 1;
+            }
+
+            if (($this->active_page - 1) > (self::block_links + 1))
+            {
+                echo "<li>...</li>\n";
+            }
+
+            if ($this->active_page >= self::block_links && $this->active_page <= $end_range)
+            {
+                for ($i = ($this->active_page - 1); $i <= ($this->active_page + 1); $i++)
+                {
+                    if (self::block_links < $i && $end_range > $i)
+                    {
+                        $this->outputPage($i);
+                    }
+                }
+
+            }
+
+            if ($end_range > ($this->active_page + 1))
+            {
+                echo "<li>...</li>\n";
+            }
+
+            while ($end_range <= $this->pages)
+            {
+                $this->outputPage($end_range);
+                $end_range++;
+            }
+        }
+
+        if ($this->active_page < $this->pages)
+        {
+            echo "<li>{$this->pager->outputLink($this->active_page + 1, '&gt;', $this->getWords()->getSilent('PagerToNextPage'))}</li>\n";
+            echo "<li>{$this->pager->outputLink($this->pages, '&gt;&gt;', $this->getWords()->getSilent('PagerToLastPage'))}</li>\n";
+        }
+        else
+        {
+            echo "<li>&gt;</li>\n";
+            echo "<li>&gt;&gt;</li>\n";
+        }
+        echo "</ul>\n";
+        echo "</div>\n";
+        return;
+
+    }
+
+    private function outputPage($page)
+    {
+            if ($page == $this->active_page)
+            {
+                echo "<li class='current'>{$page}</li>\n";
+            }
+            else
+            {
+                echo "<li>{$this->pager->outputLink($page, $page, $this->getWords()->getSilent('PagerGoToPage', $page))}</li>\n";
+            }
+    }
+}
---
+++ tools/widget/pager.widget.php
@@ -0,0 +1,199 @@
+<?php
+
+class PagerWidget extends RoxWidget
+{
+    private $_prepared = false;
+
+    /**
+     *
+     * @param object $params_object - standard object filled with vars
+     * @access public
+     * @todo use current url as default
+     * @todo grab active page from current url if not supplied - using page_url marker and page_method
+     */
+    public function __construct($params_object)
+    {
+        parent::__construct();
+        $this->pager_strategy = $params_object->strategy;
+        $this->page_url =  ((isset($params_object->page_url)) ? $params_object->page_url : null);
+        $this->page_url_marker = ((isset($params_object->page_url_marker)) ? $params_object->page_url_marker : 'page');
+        $this->page_method = ((isset($params_object->page_method)) ? strtolower($params_object->page_method) : 'get');
+        $this->items_total = $params_object->items_count;
+        $this->items_per_page = ((isset($params_object->items_per_page)) ? $params_object->items_per_page : 15);
+        $this->active_page = ((isset($params_object->active_page)) ? $params_object->active_page : null);
+    }
+
+    public function render()
+    {
+        $this->prepare();
+        $this->pager_strategy->render();
+    }
+
+    /**
+     * a check of whether number of items exceeds items per page
+     *
+     * @access public
+     * @return bool
+     */
+    public function needsPagination()
+    {
+        return ($this->numberOfPages() > 1);
+    }
+
+    /**
+     * makes sure that all needed vars are initialised
+     * also makes sure that all vars are passed to the strategy
+     *
+     * @access protected
+     * @todo implement strategy init
+     */
+    protected function prepare()
+    {
+        if ($this->_prepared) return;
+        $this->_prepared = true;
+
+        $router = new RequestRouter;
+        $req = $router->getRequestAndArgs();
+
+        if (!$this->page_url)
+        {
+            $this->page_url = $req->request_uri;
+        }
+
+        if (!$this->active_page)
+        {
+            $this->active_page = (($page = $this->checkRequestForPageNumber($req)) ? $page : 1);
+        }
+        $this->cleanPageUrl();
+
+        // need to adjust active page index
+        if ($this->active_page > $this->numberOfPages()) {
+            $this->active_page = $this->numberOfPages();
+        }
+        if ($this->active_page < 1) {
+            $this->active_page = 1;
+        }
+
+        // todo: init strategy object here
+        $this->pager_strategy->pager = $this;
+        $this->pager_strategy->pages = $this->numberOfPages();
+        $this->pager_strategy->active_page = $this->active_page;
+    }
+
+    /**
+     * removes previous page marker if present
+     * updates $this->page_url
+     *
+     * @access private
+     */
+    private function cleanPageUrl()
+    {
+        switch ($this->page_method)
+        {
+            case "url":
+                $term = "(/{$this->page_url_marker}/[0-9]+)";
+                break;
+            case "get":
+                $term = "({$this->page_url_marker}=[0-9]+&?)";
+                break;
+        }
+        $this->page_url = preg_replace("!{$term}!",'', $this->page_url);
+    }
+
+    /**
+     * checks the request uri, post and get vars for page
+     * url marker, depending upon the settings for $page_method
+     *
+     * @param object $req - request and arguments object
+     * @access private
+     * @return int|bool
+     */
+    private function checkRequestForPageNumber($req)
+    {
+        switch ($this->page_method)
+        {
+            case 'get':
+                if (isset($req->get[$this->page_url_marker]))
+                {
+                    return intval($req->get[$this->page_url_marker]);
+                }
+                break;
+            case 'url':
+                if (preg_match("!/{$this->page_url_marker}/(\d+)!", $req->request_uri, $matches))
+                {
+                    return intval($matches[1]);
+                }
+                break;
+        }
+        return false;
+    }
+
+    /**
+     * returns total number of pages the pager will show
+     *
+     * @access protected
+     * @return int
+     */
+    protected function numberOfPages()
+    {
+        $this->prepare();
+        return ceil($this->items_total / $this->items_per_page);
+    }
+
+    /**
+     * returns a link based on page number, supplied text and title
+     *
+     * @param int $page
+     * @param string $text
+     * @param string $title
+     * @access public
+     * @return string
+     */
+    public function outputLink($page, $text, $title='')
+    {
+        switch ($this->page_method)
+        {
+            case 'url':
+                if (strstr($this->page_url, '?'))
+                {
+                    $url_parts = explode('?', $this->page_url);
+                    $url_parts[1] = '?' . $url_parts[1];
+                }
+                else
+                {
+                    $url_parts = array($this->page_url, '');
+                }
+                $url = ((substr($url_parts[0], -1) == '/') ? $url_parts[0] : $url_parts[0] . '/') . "{$this->page_url_marker}/{$page}{$url_parts[1]}";
+                break;
+            case 'get':
+            default:
+                $url = $this->page_url . ((strstr($this->page_url, '?')) ? "&amp;": "?") . "{$this->page_url_marker}={$page}";
+                break;
+        }
+        return "<a href='{$url}'" . (($title) ? " title='{$title}'" : '') . ">{$text}</a>";
+    }
+
+    /**
+     * returns the subset of an array that represents the currently active page of the pager
+     * in case of arrays that are too small, it returns the start of the array
+     *
+     * @param array $set
+     * @access public
+     * @return array
+     */
+    public function getActiveSubset($set)
+    {
+        if (!is_array($set))
+        {
+            return array();
+        }
+        $this->prepare();
+
+        $set_start = ((count($set) > (($this->active_page - 1)* $this->items_per_page)) ? ($this->active_page - 1) * $this->items_per_page : 0);
+        $set_length = ((count($set) > ($set_start + $this->items_per_page)) ? $this->items_per_page : count($set) - $set_start);
+        return array_slice($set, $set_start, $set_length);
+    }
+}
+
+
+?>

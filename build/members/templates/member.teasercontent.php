6336dc47c94269e9954e2f257100ea8bb758130f
micha <micha@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-06-03 18:26:54 +0000

    bugfix #508
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@7906 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/members/templates/member.teasercontent.php
+++ build/members/templates/member.teasercontent.php
@@ -8,7 +8,8 @@
                         <strong><?=$member->Username ?></strong>
                         <?=($verification_status) ? '<img src="images/icons/shield.png" alt="'.$verification_text.'" title="'.$verification_text.'">': ''?>
                         <?=($member->Accomodation == 'anytime') ? '<img src="images/icons/door_open.png" alt="'.$member->Accomodation.'" title="'.$member->Accomodation.'">': ''?>
-                        <br /><?=$member->name() ?>
+                        <br />
+                        <?$name = $member->name(); ?><?=($name == '') ? $member->Occupation : $name;?>
 
         <?=$words->flushBuffer()?>
                     </div> <!-- username -->

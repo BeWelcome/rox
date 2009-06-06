d244c0add2216873d0f53a3e85c8a1ad9f87b4b5
crumbking <crumbking@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-06-06 10:44:14 +0000

    mygroups 2 rows
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@7922 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/groups/templates/groupsmygroups.column_col3.php
+++ build/groups/templates/groupsmygroups.column_col3.php
@@ -1,4 +1,7 @@
-<div id="groups">
+<div class="subcolumns">
+  <div class="c50l">
+    <div class="subcl">
+    <div id="groups">
         <h3><?= $words->get('GroupsMyGroups'); ?></h3>
         <?php
         $search_result = $this->search_result;
@@ -34,3 +37,48 @@ HTML;
         }
         ?>
         </div>
+	</div>
+   </div>
+
+
+  <div class="c50r">
+    <div class="subcr">
+    <div id="groups">
+    <h3>&nbsp;</h3>
+        <?php
+        $search_result = $this->search_result;
+        if ($search_result)
+        {
+            foreach($search_result as $group_data)
+            {
+                ?>
+                <div class="groupbox clearfix">
+                    <a href="groups/<?=$group_data->id ?>">
+                        <img class="framed float_left"  width="80px" alt="Group" src="<?= ((strlen($group_data->Picture) > 0) ? "groups/thumbimg/{$group_data->getPKValue()}" : 'images/icons/group.png' ) ;?>"/>
+                    </a>
+                    <div class="groupinfo">
+                    <h4><a href="groups/<?= $group_data->id ?>"><?= $group_data->Name ?></a></h4>
+                        <ul>
+                            <li><?= $words->get('GroupsMemberCount');?>: <?=$group_data->getMemberCount(); ?></li>
+                            <li><?= $words->get('GroupsNewMembers');?>: <?=count($group_data->getNewMembers()) ; ?></li>
+                            <li><?= $words->get('GroupsNewForumPosts');?>: <?=$group_data->getNewForumPosts; ?></li>
+                        </ul>
+                    </div> <!-- groupinfo -->
+                </div> <!-- groupbox clearfix -->
+<?php
+            }
+        }
+        else
+        {
+            echo <<<HTML
+            <p class="note">
+            {$words->get('GroupSearchNoResults')}
+            </p>
+HTML;
+        }
+        ?>
+        </div>
+    
+    </div>
+  </div>
+</div>

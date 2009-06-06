b97c0ad63723736c843e3d5e27b8b5d87f330194
fake51 <fake51@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-06-06 18:44:02 +0000

    updating /featured layout
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@7924 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/groups/templates/groupsfeatured.column_col3.php
+++ build/groups/templates/groupsfeatured.column_col3.php
@@ -1,13 +1,10 @@
-<div id="groups">
-    <div>
         <h3><? echo $this->words->get('GroupsFeaturedGroups'); ?></h3>
         <form action="groups/featured" method="get">
             <input type="hidden" name="GroupsFeaturedInput" value="" id="GroupsFeaturedInput" />
         </form>
         <?php
         $search_result = $this->search_result;
-        if ($search_result)
-        {
+        if ($search_result) :
             $name_order = (($this->result_order == "nameasc") ? 'namedesc' : 'nameasc');
             $member_order = (($this->result_order == "membersdesc") ? 'membersasc' : 'membersdesc');
             $created_order = (($this->result_order == "createdasc") ? 'createddesc' : 'createdasc');
@@ -24,17 +21,23 @@
 // Categories link disabled until we have categories
 //            |
 //            <a class="grey" href="groups/search?GroupsSearchInput={$this->search_terms}&amp;Order={$category_order}&Page={$this->result_page}">Category</a>
-?>
-    </div> <!-- row -->           
-<?
             $this->pager->render();
-                foreach ($search_result as $group_data) : ?>
+
+            echo <<<HTML
+<div class="subcolumns">
+    <div class="c50l">
+        <div class="subcl">
+            <div>
+HTML;
+
+            for ($i = 0; $i < 10 && $i < count($search_result); $i++) :
+                $group_data = $search_result[$i];?>
                 <div class="groupbox clearfix">
-                    <a href="groups/<?=$group_data->id ?>">
+                    <a href="groups/<?=$group_data->getPKValue() ?>">
                         <img class="framed float_left"  width="80px" height="80px" alt="group" src="<?= ((strlen($group_data->Picture) > 0) ? "groups/thumbimg/{$group_data->getPKValue()}" : 'images/icons/group.png' ) ?>"/>
                     </a>
                     <div class="groupinfo">
-                        <h4><a href="groups/<?=$group_data->id ?>"><?=$group_data->Name ?></a></h4>
+                        <h4><a href="groups/<?=$group_data->getPKValue() ?>"><?=$group_data->Name ?></a></h4>
                         <ul>
                             <li><?= $words->get('GroupsMemberCount');?>: <?=$group_data->getMemberCount(); ?></li>
                             <li><?= $words->get('GroupsDateCreation');?>: <?=$group_data->created; ?></li>
@@ -42,16 +45,39 @@
                         </ul>
                     </div> <!-- groupinfo -->
                 </div> <!-- groupbox clearfix-->
-            <?php endforeach ; 
-        }
-        else
-        {
+            <?php endfor ;?>
+            </div>
+        </div>
+    </div>
+    <div class="c50r">
+        <div class="subcr">
+            <div>
+        <?php
+            for ($i = 10; $i < 20 && $i < count($search_result); $i++) :
+                $group_data = $search_result[$i];
+                ?>
+                <div class="groupbox clearfix">
+                    <a href="groups/<?=$group_data->getPKValue() ?>">
+                        <img class="framed float_left"  width="80px" height="80px" alt="group" src="<?= ((strlen($group_data->Picture) > 0) ? "groups/thumbimg/{$group_data->getPKValue()}" : 'images/icons/group.png' ) ?>"/>
+                    </a>
+                    <div class="groupinfo">
+                        <h4><a href="groups/<?=$group_data->getPKValue() ?>"><?=$group_data->Name ?></a></h4>
+                        <ul>
+                            <li><?= $words->get('GroupsMemberCount');?>: <?=$group_data->getMemberCount(); ?></li>
+                            <li><?= $words->get('GroupsDateCreation');?>: <?=$group_data->created; ?></li>
+                            <li><?= $words->get('GroupsNewForumPosts');?>: <?=$group_data->getNewForumPosts; ?></li>
+                        </ul>
+                    </div> <!-- groupinfo -->
+                </div> <!-- groupbox clearfix-->
+            <?php endfor; ?>
+            </div>
+        </div>
+    </div>
+</div>
+        <?php else :
             echo <<<HTML
             <p class="note">
             {$words->get('GroupSearchNoResults')}
             </p>
-</div>
-</div>
 HTML;
-        }
-        ?>
+        endif;  

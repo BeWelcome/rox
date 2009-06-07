edc1bd31c4a45d89efb4919794df40b37bd1c469
micha <micha@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-06-07 20:52:33 +0000

    deactivating movie in public startpage (for now. movie is not ready)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@7995 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/rox/templates/teaser.php
+++ build/rox/templates/teaser.php
@@ -34,14 +34,96 @@ $words = new MOD_words();
 		<table>
 		<tr>
 		<td>
+		<? /*
 		  <div class="video-embedded">
 			<!--<img src="images/misc/video-placeholder.png">-->
 			<object width="306" height="172"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=3545292&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=3545292&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="306" height="172"></embed></object>
 		  </div>
+		  */ ?>
+          <style>
+          .slide {
+          	width: 350px;
+          	height: 230px;
+              margin: 0;
+              padding: 0;
+              text-align: left;
+          }
+          .slide img{
+              margin: 0;
+              padding: 0;
+          }
+          #teaser .photodesc a{
+              color: #bbb;
+              font-weight: normal;
+          }
+          </style>
+                  <div id="slideshow-content"  style="width: 360px; height: 240px">
+                      <div class="slide" id="slide1">
+                      <img src="images/tour/share4_small.jpg">
+                      </div>
+                      <div class="slide" id="slide2" style="display: none;">
+                      <img src="images/tour/syrien.jpg">
+                      </div>
+                      <div class="slide" id="slide3" style="display: none;">
+                      <img src="images/tour/mountain1.jpg">
+                      </div>
+                      <div class="slide" id="slide4" style="display: none;">
+                      <img src="images/tour/river.jpg">
+                      </div>
+                      <div class="slide" id="slide5" style="display: none;">
+                      <img src="images/tour/dancing2.jpg">
+                      </div>
+                      <div class="slide" id="slide6" style="display: none;">
+                      <img src="images/tour/mountain2.jpg">
+                      </div>
+                      <div class="slide" id="slide7" style="display: none;">
+                      <img src="images/tour/people.jpg">
+                      </div>
+                      <div class="slide" id="slide8" style="display: none;">
+                      <img src="images/tour/people2.jpg">
+                      </div>
+                      </div>
+
+          <script type="text/javascript">
+              <!--
+              function realeffect() {
+                  new Effect.toggle('real', 'appear', {duration: 2})
+              }
+              $('real').hide();
+              $('something').hide();
+              window.onload = function () {
+                  new Effect.toggle('something', 'appear', {duration: 2});
+                  setTimeout('realeffect()',2000);
+                  start_slideshow(1, 8, 10000);
+              };
+
+          // -->
+          </script>
+
+          <script type="text/javascript">
+
+              function start_slideshow(start_frame, end_frame, delay) {
+                  setTimeout(switch_slides(start_frame,start_frame,end_frame, delay), delay);
+              }
+
+              function switch_slides(frame, start_frame, end_frame, delay) {
+                  return (function() {
+                      Effect.Fade('slide' + frame);
+                      if (frame == end_frame) { frame = start_frame; } else { frame = frame + 1; }
+                      setTimeout("Effect.Appear('slide" + frame + "');", 950);
+                      setTimeout(switch_slides(frame, start_frame, end_frame, delay), delay + 950);
+                  })
+              }
+
+          </script>
 		</td>
-		<td style="vertical-align: bottom">
+		<td style="vertical-align: top">
 		  <div class="video-desc">
 		  	<p><?=$words->get('IndexPageVideoDesc','username') //TODO: Fix the 'username' to something dynamic ?></p>
+            <p class="small photodesc" style="color: #999;">
+                all pictures (cc) 
+                <?=$words->get('StartPageListofPhotographers');?>
+            </p>
 		  </div>
 		</td>
 		</tr>

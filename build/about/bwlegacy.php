696103f0fa63d3d243714020d6bf3cee706cb90d
fake51 <fake51@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-06-14 16:52:33 +0000

    removing unused file bwlegacy
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@8093 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/about/bwlegacy.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  /* Ok, guaka knows this is bad but it will move stuff forward a bit faster. */
-
-include "../htdocs/bw/lib/FunctionsTools.php";
-include "../htdocs/bw/lib/bwdb.php";
-
-?>

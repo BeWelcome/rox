b0e8bababe6b51cf6723a7e48115662ec88a2e40
tobixen <tobixen@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-06-15 11:57:47 +0000

    fixing messages not marked as read bug
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@8125 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/messages/messages.ctrl.php
+++ build/messages/messages.ctrl.php
@@ -91,7 +91,7 @@ class MessagesController extends RoxControllerBase
                         // no message with that id found
                         $page = new MessagesInboxPage();
                     } else {
-                        if (isset($message->WhenFirstRead) && $message->WhenFirstRead == '')
+                        if (empty($message->WhenFirstRead) || $message->WhenFirstRead == '0000-00-00 00:00:00')
                             $model->markReadMessage($message->id);
                         if (!isset($request[2])) {
                             $page = new ReadMessagePage();

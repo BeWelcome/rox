1136bdccf97a45f5067e08dee21e553a5ada8f31
fake51 <fake51@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-06-17 11:47:44 +0000

    adding adminedit mode to profile edit
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@8170 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/members/templates/editprofile_form.php
+++ build/members/templates/editprofile_form.php
@@ -147,7 +147,7 @@
 ?>
                 <tr align="left" >
                   <td class="label" ><?=$words->get('FirstName')?>:</td>
-                  <td><?php (($this->adminedit) ? "<input type='text' name='FirstName' value='{$vars['FirstName']}'/>" : $vars['FirstName']); ?></td>
+                  <td><?= (($this->adminedit) ? "<input type='text' name='FirstName' value='{$vars['FirstName']}'/>" : $vars['FirstName']); ?></td>
                   <td>
                     <input type="checkbox"  value="Yes"  name="IsHidden_FirstName"
                     <?php if ($vars['IsHidden_FirstName'])
@@ -158,7 +158,7 @@
                 </tr>
                 <tr align="left" >
                   <td class="label" ><?=$words->get('SecondName')?>:</td>
-                  <td><?php (($this->adminedit) ? "<input type='text' name='SecondName' value='{$vars['SecondName']}'/>" : $vars['SecondName']); ?></td>
+                  <td><?= (($this->adminedit) ? "<input type='text' name='SecondName' value='{$vars['SecondName']}'/>" : $vars['SecondName']); ?></td>
                   <td>
                     <input type="checkbox"  value="Yes"  name="IsHidden_SecondName"
                     <?php if ($vars['IsHidden_SecondName'])
@@ -169,7 +169,7 @@
                 </tr>
                 <tr align="left" >
                   <td class="label" ><?=$words->get('LastName')?>:</td>
-                  <td><?php (($this->adminedit) ? "<input type='text' name='LastName' value='{$vars['LastName']}'/>" : $vars['LastName']); ?></td>
+                  <td><?= (($this->adminedit) ? "<input type='text' name='LastName' value='{$vars['LastName']}'/>" : $vars['LastName']); ?></td>
                   <td>
                     <input type="checkbox"  value="Yes"  name="IsHidden_LastName"
                     <?php if ($vars['IsHidden_LastName'])

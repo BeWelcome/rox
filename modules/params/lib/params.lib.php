99ab8925a9a6ddb2c9d3537cd9f7e6eb961b7176
fake51 <fake51@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-06-22 19:48:09 +0000

    adding missing module params
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@8292 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ modules/params/lib/params.lib.php
@@ -0,0 +1,93 @@
+<?php
+/*
+
+Copyright (c) 2007 BeVolunteer
+
+This file is part of BW Rox.
+
+BW Rox is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BW Rox is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/> or
+write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA  02111-1307, USA.
+
+*/
+/**
+ * Collection of functions that create elements for a page.
+ *
+ * An example for its use:
+ * $layoutbits = MOD_layoutbits::get();  // get the singleton instance
+ * $id = $geo->getCityID($cityname);
+ *
+ * @author Andreas (bw/cs:lemon-head)
+ */
+class MOD_params
+{
+
+    /**
+     * Singleton instance
+     *
+     * @var MOD_layoutbits
+     * @access private
+     */
+    private static $_instance;
+    private $dao;
+
+    public function __construct()
+    {
+        $db = PVars::getObj('config_rdbms');
+        if (!$db) {
+            throw new PException('DB config error!');
+        }
+        $dao = PDB::get($db->dsn, $db->user, $db->password);
+        $this->dao =& $dao;
+    }
+
+    /**
+     * singleton getter
+     *
+     * @param void
+     * @return PApps
+     */
+    public static function get()
+    {
+        if (!isset(self::$_instance)) {
+            $c = __CLASS__;
+            self::$_instance = new $c;
+        }
+        return self::$_instance;
+    }
+
+    /**
+     * wrapper for lazy loading and instantiating an HTMLPurifier object
+     *
+     * @access public
+     * @return object
+     */
+    public static function loadParams()
+    {
+        if (empty($_SESSION["Param"]))
+        {
+            // moved from PAppModel
+            // todo: move to a PROPER place
+            // load the default current param values from the database
+            // This must be done at each page reload, but only once, because Session["Param"] might need to be updated
+            // It could also be a good idea to make some $this->BW_Param thing with it instead of using SESSION, but there is already many code with session
+            $result = self::$dao->query("SELECT * FROM `params`");
+            if (!$result) {
+                    throw new Exception('Failed to retrieve $_SESSION["Param"]!');
+            }
+            $_SESSION["Param"] = $result->fetch(PDB::FETCH_OBJ);
+        }
+    }
+
+}

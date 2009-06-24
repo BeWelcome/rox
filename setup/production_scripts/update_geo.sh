9160fb6dad788f2e36320fcef7efdff546e1beb4
fake51 <fake51@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-06-24 23:16:16 +0000

    adding geo database update script
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@8364 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ setup/production_scripts/update_geo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ -z "$1" ]
+then
+    echo "Called without any arguments. Must be called with host name of server, like test.bewelcome.org"
+    exit 1
+fi
+
+file=`date +%Y-%m-%d`
+if `/usr/bin/wget --header="Host: $1" http://localhost/geo/refreshgeo -O /tmp/geoupdate$file > /dev/null`
+then
+    if /bin/grep -q "^success$" /tmp/geoupdate$file && /bin/rm /tmp/geoupdate$file
+    then
+        exit 0
+    else
+        error="Geo update was downloaded but failed on $1"
+    fi
+else
+    error="Geo update failed on $1"
+fi
+echo $error | mail -s "Geo update failed" bw-admin-discussion@bewelcome.org bw-dev-svn@bewelcome.org

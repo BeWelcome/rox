ab86670b1a61c1aa889b8dfe36882a80d1575400
jeanyves <jeanyves@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-06-28 15:52:38 +0000

    Trying to force cities to be searched for in their country (the region is still not considerated)  (10% done)
    git-svn-id: file:///home/jaliste/hcvol/hcvol/branches/production@8477 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/places/places.model.php
+++ build/places/places.model.php
@@ -29,7 +29,7 @@ class Places extends PAppModel {
 		return $result->fetch(PDB::FETCH_OBJ);
 	}
     
-	public function getRegionInfo($regioncode) {
+	public function getRegionInfo($regioncode,$countrycode="") {
 		$query = sprintf("SELECT name AS region, id AS idregion FROM regions WHERE regions.name = '%s'",
 			$this->dao->escape($regioncode));
 		$result = $this->dao->query($query);
@@ -39,12 +39,13 @@ class Places extends PAppModel {
 		return $result->fetch(PDB::FETCH_OBJ);
 	}	
 
-	public function getCityInfo($citycode) {
-		$query = sprintf("SELECT cities.name AS city, cities.id AS IdCity FROM cities WHERE cities.name = '%s'",
-			$this->dao->escape($citycode));
+	public function getCityInfo($cityname,$regionname="",$countrycode="") {
+		$query = sprintf("SELECT geonames_cache.name AS city, geonames_cache.geonameid AS IdCity FROM geonames_cache WHERE geonames_cache.name = '%s' and geonames_cache.fk_countrycode='%s'",
+			$this->dao->escape($cityname),$this->dao->escape($countrycode));
+			
 		$result = $this->dao->query($query);
         if (!$result) {
-            throw new PException('Could not retrieve members list.');
+            throw new PException('Could not retrieve the city.');
 		}
 		return $result->fetch(PDB::FETCH_OBJ);
 	}	
@@ -91,10 +92,10 @@ class Places extends PAppModel {
 		return $this->getMembersAll($query);
         }	
 
-	public function getMembersOfCity($citycode) {
-        $query = sprintf("SELECT members.BirthDate,members.HideBirthDate,members.Accomodation,username,cities.name AS city FROM members,cities 
-                    WHERE `Status`='Active' AND members.IdCity=cities.id AND cities.name='%s'", 
-                    $this->dao->escape($citycode));
+	public function getMembersOfCity($cityname,$regionname="",$countrycode="") {
+        $query = sprintf("SELECT members.BirthDate,members.HideBirthDate,members.Accomodation,username,geonames_cache.name AS city FROM members,geonames_cache 
+                    WHERE `Status`='Active' AND members.IdCity=geonames_cache.geonameid AND geonames_cache.name='%s'  and geonames_cache.fk_countrycode='%s'", 
+                    $this->dao->escape($cityname),$this->dao->escape($countrycode));
 		return $this->getMembersAll($query);
 	}	
     

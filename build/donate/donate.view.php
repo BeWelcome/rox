998c1d0d90b293067bf44a778affc6949d850724
crumbking <crumbking@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-07-04 13:19:54 +0000

    I moved some files for the donation app from /htdocs to /build/donate/templates. I will test on test.bw.org
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@8585 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/donate/donate.view.php
+++ build/donate/donate.view.php
@@ -21,19 +21,19 @@ class DonateView extends PAppView
     public function donate($sub = false,$TDonationArray = false, $error = false)
     {
         if ($sub == 'list' && $TDonationArray) {
-            require TEMPLATE_DIR.'apps/rox/donate_list.php';
-        } else require TEMPLATE_DIR.'apps/rox/donate.php';
+        	require 'templates/donate_list.php';
+        } else require 'templates/donate.php';
     }
     
     public function donateBar($TDonationArray = false)
     {
 	  	 $Stat=$this->_model->getStatForDonations() ;
-        require TEMPLATE_DIR.'apps/rox/userbar_donate.php';
+        require 'templates/userbar_donate.php';
     }
 
 	public function submenu($sub) {
 //	  	 $Stat=$this->_model->getStatForDonations() ;
-        require TEMPLATE_DIR.'apps/rox/submenu_donate.php';
+        require 'templates/submenu_donate.php';
 	}    
     
 }

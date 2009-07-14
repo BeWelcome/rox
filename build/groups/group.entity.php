a8cd4b1f447dabd37c59d2946fd8ff28fe57f4a0
fake51 <fake51@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-07-14 11:51:17 +0000

    fixing bad display of newlines
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@8706 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/groups/group.entity.php
+++ build/groups/group.entity.php
@@ -293,6 +293,8 @@ class Group extends RoxEntityBase
             return false;
         }
 
+        $description = str_replace(array("\r\n", "\r"), "\n", $description);
+
         $words = $this->getWords();
         $description_id = ((!$this->IdDescription) ? $words->InsertInMTrad($this->dao->escape($description), 'groups.IdDescription', $this->getPKValue()) : $words->ReplaceInMTrad($this->dao->escape($description), 'groups.IdDescription', $this->getPKValue(), $this->IdDescription));
 

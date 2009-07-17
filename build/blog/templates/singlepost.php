050bcc53039c89329a460c6d8c0a2ae0048ac039
mmetzger <mmetzger@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-07-17 17:26:16 +0000

    changed path to member from /user to /member - mm
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@8778 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/blog/templates/singlepost.php
+++ build/blog/templates/singlepost.php
@@ -42,7 +42,7 @@ if (!isset($vars['errors'])) {
   <div class="corner"></div>
     <h3><span><?=htmlentities($blog->blog_title, ENT_COMPAT, 'utf-8')?></span></h3>
     <div class="author">
-        <?=$words->get('written_by')?> <a href="user/<?=$blog->user_handle?>"><?=$blog->user_handle?></a>
+        <?=$words->get('written_by')?> <a href="member/<?=$blog->user_handle?>"><?=$blog->user_handle?></a>
 <?php
 if ($blog->fk_countrycode) {
 ?>

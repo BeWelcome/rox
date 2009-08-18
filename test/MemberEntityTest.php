b210a1ba0833ba88d6a24864cda1fd7719c8cd2c
fake51 <fake51@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-08-18 17:45:05 +0000

    updating member entity test
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@9025 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- test/MemberEntityTest.php
+++ test/MemberEntityTest.php
@@ -48,4 +48,29 @@ class MemberEntityTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(is_array($array));
         $this->assertFalse(empty($array));
     }
+
+    public function testLogOut()
+    {
+        $m = $this->entity_factory->create('Member');
+        $this->assertFalse($m->logOut());
+        $m->findById(1);
+        // this is very hackish. However, the way the tests are done, the framework
+        // sends headers as it feels like. Hence, session_regenerate_id will fall over
+        // testing for that indicates success, actually (in some strange far away place)
+        try
+        {
+            $this->assertTrue($m->logOut());
+        }
+        catch (Exception $e)
+        {
+            if (substr($e->getMessage(), 0, 21) == 'session_regenerate_id')
+            {
+                $this->assertTrue(true);
+            }
+            else
+            {
+                $this->assertTrue(false);
+            }
+        }
+    }
 }

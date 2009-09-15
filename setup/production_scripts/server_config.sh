18a48bcca87ba80534e4f4a5e4c0a0083ef35e00
tobixen <tobixen@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-09-15 22:37:53 +0000

    server configuration script
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@9218 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- setup/production_scripts/server_config.sh
+++ setup/production_scripts/server_config.sh
@@ -1,10 +1,40 @@
-host_tags="common host_$HOSTNAME"
-mysql_file=`echo /etc/rc3.d/S[0-9][0-9]mysql`
-[ -x "$mysql_file" ] && tags="$tags mysql"
+## Host configuration file.  This may be a bit overkill for a setup with only
+## two hosts, but at least it scales very well.  It's built on experiences 
+## with a similar setup that tobixen has deployed at his workplace.
+
+## HOST_TAGS contains all tags for this host.
+## all hosts should have the "common"-tag
+## all hosts should have the host_$HOSTNAME tag
+HOST_TAGS="$HOST_TAGS common host_$HOSTNAME"
+
+## Add mysql to the HOST_TAGS if applicable
+MYSQL_STARTUP_FILE=`echo /etc/rc3.d/S[0-9][0-9]mysql`
+[ -x "$MYSQL_STARTUP_FILE" ] && HOST_TAGS="$HOST_TAGS mysql"
+
+## TODO: do something similar for apache, php, etc
+
+## Add host tag "production" if the host is serving www.bewelcome.org
 if [ -d "/var/www/www.bewelcome.org" ] || [ -d "/home/bwrox/www.bewelcome.org" ]
 then 
-    host_tags="$tags production www.bewelcome.org"
+    HOST_TAGS="$HOST_TAGS production"
 fi
+
+## Add site_* to host tags if applicable + find the scripts dir
+for site in www.bewelcome.org test.bewelcome.org dev.bewelcome.org alpha.bewelcome.org www.bevolunteer.org
+do
+  for rootdir in /var/www /home/bwrox
+  do
+    [ -z "$SCRIPTS_DIR" ] && \
+       [ -d "$rootdir/$site/setup/production_scripts/" ] && \
+         SCRIPTS_DIR="$rootdir/$site/setup/production_scripts/"
+    if [ -d "$rootdir/$site" ]
+    then
+      HOST_TAGS="$HOST_TAGS site_${site}"
+      break
+    fi
+  done
+done
+
 ## todo: check for bwrox and other stuff
 
-export host_tags
+export HOST_TAGS SCRIPTS_DIR

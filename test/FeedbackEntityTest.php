e587d63f8c4798bf106b4ff512818199940381e7
fake51 <fake51@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-12-26 21:41:02 +0000

    adding test for feedback entity
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@9757 4f49b16c-3c57-40ef-8e22-7452baa6c33a

---
+++ test/FeedbackEntityTest.php
@@ -0,0 +1,63 @@
+<?php
+/*
+Copyright (c) 2009 BeVolunteer
+
+This file is part of BW Rox.
+
+BW Rox is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+BW Rox is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/> or 
+write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, 
+Boston, MA  02111-1307, USA.
+*/
+
+    /**
+     * @package    Tests
+     * @subpackage EntityTests
+     * @author     Fake51
+     * @copyright  2009 BeVolunteer
+     * @license    http://www.gnu.org/licenses/gpl-2.0.html GPL 2
+     * @link       http://www.bewelcome.org
+     */
+
+    /**
+     * tests the feedback entity class
+     *
+     * @package    Tests
+     * @subpackage EntityTests
+     * @author     Fake51
+     */
+
+require_once("PHPUnit/Framework.php");
+
+class FeedbackEntityTest extends PHPUnit_Framework_TestCase
+{
+    protected $entity_factory;
+
+    public function setUp()
+    {
+        require_once 'core_includes.php';
+        $this->entity_factory = new RoxEntityFactory;
+    }
+
+    public function testCreation()
+    {
+        $f = $this->newF();
+        $this->assertEquals(true, $f instanceof Feedback);
+        $this->assertEquals(true, $f instanceof RoxEntityBase);
+    }
+
+    private function newF()
+    {
+        return $this->entity_factory->create('Feedback');
+    }
+}

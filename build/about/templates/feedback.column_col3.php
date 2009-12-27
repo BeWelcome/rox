d8e27f847e1a4c22b155c7ab80e85d726d53db87
globetrotter_tt <globetrotter_tt@4f49b16c-3c57-40ef-8e22-7452baa6c33a>
2009-12-27 10:00:32 +0000

    #Ticket #1271 (Improvement of feedback form) - styling of error field
    git-svn-id: file:///home/jaliste/hcvol/hcvol/trunk@9760 4f49b16c-3c57-40ef-8e22-7452baa6c33a

--- build/about/templates/feedback.column_col3.php
+++ build/about/templates/feedback.column_col3.php
@@ -31,50 +31,57 @@ $FeedbackQuestion = isset($mem['FeedbackQuestion']) ? $mem['FeedbackQuestion'] :
 $FeedbackEmail = isset($mem['FeedbackEmail']) ? $mem['FeedbackEmail'] : '';
 $urgent = isset($mem['urgent']) ? $mem['urgent'] : null;
 $answerneeded = isset($mem['answerneeded']) ? $mem['answerneeded'] : null;
-
-if ($errors = $this->getRedirectedMem('errors'))
-{
-    foreach ($errors as $error)
-    {
-        echo "<p>{$words->get($error)}</p>";
-    }
-}
 ?>
 
 <p><?php echo $words->get("FeedBackDisclaimer") ?></p>
 
 <form class="yform full" action="about/feedback" method="post">
     <?=$callback_tag ?>
+
     <div class="type-select">
         <h4><label for="IdCategory"><?php echo $words->get("FeedBackChooseYourCategory")?></label></h4>
         <select id="IdCategory" name="IdCategory">
             <?php foreach ($categories as $cat) { ?>
-                <option value="<?php echo $cat->id ?>" <?=($cat->id == $IdCategory) ? 'selected': '' ?>>
+                <option value="<?php echo $cat->id ?>" <?=($cat->id == $IdCategory) ? 'selected="selected"': '' ?>>
                     <?php echo $words->get("FeedBackName_" . $cat->name) ?>
                 </option>
             <?php } ?>
         </select>
     </div>
-    
+
     <div class="type-text">
         <h4><label for="FeedbackQuestion"><?php echo $words->get("FeedBackEnterYourQuestion")?></label></h4>
         <textarea id="FeedbackQuestion" name="FeedbackQuestion" class="long" cols="60" rows="9"><?php echo $FeedbackQuestion;?></textarea>
     </div>
-    
+
     <?php if (!$this->model->getLoggedInMember()) : ?>
-    <div class="type-text">
+    <div class="type-text <?php
+        if ($errors = $this->getRedirectedMem('errors'))
+        {
+            echo "error";
+        }
+        ?>"><?php
+        if ($errors = $this->getRedirectedMem('errors'))
+        {
+            foreach ($errors as $error)
+            {
+                echo "<strong class=\"message\">{$words->get($error)}</strong>";
+            }
+        }
+    ?>
+
         <h4><label for="FeedbackEmail"><?php echo $words->get("FeedBackEmail")?></label></h4>
         <input type="text" id="FeedbackEmail" name="FeedbackEmail" value="<?php echo $FeedbackEmail;?>"/>
     </div>
     <?php endif; ?>
-    
+
     <div class="type-check">
         <p><input type="checkbox" id="feedbackUrgent" name="urgent" <?php if ($urgent) echo "checked='checked'";?>/> <label for="feedbackUrgent"> <?php echo $words->get("FeedBackUrgentQuestion")?></label></p>
         <p><input type="checkbox" id="feedbackAnswerneeded" name="answerneeded" <?php if ($answerneeded) echo "checked='checked'";?>/> <label for="feedbackAnswerneeded"> <?php echo $words->get("FeedBackIWantAnAnswer")?></label></p>
     </div>
-    <p><input type="submit" id="submit" name="submit" value="submit" /></p>
-    
-    <div class="type-button">
+
+    <div>
+        <input type="submit" id="submit" name="submit" value="submit" />
         <input name="action" type="hidden" value="ask" />
     </div>
 </form>

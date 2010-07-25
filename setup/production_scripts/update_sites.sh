5345c16f14856073574acd8b23f94161e900c4d2
tobias <tobixen@gmail.com>
2010-07-25 19:43:17 +0200

    (untested) changing from svn to git

--- setup/production_scripts/update_sites.sh
+++ setup/production_scripts/update_sites.sh
@@ -1,4 +1,4 @@
-## noisy script for updating alpha and production on svn commits.
+## noisy script for updating alpha and production on git commits.
 
 [ "$USER" == "bwrox" ] || { echo "this script should only be run as bwrox user"; exit 255; }
 
@@ -9,6 +9,8 @@ TMPFILE=/home/bwrox/tmp/updates.$$
 for SITE in ${SITES}
 do
 
+## TODO: figure out how to do this under git
+if false then
 cd /home/bwrox/${SITE}
 if ! svn stat --show-updates > ${TMPFILE} 2>&1
 then
@@ -30,23 +32,29 @@ then
   cat ${TMPFILE} >> /home/bwrox/${SITE}/htdocs/update_status.txt
   continue
 fi
+fi
 
 state="Successful"
 echo -n "Update starting on " > ${TMPFILE}
 date >> ${TMPFILE}
-echo -e "\n\nOutput of svn up on ${SITE}" >> ${TMPFILE}
-svn up >> ${TMPFILE} 2>&1 || state="UNSUCCESSFUL"
-echo -e "\n\nOutput of svn stat on ${SITE}" >> ${TMPFILE}
-svn stat >> ${TMPFILE}
+echo -e "\n\nOutput of git pull on ${SITE}" >> ${TMPFILE}
+git pull >> ${TMPFILE} 2>&1 || state="UNSUCCESSFUL"
 echo -e "\n\nUpdate finished " >> ${TMPFILE}
 date >> ${TMPFILE}
-echo -e "\n\nOutput of svn info on ${SITE}" >> ${TMPFILE}
-svn info >> ${TMPFILE}
+echo -e "\n\nOutput of git stat on ${SITE}" >> ${TMPFILE}
+git stat >> ${TMPFILE}
+echo -e "\n\nOutput of git show on ${SITE}" >> ${TMPFILE}
+git show >> ${TMPFILE}
+echo -e "\n\nOutput of git diff on ${SITE}" >> ${TMPFILE}
+git diff >> ${TMPFILE}
+branch=`git branch | grep -E '^\*' | cut -f2 -d' '`
+echo -e "\n\nOutput of git diff origin/$branch $branch on ${SITE}" >> ${TMPFILE}
+git diff origin/$branch $branch >> ${TMPFILE}
 echo -e "\n\nState of update: $state" >> ${TMPFILE}
 
-mail -s "${state} update of ${SITE}" tobixen@gmail.com bw-dev-svn@bewelcome.org < ${TMPFILE}
+mail -s "${state} update of ${SITE}" bw-dev-svn@bewelcome.org < ${TMPFILE}
 
 cp ${TMPFILE} /home/bwrox/${SITE}/htdocs/update_status.txt
-svn info | grep "Last Changed Rev"|awk '{ print $4 }' > /home/bwrox/${SITE}/htdocs/revision.txt
+git show --pretty=oneline | head -n1 > /home/bwrox/${SITE}/htdocs/revision.txt
 
 done

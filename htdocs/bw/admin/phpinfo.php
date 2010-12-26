74961c0787938f5d4c860b6ea43c24baf7da7e0f
globetrotter_tt <globetrotter.tt@gmail.com>
2010-12-26 16:28:24 +0100

    testing for jean-yves

--- htdocs/bw/admin/phpinfo.php
+++ htdocs/bw/admin/phpinfo.php
@@ -25,6 +25,7 @@ chdir("..") ;
 require_once "lib/init.php";
 require_once "layout/error.php";
 
+echo "hello jean-yves";
 // test if is logged, if not logged and forward to the current page
 MustBeAdmin(); // need to be log
 phpinfo();

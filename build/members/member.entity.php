5638efbd47b96f292e2d605e04155783a1dbaa10
meinhard <meinhard@benn.org>
2011-12-17 19:47:26 +0200

    * new method getPreference() * refactoring of language preference getters

--- build/members/member.entity.php
+++ build/members/member.entity.php
@@ -137,8 +137,8 @@ ORDER BY languages.id asc
      * Get language code from preferences.
      * @return string language ShortCode (2 to 4 letters), 'en' if no preference was found.
      */
-    public function get_language_preference() {
-        $id = $this->get_language_preference_id();
+    public function getLanguagePreference() {
+        $id = $this->getLanguagePreferenceId();
         $allLanguages = $this->get_languages_all();
 
         // set default
@@ -158,19 +158,8 @@ ORDER BY languages.id asc
      * Get language ID from preferences.
      * @return integer language ID, 0 if no preference was found.
      */
-    public function get_language_preference_id() {
-        $preferences = $this->get_preferences();
-
-        // set default
-        $id = 0;
-
-        foreach ($preferences as $preference) {
-            if ($preference->codeName == "PreferenceLanguage") {
-                $id = intval($preference->Value);
-                break;
-            }
-        }
-        return $id;
+    public function getLanguagePreferenceId() {
+        return intval($this->getPreference('PreferenceLanguage'));
     }
 
     /**
@@ -866,7 +855,19 @@ ORDER BY Value asc
         return $rows;
       }
 
-
+    /**
+     * Get value of a user's preference.
+     * @param string $name codeName of preference.
+     * @return mixed preference value, null if preference not set.
+     */
+    public function getPreference($name) {
+        $preferences = $this->get_preferences();
+        foreach ($preferences as $preference) {
+            if ($preference->codeName == $name) {
+                return $preference->Value;
+            }
+        }
+    }
 
     /**
      * returns count of profile visitors

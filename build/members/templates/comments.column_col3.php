c9ff12c2a518a807d8d94f885d65f7bf63cc3713
lupochen <micha.dettbarn@gmail.com>
2011-12-30 17:37:05 +0100

    Fix Ticket #1577

--- build/members/templates/comments.column_col3.php
+++ build/members/templates/comments.column_col3.php
@@ -1,7 +1,7 @@
 <?php 
 $comment_byloggedinmember = $this->member->get_comments_commenter($this->loggedInMember->id);
 if ($this->loggedInMember && !$this->myself) { 
-    if ($comment_byloggedinmember[0]) { ?>
+    if ($comment_byloggedinmember && $comment_byloggedinmember[0]) { ?>
   <p><a href="members/<?=$username?>/comments/add" class="button"><?=$words->get('editcomments')?></a></p>
   <? } else { ?>
   <p><a href="members/<?=$username?>/comments/add" class="button"><?=$words->get('addcomments')?></a></p>

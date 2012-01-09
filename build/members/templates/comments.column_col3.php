0a60da2a1e5899545eedffb846def83541e888e5
meinhard <meinhard@benn.org>
2012-01-09 13:52:58 +0200

    no nead to show 'edit comments' at the top, because we have an inline edit button now (ticket #1577)

--- build/members/templates/comments.column_col3.php
+++ build/members/templates/comments.column_col3.php
@@ -2,16 +2,14 @@
 $comment_byloggedinmember =
     $this->member->get_comments_commenter($this->loggedInMember->id);
 
-if ($this->loggedInMember && !$this->myself) {
-    if ($comment_byloggedinmember && $comment_byloggedinmember[0]) {
-        echo '  <p><a href="members/' . $username
-            . '/comments/add" class="button">' . $words->get('editcomments')
-            . '</a></p>' . "\n";
-    } else {
-        echo '  <p><a href="members/' . $username
-            . '/comments/add" class="button">' . $words->get('addcomments')
-            . '</a></p>' . "\n";
-    }
+if ($this->loggedInMember && !$this->myself
+    && count($comment_byloggedinmember) == 0) {
+
+    // Show "Add comment" button
+    echo '  <p><a href="members/' . $username
+        . '/comments/add" class="button">' . $words->get('addcomments')
+        . '</a></p>' . "\n";
 }
+
 require_once 'comment_template.php';
 ?>

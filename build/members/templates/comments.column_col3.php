891139b8ed4bd5c5b35f214e6ebef2f77aac1f57
meinhard <meinhard@benn.org>
2012-01-09 19:39:42 +0200

    fix php notice (comment #1577)

--- build/members/templates/comments.column_col3.php
+++ build/members/templates/comments.column_col3.php
@@ -1,14 +1,14 @@
 <?php
-$comment_byloggedinmember =
-    $this->member->get_comments_commenter($this->loggedInMember->id);
+if ($this->loggedInMember && !$this->myself) {
+    $comment_byloggedinmember = $this->member->get_comments_commenter(
+        $this->loggedInMember->id);
 
-if ($this->loggedInMember && !$this->myself
-    && count($comment_byloggedinmember) == 0) {
-
-    // Show "Add comment" button
-    echo '  <p><a href="members/' . $username
-        . '/comments/add" class="button">' . $words->get('addcomments')
-        . '</a></p>' . "\n";
+    if(count($comment_byloggedinmember) == 0) {
+      // Show "Add comment" button
+      echo '  <p><a href="members/' . $username
+          . '/comments/add" class="button">' . $words->get('addcomments')
+          . '</a></p>' . "\n";
+    }
 }
 
 require_once 'comment_template.php';

575331ee6756e6ffadf75e3c5c02fbe60f277577
meinhard <meinhard@benn.org>
2012-01-09 13:33:17 +0200

    cleaning up awful php/html mix

--- build/members/templates/comments.column_col3.php
+++ build/members/templates/comments.column_col3.php
@@ -1,10 +1,17 @@
 <?php 
-$comment_byloggedinmember = $this->member->get_comments_commenter($this->loggedInMember->id);
+$comment_byloggedinmember =
+    $this->member->get_comments_commenter($this->loggedInMember->id);
+
 if ($this->loggedInMember && !$this->myself) { 
-    if ($comment_byloggedinmember && $comment_byloggedinmember[0]) { ?>
-  <p><a href="members/<?=$username?>/comments/add" class="button"><?=$words->get('editcomments')?></a></p>
-  <? } else { ?>
-  <p><a href="members/<?=$username?>/comments/add" class="button"><?=$words->get('addcomments')?></a></p>
-  
-<? } } ?>
-<?php require_once 'comment_template.php' ?>
+    if ($comment_byloggedinmember && $comment_byloggedinmember[0]) {
+        echo '  <p><a href="members/' . $username
+            . '/comments/add" class="button">' . $words->get('editcomments')
+            . '</a></p>' . "\n";
+    } else {
+        echo '  <p><a href="members/' . $username
+            . '/comments/add" class="button">' . $words->get('addcomments')
+            . '</a></p>' . "\n";
+    }
+}
+require_once 'comment_template.php'
+?>

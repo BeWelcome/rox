86fca799fd0aeadbc59090ebfc1032062f702c2e
meinhard <meinhard@benn.org>
2012-05-06 22:08:39 +0300

    delete button to the right (ticket: #1580)

--- build/members/templates/relation_item.php
+++ build/members/templates/relation_item.php
@@ -4,10 +4,11 @@
       <img class="framed" src="members/avatar/<?php echo $rel->Username; ?>?xs" height="50px" width="50px" alt="Profile">
     </a>
   </span>
+<?php if ($myself): ?>
+  <span class="float_right">
+    <a class="button" href="<?php echo PVars::getObj('env')->baseuri . "members/" . $username . "/relations?delete&IdRelation=".$rel->id; ?>" onclick="return confirm('Are you sure?');"><?php echo $words->get('Delete'); ?></a>
+  </span>
+<? endif; ?>
   <a href="<?php echo PVars::getObj('env')->baseuri."members/" . $rel->Username; ?>" ><?php echo $rel->Username; ?></a><br />
   <?php echo $purifier->purify($words->mInTrad($rel->IdTradComment, $profile_language, true)); ?>
-<?php if ($myself): ?>
-  <br />
-  <a class="button" href="<?php echo PVars::getObj('env')->baseuri . "members/" . $username . "/relations?delete&IdRelation=".$rel->id; ?>" onclick="return confirm('Are you sure?');"><?php echo $words->get('Delete'); ?></a>
-<? endif; ?>
 </li>

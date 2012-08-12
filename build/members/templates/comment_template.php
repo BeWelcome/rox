d6c682f826c184be189b0d368c109c656e22db9a
meinhard <meinhard@benn.org>
2012-08-13 03:25:40 +0900

    bonus: show 'last updated' time next to creation time (ticket #1408)

--- build/members/templates/comment_template.php
+++ build/members/templates/comment_template.php
@@ -63,7 +63,15 @@ Boston, MA  02111-1307, USA.
         <div class="comment">
             <p class="floatbox">
               <strong class="<?=$quality?>"><?=$c->comQuality?></strong><br/>
-              <span class="small grey"><?=$words->get('CommentFrom','<a href="members/'.$c->Username.'">'.$c->Username.'</a>')?> - <span title="<?php echo $c->created; ?>"><?php echo $layoutbits->ago($c->unix_created); ?></span></span>
+              <span class="small grey">
+                <?=$words->get('CommentFrom','<a href="members/'.$c->Username.'">'.$c->Username.'</a>')?> -
+                <span title="<?php echo $c->created; ?>">
+                  <?php echo $layoutbits->ago($c->unix_created); ?>
+                </span>
+                <?php if ($c->created != $c->updated): ?>
+                  (<?=$words->get('CommentLastUpdated')?>: <span title="<?php echo $c->updated; ?>"><?php echo $layoutbits->ago($c->unix_updated); ?></span>)
+                <?php endif; ?>
+              </span>
               <?php if ($this->loggedInMember) :?>
                 <a class="flagbutton" href="members/reportcomment/<?php echo $this->member->Username;?>/<?php echo $c->id;?>" title="<?=$ww->ReportCommentProblem ?>"><img src="images/icons/noun_project_flag.png" alt="<?=$ww->ReportCommentProblem ?>"></a>
               <?php endif;?>
@@ -75,11 +83,6 @@ Boston, MA  02111-1307, USA.
               <?php echo nl2br($c->TextFree); ?>
             </p>
             <p>
-              <?php if ($c->created != $c->updated): ?>
-                <em class="small">
-                  <?=$words->get('CommentLastUpdated')?>: <span title="<?php echo $c->updated; ?>"><?php echo $layoutbits->ago($c->unix_updated); ?></span>
-                </em>
-              <?php endif; ?>
               <? if ($this->loggedInMember && $c->IdFromMember == $this->loggedInMember->id): ?>
                 <a class="button small" href="members/<?= $this->member->Username ?>/comments/add" title="Edit"><?= $ww->edit ?></a>
               <? endif; ?>
@@ -121,7 +124,16 @@ Boston, MA  02111-1307, USA.
             <div class="comment">
                 <p class="floatbox">
                   <strong class="<?=$cc->comQuality?>"><?=$cc->comQuality?></strong><br/>
-                  <span class="small grey"><?= $words->get('CommentFrom', '<a href="members/' . $cc->UsernameFromMember . '">' . $cc->UsernameFromMember . '</a>') ?> <?= $words->get('CommentTo') ?> <a href="members/<?= $cc->UsernameToMember ?>"><?= $cc->UsernameToMember ?></a> - <span title="<?php echo $cc->created; ?>"><?php echo $layoutbits->ago($cc->unix_created); ?></span></span>
+                  <span class="small grey">
+                    <?= $words->get('CommentFrom', '<a href="members/' . $cc->UsernameFromMember . '">' . $cc->UsernameFromMember . '</a>') ?> <?= $words->get('CommentTo') ?> <a href="members/<?= $cc->UsernameToMember ?>"><?= $cc->UsernameToMember ?></a>
+                    <br>
+                    <span title="<?php echo $cc->created; ?>">
+                      <?php echo $layoutbits->ago($cc->unix_created); ?>
+                    </span>
+                    <?php if ($cc->created != $cc->updated): ?>
+                      (<?=$words->get('CommentLastUpdated')?>: <span title="<?php echo $cc->updated; ?>"><?php echo $layoutbits->ago($cc->unix_updated); ?></span>)
+                    <? endif; ?>
+                  </span>
                   <?php if ($this->loggedInMember) :?>
                     <a class="flagbutton" href="members/reportcomment/<?php echo $cc->UsernameToMember; ?>/<?php echo $cc->id;?>/<?php echo $cc->UsernameFromMember; ?>" title="<?=$ww->ReportCommentProblem ?>"><img src="images/icons/noun_project_flag.png" alt="<?=$ww->ReportCommentProblem ?>"></a>
                   <?php endif;?>
@@ -133,11 +145,6 @@ Boston, MA  02111-1307, USA.
                   <?php echo nl2br($cc->TextFree); ?>
                 </p>
                 <p>
-                  <?php if ($cc->created != $cc->updated): ?>
-                    <em class="small">
-                      <?=$words->get('CommentLastUpdated')?>: <span title="<?php echo $cc->updated; ?>"><?php echo $layoutbits->ago($cc->unix_updated); ?></span>
-                    </em>
-                  <? endif; ?>
                   <? if ($this->loggedInMember && $cc->IdFromMember == $this->loggedInMember->id): ?>
                     <a class="button" href="members/<?= $cc->UsernameToMember ?>/comments/add" title="Edit"><?= $ww->edit ?></a>
                   <? endif; ?>

bc7026898cc40200271ba01f59fffb28cd6b5865
Antti Siponen <antti.siponen@iki.fi>
2012-09-25 18:15:40 +0200

    [1750] html escape two links that copy parts of the request in blogs

--- build/blog/templates/sidebar_rss.php
+++ build/blog/templates/sidebar_rss.php
@@ -5,5 +5,5 @@ $requestStr = implode('/', $request);
 ?>
 <h3><?=$words->get('Meta')?></h3>
 <ul class="linklist">
-    <li><a href="rss/<?=$requestStr?>"><img src="images/icons/feed.png" alt="<?=$words->get('GetRSSFeed')?>" /> <?=$words->get('BlogEntriesRSS')?></a></li>
+    <li><a href="rss/<?=htmlspecialchars($requestStr, ENT_QUOTES)?>"><img src="images/icons/feed.png" alt="<?=$words->get('GetRSSFeed')?>" /> <?=$words->get('BlogEntriesRSS')?></a></li>
 </ul>

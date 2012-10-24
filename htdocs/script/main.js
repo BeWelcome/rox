ec9ab1bd048dd0b33a23f4b97e3343b0ed431489
meinhard <meinhard@benn.org>
2012-10-24 14:38:34 +0900

    [1776] local jquery for privacy reasons, upgrade jquery from 1.7.2 to 1.8.2 (latest)

--- htdocs/script/main.js
+++ htdocs/script/main.js
@@ -92,8 +92,7 @@ var bwrox = new BWRox;
 bwrox.selectScripts([
   {
     // JQuery has to be included before prototype to avoid conflicts
-    file: "http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js",
-    remote: true,
+    file: "jquery-1.8.2.min.js",
     pages: ["searchmembers", "signup/3", "setlocation", "blog", "trip"]
   },
   {

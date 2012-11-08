c8e73e2ab2eeec4eed46576b77f30d49c93eded8
crumbking <kingcrumb@web.de>
2012-11-08 02:23:20 +0100

    [SingleTripView] Extending the single trip view sidebar with actions, adding translations everywhere

--- build/trip/templates/singletrip_sidebar.php
+++ build/trip/templates/singletrip_sidebar.php
@@ -1,22 +1,16 @@
-<?php
+<?php 
 $words = new MOD_words();
+$member = $this->_model->getLoggedInMember();
+if ($member)
+{
+?>
+          <h3><?=$words->get('TripsSingleTripActionsHeadline')?></h3>
+          <ul class="linklist">
+			<li><a href="trip/show/<?=$member->Username?>" title="<?=$words->get('TripsShowMy')?>"><img src="images/icons/world.png" alt="<?=$words->get('TripsShowMy')?>" /></a> <a href="trip/show/<?=$member->Username?>"><?=$words->get('TripsShowMy')?></a></li>
+            <li><a href="trip/create" title="<?=$words->get('TripsShowMy')?>"><img src="images/icons/world_add.png" alt="<?=$words->get('TripTitle_create')?>" /></a> <a href="trip/create"><?=$words->get('TripTitle_create')?></a></li>
+<?php if (isset($trip->gallery_id_foreign) && $trip->gallery_id_foreign) {
+        echo '<li><a href="gallery/show/galleries/'.$trip->gallery_id_foreign.'" title="'.$words->get('Trip_GalleryAddPhotos').'"><img src="images/icons/picture_add.png"> '.$words->get('Trip_GalleryAddPhotos').'</a></li>';
+}?>
+          </ul>
+<?php } ?>
 
-// This trip's gallery  
-
-if (isset($trip->gallery_id_foreign) && $trip->gallery_id_foreign) {
-    $gallery = new GalleryModel;
-    $statement = $gallery->getLatestItems('',$trip->gallery_id_foreign);
-    if ($statement) {
-        echo '<h3>' . $words->get('TripPicturesOfThisTrip') . '</h3>';
-        // if the gallery is NOT empty, go show it
-        $p = PFunctions::paginate($statement, 1, $itemsPerPage = 8);
-        $statement = $p[0];
-        foreach ($statement as $d) {
-        	echo '<a href="gallery/show/image/'.$d->id.'"><img src="gallery/thumbimg?id='.$d->id.'" alt="image" style="height: 50px; width: 50px; padding:2px;"/></a>';
-        }
-    	echo '<p><a href="gallery/show/galleries/'.$trip->gallery_id_foreign.'" title="'.$words->getSilent('TripShowPhotoset').'"><img src="images/icons/picture.png"> '.$words->get('TripShowPhotoset').'</a></p>';
-    } elseif ($isOwnTrip) {
-        echo '<p><a href="gallery/show/galleries/'.$trip->gallery_id_foreign.'" title="'.$words->getSilent('Trip_GalleryAddPhotos').'"><img src="images/icons/picture_add.png"> '.$words->get('Trip_GalleryAddPhotos').'</a></p>';
-    }
-    echo $words->flushBuffer();
-}

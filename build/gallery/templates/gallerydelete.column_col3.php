e2ed5013b0b07a701bec1de384a67ff900317bb5
Sander Ruitenbeek <sander@grids.be>
2012-12-21 15:18:55 +0100

    [1824] fix for untranslatable buttons and texts, thanks to mahouni for pointers

--- build/gallery/templates/gallerydelete.column_col3.php
+++ build/gallery/templates/gallerydelete.column_col3.php
@@ -3,5 +3,5 @@
 /* Template for showing the deletion page of a single gallery */
 
 ?>
-    <p><?=$words->get("You're about to delete the album '%s'. Are you sure?", $this->gallery->name)?></p>
+    <p><?=$words->get('DeleteAlbumSure', $this->gallery->name)?></p>
     <p><a class="button" href="gallery/show/sets/<?=$this->gallery->id?>/delete/true" ><?=$words->get('Yep, just delete it!')?></a> <a class="button" href="gallery/show/sets/<?=$this->gallery->id?>" ><?=$words->get('Cancel')?></a></p>

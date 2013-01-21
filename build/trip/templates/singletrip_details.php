42fcf53e0e1102c4904dbd1e834eac0330206e70
crumbking <kingcrumb@web.de>
2013-01-21 20:11:17 +0100

    [1672] Adding a show my trips link on all other trip pages for better navigation

--- build/trip/templates/singletrip_details.php
+++ build/trip/templates/singletrip_details.php
@@ -17,6 +17,9 @@ if ($member)
 ?>
           <ul>
             <li class="float_left"><a href="trip/create" title="<?=$words->get('TripTitle_create')?>"><img src="images/icons/world_add.png" style="vertical-align:bottom;" alt="<?=$words->get('TripTitle_create')?>" /></a> <a href="trip/create" title="<?=$words->get('TripTitle_create')?>"><?=$words->get('TripTitle_create')?></a></li>
+    <?php if ($member && !$isOwnTrip) { ?>            
+            <li class="float_left"><a href="trip/show/<?=$member->Username?>" title="<?=$words->get('TripsShowMy')?>"><img src="images/icons/world.png" style="vertical-align:bottom;" alt="<?=$words->get('TripsShowMy')?>" /></a> <a href="trip/show/<?=$member->Username?>" title="<?=$words->get('TripsShowMy')?>"><?=$words->get('TripsShowMy')?></a></li>
+    <?php    }?>
     <?php if ($isOwnTrip) { ?>
             <li class="float_left"><a href="trip/edit/<?=$trip->trip_id; ?>"><img src="styles/css/minimal/images/iconsfam/pencil.png" style="vertical-align:bottom;" alt="<?=$words->get('Trip_EditMyTrip')?>" /></a> <a href="trip/edit/<?=$trip->trip_id; ?>" title="<?=$words->get('Trip_EditMyTrip')?>"><?=$words->get('Trip_EditMyTrip')?></a></li>
             <li class="float_left"><a href="trip/del/<?=$trip->trip_id; ?>"><img src="styles/css/minimal/images/iconsfam/delete.png" style="vertical-align:bottom;" alt="<?=$words->get('Trip_DeleteMyTrip')?>" /></a> <a href="trip/del/<?=$trip->trip_id; ?>" title="<?=$words->get('Trip_DeleteMyTrip')?>"><?=$words->get('Trip_DeleteMyTrip')?></a></li>

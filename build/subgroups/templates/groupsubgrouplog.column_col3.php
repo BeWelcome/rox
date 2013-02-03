006debb46d72a34382577371c82304408a0fbb03
mahouni-rox <mahouni@gmx.ch>
2013-02-03 20:18:08 +0100

    [1695] code review: use template for subgroup log

--- build/subgroups/templates/groupsubgrouplog.column_col3.php
+++ build/subgroups/templates/groupsubgrouplog.column_col3.php
@@ -19,5 +19,94 @@ along with this program; if not, see <http://www.gnu.org/licenses/> or
 write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, 
 Boston, MA  02111-1307, USA.
 */
-
 ?>
+<fieldset>
+    <legend><?= $words->get('AdministrateSubgroupsTitle'); ?></legend>
+    <div class="subcolumns">
+    <h3><?php echo $words->getFormatted('CurrentSubgroupsTitle');?></h3>
+        <ul class="floatbox">
+            <?php 
+            $subgroups = $this->group->findSubgroups($groupId);
+            foreach ($subgroups as $group_data) { 
+                if (strlen($group_data->Picture) > 0) {
+                    $img_link = "groups/thumbimg/{$group_data->getPKValue()}";
+                } else {
+                    $img_link = "images/icons/group.png";
+                } ?>
+                <li class="picbox_subgroup float_left">
+                    <a href="groups/<?php echo $group_data->getPKValue() ?>">
+                        <img class="framed_subgroup float_left" alt="Group" src="<?php echo $img_link; ?>"/>
+                    </a>
+                    <div class="userinfo"><span class="small">
+                    <h4><a href="groups/<?php echo $group_data->getPKValue() ?>"><?php echo htmlspecialchars($group_data->Name, ENT_QUOTES) ?></a></h4>
+                        <?php echo $words->get('GroupsMemberCount');?>: <?php echo $group_data->getMemberCount(); ?><br />
+                        <?php echo $words->get('GroupsNewMembers');?>: <?php echo count($group_data->getNewMembers()) ; ?><br />
+                    </span></div> <!-- userinfo -->
+                </li> <!-- userpicbox_subgroup -->
+                <?php
+            } ?>
+        </ul>   
+    </div><!-- subcolumns -->
+    <div class="subcolumns">   
+        <?php $logvar = $this->logs;?>
+    </div><!-- subcolumns -->
+    <div class="subcolumns"> <?php
+        if ($this->isGroupMember())  {
+            $add_button_link = "groups/{$this->group->id}/selectsubgroup";
+            $add_button_word = $words->get('AddSubgroupButton');
+            $delete_button_link = "groups/{$this->group->id}/selectdeletesubgroup";
+            $delete_button_word = $words->get('RemoveSubgroupButton');
+            ?>
+            <div class="c50l">
+                <div class="subcl">
+                    <hr>
+                        <a class="button" href="<?php echo $add_button_link; ?>">
+                            <span><?php echo $add_button_word; ?></span>
+                        </a>
+                    <hr>
+                </div><!-- subcl -->
+            </div><!-- c50l -->
+            <div class="c50r">
+                <div class="subcr"> <?php 
+                    if (!empty($subgroups)) { ?>
+                        <hr>
+                            <a class="button" href="<?php echo $delete_button_link; ?>">
+                                <span><?php echo $delete_button_word; ?></span>
+                            </a>
+                        <hr> <?php
+                    } ?>
+                </div><!-- subcr -->
+            </div><!-- c50r --> <?php
+        } ?>
+    </div><!-- subcolumns -->
+    <?php
+    if ($this->isGroupAdmin) {
+        ?>        
+        <div class="subcolumns">
+            <br />
+            <h4> <?php echo $words->get('NbOfLogEntries', count($logvar)); ?> </h4>
+            <ul class="floatbox"> <?php
+                foreach ($logvar as &$value) { ?>
+                    <li class="picbox_subgroup float_left">
+                        <img class="framed_subgroup float_left" src="members/avatar/<?php echo $value->member->Username; ?>?xs"/>
+                        <div class="userinfo">
+                            <span class="small">
+                                <?php
+                                $layoutbits->ago(strtotime($value->ts));
+                                ?>
+                                <br /><?php 
+                                $memberlink = '<a href="members/' . $value->member->Username . '">' . $value->member->Username . '</a>';
+                                $grouplink =  '<a href="groups/' . $value->subgroup->getPKValue() . '">' . htmlspecialchars($value->subgroup->Name, ENT_QUOTES) . '</a>'; 
+                                $logentry = $words->get($value->SubgroupAction, $memberlink, $grouplink);
+                                echo $logentry; ?><br />
+                            </span>
+                        </div>
+                    </li> <?php
+                } ?>
+            </ul>  <!-- floatbox --> 
+        </div><!-- subcolumns -->  
+        <?php
+    }
+    ?>    
+</fieldset>
+

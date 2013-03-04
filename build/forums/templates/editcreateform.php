6abc155f8039573edea4d146cfe6fc92667c1861
mahouni-rox <mahouni@gmx.ch>
2013-03-04 20:28:44 +0100

    [1821] fix fontsize in tinyMCE editor for bulleted lists and for blog and trip module aswell, use cache breaker

--- build/forums/templates/editcreateform.php
+++ build/forums/templates/editcreateform.php
@@ -55,7 +55,7 @@ tinyMCE.init({
     elements: "topic_text",
     plugins : "advimage,preview,fullscreen,autolink",
     theme: "advanced",
-    content_css : http_baseuri + "styles/css/minimal/screen/custom/tinymce_content.css",    
+    content_css : http_baseuri + "styles/css/minimal/screen/custom/tinymce_content.css?1",
     relative_urls:false,
     convert_urls:false,
     theme_advanced_buttons1: "bold,italic,underline,strikethrough,separator,bullist,numlist,separator,forecolor,backcolor,charmap,link,image,separator,preview,fullscreen",

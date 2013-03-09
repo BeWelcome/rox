745a8f532b43e17b538aa48134fbe88d81c46ea9
crumbking <kingcrumb@web.de>
2013-03-09 23:20:15 +0100

    [1793] Adding yaml form styles

--- build/members/templates/addnote.column_col3.php
+++ build/members/templates/addnote.column_col3.php
@@ -10,7 +10,6 @@
     $categories = $this->loggedInMember->getNoteCategories();
     $mem_redirect = $this->layoutkit->formkit->getMemFromRedirect();
     if (!$mem_redirect) {
-        echo "!mem";
         $note = $this->loggedInMember->getNote($this->member->id);
         $edit_mode = isset($note);
         if (!$edit_mode) {
@@ -43,58 +42,45 @@
     $callback_tag = $formkit->setPostCallback('MembersController', 'addnoteCallback');
     ?>
     
-    <form method="post" name="addnote">
+    <form method="post" name="addnote" class="yform">
     <?=$callback_tag ?>
-    <fieldset>
-    <legend><?php 
+    <div class="row">
+    <h2><?php 
     if (!$edit_mode) {
         echo $words->get("ProfileNoteAddNote");
     } else {
         echo $words->get("ProfileNoteUpdateNote");
     }
-    ?></legend>
+    ?></h2>
+    </div>
         <input name="IdMember" value="<?=$member->id?>" type="hidden" />
-        <table>
-          <tr>
-            <td colspan="2"><h3><?=$words->get("ProfileNoteCategory")?></h3>
-          </tr>
-          <tr>
-            <td>
-                <select name="ProfileNoteCategory" id="ProfileNoteCategory" style="font-size: 1.1em; width: 20em;">
-                    <option value=""></option>
-                    <?php foreach($categories as $category) {
-                        $catoption = '<option value="' . $category . '"';
-                        if ($category == $note->Category) {
-                            $catoption .= ' selected="selected"';
-                        }
-                        $catoption .= '>' . $category . '</option>';
-                        echo $catoption . "\n";
-                    } 
-                    ?>
-                </select>
-            </td>
-            <td rowspan="2" class="grey" style="vertical-align: top;">
-                <?=$words->get("ProfileNoteCategoryInfo")?>
-            </td>
-          </tr>
-        <tr>
-          <td>
-            <textarea name="ProfileNoteCategoryFree" id="ProfileNoteCategoryFree" cols="40" rows="1"><?php if (!in_array($note->CategoryFree, $categories)) { echo $note->CategoryFree; } ?></textarea>
-          </td>
-        </tr>
-        <tr>
-        <td>
+        <div class="type-select">
+        <label><?=$words->get("ProfileNoteCategory")?></label>
+            <select name="ProfileNoteCategory" id="ProfileNoteCategory">
+                        <option value=""></option>
+                        <?php foreach($categories as $category) {
+                            $catoption = '<option value="' . $category . '"';
+                            if ($category == $note->Category) {
+                                $catoption .= ' selected="selected"';
+                            }
+                            $catoption .= '>' . $category . '</option>';
+                            echo $catoption . "\n";
+                        } 
+                        ?>
+            </select>
+            
+        </div>
+        <div class="type-text">
+            <label for="ProfileNoteCategoryFree"><?=$words->get("ProfileNoteCategoryFree")?></label>
+            <input name="ProfileNoteCategoryFree" id="ProfileNoteCategoryFree"><?php if (!in_array($note->CategoryFree, $categories)) { echo $note->CategoryFree; } ?></textarea>
+        </div>
+        <div class="type-text">
+        <label for="ProfileNoteComment"><?php echo $words->get("ProfileNoteCommentInfo") ?></label>
         <textarea name="ProfileNoteComment" id="ProfileNoteComment" cols="40" rows="8"><?php echo $note->Comment; ?></textarea>
-        </td>
-        <td class="grey" style="vertical-align: top;">
-        <?php echo $words->get("ProfileNoteCommentInfo") ?>
-        </td>
-    </tr>
-    <tr><td colspan="2">
+        </div>
+        <div class="type-button">
         <input type="submit" id="submit" name="submit" value="<?php echo $words->getFormatted('ProfileNoteSubmit'); ?>"></td>
-    </tr>
-    </table>
-    </fieldset>
+        </div>
     </form>
     <?=$words->flushBuffer();?>
 </div>

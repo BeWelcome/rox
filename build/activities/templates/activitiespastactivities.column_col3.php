91be80ea8ff47fc4d0b749e9a0cc8cf04ff97ab6
crumbking <kingcrumb@web.de>
2013-04-12 18:13:10 +0200

    [2003] Remove the button in all lists

--- build/activities/templates/activitiespastactivities.column_col3.php
+++ build/activities/templates/activitiespastactivities.column_col3.php
@@ -1,9 +1,3 @@
-<div class="floatbox">
-    <div class="float_right">
-        <a class="bigbutton" href="activities/create"><span><?= $words->get('ActivityCreateNew'); ?></span></a>
-    </div>
-</div>
-<div class="row>">
 <?php 
 if (count($this->activities) == 0) {
     if ($this->public) {
--- build/activities/templates/activitiesshow.column_col3.php
+++ build/activities/templates/activitiesshow.column_col3.php
@@ -9,9 +9,6 @@ if ($this->activity->status == 1) {
 <div id="activity">
     <div class="floatbox">
         <h2 class="float_left" style="width: 75%;"><?php echo $this->activity->title; ?></h2>
-        <div class="float_right">
-            <a class="bigbutton" href="activities/create"><span><?= $words->get('ActivityCreateNew'); ?></span></a>
-        </div>
     </div>
     <div class="subcolumns">
         <div class="c62l">
@@ -33,7 +30,7 @@ if ($this->activity->status == 1) {
                         <div class="subcolumns">
                             <div class="c38l">
                                 <div class="subcl">
-                                    <h3 class="abitlower"><?= $words->get('ActivityMyStatus'); ?>:</h3>
+                                    <h3 class="abitlower"><?= $words->get('ActivityMyStatus'); ?></h3>
                                 </div> <!-- subcl -->
                             </div> <!-- c38l -->
                             <div class="c62r">

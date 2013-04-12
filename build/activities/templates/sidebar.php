c5bf317fd002ac4db26b9284e1bc7c8a42011736
crumbking <kingcrumb@web.de>
2013-04-12 19:03:38 +0200

    [2003] Cleanup

--- build/activities/templates/sidebar.php
+++
@@ -1,8 +0,0 @@
-<h3><?= $words->get('ActivitiesActions'); ?></h3>
-<ul class="linklist">
-<?php
-    foreach($this->sidebarItems as $sidebarItem) {
-        echo '<li><a href="' . $sidebarItem["href"] . '">' . $words->get($sidebarItem["wordCode"]) . '</a></li>';
-    }
-?>
-</ul>

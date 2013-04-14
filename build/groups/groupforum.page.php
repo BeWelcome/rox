59f08f8aaf5f4cff0200c9d5bc867fd3700dd361
Antti Siponen <antti.siponen@iki.fi>
2013-04-14 21:37:08 +0200

    [1991] Add groups search bar and create new group button to the group forums overview page

--- build/groups/groupforum.page.php
+++ build/groups/groupforum.page.php
@@ -66,6 +66,16 @@ class GroupForumsOverviewPage extends GroupsBasePage
 {
     protected function column_col3()
     {
+        $words = $this->getWords();
+
+    ?>
+        <div>
+            <h3><?= $words->get('GroupsSearchHeading'); ?></h3>
+            <form action="groups/search" method="get">
+                <input type="text" name="GroupsSearchInput" value="" id="GroupsSearchInput" /><input type="submit" value="<?= $words->get('GroupsSearchSubmit'); ?>" /><br />
+            </form>
+        </div>             
+    <?php 
         $Forums = new ForumsController;
         $Forums->index();
     }

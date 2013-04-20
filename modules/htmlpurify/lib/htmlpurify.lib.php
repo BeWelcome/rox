bbb1966ac0a7fa874e606cd2d1aad96660c47150
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2013-04-20 18:04:34 +0200

    [2003] Really allow images

--- modules/htmlpurify/lib/htmlpurify.lib.php
+++ modules/htmlpurify/lib/htmlpurify.lib.php
@@ -95,7 +95,9 @@ class MOD_htmlpure
     {
         require_once(SCRIPT_BASE . 'lib/htmlpurifier-4.0.0/library/HTMLPurifier.standalone.php');
         $config = HTMLPurifier_Config::createDefault();
-        $config->set('HTML.Allowed', 'p,b,a[href],br,i,strong,em,ol,ul,li,dl,dt,dd,img');
+        $config->set('HTML.Allowed', 'p,b,a[href],br,i,strong,em,ol,ul,li,dl,dt,dd,img[src|alt|width|height]');
+        $config->set('HTML.MaxImgLength', '500');
+        $config->set('CSS.MaxImgLength', '500px');
         $config->set('AutoFormat.AutoParagraph', true); // automatically turn double newlines into paragraphs
         $config->set('AutoFormat.Linkify', true); // automatically turn stuff like http://domain.com into links
         return new HTMLPurifier($config);

55bc9fcabf82b7b0f34f0a561cc03ab8e92bd822
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2013-04-20 16:18:18 +0200

    [2003] Allow images in the description of an activity

--- modules/htmlpurify/lib/htmlpurify.lib.php
+++ modules/htmlpurify/lib/htmlpurify.lib.php
@@ -80,12 +80,22 @@ class MOD_htmlpure
         $config->set('AutoFormat.AutoParagraph', true);
         return new HTMLPurifier($config);
     }
-    
+
     public function getAdvancedHtmlPurifier()
     {
         require_once(SCRIPT_BASE . 'lib/htmlpurifier-4.0.0/library/HTMLPurifier.standalone.php');
         $config = HTMLPurifier_Config::createDefault();
         $config->set('HTML.Allowed', 'p,b,a[href],br,i,strong,em,ol,ul,li,dl,dt,dd');
+        $config->set('AutoFormat.AutoParagraph', true); // automatically turn double newlines into paragraphs
+        $config->set('AutoFormat.Linkify', true); // automatically turn stuff like http://domain.com into links
+        return new HTMLPurifier($config);
+    }
+
+    public function getActivitiesHtmlPurifier()
+    {
+        require_once(SCRIPT_BASE . 'lib/htmlpurifier-4.0.0/library/HTMLPurifier.standalone.php');
+        $config = HTMLPurifier_Config::createDefault();
+        $config->set('HTML.Allowed', 'p,b,a[href],br,i,strong,em,ol,ul,li,dl,dt,dd,img');
         $config->set('AutoFormat.AutoParagraph', true); // automatically turn double newlines into paragraphs
         $config->set('AutoFormat.Linkify', true); // automatically turn stuff like http://domain.com into links
         return new HTMLPurifier($config);

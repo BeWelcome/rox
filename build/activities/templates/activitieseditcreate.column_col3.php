2bd338b07694e2849789cf86bc03a504a4480b18
crumbking <kingcrumb@web.de>
2013-04-29 19:00:29 +0200

    [2003] Mark mandatory fields

--- build/activities/templates/activitieseditcreate.column_col3.php
+++ build/activities/templates/activitieseditcreate.column_col3.php
@@ -48,11 +48,11 @@ if (!empty($errors)) {
 }
 ?>
     <div class="row">
-        <label for="activity-title"><?php echo $words->get('ActivityTitle'); ?>:</label><br />
+        <label class="float_left"for="activity-title"><?php echo $words->get('ActivityTitle'); ?>*</label><span class="small float_right" style="margin-right: 0.3em;">* <?php echo $words->get('ActivityMandatoryFields'); ?></span><br />
         <input type="text" id="activity-title" name="activity-title" maxlength="80" class="long" style="width:99%" value="<?php echo $vars['activity-title']; ?>" />
     </div>
     <div class="row">
-        <label for="activity-location"><?php echo $words->get('ActivityLocation'); ?>:</label><br/>
+        <label for="activity-location"><?php echo $words->get('ActivityLocation'); ?>*</label><br/>
         <input type="text" id="activity-location" name="activity-location" class="long" value="<?php echo $vars['activity-location']; ?>" style="width:70%" />
         <input class="button" type="submit" id="activity-location-button" name="activity-location-button" value="<?php echo $words->getBuffered('ActivitiesLocationSearch'); ?>" /><?php echo $words->flushBuffer(); ?> 
     </div>
@@ -60,21 +60,21 @@ if (!empty($errors)) {
         <ol id="locations" class="plain"></ol>
     </div>
     <div class="row">
-        <label for="activity-address"><?php echo $words->get('ActivityAddress'); ?>:</label><br/>
+        <label for="activity-address"><?php echo $words->get('ActivityAddress'); ?>*</label><br/>
         <textarea id="activity-address" name="activity-address" rows="3" cols="80" class="long" style="width:99%" ><?php echo $vars['activity-address']; ?></textarea>
     </div>
     <div class="subcolumns row">
     <div class="c50l"><div class="subcl">
-        <label for="activity-start-date"><?php echo $words->get('ActivityStart'); ?>:</label><br />
+        <label for="activity-start-date"><?php echo $words->get('ActivityStart'); ?>*</label><br />
         <input type="text" id="activity-start-date" name="activity-start-date" class="date" maxlength="16" style="width:90%" value="<?php echo $vars['activity-start-date'];?>" />
         </div></div>
         <div class="c50r"><div class="subcr">
-        <label for="activity-end-date"><?php echo $words->get('ActivityEnd'); ?>:</label><br />
+        <label for="activity-end-date"><?php echo $words->get('ActivityEnd'); ?>*</label><br />
         <input type="text" id="activity-end-date" name="activity-end-date" class="time" maxlength="16" style="width:98%" value="<?php echo $vars['activity-end-date']; ?>" />
         </div></div>
     </div>
     <div class="subcolumns row">
-        <label for="activity-description"><?php echo $words->get('ActivityDescription'); ?>:</label><br/>
+        <label for="activity-description"><?php echo $words->get('ActivityDescription'); ?>*</label><br/>
         <textarea id="activity-description" name="activity-description" rows="10" cols="80" style="width:99%"><?php echo $vars['activity-description']; ?></textarea>
     </div>
     <div class="subcolumns row">

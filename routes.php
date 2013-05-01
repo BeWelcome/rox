d412b7fb5caaf7bd54730882d6abd9637a5b6287
This is me on Mount Everest <bla@blafaselblubb.abcde.biz>
2013-05-01 12:01:50 +0200

    [2003] Cleanup

--- routes.php
+++ routes.php
@@ -135,8 +135,6 @@
     $this->addRoute('activities_search', 'activities/search', 'ActivitiesController', 'search');
     $this->addRoute('activities_search_results', 'activities/search/:keyword:', 'ActivitiesController', 'search');
     $this->addRoute('activities_search_results_pages', 'activities/search/:keyword:/page/:pageno:', 'ActivitiesController', 'search');
-    // $this->addRoute('activities_search_results_show', 'activities/search/:keyword:/:id:', 'ActivitiesController', 'show');    
-    // $this->addRoute('activities_search_results_show_pages', 'activities/search/:keyword:/page/:pageno:/:id:', 'ActivitiesController', 'show');    
     $this->addRoute('activities_create', 'activities/create', 'ActivitiesController', 'editcreate');
     $this->addRoute('activities_upcoming_activities', 'activities/upcoming', 'ActivitiesController', 'upcomingActivities');
     $this->addRoute('activities_upcoming_activities_pages', 'activities/upcoming/page/:pageno:', 'ActivitiesController', 'upcomingActivities');

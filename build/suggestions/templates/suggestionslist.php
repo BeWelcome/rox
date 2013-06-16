0f4ac236fc0959786330075fbd3af7101710c907
This is me on Mount Everest <bla@blafaselblubb.abcde.biz>
2013-06-16 09:02:15 +0200

    [2027] Mixed a typo

--- build/suggestions/templates/suggestionslist.php
+++ build/suggestions/templates/suggestionslist.php
@@ -1,18 +1,23 @@
-<?php $this->pager->render(); ?>
+<?php
+if (count($this->suggestions) == 0) {
+    echo "<p><strong>" . $words->get('SuggestionsNoVotes') . "</strong></p>";
+} else {
+$this->pager->render(); ?>
 <table style="width: 99%" class='decisionslist'>
 <tr><th><?php echo $words->get('Suggestion'); ?></th><th style="text-align: center"><?php echo $words->get('SuggestionVoteEndDate'); ?></th><th style="text-align: center"><?php echo $words->get('SuggestionNbVotes'); ?></th></tr>
 <?php
-$count= 0;
-foreach($this->suggestions as $suggestion) {
-    echo '<tr class="' . $background = (($count % 2) ? 'highlight' : 'blank') . '">';
-    echo '<td style="padding-bottom: 30px; width: 80%;">';
-    echo '<h3><a href="suggestions/' . $suggestion->id . '">' . htmlspecialchars($suggestion->summary) . '</a></h3></td>';
-    echo '<td style="text-align: center;">' . date('d.m.Y', $suggestion->votingendts) . '</td>';
-    echo '<td style="text-align: center;">' . $suggestion->votes . '</td>';
-//    echo '<td style="text-align: center;"><a href="suggestions/' . $suggestion->id . '/calculate"><img src="images/icons/calculator.png" /></a></td>';
-    echo '</tr>';
-    $count++;
-}
+    $count= 0;
+    foreach($this->suggestions as $suggestion) {
+        echo '<tr class="' . $background = (($count % 2) ? 'highlight' : 'blank') . '">';
+        echo '<td style="padding-bottom: 30px; width: 80%;">';
+        echo '<h3><a href="suggestions/' . $suggestion->id . '">' . htmlspecialchars($suggestion->summary) . '</a></h3></td>';
+        echo '<td style="text-align: center;">' . date('d.m.Y', $suggestion->votingendts) . '</td>';
+        echo '<td style="text-align: center;">' . $suggestion->votes . '</td>';
+    //    echo '<td style="text-align: center;"><a href="suggestions/' . $suggestion->id . '/calculate"><img src="images/icons/calculator.png" /></a></td>';
+        echo '</tr>';
+        $count++;
+    }
 ?>
 </table>
-<?php $this->pager->render(); ?>
+<?php $this->pager->render();
+} ?>

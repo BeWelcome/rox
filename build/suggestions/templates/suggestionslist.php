7f6a453271515f166dc1a96a10f4b9a77cba0980
This is me on Mount Everest <bla@blafaselblubb.abcde.biz>
2013-06-16 07:14:51 +0200

    [2027] Added translation

--- build/suggestions/templates/suggestionslist.php
+++ build/suggestions/templates/suggestionslist.php
@@ -1,16 +1,15 @@
 <?php $this->pager->render(); ?>
 <table style="width: 99%" class='decisionslist'>
-<tr><th>Suggestion</th><th style="text-align: center">End date</th><th style="text-align: center">Votes</th></tr>
+<tr><th><?php echo $words->get('Suggestion'); ?></th><th style="text-align: center"><?php echo $words->get('SuggestionVoteEndDate'); ?></th><th style="text-align: center"><?php echo $words->get('SuggestionNbVotes'); ?></th></tr>
 <?php
 $count= 0;
 foreach($this->suggestions as $suggestion) {
-    $voteEndDate = strtotime($suggestion->votingended);
-     
     echo '<tr class="' . $background = (($count % 2) ? 'highlight' : 'blank') . '">';
     echo '<td style="padding-bottom: 30px; width: 80%;">';
     echo '<h3><a href="suggestions/' . $suggestion->id . '">' . htmlspecialchars($suggestion->summary) . '</a></h3></td>';
-    echo '<td style="text-align: center;">' . date('d.m.Y', $voteEndDate) . '</td>';
+    echo '<td style="text-align: center;">' . date('d.m.Y', $suggestion->votingendts) . '</td>';
     echo '<td style="text-align: center;">' . $suggestion->votes . '</td>';
+//    echo '<td style="text-align: center;"><a href="suggestions/' . $suggestion->id . '/calculate"><img src="images/icons/calculator.png" /></a></td>';
     echo '</tr>';
     $count++;
 }

be9270a6731d5a774ce33e7829f6594a9b06c255
sitatara <sitatara@gmx.at>
2013-07-01 21:47:00 +0200

    [1998] Removed empty FAQs from Safety FAQ page.

--- build/safety/templates/safetyfaq.column_col3.php
+++ build/safety/templates/safetyfaq.column_col3.php
@@ -1,23 +1,8 @@
 <div class="row">
 <a name="general"></a>
 <h3>General</h3>
-<p><strong>Is BeWelcome safe?</strong></p>
-<p>tbc</p>
 <p><strong><?php echo $words->get('FaqQ_IsItSafe');?></strong></p>
 <p><?php echo $words->get('FaqA_IsItSafe');?></p>
-<p><strong>I had a negative experience with another BeWelcome member. What should I do?</strong></p>
-<p>tbc</p>
-<br />
-<a name="emergencies"></a>
-<h3>Emergencies</h3>
-<p><strong>What should I do if I get into an emergency situation with my host or guest (I feel threatened/harassed or a crime has happened)?</strong></p>
-<p>tbc</p>
-<p><strong>I have been robbed by my guest/host/another member. Can you help me?</strong></p>
-<p>tbc</p>
-<p><strong>I was expecting a guest but he/she never showed up. I am worried. Can you help?</strong></p>
-<p>tbc</p>
-<p><strong>A family member is missing. He/she was travelling and staying with BeWelcome members. Can you help me find him/her?</strong></p>
-<p>tbc</p>
 <br />
 <a name="comments"></a>
 <h3>Comments</h3>
@@ -25,63 +10,12 @@
 <p><?php echo $words->get('FaqA_PurposeOfComment');?></p>
 <p><strong><?php echo $words->get('FaqQ_CanIEditComment');?></strong></p>
 <p><?php echo $words->get('FaqA_CanIEditComment');?></p>
-<p><strong>Can I delete a comment that I have written for another member?</strong></p>
-<p>tbc</p>
-<p><strong>Can I delete a comment another member has written on my profile?</strong></p>
-<p>tbc</p>
-<p><strong>Somebody posted a spam comment on my profile. What can I do?</strong></p>
-<p>tbc</p>
-<p><strong>Somebody posted a false/slanderous/abusive comment on my profile? What can I do?</strong></p>
-<p>tbc</p>
-<p><strong>Someone I have never met wrote a neutral/negative comment about me. Can you remove this?</strong></p>
-<p>tbc</p>
-<p><strong>Why is it not possible to write a positive comment for someone I only know online?</strong></p>
-<p>tbc</p>
-<br />
-<a name="verification"></a>
-<h3>Verification</h3>
-<p><strong>How does verification work?</strong></p>
-<p>tbc</p>
-<p><strong>Can you verify me?</strong></p>
-<p>tbc</p>
-<p><strong>Is it safer to stay with or host verified members only?</strong></p>
-<br />
 <a name="privacy"></a>
-<h3>Privacy</h3>
-<p><strong>Who will be able to see my personal data which I have to enter at signup?</strong></p>
-<p>tbc</p>
-<p><strong>Where can I change my privacy settings?</strong></p>
-<p>tbc</p>
-<p><strong>Can I hide my age/gender/location?</strong></p>
-<p>tbc</p>
-<p><strong>Someone visits my profile again and again. I feel stalked. Can you block this member from seeing my profile?</strong></p>
-<p>tbc</p>
-<br />
 <a name="spam"></a>
 <h3>Spam and abuse</h3>
-<p><strong>I received a spam message. What should I do?</strong></p>
-<p>tbc</p>
-<p><strong>I saw a spam message or an abusive message in the forum or groups. What should I do?</strong></p>
-<p>tbc</p>
 <p><strong><?php echo $words->get('FaqQ_IFoundAPronLink');?></strong></p>
 <p><?php echo $words->get('FaqA_IFoundAPronLink');?></p>
-<p><strong>I found a blog entry which is clearly spam or against the rules and values of the site. What should I do?</strong></p>
-<p>tbc</p>
 <p><strong><?php echo $words->get('FaqQ_DontWantToReceiveInMyEmail');?></strong></p>
 <p><?php echo $words->get('FaqA_DontWantToReceiveInMyEmail');?></p>
 <br />
-<a name="report"></a>
-<h3>Reporting safety issues and abuse</h3>
-<p><strong>How can I report a safety issue?</strong></p>
-<p>tbc</p>
-<p><strong>How can I report spam?</strong></p>
-<p>tbc</p>
-<p><strong>How can I report offensive or inappropriate messages on BeWelcome (private messages and forum or group posts)?</strong></p>
-<p>tbc</p>
-<p><strong>How can I report inappropriate or offensive profiles, pictures, blogs, trips, or activities?</strong></p>
-<p>tbc</p>
-<p><strong>What happens with my reports?</strong></p>
-<p>tbc</p>
-<p><strong>Will my reports be confidential?</strong></p>
-<p>tbc</p>
 </div>

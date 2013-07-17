03e8e0c242dfd14bb89c86d0bddb623669da808a
sitatara <sitatara@gmx.at>
2013-07-17 22:23:54 +0200

    [no ticket] fix for previous commit

--- build/rox/personalstart.page.php
+++ build/rox/personalstart.page.php
@@ -93,7 +93,6 @@ class MailboxWidget_Personalstart extends MailboxWidget_Received
         
             </a><br />
             <span class="small grey" title="<?=date('d. m. Y',$message->unixtime_created)?>"><?=$words->get('from')?> <a href="members/<?=$senderUsername?>"><?=$senderUsername?>: </a>
-            <span class="small grey" title="<?=date('d. m. Y',$message->unixtime_created)?>"><?=$words->get('from')?> <a href="members/<?=$senderUsername?>"><?=$senderUsername?>: </a>
             <?=MOD_layoutbits::ago($message->unixtime_created);?></span>
         </div>
         

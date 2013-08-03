71ce8a680d32b40569f02b07dbfe8734050f3c52
sitatara <sitatara@gmx.at>
2013-08-03 13:09:34 +0200

    [no ticket] Headings on the safety FAQ page are now translatable.

--- build/safety/templates/safetyfaq.column_col3.php
+++ build/safety/templates/safetyfaq.column_col3.php
@@ -1,18 +1,17 @@
 <div class="row">
 <a name="general"></a>
-<h3>General</h3>
+<h3><?php echo $words->get('SafetyFaqCategoryGeneral');?></h3>
 <p><strong><?php echo $words->get('FaqQ_IsItSafe');?></strong></p>
 <p><?php echo $words->get('FaqA_IsItSafe');?></p>
 <br />
 <a name="comments"></a>
-<h3>Comments</h3>
+<h3><?php echo $words->get('SafetyFaqCategoryComments');?></h3>
 <p><strong><?php echo $words->get('FaqQ_PurposeOfComment');?></strong></p>
 <p><?php echo $words->get('FaqA_PurposeOfComment');?></p>
 <p><strong><?php echo $words->get('FaqQ_CanIEditComment');?></strong></p>
 <p><?php echo $words->get('FaqA_CanIEditComment');?></p>
-<a name="privacy"></a>
 <a name="spam"></a>
-<h3>Spam and abuse</h3>
+<h3><?php echo $words->get('SafetyFaqCategorySpam');?></h3>
 <p><strong><?php echo $words->get('FaqQ_IFoundAPronLink');?></strong></p>
 <p><?php echo $words->get('FaqA_IFoundAPronLink');?></p>
 <p><strong><?php echo $words->get('FaqQ_DontWantToReceiveInMyEmail');?></strong></p>

f7960e35090fcbdc53d5a8aa4f9af3c567a98477
This is me on Mount Everest <bla@blafaselblubb.abcde.biz>
2013-08-25 12:00:14 +0200

    [no ticket] Corrected DB name

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
-wget http://download.geonames.org/export/dump/allCountries.txt
-wget http://download.geonames.org/export/dump/alternateNames.txt
-mysql -u bewelcome -pbewelcome < import.sql
+curl http://download.geonames.org/export/dump/allCountries.zip > allCountries.zip
+curl http://download.geonames.org/export/dump/alternateNames.zip > alternateNames.zip
+curl http://download.geonames.org/export/dump/countryInfo.txt > countryInfo.txt
+unzip allCountries.zip
+unzip alternateNames.zip
+mysql -u bewelcome -pbewelcome bewelcome < import.sql
+rm -rf allCountries.*
+rm -f iso-languagecodes.txt
+rm -rf alternateNames.*
+rm -rf countryInfo.txt
+
 

8b18a02a012125b8792bf5e778f91ce1091f6504
This is me on Mount Everest <bla@blafaselblubb.abcde.biz>
2013-12-10 22:33:15 +0100

    [no ticket] Upgrading TinyMCE to 4.0.11

--- htdocs/script/tinymceconfig.js
+++ htdocs/script/tinymceconfig.js
@@ -1,24 +1,24 @@
 <script type="text/javascript">//<!--
-tinyMCE.srcMode = '';
-tinyMCE.baseURL = http_baseuri+'script/tiny_mce';
-tinyMCE.init({
+tinymce.srcMode = '';
+tinymce.baseURL = http_baseuri+'script/tinymce';
+tinymce.init({
+    menubar : false,
     mode: "exact",
     elements: "<?php echo $textarea; ?>",
-    plugins : "advimage,preview,fullscreen,autolink",
-    theme: "advanced",
+    // plugins : "advimage,preview,fullscreen,autolink",
+    plugins: "textcolor, image, charmap, link, autolink, paste, preview",
+    theme: "modern",
     content_css : http_baseuri + "styles/css/minimal/screen/custom/tinymce_content.css?2",
     relative_urls:false,
     convert_urls:false,
-    theme_advanced_buttons1: "bold,italic,underline,strikethrough,separator,bullist,numlist,separator,forecolor,backcolor,separator,charmap,link,image,separator,blockquote",
-    theme_advanced_buttons2: "",
-    theme_advanced_buttons3: "",
-    theme_advanced_toolbar_location: 'top',
-    theme_advanced_statusbar_location: 'bottom',
-    theme_advanced_resizing: true,
-    theme_advanced_resize_horizontal : false,
+    toolbar: "undo, redo,|,bold,italic,underline,strikethrough,|,bullist,numlist,|,forecolor,backcolor,|,charmap,link,image,|,blockquote,|,preview",
+    theme_modern_toolbar_location: 'top',
+    theme_modern_statusbar_location: 'bottom',
+    theme_modern_resizing: true,
+    theme_modern_resize_horizontal : false,
     gecko_spellcheck : true,
-    plugin_preview_width : "500",
-    plugin_preview_height : "600"
+    plugin_preview_width : "600",
+    plugin_preview_height : "500",
 });
 //-->
 </script>

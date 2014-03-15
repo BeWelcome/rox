77bba0d3410236e20e2cfaba65f23d4c2b7495f7
crumbking <kingcrumb@web.de>
2014-03-15 18:36:53 +0100

    [bw-bootstrap] add fix for namespace conflict between jquery/bootstrap and prototype

---
+++ htdocs/script/jqueryprototypefix.js
@@ -0,0 +1,25 @@
+(function() {
+    var isBootstrapEvent = false;
+    if (window.jQuery) {
+        var all = jQuery('*');
+        jQuery.each(['hide.bs.dropdown', 
+            'hide.bs.collapse', 
+            'hide.bs.modal', 
+            'hide.bs.tooltip',
+            'hide.bs.popover'], function(index, eventName) {
+            all.on(eventName, function( event ) {
+                isBootstrapEvent = true;
+            });
+        });
+    }
+    var originalHide = Element.hide;
+    Element.addMethods({
+        hide: function(element) {
+            if(isBootstrapEvent) {
+                isBootstrapEvent = false;
+                return element;
+            }
+            return originalHide(element);
+        }
+    });
+})();

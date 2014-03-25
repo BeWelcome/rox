98fd8f5201e9ba3bc4b82e874e845471e4fe4517
crumbking <kingcrumb@web.de>
2014-03-25 20:54:10 +0100

    [2102] cleanup: replace strong with spans as the small class remove the font-weight anyway

--- build/search/templates/advancedoptions.php
+++ build/search/templates/advancedoptions.php
@@ -14,28 +14,28 @@ $words = new MOD_words;
 <table id="search-advanced-options" class="full" style="width:100%">
     <tr>
         <td colspan="2"><input type="hidden" name="search-advanced" value="1" />
-            <strong class="small"><?php echo $words->getFormatted('Username'); ?></strong><br/>
+            <span class="small"><?php echo $words->getFormatted('Username'); ?></span><br/>
             <input type="text" name="search-username" id="search-username" size="30" maxlength="30" value="<?php echo $vars['search-username']; ?>"/>
         </td>
         <td colspan="2"><?php echo getGroupOptionsDropDown($vars); ?></td>
         <td valign="top" rowspan="3">
-            <strong class="small"><?php echo $words->getFormatted('FindPeopleAccomodationTitle'); ?></strong><br/>
+            <span class="small"><?php echo $words->getFormatted('FindPeopleAccomodationTitle'); ?></span><br/>
             <?php echo getAccommodationOptions($vars); ?>
-            <strong class="small floatbox">
+            <span class="small floatbox">
                 <div class="float_right"><i><?php echo $words->getFormatted('FindPeopleAccomodationTip'); ?></i></div>
-            </strong>
+            </span>
             <br>
             <br>
-         <strong class="small"><?php echo $words->getFormatted('FindPeopleOfferTypeTitle'); ?></strong><br/>
+         <span class="small"><?php echo $words->getFormatted('FindPeopleOfferTypeTitle'); ?></span><br/>
             <?php echo getTypicalOfferOptions($vars); ?>
-            <strong class="small floatbox">
+            <span class="small floatbox">
                 <div class="float_right"><i><?php echo $words->getFormatted('FindPeopleTypicOfferTip'); ?></i></div>
-            </strong>
+            </span>
         </td>
     </tr>
     <tr>
         <td colspan="2">
-            <strong class="small"><?php echo $words->getFormatted('TextToFind'); ?></strong><br/>
+            <span class="small"><?php echo $words->getFormatted('TextToFind'); ?></span><br/>
             <input type="text" name="search-text" id="search-text" size="30" maxlength="30" value="<?php echo $vars['search-text']; ?>"/>
         </td>
         <td colspan="2"><?php echo getLanguagesOptionsDropDown($vars); ?></td>
@@ -43,11 +43,11 @@ $words = new MOD_words;
     <tr>
         <td colspan="2" class="floatbox">
             <div class="float_left advance-margin">
-                <strong class="small"><?php echo $words->getFormatted('FindPeopleMinimumAge'); ?></strong><br/>
+                <span class="small"><?php echo $words->getFormatted('FindPeopleMinimumAge'); ?></span><br/>
                 <?php echo getAgeDropDown($vars, 'search-age-minimum'); ?>
             </div>
             <div class="float_left advance-margin">
-                <strong class="small"><?php echo $words->getFormatted('FindPeopleMaximumAge'); ?></strong><br/>
+                <span class="small"><?php echo $words->getFormatted('FindPeopleMaximumAge'); ?></span><br/>
                 <?php echo getAgeDropDown($vars, 'search-age-maximum'); ?>
             </div>
             <div class="float_left advance-margin">

6c2451a7cdffa93f018a6a2e11100fcdcb3c58c3
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2014-05-28 17:01:51 +0200

    [no ticket] Updated install.sh to check for curl

--- install.sh
+++ install.sh
@@ -1,9 +1,23 @@
-curl http://download.geonames.org/export/dump/allCountries.zip > allCountries.zip
-curl http://download.geonames.org/export/dump/alternateNames.zip > alternateNames.zip
-curl http://download.geonames.org/export/dump/countryInfo.txt > countryInfo.txt
+#!/bin/bash
+
+# This will download all files necessary to import the full geonames dump
+if hash curl 2>/dev/null; then
+    curl http://download.geonames.org/export/dump/allCountries.zip > allCountries.zip
+    curl http://download.geonames.org/export/dump/alternateNames.zip > alternateNames.zip
+    curl http://download.geonames.org/export/dump/countryInfo.txt > countryInfo.txt
+else
+    wget http://download.geonames.org/export/dump/allCountries.zip > allCountries.zip
+    wget http://download.geonames.org/export/dump/alternateNames.zip > alternateNames.zip
+    wget http://download.geonames.org/export/dump/countryInfo.txt > countryInfo.txt
+fi
+
 unzip allCountries.zip
 unzip alternateNames.zip
+
+# This assumes that you setup your DB as outlined in INSTALL
 mysql -u bewelcome -pbewelcome bewelcome < import.sql
+
+# Some cleanup
 rm -rf allCountries.*
 rm -f iso-languagecodes.txt
 rm -rf alternateNames.*

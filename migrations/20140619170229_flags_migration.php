14f43c35e705c0dc4988ceda56ca2e950ff8011a
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2014-06-25 11:13:14 +0200

    [2206] Added meaningful comment to the migration class.

--- migrations/20140619170229_flags_migration.php
+++ migrations/20140619170229_flags_migration.php
@@ -2,21 +2,17 @@
 
 use Phinx\Migration\AbstractMigration;
 
+/*************************************
+ * Class FlagsMigration
+ *
+ * Handles the update of the flags table
+ *
+ * See ticket: #2206
+ *
+ */
 class FlagsMigration extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * More information on this method is available here:
-     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
-     *
-     * Uncomment this method if you would like to use it.
-     *
-    public function change()
-    {
-    }
-    */
-    
+
     /**
      * Migrate Up.
      */

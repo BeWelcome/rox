56a85e38b6c703db4c4130373a0a23001da73507
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2014-07-06 23:12:01 +0200

    [1853] CSS cleanup for template file.

--- templates/shared/mail_html.php
+++ templates/shared/mail_html.php
@@ -15,19 +15,22 @@
         --></style>
     </head>
     <body style="background-color: #cccccc;font-color: #333;font-size: 12px;line-height: 1.5em;font-family: arial,verdana,sans-serif;margin: 0;padding: 0;">
-        <div style="width: 640px;margin: 0;padding: 0 0 2em 0; background-color: #ffffff; background: #ffffff url(<?php
+        <div style="width: 580px;margin: 0;padding: 0 0 2em 0; background-color: #ffffff; background: #ffffff url(<?php
         echo PVars::getObj('env')->baseuri; ?>/images/misc/email_bg.gif) top left repeat-y;">
             <div>
                 <div style="padding: 10px 20px 10px 20px; margin: 10px;background-color: #f5f5f5;border-top: 1px solid #e5e5e5;border-bottom: 1px solid #e5e5e5; text-align: right;">
                     <a href="<?php echo PVars::getObj('env')->baseuri; ?>"><img src="<?php echo PVars::getObj('env')->baseuri; ?>images/misc/email_logo.gif" alt="<?php echo PVars::getObj('env')->sitename; ?>" style="border:0"/></a>
                 </div>
                 <div style="margin: 40px; background-color: #ffffff;">
-                    <?if($title) { ?><h1 style="font-size: 30px; padding-bottom: 10px; line-height: 1.2em"><?=$title?></h1><?}?>
-                    <div><?=($body_html) ? $body_html : $body?></div>
+                    <?php if($title) { ?>
+                        <h1 style="font-size:16px; font-weight: bold; padding-bottom: 10px; line-height: 1.2em">
+                        <?=$title?></h1>
+                    <?php } ?>
+                    <div><?= $body ?></div>
                 </div>
             </div>
         </div>
-        <div style="width: 640px; height: 80px; background: #cccccc url(<?php echo PVars::getObj('env')->baseuri;
+        <div style="width: 580px; height: 80px; background: #cccccc url(<?php echo PVars::getObj('env')->baseuri;
         ?>/images/misc/email_bottom.gif) top left no-repeat; padding: 40px;">
             <?php echo $footer_message; ?>
         </div>

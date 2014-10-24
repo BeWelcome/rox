233dc8f92fabc5cf89c9c6ecbfb6dab9e98df5ec
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2014-10-24 22:59:09 +0200

    [2221] Make sure the same member states are used for member count and members queries

--- build/groups/groups.model.php
+++ build/groups/groups.model.php
@@ -153,10 +153,10 @@ class GroupsModel extends  RoxModelBase
                     $order = 'Name DESC';
                     break;
                 case "membersasc":
-                    $order = "(SELECT COUNT(*) FROM membersgroups AS mg, members as m WHERE mg.IdGroup = groups.id AND mg.Status = 'In' AND m.id = mg.idmember AND m.status IN ('Active','Pending')) ASC, Name ASC";
+                    $order = "(SELECT COUNT(*) FROM membersgroups AS mg, members as m WHERE mg.IdGroup = groups.id AND mg.Status = 'In' AND m.id = mg.idmember AND m.status IN (" . Member::ACTIVE_ALL . ")) ASC, Name ASC";
                     break;
                 case "membersdesc":
-                    $order = "(SELECT COUNT(*) FROM membersgroups AS mg, members as m WHERE mg.IdGroup = groups.id AND mg.Status = 'In' AND m.id = mg.idmember AND m.status IN ('Active','Pending')) DESC, Name ASC";
+                    $order = "(SELECT COUNT(*) FROM membersgroups AS mg, members as m WHERE mg.IdGroup = groups.id AND mg.Status = 'In' AND m.id = mg.idmember AND m.status IN (" . Member::ACTIVE_ALL . ")) DESC, Name ASC";
                     break;
                 case "actasc":
                     $order = "(SELECT MAX(forums_posts.create_time) FROM forums_threads, forums_posts WHERE groups.id = forums_threads.IdGroup AND forums_posts.postid = forums_threads.last_postid) ASC, Name ASC";

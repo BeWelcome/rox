21b534cf337894ae1525fd38116441bd848886bb
crumbking <kingcrumb@web.de>
2014-11-04 21:39:54 +0100

    [bw-bootstrap] fix bootlint errors in the nav bar search (style problems with webkit browsers)

--- templates/shared/roxpage/quicksearch.php
+++ templates/shared/roxpage/quicksearch.php
@@ -1,7 +1,7 @@
 <form class="navbar-form navbar-right" role="search" action="quicksearch" method="get" id="form-quicksearch">
-        <div class="form-group">
+        <div class="form-group-sm">
             <div class="input-group" style="width: 160px;">
-                <input class="form-control input-sm" type="text" name="vars" size="15" maxlength="30" placeholder="<?echo htmlentities($words->getSilent('TopMenuSearchtext'),ENT_QUOTES, "UTF-8");?>..." id="text-field" value="<?echo htmlentities($words->getSilent('TopMenuSearchtext'),ENT_QUOTES, "UTF-8");?>..." onclick="this.value='';" />
+                <input class="form-control" type="text" name="vars" size="15" maxlength="30" placeholder="<?echo htmlentities($words->getSilent('TopMenuSearchtext'),ENT_QUOTES, "UTF-8");?>..." id="text-field" value="<?echo htmlentities($words->getSilent('TopMenuSearchtext'),ENT_QUOTES, "UTF-8");?>..." onclick="this.value='';" />
                 <input type="hidden" name="quicksearch_callbackId" value="1"/>
                 <span class="input-group-btn">
                     <button type="submit" class="btn btn-default btn-sm" id="submit-button"><i class="fa fa-search"></i></button>

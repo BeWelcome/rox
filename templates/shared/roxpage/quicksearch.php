c830c68681f288e6df57a5a3350598d2fceaa6a0
crumbking <kingcrumb@web.de>
2014-11-10 22:16:08 +0100

    [bw-bootstrap] workaround: hide nav search on tablets as it's breaking the nav bar, try to find better solution later ....

--- templates/shared/roxpage/quicksearch.php
+++ templates/shared/roxpage/quicksearch.php
@@ -1,4 +1,4 @@
-<form class="navbar-form navbar-right" role="search" action="quicksearch" method="get" id="form-quicksearch">
+<form class="navbar-form navbar-right hidden-sm" role="search" action="quicksearch" method="get" id="form-quicksearch">
         <div class="form-group-sm">
             <div class="input-group input-group-sm">
                 <input class="form-control" type="text" name="vars" size="15" maxlength="30" placeholder="<?echo htmlentities($words->getSilent('TopMenuSearchtext'),ENT_QUOTES, "UTF-8");?>..." id="text-field" value="<?echo htmlentities($words->getSilent('TopMenuSearchtext'),ENT_QUOTES, "UTF-8");?>..." onclick="this.value='';" />

d3b9d310c25ea0fa321e08aa3b6d637eb91e2b00
crumbking <kingcrumb@web.de>
2014-11-24 14:42:59 +0000

    [2250]  add some tools to check our css like its done in bootstrap. csscomb, csslint and a autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,19 +16,64 @@ module.exports = function(grunt) {
           'htdocs/styles/css/bewelcome.css': 'htdocs/styles/less/bewelcome.less',
         }
       },
-      minify: {
+    },
+    
+  autoprefixer: {
+      options: {
+        browsers: [
+          'Android 2.3',
+          'Android >= 4',
+          'Chrome >= 20',
+          'Firefox >= 24', // Firefox 24 is the latest ESR
+          'Explorer >= 8',
+          'iOS >= 6',
+          'Opera >= 12',
+          'Safari >= 6'
+        ]
+      },
+      bewelcome: {
         options: {
-          cleancss: true,
-          report: 'min'
+          map: true
         },
-        files: {
-          'htdocs/styles/css/bewelcome.min.css': 'htdocs/styles/css/bewelcome.css',
-        }
-      }
+        src: 'htdocs/styles/css/bewelcome.css'
+      },
+    },
+    
+    csslint: {
+      options: {
+        csslintrc: 'htdocs/styles/less/.csslintrc'
+      },
+      bewelcome: [
+        'htdocs/styles/css/bewelcome.css'
+      ],
+    },
+    
+    cssmin: {
+      options: {
+        compatibility: 'ie8',
+        keepSpecialComments: '*',
+        noAdvanced: true
+      },
+      bewelcome: {
+        src: 'htdocs/styles/css/bewelcome.css',
+        dest: 'htdocs/styles/css/bewelcome.min.css'
+      },
+    },
+
+    csscomb: {
+      options: {
+        config: 'htdocs/styles/less/.csscomb.json'
+      },
+      bewelcome: {
+        expand: true,
+        cwd: 'htdocs/styles/css/',
+        src: ['*.css', '!*.min.css'],
+        dest: 'htdocs/styles/css/'
+      },
     },
     watch: {
         files: "htdocs/styles/less/*",
-        tasks: ["less"]
+        tasks: ['less:compileBeWelcome', 'autoprefixer:bewelcome', 'csscomb:bewelcome',  'cssmin:bewelcome', 'csslint:bewelcome']
     } 
 });
 
@@ -36,8 +81,12 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-recess');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-csscomb');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  
 
   // Default task(s).
   grunt.registerTask('default', ['watch']);
-
 };

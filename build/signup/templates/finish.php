c79e43035b85784af2b7691a97cd628233609268
crumbking <kingcrumb@web.de>
2014-11-26 10:13:58 +0000

    [2250] move to new version id

--- build/signup/templates/finish.php
+++ build/signup/templates/finish.php
@@ -23,9 +23,9 @@ Boston, MA  02111-1307, USA.
 */
 
 /*
- * REGISTER FORM TEMPLATE
+ * Signup confirmation template
  */
 ?>
-<div id="signuprox">
+<div id="signuprox2">
 <p><?php echo $words->get('SignupFinishedText', '<b>'.$email.'</b>'); ?></p>
 </div>

56b7ef375fb6126e1aeaeb6d1ac0d369b829b8d1
crumbking <kingcrumb@web.de>
2014-11-30 21:27:59 +0100

    [2250] actually this is only visible while js off (which we will not support anymore) but as it hurts my eyes I fixed it

--- build/geo/templates/popup.php
+++ build/geo/templates/popup.php
@@ -12,52 +12,28 @@
  */
  ?>
  <div id="geoselector" >
-        <form method="post" action="<?=$page_url?>" name="geo-form" id="geo-form" target="_self">
-            <?=$callbacktag?>
-          <fieldset id="location">
-
-            <?php
-            /*if (isset($_SESSION['GeoVars']['geonameid'])) {
-                <p>Current location:</p>
-                <ol class="geoloc plain clearfix">
-                <li style="background-color: #f5f5f5; font-weight: bold; background-image: url(images/icons/tick.png);"><a id="href_4544349">
-                <?=$_SESSION['GeoVars']['geonamename']?><br/>
-                <img alt="United States" src="images/icons/flags/<?=$_SESSION['GeoVars']['geonamecountrycode']?>.png"/>
-                <span class="small"><?=$_SESSION['GeoVars']['countryname']?> / <?=$_SESSION['GeoVars']['admincode']?></span>
-                </a></li>
-                </ol>
-             } */
-            ?>
-
-        <div class="subcolumns">
-          <div class="c50l">
-            <div class="subcl">
-              <!-- Content of left block -->
-
-              <div class="float_left">
-                <label for="geo-search"><?=$words->get('label_setlocation')?>:</label><br />
-                <input type="text" name="geo-search" id="geo-search" <?php
-                echo isset($mem_redirect->location) ? 'value="'.htmlentities($mem_redirect->location, ENT_COMPAT, 'utf-8').'" ' : '';
-                ?>
-                 /> <input type="submit" class="button" id="btn-geo-search" class="button" value="<?=$words->get('label_search_location')?>" />
-                <p class="desc"><?=$words->get('subline_location')?></p>
-              </div>
-            </div>
-          </div>
-
-          <div class="c50r">
-            <div class="subcr">
-              <!-- Content of right block -->
-            </div>
-          </div>
-        </div>
-
-          </fieldset>
-        </form>
-
-
-          <fieldset id="location_selection_nonjs" class="location_selection">
+    <form method="post" action="<?=$page_url?>" name="geo-form" id="geo-form" target="_self" class="form">
+        <?=$callbacktag?>
+        <fieldset id="location">
+                <!-- Content of left block -->
+                <div class="form-group">
+                    <div class="input-group">
+                        <label for="geo-search" class="control-label sr-only"><?=$words->get('label_setlocation')?>:</label>
+                        <input type="text" name="geo-search" class="form-control" id="geo-search" placeholder="<?=$words->get('label_setlocation')?>"
+                        <?php
+                        echo isset($mem_redirect->location) ? 'value="'.htmlentities($mem_redirect->location, ENT_COMPAT, 'utf-8').'" ' : '';
+                        ?>
+                         > 
+                         <span class="input-group-btn">
+                             <button type="submit" class="button" id="btn-geo-search"><?=$words->get('label_search_location')?></button>
+                         </span>
+                    </div>
+                    <span class="help-block text-justify"><?=$words->get('subline_location')?></span>
+                </div>
+        </fieldset>
+    </form>
+        
+    <fieldset id="location_selection_nonjs" class="location_selection">
         <?php echo $locations_print; ?>
-          </fieldset>
-
+    </fieldset>
 </div>

fd196d97973a8efac3010ba7ddd1d737e318dfa3
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2015-01-01 18:44:57 +0100

    [2235] Fixed the src of the CC licence logo

--- build/wiki/erfurtwiki/plugins/aview/fpage_copyright.php
+++ build/wiki/erfurtwiki/plugins/aview/fpage_copyright.php
@@ -11,7 +11,7 @@ $ewiki_t["en"]["ALLRIGHTSRESERVED"] = "all rights reserved.";
 $ewiki_plugins["page_final"][] = "ewiki_page_final_copyright";
 
 function ewiki_page_final_copyright(&$o, &$id, &$data, &$action){
-       $o.='<div id="copyright"><a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/"><img class="float_left" alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/88x31.png" /></a>This work is licenced under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Licence</a>.</div>';
+       $o.='<div id="copyright"><a rel="license" href="https://creativecommons.org/licenses/by-sa/3.0/"><img class="float_left" alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/3.0/88x31.png" /></a>This work is licenced under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Licence</a>.</div>';
 }
 
 ?>

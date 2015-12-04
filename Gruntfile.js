18b598de767f12c27c2c4d1aa57ead63fbc67ddb
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2015-12-04 18:43:59 +0100

    [no ticket] Update Gruntfile for sass. Copy some files around after fetching with npm. Update components for npm.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,66 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    less: {
-      compileBeWelcome: {
-        options: {
-          strictMath: true,
-          sourceMap: true,
-          outputSourceFiles: true,
-          sourceMapURL: 'bewelcome.css.map',
-          sourceMapFilename: 'htdocs/styles/css/bewelcome.css.map'
+    sass: {                              // Task
+      compileBeWelcome: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
         },
-        files: {
-          'htdocs/styles/css/bewelcome.css': 'htdocs/styles/less/bewelcome.scss',
+        files: {                         // Dictionary of files
+          'htdocs/styles/css/bewelcome.css': 'htdocs/styles/scss/bewelcome.scss'
         }
+      }
+    },
+    copy: {
+      jQuery: {
+        files: [{
+          expand: true,
+          cwd: 'node_modules/jQuery/dist/',
+          src: ['**'],
+          dest: 'htdocs/js/jQuery/'
+        }]
+      },
+      leafletjs: {
+        files: [{
+          expand: true,
+          cwd: 'node_modules/leaflet/dist/',
+          src: ['**', '!**/*.css', '!**/*.png'],
+          dest: 'htdocs/js/leaflet/'
+        }]
+      },
+      leafletcss: {
+        files: [{
+          expand: true,
+          cwd: 'node_modules/leaflet/dist/',
+          src: ['**', '!**/*.js'],
+          dest: 'htdocs/css/leaflet/'
+        }]
+      },
+      leafletimages: {
+        files: [{
+          expand: true,
+          cwd: 'node_modules/leaflet/dist/images',
+          src: ['**'],
+          dest: 'htdocs/images/'
+        }]
+      },
+      markerclusterjs: {
+        files: [{
+          expand: true,
+          cwd: 'node_modules/leaflet.markercluster/dist/',
+          src: ['**', '!**/*.css'],
+          dest: 'htdocs/js/leaflet/'
+        }]
+      },
+      markerclustercss: {
+        files: [{
+          expand: true,
+          cwd: 'node_modules/leaflet.markercluster/dist/',
+          src: ['**', '!**/*.js'],
+          dest: 'htdocs/css/leaflet/'
+        }]
       },
     },
-    
   autoprefixer: {
       options: {
         browsers: [
@@ -25,7 +70,7 @@ module.exports = function(grunt) {
           'Android >= 4',
           'Chrome >= 20',
           'Firefox >= 24', // Firefox 24 is the latest ESR
-          'Explorer >= 8',
+          'Explorer >= 9',
           'iOS >= 6',
           'Opera >= 12',
           'Safari >= 6'
@@ -36,16 +81,16 @@ module.exports = function(grunt) {
           map: true
         },
         src: 'htdocs/styles/css/bewelcome.css'
-      },
+      }
     },
     
     csslint: {
       options: {
-        csslintrc: 'htdocs/styles/less/.csslintrc'
+        csslintrc: 'htdocs/styles/scss/.csslintrc'
       },
       bewelcome: [
         'htdocs/styles/css/bewelcome.css'
-      ],
+      ]
     },
     
     cssmin: {
@@ -57,44 +102,48 @@ module.exports = function(grunt) {
       bewelcome: {
         src: 'htdocs/styles/css/bewelcome.css',
         dest: 'htdocs/styles/css/bewelcome.min.css'
-      },
+      }
     },
 
     csscomb: {
       options: {
-        config: 'htdocs/styles/less/.csscomb.json'
+        config: 'htdocs/styles/scss/.csscomb.json'
       },
       bewelcome: {
         expand: true,
         cwd: 'htdocs/styles/css/',
         src: ['*.css', '!*.min.css'],
         dest: 'htdocs/styles/css/'
-      },
+      }
     },
     watch: {
         dev: {
-            files: "htdocs/styles/less/*",
-            tasks: ['less:compileBeWelcome', 'cssmin:bewelcome']
+            files: "htdocs/styles/scss/*",
+            tasks: ['sass:compileBeWelcome', 'cssmin:bewelcome']
         },
         dist: {
-            files: "htdocs/styles/less/*",
-            tasks: ['less:compileBeWelcome', 'autoprefixer:bewelcome', 'csscomb:bewelcome', 'cssmin:bewelcome', 'csslint:bewelcome']
-        },
+            files: "htdocs/styles/scss/*",
+            tasks: ['sass:compileBeWelcome', 'autoprefixer:bewelcome', 'csscomb:bewelcome', 'cssmin:bewelcome' // , 'csslint:bewelcome'
+            ]
+        }
     } 
 });
 
   // Load the plugin that provides the ('grunt-*') task.
   grunt.loadNpmTasks('grunt-recess');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-csscomb');
   grunt.loadNpmTasks('grunt-contrib-csslint');
   
 
-  // Default task for development (simply turn less to css )
+  // Default task for development (simply turn scss to css )
   grunt.registerTask('default', ['watch:dev']);
-  // Distribution task 
+
+  // Distribution task
   grunt.registerTask('dist', ['watch:dist']);
+  grunt.registerTask('copyfiles', ['copy:jQuery', 'copy:leafletjs', 'copy:leafletimages', 'copy:leafletcss', 'copy:markerclusterjs', 'copy:markerclustercss']);
 };

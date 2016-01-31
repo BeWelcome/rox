7619b33d1bc90903a985e932846544844c0e9a94
Giulia Vergottini <hello@railslide.io>
2016-01-31 20:39:15 +0100

    run Ansible on the guest

--- ansible/windows.sh
+++ ansible/windows.sh
@@ -3,25 +3,8 @@
 # Update Repositories
 sudo apt-get update
 
-# Determine Ubuntu Version
-. /etc/lsb-release
-
-# Decide on package to install for `add-apt-repository` command
-#
-# USE_COMMON=1 when using a distribution over 12.04
-# USE_COMMON=0 when using a distribution at 12.04 or older
-USE_COMMON=$(echo "$DISTRIB_RELEASE > 12.04" | bc)
-
-if [ "$USE_COMMON" -eq "1" ];
-then
-    sudo apt-get install -y software-properties-common
-else
-    sudo apt-get install -y python-software-properties
-fi
-
-# Add Ansible Repository & Install Ansible
-sudo add-apt-repository -y ppa:ansible/ansible
-sudo apt-get update
+# Install Ansible
+sudo apt-get install -y python-software-properties
 sudo apt-get install -y ansible
 
 # Setup Ansible for Local Use and Run

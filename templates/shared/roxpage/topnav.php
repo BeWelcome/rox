6bbd4e9a807a782338b4c2f7d427979d098a0bbe
amnesiac84 <lukas@autistici.org>
2016-02-17 21:29:20 +0100

    [no ticket] SCSS fixes

--- templates/shared/roxpage/topnav.php
+++ templates/shared/roxpage/topnav.php
@@ -38,10 +38,10 @@ if ($logged_in) {
         echo '<div class="container"><div class="row"><ul class="nav nav-inline pull-xs-right m-t-1 m-b-1">';
     }
     if ($R->hasRight('Comments')) {
-        echo '<li class="nav-item"><a class="nav-link" href="bw/admin/admincomments.php" title="Review negative comments">Negative comments <span class="label label-primary">' . $numberReportedComments . '</span></a></li>';
+        echo '<li class="nav-item"><a class="nav-link" href="bw/admin/admincomments.php" title="Review negative comments">Negative comments <span class="tag tag-primary">' . $numberReportedComments . '</span></a></li>';
     }
     if ($R->hasRight('Checker')) {
-        echo '<li class="nav-item"><a class="nav-link" href="bw/admin/adminchecker.php?action=viewSpamSayMember" title="Review messages reported by users as spam">Reported messages <span class="label label-primary">' . $numberSpamToBeChecked . '</span></a></li>';
+        echo '<li class="nav-item"><a class="nav-link" href="bw/admin/adminchecker.php?action=viewSpamSayMember" title="Review messages reported by users as spam">Reported messages <span class="tag tag-primary">' . $numberSpamToBeChecked . '</span></a></li>';
     }
     if ($R->hasRight('Comments') || ($R->hasRight('Checker'))) {
         echo '</ul></div></div></div>';

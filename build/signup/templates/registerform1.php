8bc5633778bdfbee898d8d64e34202d0ba9009a7
amnesiac84 <lukas@autistici.org>
2016-03-09 23:14:30 +0100

    [no ticket] updated signup process

--- build/signup/templates/registerform1.php
+++ build/signup/templates/registerform1.php
@@ -25,22 +25,17 @@ Boston, MA  02111-1307, USA.
  * REGISTER FORM TEMPLATE
  */
  ?>
+<div class="card card-block">
     <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-xs-12 col-md-6">
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title"><?php echo $words->get('LoginInformation'); ?><small class="pull-right">Step 1/4</small></h4>
-                    <progress class="progress progress-striped progress-success" value="25" max="100">
-                        <div class="progress">
-                            <span class="progress-bar" style="width: 25%;">25%</span>
-                        </div>
-                    </progress>
-                    <h4 class="text-xs-center"><small>Please fill out all fields</small></h4>
+        <div class="card-header">
+            <h4 class="card-title"><?php echo $words->get('LoginInformation'); ?><small class="pull-right">Step 1/4</small></h4>
+            <progress class="progress progress-striped progress-success" value="25" max="100">
+                <div class="progress">
+                    <span class="progress-bar" style="width: 25%;">25%</span>
                 </div>
-            </div>
+            </progress>
+            <h4><small>Please fill out all fields</small></h4>
         </div>
-            <div class="col-md-3"></div>
     </div>
 
 
@@ -52,12 +47,12 @@ Boston, MA  02111-1307, USA.
     }
 ?>
 
-    <div class="form-group row">
-        <div class="col-md-3"></div>
-        <div class="col-xs-12 col-md-6">
+    <div class="form-group row m-t-2">
+        <div class="col-md-2"></div>
+        <div class="col-xs-12 col-md-5">
 
             <button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#SignupIntroduction">
-                <?php echo $words->get('SignupIntroductionTitle'); ?>
+                <?php // echo $words->get('SignupIntroductionTitle'); ?>What data will be public?
             </button>
 
             <div class="modal fade" id="SignupIntroduction" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
@@ -80,12 +75,12 @@ Boston, MA  02111-1307, USA.
             </div>
 
         </div>
-        <div class="col-md-3"></div>
+        <div class="col-md-5"></div>
     </div>
 
     <div class="form-group row m-t-2">
-        <div class="col-md-3"></div>
-        <div class="col-xs-12 col-md-6">
+        <div class="col-md-2"></div>
+        <div class="col-xs-12 col-md-5">
             <!-- username -->
             <div class="form-group has-feedback">
                 <label for="register-username" class="sr-only"><?php echo $words->get('SignupUsername'); ?></label>
@@ -100,12 +95,13 @@ Boston, MA  02111-1307, USA.
                     <span class="text-muted small"></span>
             </div>
         </div>
-        <div class="col-xs-12 col-md-3"><span class="text-muted small"><?=$words->get('subline_username')?></span></div>
+        <div class="col-xs-12 col-md-3 p-a-0"><span class="text-muted small"><?=$words->get('subline_username')?></span></div>
+        <div class="col-md-2"></div>
     </div>
 
     <div class="form-group row">
-        <div class="col-md-3"></div>
-        <div class="col-xs-12 col-md-6">
+        <div class="col-md-2"></div>
+        <div class="col-xs-12 col-md-5">
             <!-- password -->
             <div class="form-group has-feedback">
                 <label for="register-password" class="sr-only"><?php echo $words->get('SignupPassword'); ?></label>
@@ -118,13 +114,7 @@ Boston, MA  02111-1307, USA.
                     ?> >
                 <span class="text-muted small"></span>
             </div>
-        </div>
-        <div class="col-xs-12 col-md-3"><span class="text-muted small">Please choose a strong password. You may use all letters (a-z A-Z), numbers (0-9) and some special characters (# % ! = + - _)</span></div>
-    </div>
 
-    <div class="form-group row">
-        <div class="col-md-3"></div>
-        <div class="col-xs-12 col-md-6">
             <!-- confirm password -->
             <div class="form-group has-feedback">
                 <label for="register-passwordcheck" class="sr-only"><?php echo $words->get('SignupCheckPassword'); ?></label>
@@ -136,13 +126,15 @@ Boston, MA  02111-1307, USA.
                     ?> >
                 <span class="text-muted small"></span>
             </div>
+
         </div>
-        <div class="col-xs-12 col-md-3"><span class="text-muted small">Please type in your password again, just to be sure you didn't make a typo.</span></div>
+        <div class="col-xs-12 col-md-3 p-a-0"><span class="text-muted small">Please choose a strong password. You may use all letters (a-z A-Z), numbers (0-9) and some special characters (# % ! = + - _)</span></div>
+        <div class="col-md-2"></div>
     </div>
 
     <div class="form-group row m-t-1">
-        <div class="col-md-3"></div>
-        <div class="col-xs-12 col-md-6">
+        <div class="col-md-2"></div>
+        <div class="col-xs-12 col-md-5">
             <!-- email -->
             <div class="form-group has-feedback">
                 <label for="register-email" class="sr-only"><?php echo $words->get('SignupEmail'); ?></label>
@@ -153,13 +145,7 @@ Boston, MA  02111-1307, USA.
                     ?> />
                 <small class="text-muted text-muted"></small>
             </div>
-        </div>
-        <div class="col-xs-12 col-md-3"><span class="text-muted small">Your e-mail address is most important: we will forward the messages and request to this e-mail address</span></div>
-    </div>
 
-    <div class="form-group row">
-        <div class="col-md-3"></div>
-        <div class="col-xs-12 col-md-6">
             <!-- confirm email -->
             <div class="form-group has-feedback">
                 <label for="register-emailcheck" class="sr-only"><?php echo $words->get('SignupCheckEmail'); ?></label>
@@ -172,21 +158,24 @@ Boston, MA  02111-1307, USA.
                     ?> />
                 <small class="text-muted text-muted"></small>
             </div>
+
         </div>
-        <div class="col-md-3"></div>
+        <div class="col-xs-12 col-md-3 p-a-0"><span class="text-muted small">Your e-mail address is most important: we will forward the messages and request to this e-mail address</span></div>
+        <div class="col-md-2"></div>
     </div>
 
     <div class="form-group row m-t-1">
-        <div class="col-md-3"></div>
-        <div class="col-xs-12 col-md-6">
+        <div class="col-md-2"></div>
+        <div class="col-xs-12 col-md-5">
             <!-- Accommodation -->
 
-            <label for="accommodation"><?php echo $words->get('Accommodation'); ?>*</label>
+            <label for="accommodation"><h4><?php echo $words->get('Accommodation'); ?></h4></label>
 
             <div class="radio">
                 <label>
                     <input type="radio" name="gridRadios" id="accommodation" value="anytime" <?php
                     if (isset($vars['accommodation']) && $vars['accommodation'] == 'anytime') { echo ' checked="checked"'; } ?>>
+                    <img src="/images/icons/yesicanhost.png">
                     <?php echo $words->get('Accomodation_anytime'); ?>
                 </label>
             </div>
@@ -195,6 +184,7 @@ Boston, MA  02111-1307, USA.
                 <label>
                     <input type="radio" name="gridRadios" id="accommodation" value="dependonrequest" <?php
                     if (isset($vars['accommodation']) && $vars['accommodation'] == 'dependonrequest') { echo ' checked="checked"'; } ?>>
+                    <img src="/images/icons/maybe.png">
                     <?php echo $words->get('Accomodation_dependonrequest'); ?>
                 </label>
             </div>
@@ -203,6 +193,7 @@ Boston, MA  02111-1307, USA.
                 <label>
                     <input type="radio" name="gridRadios" id="accommodation" value="neverask" <?php
                     if (isset($vars['accommodation']) && $vars['accommodation'] == 'neverask') { echo ' checked="checked"'; } ?>>
+                    <img src="/images/icons/nosorry.png">
                     <?php echo $words->get('Accomodation_neverask'); ?>
                 </label>
             </div>
@@ -213,15 +204,17 @@ Boston, MA  02111-1307, USA.
             ?>
 
             </div>
-        <div class="col-xs-12 col-md-3"><span class="text-muted small">Are you able to provide accommodation?</span></div>
+        <div class="col-xs-12 col-md-3 p-a-0"><span class="text-muted small">Are you able to provide accommodation?</span></div>
+        <div class="col-md-2"></div>
     </div>
 
     <div class="form-group row">
-        <div class="col-md-3"></div>
-        <div class="col-xs-12 col-md-6">
+        <div class="col-md-2"></div>
+        <div class="col-xs-12 col-md-5">
             <input type="submit" class="form-control btn btn-primary" value="<?php echo $words->getSilent('NextStep'); ?>" /><?php echo $words->flushBuffer(); ?>
         </div>
-        <div class="col-md-3"></div>
+        <div class="col-md-5"></div>
     </div>
 
 </form>
+</div>

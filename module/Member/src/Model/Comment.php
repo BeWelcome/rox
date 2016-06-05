279b417f0ec15f54ef191df49da8b8adc8600660
djwinter <danielwinter@gmail.com>
2016-06-05 13:12:04 +1200

    Add toMember relationship to Comment entity. Refactor defined relationships to object property.

--- module/Member/src/Model/Comment.php
+++ module/Member/src/Model/Comment.php
@@ -8,13 +8,26 @@ class Comment extends Model
 {
     const CREATED_AT = 'created';
 
+    /**
+     * @var array
+     */
+    protected $relationships = [
+        'fromMember',
+        'toMember',
+    ];
+
     public function fromMember()
     {
         return $this->hasOne(Member::class, 'id', 'IdFromMember');
     }
 
+    public function toMember()
+    {
+        return $this->hasOne(Member::class, 'id', 'IdToMember');
+    }
+
     public function __isset($key)
     {
-        return parent::__isset($key) || in_array($key, ['fromMember'], true);
+        return parent::__isset($key) || in_array($key, $this->relationships, true);
     }
 }

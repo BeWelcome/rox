af74d8b0e0e0e7105c70009f8107a825132e5119
djwinter <danielwinter@gmail.com>
2016-06-11 11:05:23 +1200

    Helper trait for converting 0000 dates to NULL

---
+++ module/Core/src/Model/NullableDateFixTrait.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Rox\Core\Model;
+
+trait NullableDateFixTrait
+{
+    abstract function getDates();
+
+    public function getAttributeFromArray($key)
+    {
+        $value = parent::getAttributeFromArray($key);
+
+        if (in_array($key, $this->getDates()) && $value === '0000-00-00 00:00:00') {
+            return null;
+        }
+
+        return $value;
+    }
+}

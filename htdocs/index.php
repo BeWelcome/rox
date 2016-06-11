94400a27d310303af736ccaa1d2ebd66ac5ff62d
djwinter <danielwinter@gmail.com>
2016-06-11 12:08:02 +1200

    Fix misplaced comment block

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,13 +1,13 @@
 <?php
 
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
 use Dotenv\Dotenv;
 use Rox\Core\Kernel\Application;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
 chdir(dirname(__DIR__));
 
 // Decline static file requests back to the PHP built-in webserver

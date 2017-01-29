126137f64438e11f3838414904693ed54deb53c8
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2017-01-29 10:01:18 +0100

    Commit changes due to typo

--- src/AppBundle/Model/LandingModel.php
+++ src/AppBundle/Model/LandingModel.php
@@ -193,14 +193,17 @@ class LandingModel extends BaseModel {
         }
         return $travellers;
 */    }
+
     /**
      * @param Member $member
      * @param $accommodation
      * @return Member
      */
     public function updateMemberAccommodation(Member $member, $accommodation)
+    {
         $member->setAccommodation($accommodation);
         $this->em->persist($member);
         $this->em->flush($member);
         return $member;
+    }
     }

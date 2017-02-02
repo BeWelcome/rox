410b10cdc871c623ff87f5767947cedc5c29cf19
amnesiac84 <lukas@autistici.org>
2017-02-02 21:34:42 +0100

    Streamlining Grouplist for future development

--- build/groups/templates/groupssearch.column_col3.php
+++ build/groups/templates/groupssearch.column_col3.php
@@ -1,26 +1,27 @@
 <div id="groups">
-        <div class="clearfix">
-        <div class="subcolumns">
-            <div class="c50l">
-                <div class="groupbox float_left">
+
+    <div class="row mt-2">
+            <div class="col-12 col-md-6">
                     <h3><?= $words->get('GroupsSearchHeading'); ?></h3>
                     <form action="groups/search" method="get">
                         <input type="text" name="GroupsSearchInput" value="" id="GroupsSearchInput" /><input type="submit" class="button" value="<?= $words->getSilent('GroupsSearchSubmit'); ?>" /><?=$words->flushBuffer()?><br />
                     </form>
-                </div>
             </div>
-            <div class="c50r">
-                <div class="groupbox float_left">
+            <div class="col-12 col-md-6">
                     <h3><?= $words->get('GroupsCreateHeading'); ?></h3>
                     <p><?= $words->get('GroupsCreateDescription'); ?></p>
                     <a class="button" role="button" href="groups/new"><span><?= $words->get('GroupsCreateNew'); ?></span></a>
-                </div>
             </div>
-        </div>
+    </div>
 
-        <h3><?= $words->get('GroupsSearchResult'); ?></h3>
+    <div class="row mt-2">
+        <div class="col-12"><h3><?= $words->get('GroupsSearchResult'); ?></h3></div>
+        <div class="col-12">
         <?php
         $search_result = $this->search_result;
+
+        $this->pager->render();
+
         if ($search_result) :
             $act_order = (($this->result_order == "actdesc") ? 'actasc' : 'actdesc');
             $name_order = (($this->result_order == "nameasc") ? 'namedesc' : 'nameasc');
@@ -37,67 +38,59 @@
             <a class="grey" href="groups/search?GroupsSearchInput=<?=$this->search_terms;?>&order=<?=$member_order;?>&<?=$this->pager->getActivePageMarker();?>"><?= $words->get('GroupsOrderBy' . $member_order); ?></a>
             |
             <a class="grey" href="groups/search?GroupsSearchInput=<?=$this->search_terms;?>&order=<?=$created_order;?>&<?=$this->pager->getActivePageMarker();?>"><?= $words->get('GroupsOrderDate' . $created_order); ?></a></p>
-<?
+        </div>
+            <?
 // Categories link disabled until we have categories
 //            |
 //            <a class="grey" href="groups/search?GroupsSearchInput={$this->search_terms}&amp;Order={$category_order}&Page={$this->result_page}">Category</a>
-            $this->pager->render();
+
             echo <<<HTML
-<div class="clearfix">
+<div class="d-flex align-content-stretch flex-wrap">
 HTML;
-            $ii = 0;
             foreach ($search_result as $group_data) :
-                if ($ii %3 == 0) :
-                    echo '<div class="subcolumns">';
-                endif;
-                if ($ii % 3 != 2) :
-                    echo '<div class="c33l">';
-                else :
-                    echo '<div class="c33r">';
-                endif;
+
                 ?>
-                <div class="groupbox group_float_left">
+
+            <div class="d-flex flex-row m-2">
+                <div>
+                    <!-- group image -->
                     <a href="groups/<?=$group_data->getPKValue() ?>">
-                        <img class="framed float_left"  width="80px" height='80px' alt="group" src="<?= ((strlen($group_data->Picture) > 0) ? "groups/thumbimg/{$group_data->getPKValue()}" : 'images/icons/group.png' ) ?>"/>
+                        <img class="framed"  width="80px" height='80px' alt="group" src="<?= ((strlen($group_data->Picture) > 0) ? "groups/thumbimg/{$group_data->getPKValue()}" : 'images/icons/group.png' ) ?>"/>
                     </a>
-                    <div class="groupinfo">
-                        <h4><a href="groups/<?=$group_data->getPKValue() ?>"><?=htmlspecialchars($group_data->Name, ENT_QUOTES) ?></a></h4>
-                        <ul>
-                            <li><?= $words->get('GroupsMemberCount');?>: <?=$group_data->getMemberCount(); ?></li>
-                            <li><?= $words->get('GroupsDateCreation');?>: <?=date($words->getBuffered('DateHHMMShortFormat'), ServerToLocalDateTime(strtotime($group_data->created), $this->getSession())); ?></li>
-                            <?php if ($group_data !== 0) {?>
+                </div>
+                <div>
+                    <!-- group name -->
+                    <h4><a href="groups/<?=$group_data->getPKValue() ?>"><?=htmlspecialchars($group_data->Name, ENT_QUOTES) ?></a></h4>
+                    <!-- group details -->
+                    <ul>
+                        <li><?= $words->get('GroupsMemberCount');?>: <?=$group_data->getMemberCount(); ?></li>
+                        <li><?= $words->get('GroupsDateCreation');?>: <?=date($words->getBuffered('DateHHMMShortFormat'), ServerToLocalDateTime(strtotime($group_data->created), $this->getSession())); ?></li>
+                        <?php if ($group_data !== 0) {?>
                             <li><?php
                                 if ($group_data->latestPost) {
                                     echo $words->get('GroupsLastPost') . ": " . date($words->getBuffered('DateHHMMShortFormat'), ServerToLocalDateTime($group_data->latestPost, $this->getSession()));
                                 } else {
                                     echo $words->get('GroupsNoPostYet');
                                 }
-                            ?></li>
-                            <?php } ?>
-                        </ul>
-                    </div> <!-- groupinfo -->
-                </div> <!-- groupbox  -->
+                                ?></li>
+                        <?php } ?>
+                    </ul>
+                </div>
             </div>
-			<?php if ($ii % 3 == 2) :
-				echo "</div>"; // subcolumns
-		    endif;
-			$ii++;
-            endforeach ; 
-			// check if a subcolumns need to be closed
-			if ($ii % 3 != 0) :
-				echo "</div>"; // subcolumns
-			endif; ?>
-</div> <!-- clearfix -->
-            <?php
-            $this->pager->render();
-            ?>
-        <?php else :
-            echo <<<HTML
+
+                <?php
+            endforeach ;
+ ?>
+    </div> <!-- end row -->
+    <?php
+    $this->pager->render();
+    ?>
+    <?php else :
+        echo <<<HTML
             <p class="note">
             {$words->get('GroupSearchNoResults')}
             </p>
 HTML;
-        endif;
-        ?>
-</div> <!-- clearfix -->
+    endif;
+    ?>
 </div> <!-- groups -->

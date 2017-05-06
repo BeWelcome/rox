03a9a3b17266ea3b1865f4dc62f60dd7aa6dfdce
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2017-05-06 23:03:17 +0200

    Make sure the route for new message is picked up

--- src/AppBundle/Controller/MessageController.php
+++ src/AppBundle/Controller/MessageController.php
@@ -145,7 +145,7 @@ class MessageController extends Controller
     }
 
     /**
-     * @Route("/new/message/{$sername}", name="message_new")
+     * @Route("/new/message/{username}", name="message_new")
      */
     public function compose(Request $request)
     {

1bd4fe019b406cbd2e2aa0702645ab01e9b52c3a
amnesiac84 <lukas@autistici.org>
2017-08-19 23:29:26 +0200

    Updates on Country/Place browse and Help BeWelcome

--- build/places/templates/countries.column_col3.php
+++ build/places/templates/countries.column_col3.php
@@ -1,3 +1,51 @@
+<?php
+
+$columns = array();
+$lastcontinent = "";
+foreach($this->continents as $continent => $value) {
+
+    echo '<div class="col-6 col-md-4 col-lg-2">';
+    echo '<h3>' . $value[0] . '</h3>';
+
+    foreach ($this->countries[$continent] as $country) {
+
+        echo '<p><i class="famfamfam-flag-' . strtolower($country->country) . '"></i><span class="pr-1"></span>';
+        if ($country->number) {
+            echo '<a href="/places/' . htmlspecialchars($country->name) . '/' . $country->country . '">';
+        }
+        echo htmlspecialchars($country->name);;
+        if ($country->number) {
+            echo '</a><span class="ml-1 badge badge-secondary">' . $country->number . '</span>';
+        }
+        echo '</p>';
+
+    }
+
+   echo '</div>';
+}
+?>
+
+
+
+<div class="col-6 col-md-4 col-lg-3">
+    Africa
+</div>
+<div class="col-6 col-md-4 col-lg-2">
+    Eurasia
+</div>
+<div class="col-6 col-md-4 col-lg-2">
+    Antartica
+</div>
+<div class="col-6 col-md-4 col-lg-2">
+    Oceania
+</div>
+<div class="col-6 col-md-4 col-lg-3">
+    America
+</div>
+
+
+
+
 <table id="places"><tr>
 <?php
 

c942ff81e2bad1df55b97f44162249b5a7dbc354
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2018-01-27 17:23:46 +1300

    Password change: Make sure at least one small character is included.

--- build/members/members.ctrl.php
+++ build/members/members.ctrl.php
@@ -861,7 +861,7 @@ class MembersController extends RoxControllerBase
             $totalChar = 8; // number of chars in the password
             $salt = "abcdefghijklmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ123456789";  // salt to select chars from
             srand((double)microtime()*1000000); // start the random generator
-            $password=""; // set the inital variable
+            $password="Bz5"; // set the inital variable
             for ($i=0;$i<$totalChar;$i++) {  // loop and create password
                 $password = $password . substr ($salt, rand() % strlen($salt), 1);
             }

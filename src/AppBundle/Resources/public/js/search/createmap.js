b7e5bf62f800130cd7f0a3cf5d4f048857c43170
amnesiac84 <lukas@autistici.org>
2018-08-31 23:24:55 +0200

    updated map result display

--- src/AppBundle/Resources/public/js/search/createmap.js
+++ src/AppBundle/Resources/public/js/search/createmap.js
@@ -75,10 +75,10 @@ function addMarkers(map){
         }
         var icon = new L.DivIcon({ html: '<div><img src="/images/icons/' + iconFile + '.png" class="mapicon"></div>', className: '', iconSize: new L.Point(17, 17) });
         var marker = new L.marker([value.latitude, value.longitude], {icon: icon, className: 'marker-cluster marker-cluster-unique'});
-
-        var popupContent = '<div class="row px-2"><div><img src="/members/avatar/' + value.Username + '?size=50"></div>' +
-            '<div style="position: absolute;right: 0.5rem;"><img src="/images/icons/' + iconFile + '.png"><i class="fa fa-2x fa-bed p-1"></i><span class="h4">#2</span></div></div>' +
-            '<h5 class="row px-2 nowrap"><a href="/members/' + value.Username + '" target="_blank">' + value.Username + '</a></h5>';
+        value.MaxGuest = 9; //TODO: get the real maxGuest number
+        var popupContent = '<div class="d-flex"><div><img src="/members/avatar/' + value.Username + '?size=50"></div>' +
+            '<div class="hosticon nowrap"><img src="/images/icons/' + iconFile + '.png"><i class="fa fa-2x fa-bed p-1"></i><span class="h4">' + value.MaxGuest + '</span></div></div>' +
+            '<div class="d-flex"><h5 class="nowrap"><a href="/members/' + value.Username + '" target="_blank">' + value.Username + '</a></h5></div>';
 
         marker.bindPopup(popupContent).openPopup();
 

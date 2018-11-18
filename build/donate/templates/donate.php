2f5f1e982c6873467ec2c07eff513b1ed5151709
amnesiac84 <lukas@autistici.org>
2018-11-18 15:47:30 +0100

    New wordcodes

--- build/donate/templates/donate.php
+++ build/donate/templates/donate.php
@@ -111,7 +111,7 @@ if ($error) {?>
 				<p><?=$words->get('Donate_Process')?></p>
 			</div>
 			<div class="tab-pane card p-2" id="time" role="tabpanel">
-				Donate time
+                {{ 'donation.donatetime' | trans }}
 			</div>
 		</div>
 

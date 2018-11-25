473973454ae8dbfd78e56fd3dace3e3ac17b3012
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2018-11-25 10:22:32 +0100

    Test different offcanvas versions.

--- assets/public/js/offcanvas.js
+++ assets/public/js/offcanvas.js
@@ -4,4 +4,7 @@ $(document).ready(function () {
     $('[data-toggle="offcanvas"]').on('click', function () {
         $('.offcanvas-collapse').toggleClass('open');
     });
+    $('[data-toggle="offcanvas1"]').on('click', function () {
+        $('.offcanvas1-collapse').toggleClass('open');
+    });
 });

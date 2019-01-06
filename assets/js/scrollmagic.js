caf19a4259f19a28c8e7f67d500335b35369dfdb
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2019-01-06 19:33:51 +0100

    Enable ScrollMagic through Webpack.

--- assets/js/scrollmagic.js
+++ assets/js/scrollmagic.js
@@ -1,1 +1,17 @@
-import 'scrollmagic';
+import TweenMax from 'TweenMax';
+import ScrollMagic from 'ScrollMagic';
+import 'animation.gsap';
+import 'debug.addIndicators';
+
+var controller = new ScrollMagic.Controller();
+new ScrollMagic.Scene({triggerElement: "#trigger-1", duration: 600 })
+    .setClassToggle("#scrollmagic-animation-1", "parallax--fixed-active")
+    .addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#trigger-2", duration: 600 })
+    .setClassToggle("#scrollmagic-animation-2", "parallax--fixed-active")
+    .addTo(controller);
+
+new ScrollMagic.Scene({triggerElement: "#trigger-3" })
+    .setClassToggle("#scrollmagic-animation-3", "parallax--fixed-active")
+    .addTo(controller);
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-//updatecounter    .configureRuntimeEnvironment('dev')
+//    .configureRuntimeEnvironment('dev')
     .enableSingleRuntimeChunk()
     .setOutputPath('public/build/')
     .setPublicPath('/build')
-    // .cleanupOutputBeforeBuild()
+    .cleanupOutputBeforeBuild()
 
     .addEntry('bewelcome', './assets/js/bewelcome.js')
     .addEntry('jquery_ui', './assets/js/jquery_ui.js')
@@ -39,7 +39,15 @@ Encore
     .autoProvideVariables({
         Popper: ['popper.js', 'default'],
     })
-
+    .addAliases({
+        'TweenLite': 'gsap/src/minified/TweenLite.min.js',
+        'TweenMax': 'gsap/src/minified/TweenMax.min.js',
+        'TimelineLite': 'gsap/src/minified/TimelineLite.min.js',
+        'TimelineMax': 'gsap/src/minified/TimelineMax.min.js',
+        'ScrollMagic': 'scrollmagic/scrollmagic/minified/ScrollMagic.min.js',
+        'animation.gsap': 'scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js',
+        'debug.addIndicators': 'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js'
+    })
     .addLoader({
         test: require.resolve('jquery'),
         use: [{

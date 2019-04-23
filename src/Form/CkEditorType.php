036e819e9d3f36a99ef955defac21ae710e55579
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2019-04-23 16:19:50 +0200

    Enable empty check.

--- src/Form/CkEditorType.php
+++ src/Form/CkEditorType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,6 +15,13 @@ class CkEditorType extends TextAreaType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         parent::buildForm($builder, $options);
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+            // check if data is only <p>&nbsp;</p> and if so trim to empty string
+            $data = $event->getData();
+            if ('<p>&nbsp;</p>' === $data) {
+                $event->setData('');
+            }
+        });
     }
 
     /**

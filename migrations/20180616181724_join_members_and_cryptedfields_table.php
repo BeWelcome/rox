bb4bb6d2df4cf9128472c0d72314bad5a33973b2
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2019-04-23 16:21:44 +0200

    Fixing join of crypted fields into members table.

--- migrations/20180616181724_join_members_and_cryptedfields_table.php
+++ migrations/20180616181724_join_members_and_cryptedfields_table.php
@@ -1,6 +1,7 @@
 <?php
 
-
+use Doctrine\DBAL\Driver\PDOConnection;
+use Symfony\Component\Dotenv\Dotenv;
 use Rox\Tools\RoxMigration;
 
 class JoinMembersAndCryptedfieldsTable extends RoxMigration
@@ -16,38 +17,51 @@ class JoinMembersAndCryptedfieldsTable extends RoxMigration
             ->addColumn('HideAttribute', 'biginteger')
             ->save();
         ;
+
+        $dotenv = new Dotenv();
+        $dotenv->load('.env', '.env.local');
+
+        $connection = new PDOConnection(
+            'mysql:mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
+            getenv('DB_USER'),
+            getenv('DB_PASS')
+        );
+
         // Collect information for 'active' members and update members table. Don't bother with asktoleave and the other statuses
-        $sql = "SELECT id, FirstName, SecondName, LastName, Email fROM members WHERE Status IN ('Active', 'OutOfRemind', 'Banned', 'ChoiceInactive', 'Pending', 'SuspendedBeta') ORDER BY Id";
-        $stmt = $this->query($sql);
+        $sql = "SELECT id, FirstName, SecondName, LastName, Email fROM members WHERE Status IN ('Active', 'OutOfRemind', 'Banned', 'ChoiceInactive', 'Pending', 'SuspendedBeta', 'MailToConfirm') and EmailText is null ORDER BY Id";
+        $stmt = $connection->query($sql);
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $cryptedSql = "SELECT * FROM cryptedfields WHERE id IN (" . $row['FirstName'] . ", ". $row['SecondName'] . ", ". $row['LastName'] . ", ". $row['Email'] . ")";
-            $cryptedStmt = $this->query($cryptedSql);
+            $cryptedSql = "SELECT * FROM cryptedfields WHERE id IN (" . $row['FirstName'] . ", " . $row['SecondName'] . ", " . $row['LastName'] . ", " . $row['Email'] . ")";
+            $cryptedStmt = $connection->query($cryptedSql);
             $cryptedRows = $cryptedStmt->fetchAll();
-            $updateSql  = 'UPDATE members SET ';
+            $updateSql = 'UPDATE members SET ';
             $hideAttribute = 0;
             $updateNeeded = false;
-            foreach($cryptedRows as $cryptedRow)
-            {
+            foreach ($cryptedRows as $cryptedRow) {
                 if ($cryptedRow['TableColumn'] <> 'NotSet') {
-                    $updateNeeded = true;
-                    $updateSql .= str_replace('members.', '', $cryptedRow['TableColumn'])."Text = '".strip_tags(
-                        urldecode(
-                            $cryptedRow['MemberCryptedValue']
-                        ))."', ";
-                    if (($cryptedRow['IsCrypted'] == 'crypted') || ($cryptedRow['IsCrypted'] == 'always')){
-                        switch($cryptedRow['TableColumn']) {
-                            case 'members.FirstName':
-                                $hideAttribute |= \Member::MEMBER_FIRSTNAME_HIDDEN;
-                                break;
-                            case 'members.SecondName':
-                                $hideAttribute |= \Member::MEMBER_SECONDNAME_HIDDEN;
-                                break;
-                            case 'members.LastName':
-                                $hideAttribute |= \Member::MEMBER_LASTNAME_HIDDEN;
-                                break;
-                            case 'members.Email':
-                                $hideAttribute |= \Member::MEMBER_EMAIL_HIDDEN;
-                                break;
+                    if (!empty($cryptedRow['MemberCryptedValue'])) {
+                        $updateNeeded = true;
+                        $updateSql .= str_replace('members.', '', $cryptedRow['TableColumn']) . "Text = " .
+                            $connection->quote(strip_tags(
+                                urldecode(
+                                    $cryptedRow['MemberCryptedValue']
+                                )
+                            )) . ", ";
+                        if (($cryptedRow['IsCrypted'] == 'crypted') || ($cryptedRow['IsCrypted'] == 'always')) {
+                            switch ($cryptedRow['TableColumn']) {
+                                case 'members.FirstName':
+                                    $hideAttribute |= \Member::MEMBER_FIRSTNAME_HIDDEN;
+                                    break;
+                                case 'members.SecondName':
+                                    $hideAttribute |= \Member::MEMBER_SECONDNAME_HIDDEN;
+                                    break;
+                                case 'members.LastName':
+                                    $hideAttribute |= \Member::MEMBER_LASTNAME_HIDDEN;
+                                    break;
+                                case 'members.Email':
+                                    $hideAttribute |= \Member::MEMBER_EMAIL_HIDDEN;
+                                    break;
+                            }
                         }
                     }
                 }
@@ -55,11 +69,13 @@ class JoinMembersAndCryptedfieldsTable extends RoxMigration
             $updateSql = substr($updateSql, 0, -2);
             $updateSql .= ", HideAttribute = " . $hideAttribute;
             $updateSql .= " WHERE id = " . $row['id'];
+            file_put_contents('members.sql', $updateSql . PHP_EOL, FILE_APPEND);
             // echo $updateSql . PHP_EOL;
             if ($updateNeeded) {
-                $this->execute($updateSql);
+                $connection->query($updateSql);
             }
         };
+
         // drop original fields
         $members
             ->removeColumn('email')

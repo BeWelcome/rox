9720ab601552d2ed44c0f1d461dc4cefce00d8a8
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2019-10-27 13:19:04 +0100

    Add admin interface to (un-)archive groups.

--- src/Controller/Admin/GroupController.php
+++ src/Controller/Admin/GroupController.php
@@ -2,14 +2,19 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\FaqCategory;
 use App\Entity\Group;
 use App\Entity\Member;
 use App\Entity\Right;
 use App\Logger\Logger;
+use App\Repository\GroupRepository;
 use App\Utilities\MailerTrait;
+use App\Utilities\ManagerTrait;
 use App\Utilities\TranslatedFlashTrait;
 use App\Utilities\TranslatorTrait;
 use Exception;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,6 +30,7 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 class GroupController extends AbstractController
 {
     use MailerTrait;
+    use ManagerTrait;
     use TranslatorTrait;
     use TranslatedFlashTrait;
 
@@ -48,6 +54,7 @@ class GroupController extends AbstractController
         }
 
         // Fetch unapproved groups and decide on their fate
+        // no pagination as there shouldn't be too many
         $groupsRepository = $this->getDoctrine()->getRepository(Group::class);
         $groups = $groupsRepository->findBy([
             'approved' => [Group::NOT_APPROVED, Group::IN_DISCUSSION],
@@ -55,6 +62,92 @@ class GroupController extends AbstractController
 
         return $this->render('admin/groups/approve.html.twig', [
             'groups' => $groups,
+            'submenu' => [
+                'items' => $this->getSubmenuItems(),
+                'active' => 'approval',
+            ],
+        ]);
+    }
+
+    /**
+     * Allows to archive a group.
+     *
+     * @Route("/admin/groups/archival", name="admin_groups_archival")
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function archiveGroups(Request $request)
+    {
+        if (!$this->isGranted([Member::ROLE_ADMIN_GROUP])) {
+            throw $this->createAccessDeniedException('You need to have Group right to access this.');
+        }
+
+        if (!$this->hasGroupRightLevel(10)) {
+            throw $this->createAccessDeniedException('You need to have Group right to access this.');
+        }
+
+        // Build Pagerfanta for groups
+        $queryBuilder = $this->getManager()->createQueryBuilder()
+            ->select('g')
+            ->from('App:Group', 'g')
+            ->where("g.name NOT LIKE '[Archived] %'")
+        ;
+        $adapter = new DoctrineORMAdapter($queryBuilder);
+        $pagerfanta = new Pagerfanta($adapter);
+
+        $pagerfanta->setMaxPerPage(30); // 10 by default
+
+        $currentPage = $request->get('page', '1');
+        $pagerfanta->setCurrentPage($currentPage); // 1 by default
+
+        return $this->render('admin/groups/archive.html.twig', [
+            'groups' => $pagerfanta,
+            'submenu' => [
+                'items' => $this->getSubmenuItems(),
+                'active' => 'archival',
+            ],
+        ]);
+    }
+
+    /**
+     * Allows to unarchive a group.
+     *
+     * @Route("/admin/groups/unarchival", name="admin_groups_unarchival")
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function unarchiveGroups(Request $request)
+    {
+        if (!$this->isGranted([Member::ROLE_ADMIN_GROUP])) {
+            throw $this->createAccessDeniedException('You need to have Group right to access this.');
+        }
+
+        if (!$this->hasGroupRightLevel(10)) {
+            throw $this->createAccessDeniedException('You need to have Group right to access this.');
+        }
+
+        // Build Pagerfanta for groups
+        $queryBuilder = $this->getManager()->createQueryBuilder()
+            ->select('g')
+            ->from('App:Group', 'g')
+            ->where("g.name LIKE '[Archived] %'")
+        ;
+        $adapter = new DoctrineORMAdapter($queryBuilder);
+        $pagerfanta = new Pagerfanta($adapter);
+
+        $pagerfanta->setMaxPerPage(30); // 10 by default
+
+        $currentPage = $request->get('page', '1');
+        $pagerfanta->setCurrentPage($currentPage); // 1 by default
+
+        return $this->render('admin/groups/archive.html.twig', [
+            'groups' => $pagerfanta,
+            'submenu' => [
+                'items' => $this->getSubmenuItems(),
+                'active' => 'archival',
+            ],
         ]);
     }
 
@@ -165,6 +258,75 @@ class GroupController extends AbstractController
         return $this->redirect($referrer);
     }
 
+    /**
+     * Archive a group .
+     *
+     * @Route("/admin/groups/{id}/archive", name="admin_groups_archive")
+     *
+     * @param Request $request
+     * @param Group   $group
+     * @param Logger  $logger
+     *
+     * @throws Exception
+     *
+     * @return RedirectResponse
+     */
+    public function archiveGroup(Request $request, Group $group, Logger $logger)
+    {
+        if (!$this->isGranted([Member::ROLE_ADMIN_GROUP])) {
+            throw $this->createAccessDeniedException('You need to have Group right to access this.');
+        }
+
+        $group->setName('[Archived] ' . $group->getName());
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($group);
+        $em->flush();
+
+        $this->addTranslatedFlash('notice', 'flash.group.archived', [
+            '%name%' => $group->getName(),
+        ]);
+
+        $logger->write('Group '.$group->getName().' archived by '.$this->getUser()->getUsername().'.', 'Group');
+
+        $referrer = $request->headers->get('referer');
+
+        return $this->redirect($referrer);
+    }
+    /**
+     * Un-archive a group .
+     *
+     * @Route("/admin/groups/{id}/unarchive", name="admin_groups_unarchive")
+     *
+     * @param Request $request
+     * @param Group   $group
+     * @param Logger  $logger
+     *
+     * @throws Exception
+     *
+     * @return RedirectResponse
+     */
+    public function unarchiveGroup(Request $request, Group $group, Logger $logger)
+    {
+        if (!$this->isGranted([Member::ROLE_ADMIN_GROUP])) {
+            throw $this->createAccessDeniedException('You need to have Group right to access this.');
+        }
+
+        $group->setName(str_replace('[Archived] ',  '', $group->getName()));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($group);
+        $em->flush();
+
+        $this->addTranslatedFlash('notice', 'flash.group.unarchived', [
+            '%name%' => $group->getName(),
+        ]);
+
+        $logger->write('Group '.$group->getName().' un-archived by '.$this->getUser()->getUsername().'.', 'Group');
+
+        $referrer = $request->headers->get('referer');
+
+        return $this->redirect($referrer);
+    }
+
     private function sendNewGroupApprovedNotification(Group $group, Member $creator)
     {
         $subject = '[New Group] '.strip_tags($group->getName()).' approved';
@@ -179,5 +341,26 @@ class GroupController extends AbstractController
     {
         $admin = $this->getUser();
         return ($admin->getLevelForRight(Member::ROLE_ADMIN_GROUP) == $level);
+    }
+
+    /**
+     * @return array
+     */
+    private function getSubMenuItems()
+    {
+        return [
+            'approval' => [
+                'key' => 'admin.groups.approval',
+                'url' => $this->generateUrl('admin_groups_approval'),
+            ],
+            'archival' => [
+                'key' => 'admin.groups.archival',
+                'url' => $this->generateUrl('admin_groups_archival'),
+            ],
+            'unarchival' => [
+                'key' => 'admin.groups.unarchival',
+                'url' => $this->generateUrl('admin_groups_unarchival'),
+            ],
+        ];
     }
 }

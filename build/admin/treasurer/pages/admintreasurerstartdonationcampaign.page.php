9aca91ba92ba2937ebd8fe34fef3e7c4e1722bed
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2019-12-05 18:48:59 +0100

    Fix broken donation campaign start page

--- build/admin/treasurer/pages/admintreasurerstartdonationcampaign.page.php
+++ build/admin/treasurer/pages/admintreasurerstartdonationcampaign.page.php
@@ -15,17 +15,17 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, see <http://www.gnu.org/licenses/> or 
-write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, 
+along with this program; if not, see <http://www.gnu.org/licenses/> or
+write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 Boston, MA  02111-1307, USA.
 */
-    /** 
+    /**
      * @author crumbking
      */
 
-    /** 
+    /**
      * Treasurer management page
-     * 
+     *
      * @package Apps
      * @subpackage Admin
      */
@@ -33,6 +33,7 @@ Boston, MA  02111-1307, USA.
 class AdminTreasurerStartDonationCampaignPage extends AdminTreasurerBasePage
 {
     public function __construct(AdminTreasurerModel $model) {
+        parent::__construct();
         $this->model = $model;
         $this->member = $model->getLoggedInMember();
         list($amount, $date) = $this->model->getDonationCampaignValues();
@@ -40,7 +41,7 @@ class AdminTreasurerStartDonationCampaignPage extends AdminTreasurerBasePage
         list($year, $month, $day) = preg_split('/[\/.-]/', $date);
         $this->date = $day . "." . $month . "." . $year;
     }
-    
+
     public function teaserHeadline()
     {
         return "<a href='admin'>{$this->words->get('AdminTools')}</a> &raquo; <a href='admin/treasurer'>{$this->words->get('AdminTreasurer')}</a> &raquo; {$this->words->get('AdminTreasurerStartDonationCampaign')}</a>";

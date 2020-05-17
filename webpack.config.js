7725e16d32f6eda1a88f089f9e19adcce25ed61b
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2020-05-17 12:20:05 +0200

    Bump versions of dependencies.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
-const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
+const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
+const {styles} = require('@ckeditor/ckeditor5-dev-utils');
 
-var Encore = require('@symfony/webpack-encore');
+let Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -14,27 +14,12 @@ Encore
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
-    .addPlugin( new CKEditorWebpackPlugin( {
-        // Main language that will be built into the main bundle.
-        language: 'de',
-
-        // Additional languages that will be emitted to the `outputDirectory`.
-        // This option can be set to an array of language codes or `'all'` to build all found languages.
-        // The bundle is optimized for one language when this option is omitted.
+    .addPlugin(new CKEditorWebpackPlugin({
+        language: 'en',
         additionalLanguages: 'all',
-
-        // Optional directory for emitted translations. Relative to the webpack's output.
-        // Defaults to `'translations'`.
         outputDirectory: 'cktranslations',
-
-        // Whether the build process should fail if an error occurs.
-        // Defaults to `false`.
-        // strict: true,
-
-        // Whether to log all warnings to the console.
-        // Defaults to `false`.
-        // verbose: true
-    } ) )
+        buildAllTranslationsToSeparateFiles: true
+    }))
     .splitEntryChunks()
     .enableSingleRuntimeChunk()
     .cleanupOutputBeforeBuild()
@@ -64,7 +49,7 @@ Encore
     .addEntry('email', './assets/scss/email.scss')
     .addEntry('roxeditor', './assets/js/roxeditor.js')
     .addEntry('rangeslider', './assets/js/rangeslider.js')
-//    .addEntry('roxinlineeditor', './assets/js/roxinlineeditor.js')
+    //    .addEntry('roxinlineeditor', './assets/js/roxinlineeditor.js')
 
     .enableSassLoader()
     // allow legacy applications to use $/jQuery as a global variable, make popper visible for bootstrap
@@ -86,7 +71,7 @@ Encore
         use: [{
             loader: 'expose-loader',
             options: 'jQuery'
-        },{
+        }, {
             loader: 'expose-loader',
             options: '$'
         }]
@@ -97,33 +82,27 @@ Encore
     })
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(true)
-    .addPlugin( new CKEditorWebpackPlugin( {
-        // See https://ckeditor.com/docs/ckeditor5/latest/features/ui-language.html
-        language: 'en',
-        additionalLanguages: 'all',
-    } ) )
-
     // Use raw-loader for CKEditor 5 SVG files.
-    .addRule( {
+    .addRule({
         test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
         loader: 'raw-loader'
-    } )
+    })
 
     // Configure other image loaders to exclude CKEditor 5 SVG files.
-    .configureLoaderRule( 'images', loader => {
+    .configureLoaderRule('images', loader => {
         loader.exclude = /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/;
-    } )
+    })
 
     // Configure PostCSS loader.
     .addLoader({
         test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
         loader: 'postcss-loader',
-        options: styles.getPostCssConfig( {
+        options: styles.getPostCssConfig({
             themeImporter: {
                 themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
             }
-        } )
-    } )
+        })
+    })
 ;
 
 // console.log(JSON.stringify(Encore.getWebpackConfig(), null, 4));

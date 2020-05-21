8c5926c5152661bac199a9ba1887ae22b273f106
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2020-05-21 07:36:05 +0200

    Fix layout problem with My Visitor page.

--- build/members/pages/myvisitors.page.php
+++ build/members/pages/myvisitors.page.php
@@ -30,13 +30,19 @@ class MyVisitorsPage extends ProfilePage
         $params->items_per_page = 20;
         $pager = new PagerWidget($params);
 
+        echo '<div class="row no-gutters">';
+        echo '<div class="col-12 col-sm-6 offset-sm-6">';
         $pager->render();
+        echo '</div>';
+        echo '</div>';
 
         echo '<div class="myvisitors card-columns">';
 
         foreach ($member->getVisitorsSubset($pager) as $m)
         {
             $image = MOD_layoutbits::PIC_50_50($m->Username,'',$style='float_left framed');
+            $aboutMe = MOD_layoutbits::truncate_words(stripslashes($words->mInTrad($m->ProfileSummary, $language_id=0, true)), 70);
+
             if ($m->HideBirthDate=="No") $m->age = floor($layoutbits->fage_value($m->BirthDate));
             else $m->age = $words->get("Hidden");
             echo <<<HTML
@@ -47,16 +53,21 @@ class MyVisitorsPage extends ProfilePage
             <a class="username" href="members/{$m->Username}">{$m->Username}</a><br />
         </div>
     </div>
-    <div class="card-body">
-            <p class="small">{$words->getFormatted("visited")}: {$layoutbits->ago(strtotime($m->visited))}</p>
-            <p class="small">{$words->getFormatted("yearsold",$m->age)}, {$m->city}</p>
-            <div class="profilesummary">{$purifier->purify(stripslashes($words->mInTrad($m->ProfileSummary, $language_id=0, true)))}</div>
+    <div class="card-body py-1">
+            <p><small>{$words->getFormatted("visited")}: {$layoutbits->ago(strtotime($m->visited))}</small><br>
+            <small>{$words->getFormatted("yearsold",$m->age)}, {$m->city}</small></p>
+            <div class="profilesummary">{$purifier->purify($aboutMe)}</div>
     </div>
 </div>
 HTML;
         }
         echo "</div>";
+
+        echo '<div class="row no-gutters">';
+        echo '<div class="col-12 col-sm-6 offset-sm-6">';
         $pager->render();
+        echo '</div>';
+        echo '</div>';
 
     }
 }

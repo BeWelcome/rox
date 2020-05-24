ce462db7f62a0bf2028c0ac33490b3b645cd5f98
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2020-05-24 13:14:13 +0200

    Adding Doctrine migrations for sessions table and forum_trads view.

---
+++ src/Migrations/Version20200522172912.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200522172912 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Add forum_trads view';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->addSql("
+            CREATE 
+                ALGORITHM = MERGE 
+                SQL SECURITY DEFINER
+            VIEW `forum_trads` AS
+                SELECT 
+                    `translations`.`id` AS `id`,
+                    `translations`.`IdLanguage` AS `IdLanguage`,
+                    `translations`.`IdOwner` AS `IdOwner`,
+                    `translations`.`IdTrad` AS `IdTrad`,
+                    `translations`.`IdTranslator` AS `IdTranslator`,
+                    `translations`.`updated` AS `updated`,
+                    `translations`.`created` AS `created`,
+                    `translations`.`Type` AS `Type`,
+                    `translations`.`Sentence` AS `Sentence`,
+                    `translations`.`IdRecord` AS `IdRecord`,
+                    `translations`.`TableColumn` AS `TableColumn`
+                FROM
+                    `translations`;
+        ");
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
---
+++ src/Migrations/Version20200524104123.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200524104123 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Add sessions table';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("
+            CREATE TABLE `sessions` (
+                `sess_id` VARCHAR(128) NOT NULL PRIMARY KEY,
+                `sess_data` BLOB NOT NULL,
+                `sess_time` INTEGER UNSIGNED NOT NULL,
+                `sess_lifetime` INTEGER UNSIGNED NOT NULL
+            ) COLLATE utf8mb4_bin, ENGINE = InnoDB;
+        ");
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}

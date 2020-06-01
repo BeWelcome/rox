8fd36b6593318af12d784f6593a21e276349211e
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2020-06-01 13:16:06 +0200

    Use a Cache for production.

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Kernel;
+use App\CacheKernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -21,6 +22,11 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
 }
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+// Wrap the default Kernel with the CacheKernel one in 'prod' environment
+if ('prod' === $kernel->getEnvironment()) {
+     $kernel = new CacheKernel($kernel);
+}
+
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
---
+++ src/CacheKernel.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App;
+
+use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
+
+class CacheKernel extends HttpCache
+{
+}

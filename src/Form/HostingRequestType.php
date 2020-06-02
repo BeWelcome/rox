8d7f7c5f233fb6bbc03f47183120d5f84332a92b
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2020-06-02 20:13:47 +0200

    Fixing propertypath problem blocking sending a hosting request.

--- src/Form/HostingRequestType.php
+++ src/Form/HostingRequestType.php
@@ -42,7 +42,7 @@ class HostingRequestType extends AbstractType
                         'message' => 'request.error.arrival.empty',
                     ]),
                     new LessThanOrEqual([
-                        'propertyPath' => 'parent.all[departure].data',
+                        'propertyPath' => '[departure][data]',
                         'message' => 'request.error.arrival.after.departure',
                     ]),
                 ],

d9dffb85e1c271a7cd2ab0ce38f443c877a31b0c
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2020-07-04 11:50:42 +0200

    Generating service worker with workbox and webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
 const {styles} = require('@ckeditor/ckeditor5-dev-utils');
-const WorkboxPlugin = require('workbox-webpack-plugin');
 
 let Encore = require('@symfony/webpack-encore');
 
@@ -20,29 +19,6 @@ Encore
         additionalLanguages: 'all',
         outputDirectory: 'cktranslations',
         buildAllTranslationsToSeparateFiles: true
-    }))
-    .addPlugin(new WorkboxPlugin.GenerateSW({
-        // Do not precache images
-        exclude: [/\.(?:png|jpg|jpeg|svg)$/],
-
-        // Define runtime caching rules.
-        runtimeCaching: [{
-            // Match any request that ends with .png, .jpg, .jpeg or .svg.
-            urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-
-            // Apply a cache-first strategy.
-            handler: 'CacheFirst',
-
-            options: {
-                // Use a custom cache name.
-                cacheName: 'images',
-
-                // Only cache 10 images.
-                expiration: {
-                    maxEntries: 10,
-                },
-            },
-        }],
     }))
     .splitEntryChunks()
     .enableSingleRuntimeChunk()
@@ -132,6 +108,31 @@ Encore
     })
 ;
 
-// console.log(JSON.stringify(Encore.getWebpackConfig(), null, 4));
+const assetsConfig = Encore.getWebpackConfig();
 
-module.exports = Encore.getWebpackConfig();
+const path = require('path');
+const webpack = require('webpack');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+workboxConfig = {
+        mode: process.env.NODE_ENV,
+        entry: {
+            main: "./assets/js/index.js"
+        },
+        output: {
+            filename: "[name].[chunkhash].js",
+            chunkFilename: "[name].[chunkhash].bundle.js",
+            path: path.resolve(__dirname, "public")
+        },
+        plugins: [
+            new WorkboxPlugin.GenerateSW({
+               // these options encourage the ServiceWorkers to get in there fast
+               // and not allow any straggling "old" SWs to hang around
+               clientsClaim: true,
+               skipWaiting: true,
+             }),
+        ],
+        devtool: "source-map"
+};
+
+module.exports = [ assetsConfig, workboxConfig ];

7b8ed21937c3055101309f6e75b7be39bdbddacf
crumbking <kingcrumb@web.de>
2020-07-14 19:48:54 +0200

    faq routes

--- src/Controller/FaqController.php
+++ src/Controller/FaqController.php
@@ -6,9 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\FaqCategory;
+use App\Model\FaqModel;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
 
 class FaqController extends AbstractController
 {
+
+   /**
+     * @var FaqModel
+     */
+    private $faqModel;
+
+    public function __construct(FaqModel $faqModel)
+    {
+        $this->faqModel = $faqModel;
+    }
+
     /**
      * @Route("/faq/", name="faq_redirect",
      *     defaults = {"path":""})
@@ -24,5 +40,74 @@ class FaqController extends AbstractController
         $pathInfo = str_replace('/faq/', '/about/faq/', $request->getPathInfo());
 
         return new RedirectResponse($pathInfo);
+    }
+
+    /**
+     *
+     *
+     * @return array
+     */
+    protected function getSubMenuItems(FaqCategory $faqCategory = null)
+    {
+        $repository = $this->getDoctrine()->getRepository(FaqCategory::class);
+        $faqCategories = $repository->findBy([], ['sortOrder' => 'ASC']);
+
+        $subMenu = [];
+            $subMenu['about'] = [
+                'key' => 'AboutUsSubmenu',
+                'url' => $this->generateUrl('about'),
+            ];
+            $subMenu['about_faq'] = [
+                'key' => 'Faq',
+                'url' => $this->generateUrl('faqs_overview', [
+                ]),
+            ];
+            $subMenu['about_feedback'] = [
+                'key' => 'ContactUs',
+                'url' => $this->generateUrl('contactus'),
+            ];
+            $subMenu['separator'] = [
+                'key' => 'Faq',
+                'url' => '',
+            ];
+        /** @var FaqCategory $category */
+        foreach ($faqCategories as $category) {
+            $subMenu[$category->getId()] = [
+                'key' => $category->getDescription(),
+                'url' => $this->generateUrl('faqs_overview', ['categoryId' => $category->getId()]),
+            ];
+        }
+
+        return $subMenu;
+    }
+
+    /**
+     * @Route(
+     *     "/about/faq/{categoryId}",
+     *     name="faqs_overview",
+     *     defaults={"categoryId": "1"},
+     *     requirements={"categoryId": "\d+"}
+     * )
+     *
+     * @ParamConverter("faqCategory", class="App\Entity\FaqCategory", options={"id" = "categoryId"})
+     *
+     *
+     *
+     * @return Response
+     */
+    public function showOverview(Request $request, FaqCategory $faqCategory)
+    {
+      
+        $faqs = $this->faqModel->getFaqsForCategory($faqCategory);
+        $faqCategories = $this->getSubMenuItems();
+
+        return  $this->render('faq/faq.html.twig', [
+            'submenu' => [
+                'items' => $faqCategories,
+                'active' => $faqCategory->getId(),
+            ],
+            'faqCategory' => $faqCategory,
+            'faqs' => $faqs,
+        ]);
     }
 }

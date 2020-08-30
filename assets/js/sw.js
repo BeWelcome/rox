d239cfeb0e49c40d44729b7b4f6fa01f32a6ae9e
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2020-08-30 18:02:14 +0200

    Allow PWA to cache messages, requests, profiles and the homepage and shown when offline.

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,7 +1,32 @@
-workbox.precaching.precacheAndRoute(self.__precacheManifest);
+import {precacheAndRoute} from 'workbox-precaching';
+import {NetworkFirst} from 'workbox-strategies';
+import {registerRoute} from 'workbox-routing';
+
+precacheAndRoute(self.__WB_MANIFEST);
 
 addEventListener("message", event => {
     if (event.data && event.data.type === "SKIP_WAITING") {
         skipWaiting();
     }
 });
+
+registerRoute(
+    ({url}) => url.pathname === '/',
+    new NetworkFirst()
+);
+
+registerRoute(
+    ({url}) => url.pathname.startsWith('/message'),
+    new NetworkFirst()
+);
+
+registerRoute(
+    ({url}) => url.pathname.startsWith('/request'),
+    new NetworkFirst()
+);
+
+registerRoute(
+    ({url}) => url.pathname.startsWith('/members'),
+    new NetworkFirst()
+);
+
--- webpack.config.js
+++ webpack.config.js
@@ -112,11 +112,10 @@ Encore
 const assetsConfig = Encore.getWebpackConfig();
 
 const path = require('path');
-const webpack = require('webpack');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
 workboxConfig = {
-        mode: "development",
+        mode: process.env.NODE_ENV,
         entry: {
             main: "./assets/js/index.js"
         },
@@ -126,12 +125,14 @@ workboxConfig = {
             path: path.resolve(__dirname, "public")
         },
         plugins: [
-            new WorkboxPlugin.GenerateSW({
-               // these options encourage the ServiceWorkers to get in there fast
-               // and not allow any straggling "old" SWs to hang around
-               clientsClaim: true,
-               skipWaiting: true,
-             }),
+        new WorkboxPlugin.InjectManifest({
+           // these options encourage the ServiceWorkers to get in there fast
+           // and not allow any straggling "old" SWs to hang around
+           // clientsClaim: true,
+           // skipWaiting: true,
+           swSrc: './assets/js/sw.js',
+            swDest: './service-worker.js'
+        }),
         ],
         devtool: "source-map"
 };

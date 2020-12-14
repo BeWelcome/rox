0e3b47a73209c0882cbd255da4f93d30254c8ebe
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2020-12-14 20:41:43 +0100

    Fix Doctrine types.

---
+++ src/Doctrine/TranslationAllowedType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Doctrine;
+
+class TranslationAllowedType extends EnumType
+{
+    // This is reversed as the database uses a field named do_not_translate!
+    public const TRANSLATION_NOT_ALLOWED = 'Yes';
+    public const TRANSLATION_ALLOWED = 'No';
+
+    /** @var string */
+    protected $name = 'translation_allowed';
+
+    /** @var array */
+    protected $values = [
+        self::TRANSLATION_NOT_ALLOWED,
+        self::TRANSLATION_ALLOWED,
+    ];
+}

79e12ab4179e4db026eb0f4e83a8bef8c2323a24
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2021-04-06 20:08:06 +0200

    Update trips display.

--- assets/js/trips.js
+++ assets/js/trips.js
@@ -1,8 +1,11 @@
 import SearchPicker from "./search/searchpicker";
 import Litepicker from 'litepicker';
 import moment from 'moment';
-
 import '../scss/_daterangepicker.scss';
+
+var L = require('leaflet');
+
+require('leaflet-polylinedecorator');
 
 let searchPicker = new SearchPicker( "/search/locations/places", 'js-search-picker');
 
@@ -12,13 +15,11 @@ let latestDeparture = moment().add(1, 'day');
 pickers.forEach(initializePicker);
 
 function initializePicker(value) {
-    console.log("value=" , value);
     const parent = value.id.replace('_duration', '');
-    console.log("parent = ", parent);
     const picker = new Litepicker({
         element: value,
         singleMode: false,
-        minDate: latestDeparture.format("YYYY-MM-DD"),
+        minDate: moment().format('YYYY-MM-DD'),
         numberOfMonths: 2,
         numberOfColumns: 2,
         format: "YYYY-MM-DD",
@@ -26,14 +27,10 @@ function initializePicker(value) {
         setup: (picker) => {
             picker.on('selected', (start, end) => {
                 const leg = picker.options.element.id.replace('_duration', '');
-                console.log(leg);
-                console.log(start, end);
                 const arrival = document.getElementById(leg + '_arrival');
                 arrival.value = start ? start.format('YYYY-MM-DD') : '';
-                console.log(arrival.value);
                 const departure = document.getElementById(leg + '_departure');
                 departure.value = end ? end.format('YYYY-MM-DD') : '';
-                console.log(departure.value);
             });
         }
     });
@@ -41,17 +38,8 @@ function initializePicker(value) {
     const arrival = document.getElementById(parent + '_arrival').value;
     const departure = document.getElementById(parent + '_departure').value;
 
-    console.log("Arrival: " + arrival);
-    console.log("Departure: " + departure);
-
     if (arrival !== "") {
         picker.setDateRange(arrival, departure);
-    }
-
-    const departureMoment =  moment(departure);
-
-    if (departureMoment > latestDeparture) {
-        latestDeparture = departureMoment;
     }
 }
 
@@ -86,3 +74,60 @@ $(document).on('click', '.js-btn-remove[data-related]', function (event) {
 
     event && event.preventDefault();
 });
+
+$( function() {
+if ($('#map').length) {
+
+    var map = L.map('map', {
+        center: [0, 0],
+        zoom: 0,
+        zoomSnap: 0.1
+    });
+
+    let allData = $('.js-data').map((_, el) => [el.value.split(',')]).get()
+
+    let circlesArray = []
+
+    let locationsArray = []
+
+    for (let i = 0; i < allData.length; i++) {
+
+        let location = allData[i][0]
+        let latitude = allData[i][1]
+        let longitude = allData[i][2]
+        locationsArray.push([latitude, longitude]);
+
+        let countryName = allData[i][3]
+        let tripDate = allData[i][4]
+
+        let marker = L.marker([latitude, longitude]).addTo(map);
+        marker.bindPopup("<strong>" + location + "</strong> (" + countryName + ")<br>" + tripDate).openPopup();
+
+        let circle = null;
+        circle = L.circle([latitude, longitude], {
+            color: 'rgb(112,0,243)',
+            fillColor: 'rgba(112, 0, 243, 0.1)',
+            fillOpacity: 1,
+            radius: 25000
+        }).addTo(map);
+        circlesArray.push(circle)
+    }
+
+    let group = new L.featureGroup(circlesArray);
+
+    // \todo In case of trip of someone else check if own circle intersects with travel
+    // if so show on map as well.
+    map.fitBounds(group.getBounds());
+
+    let journey = L.polyline(locationsArray).addTo(map);
+    var arrows = L.polylineDecorator(journey, {
+        patterns: [
+            {offset: 25, repeat: 50, symbol: L.Symbol.arrowHead({pixelSize: 15, pathOptions: {fillOpacity: 1, weight: 0}})}
+        ]
+    }).addTo(map);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="/about/credits#OSM">OpenStreetMap contributors</a>',
+        subdomains: ['a', 'b', 'c']
+    }).addTo(map);
+}});

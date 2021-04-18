192223298483411e5d0afbeb900f086aa9d49c2f
Pavel Schoffer <xschof00@gmail.com>
2021-04-18 19:55:36 +0200

    Make pure JS notices from React (#234)

--- assets/js/utils/alerts.js
+++ assets/js/utils/alerts.js
@@ -1,34 +1,21 @@
-const ALERT_TARGET_ID = 'js-alert'
-const ALERT_SUCCESS = 'alert-success';
-const ALERT_FAIL = 'alert-danger';
+import Toastify from 'toastify-js';
+
+const ERROR_BCG = 'linear-gradient(90deg, rgba(170,40,28,1) 0%, rgba(224,106,15,1) 100%)';
+const SUCCESS_BCG = 'linear-gradient(90deg, rgba(52,170,28,1) 0%, rgba(29,213,35,1) 100%)';
 
 export const alertError = (msg) => {
-    const updated = updateNotice(msg, ALERT_FAIL);
-
-    if (!updated) {
-        console.error(msg);
-    }
+      showAlert(msg, ERROR_BCG)
 }
 
 export const alertSuccess = (msg) => {
-    updateNotice(msg, ALERT_SUCCESS);
+    showAlert(msg, SUCCESS_BCG)
 }
 
-const updateNotice = (msg, newClass) => {
-    const alertTarget = document.getElementById(ALERT_TARGET_ID);
-    if (alertTarget) {
-        alertTarget.innerHTML = msg;
-        // remove previous classes
-        alertTarget.classList.remove(ALERT_SUCCESS);
-        alertTarget.classList.remove(ALERT_FAIL);
-
-        // set new
-        alertTarget.classList.add(newClass);
-
-        // show (unless it is already visible, then this is no operation)
-        alertTarget.classList.remove('d-none');
-
-        return true;
-    }
-    return false;
+const showAlert = (msg, background) => {
+    Toastify({
+        text: msg,
+        close: true,
+        position: "center",
+        backgroundColor: background,
+      }).showToast();
 }

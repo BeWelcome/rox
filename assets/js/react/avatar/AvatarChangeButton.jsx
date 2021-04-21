54b3dc72822551461e8fb728f183b997e4c0f975
Pavel Schoffer <xschof00@gmail.com>
2021-04-21 19:17:01 +0200

    remove animation

--- assets/js/react/avatar/AvatarChangeButton.jsx
+++ assets/js/react/avatar/AvatarChangeButton.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import {uploadTemporaryAvatar} from '../../api/avatar'
-import {getText} from '../../utils/texts';
-import {alertError, alertSuccess} from '../../utils/alerts';
+import { uploadTemporaryAvatar } from '../../api/avatar'
+import { getText } from '../../utils/texts';
+import { alertError, alertSuccess } from '../../utils/alerts';
 
-const BLINKING_BTN_COLORS = ['#0083f3', '#53a8f1'];
 
 const AvatarChangeButton = (props) => {
     const [uploading, setUploading] = React.useState(false);
-    const [btnColorIx, setBtnColorIx] = React.useState(0);
     const inputFile = React.useRef(null)
-    // Timeout closure's state doesnt get updated. That's why we are using ref so that the closeru has up to data info
-    const animationState = React.useRef();
-    animationState.current = {btnColorIx, uploading};
-
-    const animateBtn = () => {
-        setBtnColorIx(animationState.current ? 0 : 1);
-        setTimeout(animateBtn, 700);
-    }
 
     const onChangeFile = async (event) => {
         event.stopPropagation();
@@ -24,7 +14,6 @@ const AvatarChangeButton = (props) => {
         var file = event.target.files[0];
 
         setUploading(true);
-        animateBtn();
 
         if (file) {
             const result = await uploadTemporaryAvatar(file);
@@ -44,18 +33,17 @@ const AvatarChangeButton = (props) => {
     }
 
     const onButtonClick = () => {
-       inputFile.current.click();
+        inputFile.current.click();
     };
 
     const btnStyle = {
         cursor: uploading ? 'wait' : 'pointer',
-        transition: 'all .7s',
-        backgroundColor: BLINKING_BTN_COLORS[btnColorIx],
     }
+    const btnText = uploading ? getText('uploading') + ' ...' : getText('profile.change.avatar');
 
     return <>
-        <input type='file' id='file' ref={inputFile} style={{display: 'none'}} onChange={onChangeFile} />
-        <button type="button" onClick={onButtonClick} className="btn btn-info btn-block" disabled={uploading} style={btnStyle} >{getText('profile.change.avatar')}</button>
+        <input type='file' id='file' ref={inputFile} style={{ display: 'none' }} onChange={onChangeFile} />
+        <button type="button" onClick={onButtonClick} className="btn btn-info btn-block" disabled={uploading} style={btnStyle}>{btnText}</button>
     </>
 }
 

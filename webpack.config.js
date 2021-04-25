e3c20b193f6a9f43eb55fa4ba617bda3569c6b70
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2021-04-25 19:06:25 +0200

    Chart.js changed there main entry from Chart.js to chart.js.

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ Encore
     .addEntry('leaflet', './assets/js/leaflet.js')
     .addEntry('member/autocomplete', './assets/js/member/autocomplete.js')
     .addEntry('admin/faqs', './assets/js/admin/faqs.js')
-    .addEntry('chartjs', './node_modules/chart.js/dist/Chart.js')
+    .addEntry('chartjs', './node_modules/chart.js/dist/chart.js')
     .addEntry('offcanvas', './assets/js/offcanvas.js')
     .addEntry('profile/profile', './assets/js/profile.js')
     .addEntry('updatecounters', './assets/js/updateCounters.js')

f694c2e765d170e2fb738d8780366f34a02bf300
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2021-06-09 20:47:00 +0200

    Bump versions, fix imports for CKEditor.

--- assets/js/roxeditor.js
+++ assets/js/roxeditor.js
@@ -1,23 +1,16 @@
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapterPlugin from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
-import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import UnderlinePlugin from '@ckeditor/ckeditor5-basic-styles/src/underline';
-import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import BlockQuotePlugin from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import EasyImagePlugin from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-import ImagePlugin from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaptionPlugin from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageStylePlugin from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbarPlugin from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUploadPlugin from '@ckeditor/ckeditor5-image/src/imageupload';
-import LinkPlugin from '@ckeditor/ckeditor5-link/src/link';
-import ListPlugin from '@ckeditor/ckeditor5-list/src/list';
-import Mention from '@ckeditor/ckeditor5-mention/src/mention';
-import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters';
-import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials';
-import CloudServicesPlugin from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic/src/index';
+import { Essentials } from '@ckeditor/ckeditor5-essentials/src/index';
+import { UploadAdapter } from '@ckeditor/ckeditor5-adapter-ckfinder/src/index';
+import { Bold, Underline, Italic } from '@ckeditor/ckeditor5-basic-styles/src/index';
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote/src/index';
+import { EasyImage } from '@ckeditor/ckeditor5-easy-image/src/index';
+import { Image, ImageCaption, ImageStyle, ImageToolbar, ImageUpload }  from '@ckeditor/ckeditor5-image/src/index';
+import { Link } from '@ckeditor/ckeditor5-link/src/index';
+import { List } from '@ckeditor/ckeditor5-list/src/index';
+import { Mention } from '@ckeditor/ckeditor5-mention/src/index';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph/src/index';
+import { SpecialCharacters, SpecialCharactersEssentials }  from '@ckeditor/ckeditor5-special-characters/src/index';
+import { CloudServices } from '@ckeditor/ckeditor5-cloud-services/src/index';
 
 function SpecialCharactersTextExtended( editor ) {
     editor.plugins.get( 'SpecialCharacters' ).addItems( 'Text', [
@@ -43,26 +36,26 @@ for (let i = 0; i < allEditors.length; ++i) {
     ClassicEditor.create(allEditors[i], {
         // The plugins are now passed directly to .create().
         plugins: [
-            EssentialsPlugin,
-            BoldPlugin,
-            UnderlinePlugin,
-            ItalicPlugin,
-            BlockQuotePlugin,
-            ImagePlugin,
-            ImageCaptionPlugin,
-            ImageStylePlugin,
-            ImageToolbarPlugin,
-            EasyImagePlugin,
-            LinkPlugin,
-            ListPlugin,
+            Essentials,
+            Bold,
+            Underline,
+            Italic,
+            BlockQuote,
+            Image,
+            ImageCaption,
+            ImageStyle,
+            ImageToolbar,
+            EasyImage,
+            Link,
+            List,
             Mention,
-            ParagraphPlugin,
-            UploadAdapterPlugin,
+            Paragraph,
+            UploadAdapter,
             SpecialCharacters,
             SpecialCharactersEssentials,
             SpecialCharactersTextExtended,
-            ImageUploadPlugin,
-            CloudServicesPlugin
+            ImageUpload,
+            CloudServices
         ],
         ckfinder: {
             uploadUrl: uploadUrl

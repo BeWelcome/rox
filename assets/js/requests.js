d7be8d440290871bd6bec781292e6de26ef625c0
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2022-04-24 13:24:47 +0200

    Streamline thread display. Show status in request reply.
    Threads now look more like signal and telegram.
    Requests do not show a JS error anymore if not flexible.

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -5,35 +5,37 @@ import '../scss/_daterangepicker.scss';
 
 $(function () {
     const input = document.getElementsByClassName('js-litepicker')[0];
-    const parent = input.id.replace('_duration', '');
+    if (input !== undefined) {
+        const parent = input.id.replace('_duration', '');
 
-    const picker = new Litepicker({
-        element: input,
-        singleMode: false,
-        minDate: moment().add(1, 'day'),
-        numberOfMonths: 2,
-        numberOfColumns: 2,
-        lang: document.documentElement.lang,
-        tooltipNights: true,
-        setup: (picker) => {
-            picker.on('selected', (start, end) => {
-                const arrival = document.getElementById(parent + '_arrival');
-                arrival.value = start ? start.format('YYYY-MM-DD') : '';
-                const departure = document.getElementById(parent + '_departure');
-                departure.value = end ? end.format('YYYY-MM-DD') : '';
+        const picker = new Litepicker({
+            element: input,
+            singleMode: false,
+            minDate: moment().add(1, 'day'),
+            numberOfMonths: 2,
+            numberOfColumns: 2,
+            lang: document.documentElement.lang,
+            tooltipNights: true,
+            setup: (picker) => {
+                picker.on('selected', (start, end) => {
+                    const arrival = document.getElementById(parent + '_arrival');
+                    arrival.value = start ? start.format('YYYY-MM-DD') : '';
+                    const departure = document.getElementById(parent + '_departure');
+                    departure.value = end ? end.format('YYYY-MM-DD') : '';
 
-                let original = document.getElementById('duration-original');
-                if (null !== original) {
-                    original.classList.remove("d-none");
-                }
-            });
+                    let original = document.getElementById('duration-original');
+                    if (null !== original) {
+                        original.classList.remove("d-none");
+                    }
+                });
+            }
+        });
+
+        const arrival = document.getElementById(parent + '_arrival').value;
+        const departure = document.getElementById(parent + '_departure').value;
+
+        if (arrival !== "") {
+            picker.setDateRange(arrival, departure);
         }
-    });
-
-    const arrival = document.getElementById(parent + '_arrival').value;
-    const departure = document.getElementById(parent + '_departure').value;
-
-    if (arrival !== "") {
-        picker.setDateRange(arrival, departure);
     }
 });

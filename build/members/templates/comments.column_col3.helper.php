9e94bb9b31974ed7085d2f7b6fa3a2162f1aa659
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2022-09-21 21:09:24 +0200

    Use same method to build comments array for comments pages.

--- build/members/templates/comments.column_col3.helper.php
+++ build/members/templates/comments.column_col3.helper.php
@@ -1,84 +1,40 @@
 <?php
-        $member = $this->member;
-        $comments_received = $this->member->comments;
-        $comments_written = $this->member->get_comments_written();
+$comments = [];
+$commentsReceived = $this->member->get_comments();
+$commentsWritten = $this->member->get_comments_written();
 
-        $comments_from = array();
-        $comment_for_self = false;
-        // Get member ids into one array
-        $member_ids = array();
-        foreach($comments_received as $c) {
-            if (!in_array($c->IdFromMember, $member_ids)) {
-                $member_ids[] = $c->IdFromMember;
-                $comments_from[$c->IdFromMember] = $c;
-            }
-        }
+foreach ($commentsReceived as $value) {
+    $key = $value->UsernameFromMember;
+    $comments[$key] = [
+        'from' => $value,
+    ];
+}
+foreach ($commentsWritten as $value) {
+    $key = $value->UsernameToMember;
+    if (isset($comments[$key])) {
+        $comments[$key] = array_merge($comments[$key], [
+            'to' => $value,
+        ]);
+    } else {
+        $comments[$key] = [
+            'to' => $value,
+        ];
+    }
+}
+$farFuture = new DateTimeImmutable('01-01-3000');
+usort($comments,
+    function ($a, $b) use ($farFuture) {
+        // get latest updates on to and from part of comments and order desc
+        $createdATo = isset($a['to']) ? new DateTime($a['to']->created) : $farFuture;
+        $createdAFrom = isset($a['from']) ? new DateTime($a['from']->created) : $farFuture;
+        $createdA = min($createdATo, $createdAFrom);
+        $createdBTo = isset($b['to']) ? new DateTime($b['to']->created) : $farFuture;
+        $createdBFrom = isset($b['from']) ? new DateTime($b['from']->created) : $farFuture;
+        $createdB = min($createdBTo, $createdBFrom);
+        return -1*($createdA <=> $createdB);
+    }
+);
 
-        $comments_to = array();
-        foreach($comments_written as $c) {
-            if (!in_array($c->IdToMember, $member_ids)) {
-                $member_ids[] = $c->IdToMember;
-            }
-            $comments_to[$c->IdToMember] = $c;
-        }
-
-        // check if member_ids contains a comment for the member browsing the
-        // the profile
-        $comment_to_self_exists = false;
-        $visitor = $this->model->getLoggedInMember();
-        if ($visitor && ($visitor->id <> $member->id) && (in_array($visitor->id, $member_ids))) {
-            $comment_to_self_exists = true;
-        }
-
-        // now that we have all members we create one array with all comments
-        // containing "timestamp", "from", "to" as fields
-        $i = 0;
-        $comments = array();
-        foreach($member_ids as $id) {
-            $comment = array();
-            $comment_to_self = false;
-            if ($visitor && ($visitor->id == $id)) {
-                $comment_to_self = true;
-            }
-
-            $ts_from = $ts_to = 0;
-            if (isset($comments_from[$id])) {
-                $comment['from'] = $comments_from[$id];
-                $ts_from = $comments_from[$id]->unix_created;
-            }
-            if (isset($comments_to[$id])) {
-                $comment['to'] = $comments_to[$id];
-                $ts_to = $comments_to[$id]->unix_created;
-            }
-
-            // Add comments to list if it isn't for the current visitor
-            // or if the visitor left a comment him-/herself
-            if (!$comment_to_self || isset($comment['from'])) {
-                $comment['timestamp'] = min($ts_from, $ts_to);
-                $comments[] = $comment;
-            }
-        }
-
-        // sort the comments descending by timestamp
-        function cmp($a, $b)
-        {
-            if ($a['timestamp'] == $b['timestamp']) {
-                return 0;
-            }
-            return ($a['timestamp'] > $b['timestamp']) ? -1 : 1;
-        }
-
-        usort($comments, "cmp");
-
-        // if current visitor didn't leave a comment add entry to the beginning
-        // of the comment list
-
-        if ($comment_to_self_exists && (!isset($comments_from[$visitor->id]))) {
-            $comment = array();
-            $comment['to'] = $comments_to[$visitor->id];
-            array_unshift($comments, $comment);
-        }
-
-        $username = $this->member->Username;
-        $layoutbits = new MOD_layoutbits();
-?>
+$member = $this->member;
+$username = $this->member->Username;
+$layoutbits = new MOD_layoutbits();

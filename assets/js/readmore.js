564ecbd638f9df7215a65ec97af1caebcbe54964
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2022-10-29 19:14:25 +0200

    Add missing file.

--- assets/js/readmore.js
+++ assets/js/readmore.js
@@ -3,6 +3,14 @@ import ShowMore from 'show-more-read/dist/js/showMore.esm.min.js';
 import 'show-more-read/dist/css/show-more.min.css';
 
 document.addEventListener('DOMContentLoaded', function () {
+    new ShowMore('.js-read-more', {
+        config: {
+            type: "text",
+            limit: 160,
+            more: document.getElementById('read.more').value,
+            less: document.getElementById('show.less').value
+        }
+    });
     new ShowMore('.js-read-more-received', {
         config: {
             type: "text",

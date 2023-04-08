30726e1ab0cd81680e349227272570d965d5ff67
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2023-04-08 13:07:04 +0200

    Update CKEditor to 37.0.1.

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     plugins: {
         'postcss-import': {},
+        'tailwindcss/nesting': 'postcss-nesting',
         'postcss-url': {},
-        'tailwindcss/nesting': {},
         tailwindcss: {},
         autoprefixer: {},
     }

f65497e42129abacba63b2896e3d327157db1fac
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2023-10-02 10:59:21 +0200

    Allow third parameter to be omitted (null)

--- src/Model/ConversationModel.php
+++ src/Model/ConversationModel.php
@@ -91,7 +91,7 @@ class ConversationModel
         $this->entityManager->flush();
     }
 
-    public function markConversationAsSpam(Member $member, array $conversation, ?string $comment): void
+    public function markConversationAsSpam(Member $member, array $conversation, ?string $comment = null): void
     {
         /** @var Message $message */
         foreach ($conversation as $message) {

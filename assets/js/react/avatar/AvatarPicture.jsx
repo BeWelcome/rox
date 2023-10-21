44eab7fb7bc17f4e4b5116c390ce1b860adf9da4
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2023-10-21 11:24:46 +0200

    Final fix for profile picture display.

--- assets/js/react/avatar/AvatarPicture.jsx
+++ assets/js/react/avatar/AvatarPicture.jsx
@@ -3,21 +3,15 @@ import {getText} from '../../utils/texts';
 
 const AvatarPicture = (props) => {
     const config = window.globals.config;
-
-    const useLightbox = config.avatarUseLightbox;
     const basePictureUrl = config.avatarUrl;
-    const pictureUrl = useLightbox ? `${basePictureUrl}/original` : `/members/avatar/${config.username}/500`;
+    const avatarOriginalUrl = `${basePictureUrl}/original`;
+    const pictureUrl = `/members/avatar/${config.username}/500`;
     const pictureTitle = getText('profile.picture.title');
-
-    const imageStyle = {
-        // We are adding changeCount here to make browser skips its cache and re-fetches the picture
-        backgroundImage: `url('${basePictureUrl}/original?${props.changeCount}')`,
-    };
 
     return (
         <div className="u-w-full u-relative u-pb-[100%]">
             <div className="u-absolute u-left-0 u-top-0 u-w-full u-h-full">
-                <a href={pictureUrl} title={pictureTitle}  data-toggle="lightbox" data-type="image">
+                <a href={avatarOriginalUrl} title={pictureTitle} className="js-profile-picture" data-toggle="lightbox" data-type="image">
                     <img className="u-rounded-8 u-w-full u-h-full u-object-cover js-profile-picture"
                          src={pictureUrl} alt={pictureTitle}/>
                 </a>

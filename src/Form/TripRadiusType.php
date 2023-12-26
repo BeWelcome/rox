3e30db15f8bf6d1db3df95881a51a3a9fd36f63d
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2023-12-26 10:57:08 +0100

    Align radii in different trip selects. move anchor for trip's leg higher.

--- src/Form/TripRadiusType.php
+++ src/Form/TripRadiusType.php
@@ -23,12 +23,7 @@ class TripRadiusType extends AbstractType
                     'search.radius.20km' => 20,
                     'search.radius.50km' => 50,
                     'search.radius.100km' => 100,
-/*                    'search.radius.200km' => 200,
-                    'search.radius.500km' => 500,
-                    'search.radius.1000km' => 1000,
-                    'search.radius.2000km' => 2000,
-                    'search.radius.5000km' => 5000,
-*/],
+                ],
                 'attr' => [
                     'id' => 'trips_radius',
                 ],

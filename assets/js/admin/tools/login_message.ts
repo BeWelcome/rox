825bd106e24a51cedffd34f6a4188dc45fe469af
thisismeonmounteverest <bla@blafaselblubb.abcde.biz>
2023-12-26 11:03:15 +0100

    Add missing updated file.

--- assets/js/admin/tools/login_message.ts
+++ assets/js/admin/tools/login_message.ts
@@ -1,14 +1,33 @@
-import flatpickr from "flatpickr";
+import VanillaCalendar, { Options } from 'vanilla-calendar-pro';
+import 'vanilla-calendar-pro/build/vanilla-calendar.min.css';
 
-import "flatpickr/dist/themes/light.css";
-
-const expiresDateTimePicker = flatpickr(".flatpickr", {
-    dateFormat: "Y-m-d H:i",
-    enableTime: true,
-/*    onChange: function(selectedDates, dateStr, instance) {
-        console.log(selectedDates);
-        console.log(dateStr );
-        console.log(instance);
-        expires.value = dateStr;
+const options: Options = {
+    input: true,
+    type: 'default',
+    actions: {
+        changeToInput(e, calendar, self) {
+            if (!self.HTMLInputElement) return;
+            if (self.selectedDates[0]) {
+                self.HTMLInputElement.value = self.selectedDates[0] + " " + self.selectedTime;
+            } else {
+                self.HTMLInputElement.value = '';
+            }
+        },
     },
-*/});
+    settings: {
+        range: {
+            disablePast: true,
+        },
+        selection: {
+            time: 24,
+        },
+        visibility: {
+            positionToInput: 'center',
+            theme: 'light',
+        },
+    },
+};
+
+const expires = document.getElementById('login_message_expires');
+const calendar = new VanillaCalendar(expires, options);
+calendar.init();

7928fd59cabddbf4b343d31fd310f4190e7fc5b8
jj2bw <226866852+jj2bw@users.noreply.github.com>
2025-08-16 15:57:37 +0200

    chore: review config .php-cs-fixer.dist.php
    https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/v3.0.0/UPGRADE-v3.md#renamed-ruless

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -14,25 +14,9 @@ $config
         '@Symfony:risky' => true,
         'array_syntax' => ['syntax' => 'short'],
         'combine_consecutive_unsets' => true,
-        // one should use PHPUnit methods to set up expected exception instead of annotations
-        'general_phpdoc_annotation_remove' => [
-            'expectedException',
-            'expectedExceptionMessage',
-            'expectedExceptionMessageRegExp'
-        ],
         'heredoc_to_nowdoc' => true,
-        'no_extra_consecutive_blank_lines' => [
-            'break',
-            'continue',
-            'extra',
-            'return',
-            'throw',
-            'use',
-            'parenthesis_brace_block',
-            'square_brace_block',
-            'curly_brace_block'
-        ],
-        'no_short_echo_tag' => true,
+        'no_extra_blank_lines' => true,
+        'echo_tag_syntax' => true,
         'no_unreachable_default_argument_value' => true,
         'no_useless_else' => true,
         'no_useless_return' => true,

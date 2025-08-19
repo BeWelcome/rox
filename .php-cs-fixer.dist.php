9a46a87a2bfad498dfaf0898057c426b04eb3d46
thisismeonmounteverest <C:\Users\bla\AppData\Roaming\The Bat!>
2025-08-19 14:41:38 +0200

    Fix broken config for php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,52 +1,56 @@
 <?php
 $finder = new PhpCsFixer\Finder();
 $finder
-    ->exclude('src/Mike42')
     ->notName('*.twig')
     ->notName('*.yml')
-    ->in('src');
+    ->in('src/');
 
 $config = new PhpCsFixer\Config();
 $config
+    ->setParallelConfig(new PhpCsFixer\Runner\Parallel\ParallelConfig(4, 20))
     ->setRiskyAllowed(true)
     ->setRules([
+        '@PHP83Migration' => true,
         '@Symfony' => true,
         '@Symfony:risky' => true,
         'array_syntax' => ['syntax' => 'short'],
         'combine_consecutive_unsets' => true,
-        // one should use PHPUnit methods to set up expected exception instead of annotations
-        'general_phpdoc_annotation_remove' => [
-            'expectedException',
-            'expectedExceptionMessage',
-            'expectedExceptionMessageRegExp'
-        ],
-        'heredoc_to_nowdoc' => true,
-        'no_extra_consecutive_blank_lines' => [
-            'break',
-            'continue',
-            'extra',
-            'return',
-            'throw',
-            'use',
-            'parenthesis_brace_block',
-            'square_brace_block',
-            'curly_brace_block'
-        ],
-        'no_short_echo_tag' => true,
-        'no_unreachable_default_argument_value' => true,
-        'no_useless_else' => true,
-        'no_useless_return' => true,
-        'ordered_class_elements' => true,
-        'ordered_imports' => true,
-        'php_unit_strict' => true,
-        'phpdoc_add_missing_param_annotation' => true,
-        'phpdoc_order' => true,
-        'semicolon_after_instruction' => true,
-        'strict_comparison' => true,
-        'strict_param' => true,
-        'concat_space' => ['spacing' => 'one'],
-    ])
-    ->setFinder($finder)
+            // one should use PHPUnit methods to set up expected exception instead of annotations
+                    'general_phpdoc_annotation_remove' => [
+                        'annotations' => [
+                    'expectedException',
+                    'expectedExceptionMessage',
+                    'expectedExceptionMessageRegExp',
+                            ]
+                ],
+                        'heredoc_to_nowdoc' => true,
+                        'no_extra_blank_lines' => [
+                            'tokens' => [
+                            'break',
+                            'continue',
+                            'extra',
+                            'return',
+                            'throw',
+                            'use',
+                            'parenthesis_brace_block',
+                            'square_brace_block',
+                            'curly_brace_block'
+                        ],
+                        ],
+                        'no_unreachable_default_argument_value' => true,
+                        'no_useless_else' => true,
+                        'no_useless_return' => true,
+                        'ordered_class_elements' => true,
+                        'ordered_imports' => true,
+                        'php_unit_strict' => true,
+                        'phpdoc_add_missing_param_annotation' => true,
+                        'phpdoc_order' => true,
+                        'semicolon_after_instruction' => true,
+                        'strict_comparison' => true,
+                        'strict_param' => true,
+                        'concat_space' => ['spacing' => 'one'],
+                   ])
+        ->setFinder($finder)
 ;
 
 return $config;
